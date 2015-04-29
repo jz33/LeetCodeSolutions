e49d41f10616f548b52e5dc95abca13e58c704fc
Jun <junzhengrice@gmail.com>
2015-04-29 12:41:27 -0500

    Slightly change

--- 032 Longest Valid Parentheses.py
+++ 032 Longest Valid Parentheses.py
@@ -2,12 +2,11 @@ import random
 '''
 32 Longest Valid Parentheses
 https://oj.leetcode.com/problems/longest-valid-parentheses/
-O(n^2)
 '''
 # generate a random Parentheses string with size n
 def randParentheses(n):
     return ''.join(random.choice('('+')') for _ in range(n))
-
+ 
 def longestValidParentheses(sent):
     '''
     Notations for 'buf' :
@@ -18,33 +17,32 @@ def longestValidParentheses(sent):
     buf = -1
     lt  = 0
     rt  = -1
-    
+     
     for i in range(0,len(sent)-1):
         if sent[i] == ')': continue
-        buf = 1 # sent[i] == '('
+         
+        buf = 1
         for j in range(i+1,len(sent)):
             if sent[j] == ')':
-                c = buf - 1
-                if c < 0 : break
-                buf = c
-            else:
-                buf += 1
-                
+                buf -= 1
+                if buf < 0 : 
+                    i = j # speed-up
+                    break
+            else: buf += 1
+                 
             # update result
             if buf == 0:
                 if j - i > rt - lt :
                     rt = j
                     lt = i
-            
-    if lt < rt : print sent[lt:rt+1] #debug 
-    return rt-lt+1;
-    
+    print lt, rt
+       
 def main():
     repeat = 5
     for i in range(0,repeat):
         sent = randParentheses(10)
         print sent
-        print longestValidParentheses(sent)
-    
+        longestValidParentheses(sent)
+     
 if __name__ == "__main__":
     main()
