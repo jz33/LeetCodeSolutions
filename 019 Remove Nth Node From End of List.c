e07550379f2ab00728ae498f5b981cfa7710e7bd
Jun <junzhengrice@gmail.com>
2015-05-02 13:08:25 -0500

    Slight refactoring

--- 019 Remove Nth Node From End of List.c
+++ 019 Remove Nth Node From End of List.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 /*
-19 Remove Nth Node From End of List
-https://oj.leetcode.com/problems/remove-nth-node-from-end-of-list/
+21 Merge Two Sorted Lists
+https://oj.leetcode.com/problems/merge-two-sorted-lists/
 */
-struct LinkedListNode
-{
+struct LinkedListNode{
     int rank;
     struct LinkedListNode* next;
 };
@@ -18,56 +17,60 @@ void printList(node* p){
     }
     printf("\n");
 }
-/*
-Remove Nth node from end of a single linked list
-http://yucoding.blogspot.ca/2013/04/leetcode-question-82-remove-nth-node.html
- 0->1->2->3->4, 2  => 0->1->3->4
-*/
-void removeFromBackRec(node* head, int* counter, const int tag)
-{
-    node* t;
-    if(head->next != 0) 
-        removeFromBackRec(head->next,counter,tag);
 
-    // notice the current node is 1 before to-be-deleted node
-    if(*counter == tag)
-    {
-        t = head->next;
-        head->next = t->next;
-        t->next = 0;
-        free(t);
+void freeList(node* h){
+    node* p = h;
+    while(p != 0){
+        p = h->next;
+        free(h);
+        h = p;
     }
-    *counter = *counter+1;
 }
-/*
-tag = [1,2,3...]
-*/
-node* removeFromBack(node* head, const int tag)
+
+node* mergeTwoSorted(node* la, node* lb)
 {
-    int counter = 0;
-    node* t;
-    if (head==0) 
-        return 0;
-    removeFromBackRec(head,&counter,tag);
+    node *ret, *p;
+    if(la == 0) return lb;
+    if(lb == 0) return la;
+
+    if(la->rank < lb->rank){
+        ret = la;
+        la = la->next;
+    } else {
+        ret = lb;
+        lb = lb->next;
+    }
+
+    p = ret;
+    while(la != 0 && lb != 0){
+        if(la->rank < lb->rank){
+            p->next = la;
+            la = la->next;
+        } else {
+            p->next = lb;
+            lb = lb->next;
+        }
+        p = p->next;
+    }
     
-    // if the head node is to be deleted
-    if(counter == tag)
-    {
-        t = head->next;
-        free(head);
-        return t;
-    }
-    else return head;
+    // notice here is different from "Merge 2 sorted array"
+    if(la != 0) p->next = la;
+    if(lb != 0) p->next = lb;
+    return ret;
 }
 
-int main(){
-    int repeat = 6;
-    int i,tag;
+int main()
+{
+    int repeat = 7; // will create repeat + 1 nodes
+    int K;
+    int i;
     node* head = (node*)malloc(sizeof(node));
-    node* p = head;
-    
+    node* hea2 = (node*)malloc(sizeof(node));
+    node* p;
+
     // init
-    head->rank = 0;
+    p = head;
+    p->rank = 0;
     for(i=0;i<repeat;i++){
         p->next = (node*)malloc(sizeof(node));
         p = p->next;
@@ -75,17 +78,23 @@ int main(){
         p->next = 0;
     }
     printList(head);
-    
-    tag = 7;
-    head = removeFromBack(head,tag);
-    
-    printList(head);
-    
-    //free
-    while(head != 0){
-        p = head->next;
-        free(head);
-        head = p;
+
+    // init
+    p = hea2;
+    p->rank = 0;
+    for(i=0;i<repeat;i++){
+        p->next = (node*)malloc(sizeof(node));
+        p = p->next;
+        p->rank = i*2+1;
+        p->next = 0;
     }
+    printList(hea2);
+
+    // test
+    p = mergeTwoSorted(head,hea2);
+    printList(p);
+
+    //free
+    freeList(p);
     return 0; 
 }
