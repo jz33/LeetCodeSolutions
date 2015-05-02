f1674fcfe33a3764e569e5a51c0beb3c2d1584a4
Jun <junzhengrice@gmail.com>
2015-05-01 21:01:14 -0500

    Slight refactoring

--- 008 String to Integer (atoi).c
+++ 008 String to Integer (atoi).c
@@ -3,22 +3,19 @@
 08 String to Integer (atoi)
 https://oj.leetcode.com/problems/string-to-integer-atoi/
 */
-/*
-int can be minus !
-*/
-int _atoi(char* p){
+int _atoi(char* p)
+{
     int s = *p == '-' ? -1:1;
 	int r = 0;
     
-    if(s==-1) p++;
-	while(*p != '\0'){
-		r = r*10+*p-'0';
-		p++;
-	}
+    if (s==-1) p++;
+	while(*p != '\0')
+		r = r*10 + *p++ - '0';
 	return r*s;
 }
 
-int main(){
+int main()
+{
     char* x = "-123456789";
     printf("%d\n",_atoi(x));
     return 0;  
