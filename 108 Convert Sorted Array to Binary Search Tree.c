f475f2bac28f0798695470cae488e8300dc6b5b5
Jun <junzhengrice@gmail.com>
2015-05-04 14:04:31 -0500

    Removed "emptynode" function

--- 108 Convert Sorted Array to Binary Search Tree.c
+++ 108 Convert Sorted Array to Binary Search Tree.c
@@ -14,46 +14,44 @@ struct Binary_Search_Tree_Node
 };
 typedef struct Binary_Search_Tree_Node node;
 
-node* emptyNode(){
-    node* n = (node*)malloc(sizeof(node));
-    n->rank = -1;
-    n->pl = 0;
-    n->pr = 0;
-    return n;
-}
-
 void inorder(node* n)
 {
-	if(n != 0) {
-		inorder(n->pl);
-		printf("%d ", n->rank);
-		inorder(n->pr);
-	}
+    if(n != 0){
+        inorder(n->pl);
+        printf("%d ", n->rank);
+        inorder(n->pr);
+    }
 }
 
 void initInOrder(node* n, int* arr, int start, int end){
-	int mid = (start+end)>>1;
-	n->rank = arr[mid];
-	
-	if(start <= mid-1) {
-		n->pl = emptyNode();
-		initInOrder(n->pl, arr, start, mid-1);
-	}
-	if(mid+1 <= end) {
-		n->pr = emptyNode();
-		initInOrder(n->pr, arr, mid+1, end);
-	}
+    int mid = (start+end)>>1;
+    n->rank = arr[mid];
+    
+    if(start <= mid-1 || mid+1 <= end){
+        if(start <= mid-1){
+            n->pl = (node*)malloc(sizeof(node*));
+            initInOrder(n->pl, arr, start, mid-1);
+        }
+        if(mid+1 <= end){
+            n->pr = (node*)malloc(sizeof(node*));
+            initInOrder(n->pr, arr, mid+1, end);
+		}
+    } else {
+        n->pl = 0;
+        n->pr = 0;
+    }
 }
-// tester
-int main(int argc, char** argv){
+
 #define NUM 7
+
+int main(int argc, char** argv){
     int arr[NUM];
     int i;
-    node* n = emptyNode();
+    node* n = (node*)malloc(sizeof(node*));
     for(i=0;i<NUM;i++){
         arr[i] = i;
     }
     initInOrder(n,arr,0,NUM-1);
-    inorder(n);putchar('\n');
+    inorder(n);
     return 0;
 }
