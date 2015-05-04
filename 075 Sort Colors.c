8c20677d7c806010b63abcc63fdbd654a227a8e9
Jun <junzhengrice@gmail.com>
2015-05-03 21:21:50 -0500

    Clean up

--- 075 Sort Colors.c
+++ 075 Sort Colors.c
@@ -6,12 +6,12 @@
 https://oj.leetcode.com/problems/sort-colors/
 */
 /*
-Generate a random interger in [0,limit]
+Generate a random integer in [0,limit]
 Avoid using previous one
 */
 int randLimit(const int limit)
 {
-	static int prev;
+    static int prev;
     int div = RAND_MAX/(limit+1);
     int res = -1;
     do{ 
@@ -29,26 +29,14 @@ void swap(int* a, int lt, int rt)
 void test(int* a, int N)
 {
     int i = 0;
-	int valid = 1;
+    int valid = 1;
     for(;i<N-1 && valid;i++)
          if(a[i]>a[i+1]) valid = 0;
     if(valid){
-		printf("Test of array passed!\n");
-	} else{
-		printf("Failed!\n");
-	}
-}
-//Create a random int array
-void creat(int* a, int N, const int limit)
-{
-    int i = 0;
-    for(;i<N;i++) a[i] = randLimit(limit);
-}
-//Copy a random int array
-void copy(int* src, int N, int* tag)
-{
-    int i = 0;
-    for(;i<N;i++) tag[i] = src[i];
+        printf("Test of array passed!\n");
+    } else{
+	printf("Failed!\n");
+    }
 }
 // print out int array
 void dump(int* a, int N)
@@ -57,15 +45,21 @@ void dump(int* a, int N)
     for(;i<N;i++) printf("%d ",a[i]);
     printf("\n");
 }
-
+/*
+RGB
+012
+*/
 void color3(int* arr, int lo, int hi){
-	int nextRPos = lo;
+    int nextRPos = lo;
     int nextBPos = hi;
-	int i  = 0;
+    int i  = 0;
     while(i <= nextBPos){
-        if(arr[i] == 0) swap(arr,i++,nextRPos++);
-        else if(arr[i] == 2) swap(arr,i,nextBPos--);
-		else i++;
+        if(arr[i] == 0) 
+            swap(arr,i++,nextRPos++);
+        else if(arr[i] == 2) 
+            swap(arr,i,nextBPos--);
+        else // arr[i] == 1
+            i++;
     }
 }
 // Tester  
@@ -76,13 +70,8 @@ int main(int argc, char* argv[])
     int i;
     int colors[N];
 
-    /* 
-		Test sort 3 colors. Notation:
-		R G B
-		0 1 2
-	*/
-	for(i=0;i<N;i++) colors[i] = randLimit(2);
-	dump(colors,N);
+    for(i=0;i<N;i++) colors[i] = randLimit(2);
+    dump(colors,N);
     color3(colors,0,N-1);
     dump(colors,N);test(colors,N);
 
