4cc973bbaba4040f48b86f73945408a8f3d5f6b5
Jun <junzhengrice@gmail.com>
2015-05-13 10:59:26 -0500

    Corrected initial values of $candies array

--- 135 Candy.py
+++ 135 Candy.py
@@ -6,24 +6,24 @@ def candy(ratings):
     n = len(ratings)
     if n == 0 : return
 
-    candies = [0]
+    candies = [1 for x in xrange(0,n)]
     for i in range(1,n):
         if ratings[i] > ratings[i-1]:
-            candies.append(candies[i-1]+1)
+            candies[i] = candies[i-1]+1
         else:
-            candies.append(1)
+            candies[i] = 1
+            
     for i in range(n-2,-1,-1):
         if ratings[i] > ratings[i+1] and candies[i] <= candies[i+1]:
             candies[i] = candies[i+1] + 1;
 
-    print candies
+    print candies # debug
     sum = 0
     for v in candies: sum += v
     return sum
 
 def main():
-    ratings = [5,1,3,5,7,9,3,1,5,7,11,9]
-    candies = []
+    ratings = [1,2,2]
     print candy(ratings)
     
 if __name__ == "__main__":
