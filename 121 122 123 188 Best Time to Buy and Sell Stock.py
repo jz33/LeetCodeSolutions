a61ead64fb0b4a104a689e7758b55753c9d3d7cf
Jun <junzhengrice@gmail.com>
2015-05-29 15:25:42 -0500

    add 188 Best Time to Buy and Sell Stock IV

--- 121 122 123 188 Best Time to Buy and Sell Stock.py
+++ 121 122 123 188 Best Time to Buy and Sell Stock.py
@@ -3,18 +3,22 @@ import random,sys
 121 Best Time to Buy and Sell Stock
 122 Best Time to Buy and Sell Stock II
 123 Best Time to Buy and Sell Stock III
-https://oj.leetcode.com/problems/best-time-to-buy-and-sell-stock/
-https://oj.leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
-https://oj.leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/
+188 Best Time to Buy and Sell Stock IV
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iv/
 '''
 # 121
 def singleTrade(prices):
-    if len(prices) < 2: print "len(prices) < 2"
+    if len(prices) < 2: 
+        print "len(prices) < 2"
+        return
 
     start = 0; ended = 0; profit = 0 # result vars
     lo = 0; # tracing var
 
-    print "prices: {}\n".format(prices)
+    #print "prices: {}\n".format(prices)
     
     # append min int for last transaction
     prices.append(-1)
@@ -35,10 +39,12 @@ def singleTrade(prices):
 
 # 122
 def multiTrades(prices):
-    if len(prices) < 2: print "len(prices) < 2"
+    if len(prices) < 2: 
+        print "len(prices) < 2"
+        return
 
     profit = 0;lo = 0; 
-    print "prices: {}\n".format(prices)
+    #print "prices: {}\n".format(prices)
     
     # append min int for last transaction
     prices.append(-1)
@@ -52,12 +58,14 @@ def multiTrades(prices):
 
 # 123
 def towTrades(prices):
-    if len(prices) < 2: print "len(prices) < 2"
+    if len(prices) < 2: 
+        print "len(prices) < 2"
+        return
 
     profit = 0 ; leftEnd = 0; # result vars
     hi =0; lo = 0; # tracing var
     
-    print "prices: {}\n".format(prices)
+    #print "prices: {}\n".format(prices)
     
     # compute max profit of single trade from 0 to each index,
     # as if that index is the last day
@@ -99,6 +107,25 @@ def towTrades(prices):
     print "mp: {}".format(mp)
     print "profit: {}, leftEnd: {}\n\n".format(profit, leftEnd)
         
+
+#188
+# http://www.programcreek.com/2014/03/leetcode-best-time-to-buy-and-sell-stock-iv-java/
+def kTrades(prices,k):
+    if len(prices) < 2: 
+        print "len(prices) < 2"
+        return
+ 
+    inner = [0 for i in xrange(k + 1)];
+    outer = [0 for i in xrange(k + 1)];
+
+    for i in xrange(0,len(prices)-1):
+        diff = prices[i + 1] - prices[i]
+        for j in xrange(k,0,-1):
+            inner[j] = max(outer[j - 1] + max(diff, 0), inner[j] + diff)
+            outer[j] = max(inner[j], outer[j])
+            
+    print "inner: {}".format(inner)
+    print "outer: {}".format(outer)
     
 def simpleTest(func):
     print func.__name__
@@ -120,11 +147,28 @@ def simpleTest(func):
     prices = [] # random
     for i in range(0,10): prices.append(random.randint(1,30))
     func(prices)
+ 
+def kTradesTest():
+    prices = [4,4,4,4,4] # flat
+    kTrades(prices,len(prices))
+    
+    prices = [] # ascending
+    for i in range(0,10): prices.append(i*2+1)
+    multiTrades(prices)
+    kTrades(prices,len(prices))
+    
+    prices = [4,10,2,3,4] # not lowest as start
+    towTrades(prices)
+    kTrades(prices,2)
+    multiTrades(prices)
+    kTrades(prices,len(prices))
     
 def main():
-    simpleTest(singleTrade)
-    simpleTest(multiTrades)
-    simpleTest(towTrades)
+    #simpleTest(singleTrade)
+    #simpleTest(multiTrades)
+    #simpleTest(towTrades)
+    
+    kTradesTest()
     
 if __name__ == "__main__":
     main()
