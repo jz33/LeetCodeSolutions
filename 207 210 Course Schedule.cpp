e28238293f498a946df3f5afeaab3fb9a7c9e299
Jun <junzhengrice@gmail.com>
2015-06-01 15:55:16 -0500

    Update 207 210 Course Schedule.cpp

--- 207 210 Course Schedule.cpp
+++ 207 210 Course Schedule.cpp
@@ -10,19 +10,13 @@
 https://leetcode.com/problems/course-schedule/
 https://leetcode.com/problems/course-schedule-ii/
 
-A standard deep-first traversal
+This is assumed that "1 node is reachable only if
+ALL parent nodes are reached", then a standard
+breath-first traversal is used to detect cycle.
 
-At start, nodes are divided into 2 groups,
-"root nodes", who has no dependences,
-"inner nodes", who has dependences.
-
-If "one node is reachable if one of its parent is reached",
-then all inner nodes are reachable if and only
-if at least 1 root node is pointed to at least
-1 inner node. This is very simple.
-
-Else if "one node is reachable only if all of its parents are reached",
-then the problem is to find whether the graph has cycle
+If condition is "1 node is reachable if
+1 of its parent nodes is reached", detecting cycle
+is unnecessary
 */
 #define MAKE(a,b) std::make_pair(a,b)
 #define DEBUG 1
@@ -31,83 +25,92 @@ typedef std::pair<int, int> Pair;
 typedef std::unordered_set<int> Uset;
 typedef std::unordered_map<int, Uset> Graph;
 
-// print dfs procedure if @ 210
-bool dfs(int numCourses, std::vector<Pair>& in)
+// print procedure if @ 210
+bool compute(int numCourses, std::vector<Pair>& in)
 {
-	/*
-	-1 : not reached yet
-	0 : starting node
-	1 : reached
-	*/
-	std::vector<int> ref(numCourses, 0);
-	Graph graph;
+    /*
+    count parents. 0 : no parents
+    */
+    std::vector<int> ref(numCourses, 0);
+    Graph graph;
 
-	// initialize starting nodes, graph
-	int togo, from;
-	for (auto i = in.begin(); i != in.end(); i++)
-	{
-		togo = i->first;
-		from = i->second;
+    // initialize starting nodes, graph
+    int togo, from;
+    for (auto i = in.begin(); i != in.end(); i++)
+    {
+        togo = i->first;
+        from = i->second;
 
-		ref[togo] = -1;
-		graph[from].insert(togo);
-	}
+        ref[togo]++;
+        graph[from].insert(togo);
+    }
 
-	// gather starting nodes
-	std::deque<int> dq;
-	int reached = 0;
-	for (int i = 0; i<numCourses; i++){
-		if (ref[i] == 0){
-			dq.push_back(i);
-			reached++;
-		}
-	}
+    // gather starting nodes
+    std::deque<int> dq;
+    int reached = 0;
+    for (int i = 0; i<numCourses; i++){
+        if (ref[i] == 0){
+            dq.push_back(i);
+            reached++;
+        }
+    }
 
-	// dfs
-	while (!dq.empty() && reached != numCourses)
-	{
-		from = dq.front();
-		dq.pop_front();
+    // bfs
+    while (!dq.empty())
+    {
+        from = dq.front();
+        dq.pop_front();
 
-		Uset& cands = graph[from];
-		for (auto i = cands.begin(); i != cands.end(); i++)
-		{
-			if (ref[*i] == -1)
-			{
-				ref[*i] = 1;
-				reached++;
-				dq.push_back(*i);
-			}
-            // if ref[*i] == 1, cycle !
-		}
-	}
+        Uset& cands = graph[from];
+        for (auto i = cands.begin(); i != cands.end(); i++)
+        {
+            if (ref[*i] == 1)
+            {
+                reached++;
+                dq.push_back(*i);
+            }
+            else
+                ref[*i]--;
+        }
+    }
 
-	if (DEBUG)
-	{
-		std::cout << "ref: \n";
-		for (int i = 0; i<numCourses; i++)
-			std::cout << ref[i] << " ";
-		std::cout << "\n";
-	}
-	return reached == numCourses;
+    if (DEBUG)
+    {
+        std::cout << "ref: \n";
+        for (int i = 0; i<numCourses; i++)
+            std::cout << ref[i] << " ";
+        std::cout << "\n";
+    }
+    return reached == numCourses;
+}
+void test(void)
+{
+    int numCourses = 4;
+    std::vector<Pair> in;
+    in.push_back(MAKE(1, 0));
+    in.push_back(MAKE(2, 0));
+    in.push_back(MAKE(3, 1));
+    in.push_back(MAKE(3, 2));
+
+    std::cout << compute(numCourses, in) << "\n";
+
+    in.clear();
+    in.push_back(MAKE(1, 0));
+    in.push_back(MAKE(2, 1));
+    in.push_back(MAKE(3, 2));
+    in.push_back(MAKE(1, 3));
+
+    std::cout << compute(numCourses, in) << "\n";
+
+    in.clear();
+    in.push_back(MAKE(2, 1));
+    in.push_back(MAKE(3, 2));
+    in.push_back(MAKE(1, 3));
+
+    std::cout << compute(numCourses, in) << "\n";
 }
 int main()
 {
-	int numCourses = 4;
-	std::vector<Pair> in;
-	in.push_back(MAKE(1, 0));
-	in.push_back(MAKE(2, 0));
-	in.push_back(MAKE(3, 1));
-	in.push_back(MAKE(3, 2));
-
-	std::cout << dfs(numCourses, in) << "\n";
-
-	in.clear();
-	in.push_back(MAKE(1, 0));
-	in.push_back(MAKE(2, 1));
-	in.push_back(MAKE(1, 3));
-
-	std::cout << dfs(numCourses, in) << "\n";
-
-	return 0;
+    test();
+    return 0;
 }
