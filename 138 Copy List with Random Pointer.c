8e46b0e80de9f4a75f4b0796600d42e03a601d68
Jun <junzhengrice@gmail.com>
2015-06-04 16:54:08 -0700

    Fix NULL bug

--- 138 Copy List with Random Pointer.c
+++ 138 Copy List with Random Pointer.c
@@ -24,37 +24,45 @@ A similar method can be applied on binary tree copy
 A specific copy method for "copyRandom"
 Append "tag" node behind "src" node
 */
-#define COPY(tag,src) \
-    tag->rank = src->rank;\
-    tag->next = src->next;\
-    tag->jump = src->jump;\
-    src->next = tag;  // insert
-     
-node* copyRandom(node* old){
-    node* res;
-    node* pold = old;
-    node* pnew = res;
+node* copyRandom(node* old)
+{
+    node *res, *pold, *pnew;
+    if(old == 0) return 0;
     
-    if(pold == 0) return 0;
+    res = NEWNODE;
+    pold = old;
+    pnew = res;
     
     // 0. insert new nodes
-    while(pold != 0){
-        pnew = NEWNODE;
-        COPY(pnew,pold);
+    while(pold != 0)
+    {
+        if(pnew != res) pnew = NEWNODE;
+        
+        //copy
+        pnew->rank = pold->rank;
+        pnew->next = pold->next;
+        pnew->jump = pold->jump;
+        
+        // insert & move
+        pold->next = pnew;  
         pold = pnew->next;
     }
+    
     // 1. modify jump nodes
     pnew = old;
-    while(pnew != 0){
+    while(pnew != 0)
+    {
         pnew = pnew->next; // now at new node
         if(pnew->jump != 0) 
             pnew->jump = pnew->jump->next;
         pnew = pnew->next; // now at old node or NULL
     }
-    // 2. split 2 lists
+    
+    // 2. split
     pold = old;
     pnew = res;
-    while(pnew->next != 0){
+    while(pnew->next != 0)
+    {
         pold->next = pnew->next;
         pold = pold->next;
         pnew->next = pold->next;
