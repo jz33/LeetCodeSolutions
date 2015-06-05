c53478b5827f660bbe141c5bd5bf3eb887023527
Jun <junzhengrice@gmail.com>
2015-06-04 17:53:53 -0700

    add error check

--- 008 String to Integer (atoi).c
+++ 008 String to Integer (atoi).c
@@ -3,17 +3,19 @@
 08 String to Integer (atoi)
 https://oj.leetcode.com/problems/string-to-integer-atoi/
 */
-int _atoi(char* p)
-{
-    int s = *p == '-' ? -1:1;
-    int r = 0;
-    
-    if(s == -1) p++;
-    
-    while(*p != '\0')
-        r = r*10 + *p++ - '0';
-		
-    return r*s;
+int _atoi(const char* str){
+    char sign = 0;
+    int z = 0;
+    if (*str == '-'){
+        sign = 1;
+        str++;
+    }
+    while (*str != '\0'){
+        if (*str >= '0' && *str <= '9')
+            z = z * 10 + *str - '0';
+        str++;
+    }
+    return sign == 0 ? z : -z;
 }
 
 int main()
