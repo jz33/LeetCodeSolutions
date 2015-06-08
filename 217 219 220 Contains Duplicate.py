8e149f3a1d839eb200168be1580edae1b4e6d627
Jun <junzhengrice@gmail.com>
2015-06-08 13:45:53 -0500

    Accepted

--- 217 219 220 Contains Duplicate.py
+++ 217 219 220 Contains Duplicate.py
@@ -11,11 +11,11 @@ def containsDuplicate(nums):
     return len(nums) > len(set(nums))       
 
 # 219
-def containsDuplicate2(nums,dist):
+def containsDuplicate2(nums,k):
     ref = set()
     for i,v in enumerate(nums):
-        if i > dist: 
-            ref.remove(nums[i-dist-1])
+        if i > k: 
+            ref.remove(nums[i-k-1])
         if v in ref: return True
         else: ref.add(v)
     return False
@@ -24,34 +24,31 @@ def containsDuplicate2(nums,dist):
 220
 https://leetcode.com/discuss/38206/ac-o-n-solution-in-java-using-buckets-with-explanation
 Assume all numbers are >= 0
+
+k: difference of i,j
+t: distance of nums[i],nums[j]
 '''
-def containsDuplicate3(nums,dist,diff):
-    if dist < 1 or diff < 0: return False
+def containsDuplicate3(nums,k,t):
+    if k < 1 or t < 0: return False
     
     buckets = {} # bucket : value
     for i,v in enumerate(nums):
-        if i > dist:
-            b = nums[i-dist-1] / (diff+1); 
+        if i > k:
+            b = nums[i-k-1] / (t+1);
             del buckets[b]
             
-        b = v / (diff+1);
+        b = v / (t+1);
         if b in buckets or \
-            b-1 in buckets and v - buckets[b-1] <= diff or \
-            b+1 in buckets and buckets[b+1] - v >= diff:
+            b-1 in buckets and v - buckets[b-1] <= t or \
+            b+1 in buckets and buckets[b+1] - v <= t:
             return True
             
         buckets[b] = v  
     return False
     
 def main():
-    arr = [0,1,2,3,4,5,0,1,2,3,4,5]
-    print containsDuplicate(arr)
-    print containsDuplicate2(arr,5)
-    print containsDuplicate2(arr,6)
-    
-    arr = [0,100,200,3,300,400,6,500,600,9]
-    print containsDuplicate3(arr,2,3)
-    print containsDuplicate3(arr,3,3)
+    arr = [4,2]
+    print containsDuplicate3(arr,2,1)
     
 if __name__ == '__main__':
     main()
