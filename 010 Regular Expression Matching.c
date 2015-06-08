730cc9cb55ef6572a32ff9bd812f8b3f1e1a3177
Jun <junzhengrice@gmail.com>
2015-06-08 12:26:30 -0500

    Accepted answer by OJ

--- 010 Regular Expression Matching.c
+++ 010 Regular Expression Matching.c
@@ -1,44 +1,39 @@
 #include <stdio.h>
-#include <string.h>
 /*
 10 Regular Expression Matching
 https://oj.leetcode.com/problems/regular-expression-matching/
+
+Accepted
+
 '.' matches any single char
 '*' matches 0 or more previous char
 O(2^N!), N is '*' numbers
 */
-int isMatch(char* tag,char* pat)
+bool isMatch(char* s, char* p)
 {
-    int res = 0;
-
-    // base case
-    if(*pat == '\0' ) 
-        return *tag == '\0';
-    if(*tag == '\0' )
-        return strlen(pat) == 2 && *(pat+1) == '*';
-            
-    if(*(pat+1) != '*')
-    {
-        if(*pat == '.' || *pat == *tag) 
-            res = isMatch(tag+1,pat+1);
-    } 
-    else 
-    {
-        // 0. skip '*'
-        res = isMatch(tag,pat+2);
-        
-        // 1. '*' matches more than 1 chars
-        if(res == 0)
-            if(*pat == '.' || *pat == *tag) 
-                res = isMatch(tag+1,pat);
-    }
-    return res;
+    // "" -> ""
+    if(*p == '\0') 
+        return *s == '\0';
+    
+    // "" -> "" || ".*.*"
+    if(*s == '\0')
+        return p[1] == '*' && isMatch(s,p+2);
+    
+	if(p[1] == '\0' || p[1] != '*')
+        return (*p == '.' || *p == *s) && isMatch(s+1,p+1);
+    
+    // p[1] == '*'
+    // 0. skip '*'
+    return isMatch(s,p+2) ||
+                
+    // 1. '*' matches more than 1 chars
+    ((*p == '.' || *p == *s) && isMatch(s+1,p));
 }
 
 int main()
 {
-    char* pat = ".*";
-    char* tag = "a";
-    printf("%d\n",isMatch(tag,pat));
+    printf("%u\n",isMatch("ab", ".*c")); // false
+    printf("%u\n",isMatch("aa", "a*")); // true
+    printf("%u\n",isMatch("", "c*c*")); // true
     return 0;  
 }
