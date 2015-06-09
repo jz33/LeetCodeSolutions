e22f0cebc80f647ab449fb8b287a04538ffd1503
Jun <junzhengrice@gmail.com>
2015-06-09 10:01:00 -0500

    Update 074 Search a 2D Matrix.cpp

--- 074 Search a 2D Matrix.cpp
+++ 074 Search a 2D Matrix.cpp
@@ -1,47 +1,45 @@
 #include <iostream>
+#include <vector>
 /*
 74 Search a 2D Matrix
-A naive version requires: 
-"The first integer of each row is greater than the last integer of the previous row."
-See:
 https://oj.leetcode.com/problems/search-a-2d-matrix/
-Can be simply done by binary search first column, then binary search at most 1 row
-
-A more general case:
-http://articles.leetcode.com/2010/10/searching-2d-sorted-matrix-part-ii.html
-
-Table[i][j] <= Table[i][j + 1] 
-Table[i][j] <= Table[i + 1][j]
 */
-#define COL_NUM 5
-
+typedef std::vector<std::vector<int> > Matrix;
 /*
 Step-wise approach, starting from upper-right
 Worst: O(n)
+$row, $col : output
+
+An alternative way is to binary search column,
+then binary search row.
 */
-int stepWise(int mat[COL_NUM][COL_NUM],int target,int& row,int& col)
+int stepWise(Matrix& mat,int target,int& row,int& col)
 {
-    if(target < mat[0][0] || target > mat[COL_NUM-1][COL_NUM-1])
+    size_t ROW_NUM = mat.size();
+    size_t COL_NUM = mat[0].size();
+    if(target < mat[0][0] || target > mat[ROW_NUM-1][COL_NUM-1])
         return 0;
 
     row = 0;
     col = COL_NUM - 1;
-    while(row <= COL_NUM -1 && col >= 0)
+    while(row <= ROW_NUM -1 && col >= 0)
     {
         if(mat[row][col] < target) 
             row++;
         else if (mat[row][col] > target)
             col--;
         else
-            return 0;
+            return 1;
     }
-    return 1;
+    return 0;
 }
 /*
-A binary search for ascending sorted array
-Return insertion position, i.e.,
-the index of the new element if is inserted
+Below is a more general case solution, i.e.,
+The first integer of each row is NOT necessarily 
+greater than the last integer of the previous row.
 */
+#define SIZE 5
+
 int binary(int arr[], int tag, int lt, int rt)
 {
     int mid;
@@ -70,7 +68,7 @@ int binary(int arr[], int tag, int lt, int rt)
 Binary Partition by row
 O((lg n)^2)
 */
-int partition(int mat[COL_NUM][COL_NUM],int tag,int lt,int rt,int up,int dn,int& row,int& col)
+int partition(int mat[SIZE][SIZE],int tag,int lt,int rt,int up,int dn,int& row,int& col)
 {
     if(lt > rt || up > dn)
         return 0;
@@ -93,14 +91,14 @@ int partition(int mat[COL_NUM][COL_NUM],int tag,int lt,int rt,int up,int dn,int&
         partition(mat, tag, lt,    index-1, mid+1, dn, row, col);
 }
  
-int binaryPartition(int mat[COL_NUM][COL_NUM],int tag,int &row,int &col)
+int binaryPartition(int mat[SIZE][SIZE],int tag,int &row,int &col)
 {
-    return partition(mat,tag,0,COL_NUM-1,0,COL_NUM-1,row,col);
+    return partition(mat,tag,0,SIZE-1,0,SIZE-1,row,col);
 }
 
 int main()
 {
-    int mat[COL_NUM][COL_NUM] = {\
+    int mat[SIZE][SIZE] = {\
         { 1, 4, 5,11,15},\
         { 2, 5, 8,12,19},\
         { 3, 6, 9,16,22},\
@@ -111,8 +109,8 @@ int main()
     int col = -1;
     
     int tag,r0,r1,i,j;
-    for(i=0;i<COL_NUM;i++){
-        for(j=0;j<COL_NUM;j++){
+    for(i=0;i<SIZE;i++){
+        for(j=0;j<SIZE;j++){
             tag = mat[i][j];
             row = -1,col = -1;
             
