b4e09fef8f5e09049837f06844213f67b25fad78
Jun <junzhengrice@gmail.com>
2015-06-10 15:37:42 -0500

    Accepted; corrected negative cases

--- 053 Maximum Subarray.c
+++ 053 Maximum Subarray.c
@@ -1,24 +1,46 @@
 /*
 Maximum Subarray
 https://oj.leetcode.com/problems/maximum-subarray/
-
-Kadane’s Algorithm for 
-largest sum of a contiguous array
 */
-int MaxSubArrSum(int* arr, const int N){
-    int max_so_far = 0, max_ending_here = 0;
-    int i;
-    for(i = 0;i<N;i++)
-    {
-        max_ending_here += arr[i];
-        if(max_ending_here < 0) 
-            max_ending_here = 0;
-        else if(max_so_far < max_ending_here)
-            max_so_far = max_ending_here;
-    }
-    return max_so_far;
-}  
+/*
+Kadane’s Algorithm
+Assume $size >= 1
+And notice $global CANNOT start from 0
+*/
+int kadaneMax(int* arr, int size)
+{
+    int local, global, i;
+    if(size == 0) return -1;
 
+    local = arr[0];
+    global = local;
+
+    for(i = 1;i<size;i++)
+    {
+        if(local < 0) local = 0;
+        
+        local += arr[i];
+        if(global < local) global = local;
+    }
+    return global;
+}  
+int kadaneMin(int* arr, int size)
+{
+    int local, global, i;
+    if(size == 0) return -1;
+
+    local = arr[0];
+    global = local;
+
+    for(i = 1;i<size;i++)
+    {
+        if(local > 0) local = 0;
+        
+        local += arr[i];
+        if(global > local) global = local;
+    }
+    return global;
+} 
 int main()
 {
     //TODO: test
