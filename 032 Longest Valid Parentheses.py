b19aa55423e6bd5ca3120e3f5bf85e148a8f9901
Jun <junzhengrice@gmail.com>
2015-06-10 15:18:58 -0500

    Accepted;change to O(n) method

--- 032 Longest Valid Parentheses.py
+++ 032 Longest Valid Parentheses.py
@@ -2,47 +2,54 @@ import random
 '''
 32 Longest Valid Parentheses
 https://oj.leetcode.com/problems/longest-valid-parentheses/
+
+O(n)
 '''
 # generate a random Parentheses string with size n
 def randParentheses(n):
     return ''.join(random.choice('('+')') for _ in range(n))
  
 def longestValidParentheses(sent):
+    if len(sent) < 2:
+        return 0
     '''
     Notations for 'buf' :
-    '-1': invalid from here till end of string
     '0' : currently paired
     >0  : num of '(' - num of ')'
     '''
-    buf = -1
-    lt  = 0
-    rt  = -1
-     
-    for i in range(0,len(sent)-1):
-        if sent[i] == ')': continue
-         
-        buf = 1
-        for j in range(i+1,len(sent)):
-            if sent[j] == ')':
-                buf -= 1
-                if buf < 0 : 
-                    i = j # speed-up
-                    break
-            else: buf += 1
-                 
-            # update result
+    buf = 0
+    max_len = 0
+    start = 0
+    for i in xrange(0, len(sent)):
+        if sent[i] == '(':
+            buf += 1
+        else:
+            buf -= 1
             if buf == 0:
-                if j - i > rt - lt :
-                    rt = j
-                    lt = i
-    print lt, rt
+                max_len = max(max_len, i - start + 1)
+            elif buf < 0:
+                start = i + 1
+                buf = 0
+
+    end = len(sent) - 1
+    buf = 0
+    for i in xrange(len(sent)-1, -1,-1):
+        if sent[i] == ')':
+            buf += 1
+        else:
+            buf -= 1
+            if buf == 0:
+                max_len = max(max_len, end - i + 1)
+            elif buf < 0:
+                end = i - 1
+                buf = 0
+    return max_len
        
 def main():
     repeat = 5
     for i in range(0,repeat):
         sent = randParentheses(10)
-        print sent
-        longestValidParentheses(sent)
+        print sent, longestValidParentheses(sent)
      
 if __name__ == "__main__":
     main()
