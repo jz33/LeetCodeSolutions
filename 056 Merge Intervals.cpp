2b9e450348da37d8ae2495bd736a1f23ee95e629
Jun <junzhengrice@gmail.com>
2015-07-02 12:16:14 -0500

    Accepted; Rewrite

--- 056 Merge Intervals.cpp
+++ 056 Merge Intervals.cpp
@@ -1,38 +1,51 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <utility>
 /*
-56 Merge Intervals
+Merge Intervals
 https://oj.leetcode.com/problems/merge-intervals/
 */
-typedef std::pair<int,int> Interval;
-
-bool compareIntevals(Interval i,Interval j)
+struct Interval {
+    int start;
+    int end;
+};
+Interval make_pair(int x,int y)
 {
-    if(i.first != j.first) return i.first < j.first;
-    else return i.second < j.second;
+    Interval r;
+    r.start = x;
+    r.end = y;
+    return r;
 }
 void dump(std::vector<Interval>& input)
 {
     for(std::vector<Interval>::iterator i = input.begin();i!=input.end();i++)
     {
-        std::cout<<"("<<i->first<<","<<i->second<<") ";
+        std::cout<<"("<<i->start<<","<<i->end<<") ";
     }
     printf("\n");
 }
-std::vector<Interval> mergeIntevals(std::vector<Interval> input)
+
+bool compareIntevals(Interval i,Interval j)
 {
-    std::stable_sort(input.begin(),input.end(),compareIntevals);
+    if(i.start != j.start) return i.start < j.start;
+    else return i.end < j.end;
+}
+
+std::vector<Interval> mergeIntevals(std::vector<Interval>& input)
+{
     std::vector<Interval> ret;
+    if(input.size() == 0) return ret;
+    
+    std::stable_sort(input.begin(),input.end(),compareIntevals);
+    
     std::vector<Interval>::iterator prev = input.begin();
     for(std::vector<Interval>::iterator i = input.begin()+1;i!=input.end();i++)
     {
-        if(prev->second < i->first){
+        if(prev->end < i->start){
             ret.push_back(*prev);
             prev = i;
-        } else if(prev->second <= i->second){
-            prev->second = i->second;
+        } else if(prev->end <= i->end){
+            prev->end = i->end;
         }
     }
     ret.push_back(*prev);
@@ -42,10 +55,10 @@ std::vector<Interval> mergeIntevals(std::vector<Interval> input)
 int main(int argc, char** argv)
 {
     std::vector<Interval> input;
-    input.push_back(std::make_pair(1,3));
-    input.push_back(std::make_pair(2,6));
-    input.push_back(std::make_pair(8,10));
-    input.push_back(std::make_pair(15,18));
+    input.push_back(make_pair(1,3));
+    input.push_back(make_pair(2,6));
+    input.push_back(make_pair(8,10));
+    input.push_back(make_pair(15,18));
     
     std::vector<Interval> out = mergeIntevals(input);
     
