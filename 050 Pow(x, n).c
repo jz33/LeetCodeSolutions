afa9e62349929a55ac7966c2c2c422ae821f89d2
Jun <junzhengrice@gmail.com>
2015-07-09 15:56:09 -0500

    Accepted

--- 050 Pow(x, n).c
+++ 050 Pow(x, n).c
@@ -5,31 +5,27 @@ https://oj.leetcode.com/problems/powx-n/
 http://www.programcreek.com/2012/12/leetcode-powx-n/
 
 A divide-conquer approach,
-very inpractical
+very impractical
 */
-double _power(double x, int n){
-    double v = 1;
-	if (n == 0) return v;
- 
-	v = _power(x, n / 2);
- 
-	if (n % 2 == 0) {
-		return v * v;
-	} else {
-		return v * v * x;
-	}
-}
- 
-double _pow(double x, int n){
-	if (n < 0) {
-		return 1 / _power(x, -n);
-	} else {
-		return _power(x, n);
-	}
+double myPow(double x, int n) {
+    double ret = 1.0;
+    unsigned long long p;
+    if (n < 0) {
+        p = -n;
+        x = 1 / x;
+    } else {
+        p = n;
+    }
+    while (p) {
+        if (p & 1)
+            ret *= x;
+        x *= x;
+        p >>= 1;
+    }
+    return ret;
 }
 
 int main()
 {
-    printf("%d\n",_pow(12,12));
     return 0;
 }
