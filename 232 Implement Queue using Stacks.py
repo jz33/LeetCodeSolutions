7efda916300610b439f68de23828ff1f4379a0c8
Jun <junzhengrice@gmail.com>
2015-07-10 15:55:34 -0500

    Accepted

--- 232 Implement Queue using Stacks.py
+++ 232 Implement Queue using Stacks.py
@@ -5,19 +5,38 @@ https://leetcode.com/problems/implement-queue-using-stacks/
 No error or exception checking
 '''
 class Queue:
+    # initialize your data structure here.
     def __init__(self):
         self.data = []
-        
+        self.help = []
+
+    # @param x, an integer
+    # @return nothing
     def push(self, x):
         self.data.append(x)
 
+    # @return nothing
     def pop(self):
-        d = self.data[0]
-        self.data.pop(0)
-        return d
+        while len(self.data) != 0:
+            self.help.append(self.data.pop())
+            
+        self.help.pop()
+        
+        while len(self.help) != 0:
+            self.data.append(self.help.pop())
 
+    # @return an integer
     def peek(self):
-        return self.data[0]
-
+        while len(self.data) != 1:
+            self.help.append(self.data.pop())
+            
+        d = self.data.pop()
+        self.data.append(d)
+        
+        while len(self.help) != 0:
+            self.data.append(self.help.pop())
+        return d
+        
+    # @return an boolean
     def empty(self):
         return len(self.data) == 0
