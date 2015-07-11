cd4816de002d63a79fdc187c9557b49dd1136858
Jun <junzhengrice@gmail.com>
2015-07-11 14:00:56 -0500

    Rewrite; Accepted; Found C++ <multiset> might be screwd sometime (wrong answer of get value query); use <map> instead;

--- 218 The Skyline Problem.cpp
+++ 218 The Skyline Problem.cpp
@@ -2,103 +2,140 @@
 #include <sstream>
 #include <vector>
 #include <algorithm>
-#include <set>
+#include <map>
 /*
 218 The Skyline Problem
 https://leetcode.com/problems/the-skyline-problem/
-
-An example:
-https://www.codeeval.com/open_challenges/120/
 http://www.geeksforgeeks.org/divide-and-conquer-set-7-the-skyline-problem/
 */
-typedef std::string Str;
-typedef std::pair<int,int> Pair;
+#define MAKE std::make_pair
 
-#define PARSEINT(p,e,del)\
-	while(*p!=del){\
-		e = e*10 + *p -'0';\
-		p++;\
-	}p++;
+typedef long long LONG; // in case of int32_t overflow
+typedef std::vector<LONG> VEC;
+typedef std::vector<VEC> MAT;
+typedef std::pair<LONG,LONG> PAIR;
+typedef std::map<LONG,LONG> MAP;
 
-/*
-Parse the input following Codeeval.com format
-Here the input format is (Li,Hi,Ri) (Li,Hi,Ri) ... 
-*/	
-void parse(std::string& in, std::vector<Pair>& out)
+void dump(std::vector<PAIR>& res)
 {
-    std::istringstream iss(in);
-    Str unit;
-    out.clear();
-    
-    while(iss>>unit)
+    for(auto i = res.begin();i!=res.end();i++)
     {
-        int x=0,y=0,h=0;
-        char* p = &unit[1];//*(p-1) == '('
-        PARSEINT(p,x,',')
-        PARSEINT(p,h,',')
-        PARSEINT(p,y,')')
-        out.push_back(std::make_pair(x,h));
-        out.push_back(std::make_pair(y,-h));//minus indicates end of building
+        std::cout<<"["<<i->first<<","<<i->second<<"],";
+    }
+    std::cout<<"\n";
+}
+
+void dumpMat(MAT& res)
+{
+    for(auto i = res.begin();i!=res.end();i++)
+    {
+        std::cout<<"[";
+        for(auto j = i->begin();j!=i->end();j++)
+        {
+            std::cout<<*j<<" ";
+        }
+        std::cout<<"]\n";
+    }
+}
+
+void parse(MAT& in, std::vector<PAIR>& out)
+{
+    LONG li,ri,hi;
+    
+    out.clear();
+    for(auto i = in.begin();i!=in.end();i++)
+    {
+        li = (*i)[0];
+        ri = (*i)[1];
+        hi = (*i)[2];
+        out.push_back(MAKE(li,hi));
+        out.push_back(MAKE(ri,-hi));//minus indicates end of building
     }
 
-    std::stable_sort(out.begin(),out.end(),[](const Pair& x, const Pair& y)
+    std::stable_sort(out.begin(),out.end(),[](const PAIR& x, const PAIR& y)
     {
         return x.first < y.first;
     });
 }
-void compute(std::vector<Pair>& vec)
+
+std::vector<PAIR> getSkyline(MAT& buildings)
 {
-    if(vec.size()==2)
-        std::cout<<vec.begin()->first<<" "<<vec.begin()->second<<" ";
+    std::vector<PAIR> res;
+    std::vector<PAIR> vec;
+    parse(buildings,vec);
+    
+    if(vec.size() == 0)
+        return res;
+        
+    if(vec.size() == 2)
+    {
+        res.push_back(MAKE(vec.begin()->first,vec.begin()->second));
+    }
     else
     {
-        std::multiset<int> heights; // previous heights
-        int prev; // previous max height
+        MAP heights; // previous heights, possible duplicates
+        LONG prev; // previous max height
         
-        heights.insert(0);
-        prev = *heights.rbegin();
+        heights.insert(MAKE(0,0));
+        prev = 0;
         
         for(auto i = vec.begin();i!=vec.end()-1;i++)
         {
-            int p = i->first;
-            int h = i->second;
+            LONG p = i->first;
+            LONG h = i->second;
             
-            if(h>0) heights.insert(h);
-            else heights.erase(-h);
+            if(h>0){
+                heights[h]++;
+            }
+            else{
+                heights[-h]--;
+                if(heights[-h] == 0) heights.erase(-h);
+            }
             
             // if next building's position is not same as current's
             if((i+1)->first != p)
             {  
                 // if $prev has changed
-                if(prev != *heights.rbegin())
-                    std::cout<<p<<" "<<*heights.rbegin()<<" ";
-                prev = *heights.rbegin(); 
+                if(prev != heights.rbegin()->first)
+                {
+                    res.push_back(MAKE(p,heights.rbegin()->first));
+                }
+                prev = heights.rbegin()->first; 
             }
         }
     }
-    std::cout<<vec.rbegin()->first<<" 0\n";
-}		
-void test_skyline(void)
+    res.push_back(MAKE(vec.rbegin()->first,0));
+    return res;
+}
+
+void test(void)
 {
-    std::vector<Str> input(10,"");
-    input[0] = "(1,11,5)";
-    input[1] = "(0,10,3) (1,15,2)";
-    input[2] = "(1,2,3) (2,4,6) (4,5,5) (7,3,11) (9,2,14) (13,7,15) (14,3,17)";
-    input[3] = "(1,11,5) (2,6,7) (3,13,9) (12,7,16), (14,3,25) (19,18,22) (23,13,29) (24,4,28)";
+    MAT buildings;
+    LONG arr[] = {0,3,3,1,5,3,2,4,3,3,7,3};
+    for(int i =0;i<(sizeof(arr)/sizeof(LONG)/3);i++)
+    {
+        VEC v(arr+i*3,arr+(i+1)*3);
+        buildings.push_back(v);
+    }
+    std::vector<PAIR> res = getSkyline(buildings);
+    dump(res);
+}
+void test1(void)
+{
+    MAT buildings;
+    LONG arr[] = {1,2,1,2147483646,2147483647,2147483647};
+    for(int i =0;i<(sizeof(arr)/sizeof(LONG)/3);i++)
+    {
+        VEC v(arr+i*3,arr+(i+1)*3);
+        buildings.push_back(v);
+    }
+    dumpMat(buildings);
     
-    input[4] = "(0,3,1) (0,2,2) (0,1,3)";
-    input[5] = "(0,3,3) (0,2,2) (0,1,1)";
-    
-    std::vector<Pair> out;
-    parse(input[5],out);
-    /*
-    for(auto i = out.begin();i!=out.end();i++)
-        std::cout<<i->first<<" "<<i->second<<"\n";
-    */
-    compute(out);
+    std::vector<PAIR> res = getSkyline(buildings);
+    dump(res);
 }
 int main()
 {
-    test_skyline();
+    test();
     return 0;
 }
