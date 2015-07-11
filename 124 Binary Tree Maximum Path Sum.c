268ac7225bfc4af8e4897a960d2639338c4df964
Jun <junzhengrice@gmail.com>
2015-07-11 12:36:00 -0500

    Accepted; fit in leetcode's format

--- 124 Binary Tree Maximum Path Sum.c
+++ 124 Binary Tree Maximum Path Sum.c
@@ -5,90 +5,45 @@
 124 Binary Tree Maximum Path Sum
 https://oj.leetcode.com/problems/binary-tree-maximum-path-sum/
 */
-#define MAX(a,b) (a)>(b)?(a):(b)
-struct Binary_Search_Tree_Node
+#define MAX(a,b) ((a)>(b)?(a):(b))
+struct TreeNode
 {
-    int rank;
-    struct Binary_Search_Tree_Node *pl;
-    struct Binary_Search_Tree_Node *pr;
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
 };
-typedef struct Binary_Search_Tree_Node node;
+typedef struct TreeNode node;
 
-node* assign(int rank)
+int rec(node* n, int* sum)
 {
-    node* n = (node*)malloc(sizeof(node));
-    n->rank = rank;
-    n->pl = 0;
-    n->pr = 0;
-    return n;
-}
-void initInOrder(node* n, int* arr, int start, int end)
-{
-	int mid = (start+end)>>1;
-	n->rank = arr[mid];
-	
-	if(start <= mid-1) {
-		n->pl = assign(-1);
-		initInOrder(n->pl, arr, start, mid-1);
-	}
-	if(mid+1 <= end) {
-		n->pr = assign(-1);
-		initInOrder(n->pr, arr, mid+1, end);
-	}
-}
-void inorder(node* n)
-{
-	if(n != 0){
-		inorder(n->pl);
-		printf("%d ",n->rank);
-        inorder(n->pr);
-	}
-}
-void destroyPostOrder(node* n)
-{
-	if(n != 0){
-		destroyPostOrder(n->pl);
-		destroyPostOrder(n->pr);
-        free(n);
-	}
-}
-int maxPathSum(node* n, int* sum){
     int lt,rt,singleChoice;
     if(n==0) return 0;
     
-    lt = maxPathSum(n->pl,sum);
-    rt = maxPathSum(n->pr,sum);
+    lt = rec(n->left,sum);
+    rt = rec(n->right,sum);
+    
 	/*
 	"singleChoice" is to choose one of the children 
 	plus current node value
 	*/
-    singleChoice = MAX(n->rank,MAX(lt,rt)+n->rank);
+    singleChoice = MAX(n->val,MAX(lt,rt) + n->val);
 
 	/*
 	"sum" is either "singleChoice" or use current node as
 	root plus both children
 	*/
-    *sum = MAX(*sum,MAX(singleChoice,lt+rt+n->rank));
+    *sum = MAX(*sum, MAX(singleChoice,lt + rt + n->val));
     return singleChoice;
 }
-
-// tester
+int maxPathSum(node* root)
+{
+    int sum;
+    if(root == 0) return 0;
+    sum = root->val;
+    rec(root,&sum);
+    return sum;
+}
 int main()
 {
-#define NUM 7
-    int arr[NUM];
-    int i,sum;
-    node* n = assign(-1);
-    for(i=0;i<NUM;i++){
-        arr[i] = i;
-    }
-    initInOrder(n,arr,0,NUM-1);
-	inorder(n);printf("\n");
-
-    sum = INT_MIN;
-    maxPathSum(n,&sum);
-    printf("%d\n",sum);
-    
-    destroyPostOrder(n);
     return 0;
 }
