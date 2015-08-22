928e3a7c8f5000fa612a9bc6d170df21c0f34901
Jun <junzhengrice@gmail.com>
2015-08-22 16:12:13 -0500

    Update 067 Add Binary.c

--- 067 Add Binary.c
+++ 067 Add Binary.c
@@ -1,70 +1,81 @@
-#include <stdio.h>
-#include <stdlib.h>
-/*
-67 Add Binary
-https://oj.leetcode.com/problems/add-binary/
-*/
-#define MAX(a,b) (a)>(b)?(a):(b)
-
-char* binaryPlus(char* a, char* b){
+char* addBinary(char* a, char* b)
+{
     char *at = a,*bt = b,*rt;
     size_t al = 0, bl = 0, rl;
     int i,j,c=0,s;
-    
-    while(*at != '\0'){
+
+    // move to least significant bits
+    // count length
+    while(*at != '\0')
+    {
         at++;
         al++;
     }
     at--;
-    
-    while(*bt != '\0'){
+    while(*bt != '\0')
+    {
         bt++;
         bl++;
     }
     bt--;
-    
-    rl = MAX(al,bl)+1;
+
+    // return length
+    rl = (al > bl ? al : bl) + 1;
     rt = (char*)malloc(sizeof(char)*(rl+1));
-	i=0;
-    while(i<rl){
+    i=0;
+    
+    // terminate by '\0'
+    while(i < rl)
+    {
         rt++;
         i++;
     }
     *rt-- = '\0';
-    
-    i = al-1;
-    j = bl-1;
-    while(i != -1 && j != -1){
-        s = c + *at---'0'+*bt---'0';
+
+    // sum from right
+    i = al - 1;
+    j = bl - 1;
+    while(i != -1 && j != -1)
+    {
+        s = c + *at---'0' + *bt---'0';
         c = s >> 1;
-        *rt-- = (s & 1)+48; // notice the operator precedence!
-		i--,j--;
+        *rt-- = (s & 1) + 48; // notice the operator precedence!
+        i--,j--;
     }
-    
-    while(i != -1){
-        s = c+*at---'0';
+
+    // rest
+    while(i != -1)
+    {
+        s = c + *at---'0';
         c = s >> 1;
-        *rt-- = (s & 1)+48;
-		i--;
+        *rt-- = (s & 1) + 48;
+        i--;
     }
-    
     while(j != -1){
-        s = c+*bt---'0';
+        s = c + *bt---'0';
         c = s >> 1;
         *rt-- = (s & 1)+48;;
-		j--;
+        j--;
     }
-    
+
+    // return
     if(c == 1){
         *rt = '1';
         return rt;
     } else {
-		*rt = '\0';
+        *rt = '\0';
         return rt+1;
     }
 }
 
-int main(){
-	// TODO: test
+int main(int argc, char* argv[])
+{
+    char *a, *b, *c;
+    if(argc < 3) return 0;
+    a = argv[1]; puts(a);
+    b = argv[2]; puts(b);
+
+    c = addBinary(a,b);
+    puts(c);
     return 0; 
 }
