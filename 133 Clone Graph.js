954d11e1dd3d64a9396c29366bca6ff2bdafe280
Jun <junzhengrice@gmail.com>
2015-09-12 21:01:40 -0500

    Update and rename 113 Clone Graph.js to 133 Clone Graph.js

--- 133 Clone Graph.js
+++ 133 Clone Graph.js
@@ -5,14 +5,17 @@ https://leetcode.com/problems/clone-graph/
 
 DFS, UNTESTED
 */
-var HashMap = require('hashmap');
-
 function UndirectedGraphNode(label){
     this.label = label;
     this.neighbors = [];
 }
 
-var map = new HashMap();
+/*
+Standard built-in "Map" object
+Alternaitvely in Node.js, use:
+var HashMap = require('hashmap');
+*/
+var map = new Map();
 var cloneGraph = function(n) {
     if(n === undefined) return undefined;
     if(map.has(n) === false) {
