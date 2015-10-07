45cd553cded1b852044e94d31d34eeb3812be530
Jun <junzhengrice@gmail.com>
2015-10-07 13:41:34 -0500

    Update 100 Same Tree.c

--- 100 Same Tree.c
+++ 100 Same Tree.c
@@ -1,32 +1,18 @@
 /*
-100 Same Tree
-https://oj.leetcode.com/problems/same-tree/
+Same Tree
+https://leetcode.com/problems/same-tree/
 */
-struct Binary_Search_Tree_Node{
-    int rank;
-    struct Binary_Search_Tree_Node *pl;
-    struct Binary_Search_Tree_Node *pr;
-};
-typedef struct Binary_Search_Tree_Node node;
-/*
-"flag" is init as 1
-*/
-void isSameTree(node* x, node* y, int* flag){
-    if(*flag == 1){
-        if(x == 0 && y ==0) return;
-        if((x != 0 && y ==0) || (x== 0 && y !=0)){
-            *flag = 0;
-            return;
-        }
-        if(x->rank == y->rank){
-            isSameTree(x->pl,y->pl,flag);
-            if(*flag == 1) isSameTree(x->pr,y->pr,flag);
-        } else {
-            *flag = 0;
-        }
-    }
-}
-int main(int argc, char** argv){
-    //TODO: test
-    return 0;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
+    if(p == 0 && q == 0) return 1;
+    if(p == 0 || q == 0) return 0;
+    if(p->val != q->val) return 0;
+    return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
 }
