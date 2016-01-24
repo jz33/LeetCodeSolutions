e1277690626e2f8684ccd4f9045c741bbcd3f334
Jun <junzhengrice@gmail.com>
2016-01-24 17:54:44 -0600

    Update 315 Count of Smaller Numbers After Self.py

--- 315 Count of Smaller Numbers After Self.py
+++ 315 Count of Smaller Numbers After Self.py
@@ -4,33 +4,33 @@ Count of Smaller Numbers After Self
 https://leetcode.com/problems/count-of-smaller-numbers-after-self/
 '''
 '''
-lt    m      rt
+lt      m        rt
 0 1 2 3 4 5 6 7
 '''
 def merge(arr, buf, inv, lt, mid, rt):
-    i = mid
-    j = rt
-    k = rt
-    while i >= lt and j > mid:
+    i = mid - 1
+    j = rt - 1
+    k = rt - 1
+    while i >= lt and j >= mid:
         if arr[i][1] <= arr[j][1]:
             buf[k] = arr[j]
             k -= 1
             j -= 1     
         else:
-            inv[arr[i][0]] += j - mid
+            inv[arr[i][0]] += j - mid + 1
             buf[k] = arr[i]
             k -= 1
             i -= 1
 
-    while j > mid:
+    while j >= mid:
         buf[k] = arr[j]
         k -= 1
         j -= 1
                 
-    for i in xrange(lt,rt+1):
+    for i in xrange(lt,rt):
         arr[i] = buf[i]
  
-def mergesort(nums):
+def countSmaller(nums):
     """
     :type nums: List[int]
     :rtype: List[int]
@@ -43,12 +43,11 @@ def mergesort(nums):
     stride = 2
     while stride < size * 2:
         for lt in xrange(0,size,stride):
-            mid = min(lt + (stride >> 1) - 1,size - 1);
-            rt  = min(lt + stride - 1,size - 1);
+            mid = min(lt + (stride >> 1),size);
+            rt  = min(lt + stride,       size);
             merge(arr,buf,inv,lt,mid,rt);
         stride <<= 1
     return inv
 
-
 nums = [7,6,5,4,3]
-print mergesort(nums)
+print countSmaller(nums)
