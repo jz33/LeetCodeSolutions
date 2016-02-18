eaa66844b9b04c7b991773ad88dc3719c727d958
Jun <junzhengrice@gmail.com>
2016-02-18 13:16:14 -0600

    Update 022 Generate Parentheses.py

--- 022 Generate Parentheses.py
+++ 022 Generate Parentheses.py
@@ -14,21 +14,14 @@ def generateParenthesesRec(buf,lt,rt):
     elif lt < rt:
         if lt > 0:
             generateParenthesesRec(buf + '(',lt-1,rt)
-        buf += ')'
-        generateParenthesesRec(buf,lt,rt-1)
+        generateParenthesesRec(buf + ')',lt,rt-1)
             
 def generateParentheses(n):
-    buf = "("
-    generateParenthesesRec(buf,n-1,n)
+    generateParenthesesRec('',n,n)
 
-def main():
-    global ctr
-    repeat = 6
-    for i in range(1,repeat):
-        ctr = 0
-        print("repeat: {0}".format(i))
-        generateParentheses(i)
-        print("count: {0}\n".format(ctr))
-    
-if __name__ == "__main__":
-    main()
+repeat = 6
+for i in range(1,repeat):
+    ctr = 0
+    print("repeat: {0}".format(i))
+    generateParentheses(i)
+    print("count: {0}\n".format(ctr))
