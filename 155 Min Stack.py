5a94f33552576da12484b262c2547bb2f14ba77c
Jun <junzhengrice@gmail.com>
2016-02-23 10:19:29 -0600

    Remove "minValue" field

--- 155 Min Stack.py
+++ 155 Min Stack.py
@@ -3,63 +3,40 @@ import random,sys
 155 Min Stack
 https://oj.leetcode.com/problems/min-stack/
 
-Using 2 stacks
+Use 2 stacks
 '''
-def randomList(size = 8):
-    ls = []
-    for i in range(0,size):
-        ls.append(random.randint(1,size*2));
-    return ls
-            
-class min_stack:
+class MinStack:
+    # initialize your data structure here.
     def __init__(self):
         self.contents = []
         self.minimums = []
-        self.minValue = sys.maxint
 
+    # @param x, an integer
+    # @return an integer
     def push(self, e):
         self.contents.append(e)
-        if e <= self.minValue:
+        if len(self.minimums) == 0 or e <= self.minimums[-1]:
             self.minimums.append(e)
-            self.minValue = e
 
+    # @return nothing
     def pop(self):
         if len(self.contents) == 0:
             raise Exception("len(self.contents) == 0")
         
         e = self.contents.pop()
-        if e == self.minValue:
+        if e == self.minimums[-1]:
             self.minimums.pop()
-            self.minValue = self.minimums[-1] \
-                            if len(self.minimums) > 0 \
-                            else sys.maxint
         return e
-        
+    
+    # @return an integer
     def top(self):
         if len(self.contents) == 0:
             raise Exception("len(self.contents) == 0")      
         return self.contents[-1]
-    ''' 
-    def minValue(self):
-        return self.minValue
-    '''
-    def empty(self):
-        return True if len(self.contents) == 0 else False
-    
-    def dump(self):
-        print "contents: ", len(self.contents), self.contents
-        print "minimums: ", len(self.minimums), self.minimums
-        print "minValue: ", self.minValue
-    
-def main():
-    ms = min_stack()
-    ls = randomList()
-    for i in range(0,len(ls)):
-        ms.push(ls[i])
-    ms.dump()
-    while not ms.empty():
-        print ms.pop(), ms.minValue
-    ms.dump()
-    
-if __name__ == "__main__":
-    main()
+
+    # @return an integer
+    def getMin(self):
+        if len(self.minimums) == 0:
+            raise Exception("len(self.minimums) == 0")   
+        return self.minimums[-1]
+        
