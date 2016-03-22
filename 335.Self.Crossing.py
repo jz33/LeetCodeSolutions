b543a009a4b4ccb3d43ffb804c57c3595c2265bd
Jun <junzhengrice@gmail.com>
2016-03-22 14:04:22 -0500

    Update 335.Self.Crossing.py

--- 335.Self.Crossing.py
+++ 335.Self.Crossing.py
@@ -1,27 +1,18 @@
 '''
-Self Crossing
+335 Self Crossing
 https://leetcode.com/problems/self-crossing/
 '''
 def isSelfCrossing(dirs):
-    if len(dirs) == 0: return False
-    s = set()
-    c = (0,0)
-    s.add(c)
-    for i,d in enumerate(dirs):
-        r = i % 4
-        for _ in xrange(d):           
-            if r == 0:
-                c = (c[0]-1,c[1])
-            elif r == 1:
-                c = (c[0],c[1]-1)
-            elif r == 2:
-                c = (c[0]+1,c[1])
-            else:
-                c = (c[0],c[1]+1)
-            if c in s:
-                return True
-            s.add(c)
+    if len(dirs) < 3: return False
+    for i in xrange(3,len(dirs)):
+        # if intersected with i-3
+        if dirs[i] - dirs[i-2] >= 0 and dirs[i-3] - dirs[i-1] >= 0:
+            return True
+        # if intersected with i-4
+        if i >= 4 and dirs[i-3] - dirs[i-1] == 0 and dirs[i-2] - dirs[i-4] <= dirs[i]:
+            return True
+        # if intersected with i-5
+        if i >= 5 and dirs[i-3] - dirs[i-1] >= 0 and dirs[i-3] - dirs[i-1] <= dirs[i-5] \
+        and dirs[i-2] - dirs[i-4] >= 0 and dirs[i-2] - dirs[i-4] <= dirs[i]:
+            return True       
     return False
-
-dirs = [1, 2, 3, 4]
-print isSelfCrossing(dirs)
