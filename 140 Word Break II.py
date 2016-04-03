26406a1a74cac1d508653c46504c698c77053510
Jun <junzhengrice@gmail.com>
2016-04-03 12:05:53 -0500

    Update 140 Word Break II.py

--- 140 Word Break II.py
+++ 140 Word Break II.py
@@ -1,102 +1,63 @@
-from copy import deepcopy
 '''
-140 Word Break II
-https://oj.leetcode.com/problems/word-break-ii/
+Word Break II
+https://leetcode.com/problems/word-break-ii/
 '''
-'''
-A recursive approach with prefix check
-Passed
-'''
-def wordBreak(tag, book):
-    ret = []
-    if tag == '': return ret
-    
+def wordBreakRec(s, src):
+    ret = [s] if s in src else []
     # At least 1 suffix is in dict
-    i = len(tag) - 1
-    while i > -1:
-        if tag[i:] in book: break
-        i -= 1
-            
-    # Not found        
-    if i == -1:return ret
+    for i in xrange(len(s)-1,-1,-1):
+        if s[i:] in src: break
+    else:     
+        return ret # Not found
     
     # Loop each prefix
-    i = 0
-    while i < len(tag)-1:
-        prefix = tag[:i+1]
-        if prefix in book:
-            suffix = tag[i+1:]
-            child = wordBreak(suffix,book)
-            for c in child:
-                ret.append(prefix + ' ' + c)
-        i += 1
-        
-    if tag in book: ret.append(tag)
+    for i in xrange(1,len(s)):
+        prefix = s[:i]
+        if prefix in src:
+            tails = wordBreakRec(s[i:],src)
+            for t in tails:
+                ret.append(prefix + ' ' + t)
     return ret
 
-'''
-Debug
-'''
-def dump(mat):
-    for r in mat:
-        for c in r:
-            print c,
-        print
-    print
- 
-'''
-A DP approach, O(n^2)
-But with intense memory usage, this will not pass
-'''
-def wordBreakDP(tag,book):
-    ret = []
-    if tag == '': return ret
+def wordBreakFromTail(s,src):
+    '''
+    Best DP approach from tail
+    'ref' contains all word breaks in s[k:]
+    '''
+    ref = {len(s) : ['']}
+    for i in xrange(len(s)-1,-1,-1):
+        values = []
+        for j in xrange(i+1, len(s)+1):
+            sub = s[i:j]
+            if sub in src:
+                for suffix in ref.get(j,[]):
+                    if suffix == '':
+                        values.append(sub)
+                    else:
+                        values.append(sub + ' ' + suffix)
+        ref[i] = values
+    return ref.get(0,[])
 
-    # $buf: [[end Index, word0,word1...]]
-    buf = []
-    
-    # Found first prefix
-    for i in xrange(0,len(tag)):
-        prefix = tag[:i+1]
-        if prefix in book:
-            buf.append([i,prefix])
-            break
-    
-    # Not found        
-    if len(buf) == 0:return ret
+def wordBreak2(s,src):
+    '''
+    A DP approach from head
+    'ref' contains all word breaks in s[:k]
+    '''
+    ref = {0 : ['']}
+    for i in xrange(len(s)):
+        for j in xrange(i+1, len(s)+1):
+            values = ref.get(j,[])
+            sub = s[i:j]
+            if sub in src:
+                for prefix in ref.get(i,[]):
+                    if prefix == '':
+                        values.append(sub)
+                    else:
+                        values.append(prefix + ' ' + sub)
+            ref[j] = values
+    return ref.get(len(s),[])
 
-    start = buf[0][0] + 1
-    for i in xrange(start,len(tag)):
-        # for each previous word break
-        buf_len = len(buf)
-        for j in xrange(0,buf_len):
-            suffix = tag[buf[j][0]+1:i+1]
-            if suffix in book:
-                # add new entry
-                entry = deepcopy(buf[j])
-                entry.append(suffix)
-                entry[0] = i
-                buf.append(entry)
-                
-        # whole word in dict
-        prefix = tag[:i+1]
-        if prefix in book:
-            buf.append([i,prefix])
-        
-    for b in buf:
-        if b[0] == len(tag)-1:
-            ret.append(' '.join(b[1:]))
-    
-    return ret
-    
-def main():
-    tag = 'aaaaaaa'
-    book = set(['aaaa','aaa'])
-    
-    rec = wordBreak(tag,book)
-    print rec
-    dps = wordBreakDP(tag,book)
-    print dps
-      
-if __name__ == "__main__":
-    main()
+s = 'catsanddog'
+ref = set(["cat", "cats", "and", "sand", "dog"])
+r = wordBreakRec(s,ref)
+for row in r: print row
