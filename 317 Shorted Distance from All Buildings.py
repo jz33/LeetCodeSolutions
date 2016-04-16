4a0e4a68a2199e631c6e28d6013707d95f940a48
Jun <junzhengrice@gmail.com>
2016-04-16 09:45:38 -0500

    Add comments

--- 317 Shorted Distance from All Buildings.py
+++ 317 Shorted Distance from All Buildings.py
@@ -1,13 +1,9 @@
-from collections import deque
-
-def dump(mat):
-    for row in mat:
-        print row
-
 '''
 Shorted Distance from All Buildings
 https://leetcode.com/problems/shortest-distance-from-all-buildings/
 '''
+from Queue import Queue
+
 class Solution(object):
     def shortestDistance(self, grid):
         """
@@ -18,40 +14,43 @@ class Solution(object):
         if R == 0: return 0
         C = len(grid[0])
 
+        #BFS
         INF = (R+C)*R*C
         directions = [0,1,0,-1,0]
         minDist = 0
+        
+        # recording all distances from each BFS 
         total_grid = [[0 for _ in xrange(C)] for _ in xrange(R)]
         
-        '''
-        0. Mark visited node; 1. Detect unreachable node
-        '''
+    
+        # 0. Mark visited node; 1. Detect unreachable node
         walk = 0
         for i in xrange(R):
             for j in xrange(C):
                 if grid[i][j] == 1:
+                    # start BFS
+                    # current distances reachable by current start point
                     dist_grid = [[0 for _ in xrange(C)] for _ in xrange(R)]
-                    dq = deque()
-                    dq.append((i,j))
-                    minDist = INF
-                    while len(dq) > 0:
-                        (a,b) = dq.popleft()
+                    dq = Queue()
+                    dq.put((i,j))
+                    minDist = INF # for each BFS, minDist is reset
+                    while not dq.empty():
+                        a,b = dq.get()
                         dab = dist_grid[a][b]
                         for k in xrange(4):
                             x = a + directions[k]
                             y = b + directions[k+1]
                             if x > -1 and x < R and y > -1 and y < C and grid[x][y] == walk:
-                                grid[x][y] -= 1
+                                grid[x][y] -= 1 # mark as visited
                                 d = dab + 1
                                 dist_grid[x][y] = d
                                 total_grid[x][y] += d
                                 minDist = min(minDist,total_grid[x][y])
-                                dq.append((x,y))
+                                dq.put((x,y))
                     if minDist == INF: return -1
                     walk -= 1
-        dump(grid)                
-        dump(total_grid)
         return minDist
+        
         
 sol = Solution()
 grid = [
