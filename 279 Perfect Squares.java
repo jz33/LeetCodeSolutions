615fc7e88d5b72539a625b888ee3d56e274ec33b
Jun <junzhengrice@gmail.com>
2016-04-25 10:36:52 -0500

    Update 279 Perfect Squares.java

--- 279 Perfect Squares.java
+++ 279 Perfect Squares.java
@@ -3,25 +3,16 @@ import java.util.Arrays;
 /**
  * Perfect Squares
  * https://leetcode.com/problems/perfect-squares/
- *
+ * Similar to "Coin Changes"
  */
-public class _279 {
-
-    public int numSquares(int n){
-        if(n < 1) return 0;
-
-        // Notice the different: 
-        //java.util.List<Integer> buf = new java.util.Vector<Integer>(Arrays.asList(new Integer[n+1]));
-        java.util.List<Integer> buf = java.util.Arrays.asList(new Integer[n+1]);
-        
-        for(int i = 0;i <= n;i++){
-            
-            // $i is upper bound of buf[i]
-            buf.set(i, i);
-            for(int j = 2; j <= (int)Math.sqrt(i);j++){
-                buf.set(i, Math.min(buf.get(i), 1 + buf.get(i - j * j)));
-            }
+public int numSquares(int n) {
+    int[] buf = new int[n+1];
+    for(int i = 1;i <= n;i++){
+        buf[i]  = i;
+        int bound = (int)Math.sqrt(i);
+        for(int j = 2; j <= bound;j++){
+            buf[i] = Math.min(buf[i], 1 + buf[i-j*j]);
         }
-        return buf.get(n);
     }
+    return buf[n];
 }
