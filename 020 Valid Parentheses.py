f9cb13e5e83ebfea6006a4a359ceef0f6c2ad4b8
Jun <junzhengrice@gmail.com>
2017-09-13 20:45:02 -0700

    Update 020 Valid Parentheses.py

--- 020 Valid Parentheses.py
+++ 020 Valid Parentheses.py
@@ -2,38 +2,34 @@
 20 Valid Parentheses
 https://oj.leetcode.com/problems/valid-parentheses/
 '''
-def validParentheses(arg):
-    if len(arg) < 2: return False
-    stack = []
-    for i in range(0,len(arg)):
-        curr = arg[i]
-        if curr == '(' or curr == '[' or curr == '{':
-            stack.append(curr)
+def isValid(s):
+    """
+    :type s: str
+    :rtype: bool
+    """
+    buf = [-1]
+    for c in s:
+        if c == '(': 
+            buf.append(0)
+        elif c == '{':
+            buf.append(1)
+        elif c == '[':
+            buf.append(2)
+        elif c == ')':
+            if buf[-1] != 0: 
+                return False
+            else:
+                buf.pop()
+        elif c == '}':
+            if buf[-1] != 1:
+                return False
+            else:
+                buf.pop()
+        elif c == ']':
+            if buf[-1] != 2:
+                return False
+            else:
+                buf.pop()
         else:
-            if len(stack) == 0 : return False
-            prev = stack[len(stack)-1]
-            if curr == ')':
-                if prev == '(': stack.pop()
-                else: return False
-            elif curr == ']':
-                if prev == '[': stack.pop()
-                else: return False
-            elif curr == '}':
-                if prev == '{': stack.pop()
-                else: return False
-            else: return False
-            
-    return True if len(stack) == 0 else False
-
-def main():
-    s = r"{[()]}"
-    print validParentheses(s)
-    s = r"{[(]]}"
-    print validParentheses(s)
-    s = r"{[("
-    print validParentheses(s)
-    s = r"a{[()]}"
-    print validParentheses(s)
-    
-if __name__ == "__main__":
-    main()
+            return False
+    return len(buf) == 1
