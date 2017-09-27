68246b35a704a1b21e4b3ba26515ce86dd6d6b4c
Jun <junzhengrice@gmail.com>
2017-09-27 13:20:28 -0700

    Draw pics

--- 335 Self Crossing.py
+++ 335 Self Crossing.py
@@ -5,13 +5,34 @@ https://leetcode.com/problems/self-crossing/
 def isSelfCrossing(dirs):
     if len(dirs) < 3: return False
     for i in xrange(3,len(dirs)):
-        # if intersected with i-3
+        '''
+        if intersected with i-3
+        ******
+             *
+         *   *
+         *****    
+        '''
         if dirs[i] - dirs[i-2] >= 0 and dirs[i-3] - dirs[i-1] >= 0:
             return True
-        # if intersected with i-4
+        '''
+        if intersected with i-4       
+        **  ****
+        *      *
+        *      *
+        ********  
+        '''
         if i >= 4 and dirs[i-3] - dirs[i-1] == 0 and dirs[i-2] - dirs[i-4] <= dirs[i]:
             return True
-        # if intersected with i-5
+        '''
+        if intersected with i-5
+        *****
+        *   *
+        *
+        *  *****
+        *      *
+        *      *
+        ******** 
+        '''
         if i >= 5 and dirs[i-3] - dirs[i-1] >= 0 and dirs[i-3] - dirs[i-1] <= dirs[i-5] \
         and dirs[i-2] - dirs[i-4] >= 0 and dirs[i-2] - dirs[i-4] <= dirs[i]:
             return True       
