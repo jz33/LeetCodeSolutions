0db1041ee304ebd6e35bcbf174774b159a253322
Jun <junzhengrice@gmail.com>
2017-10-07 13:01:19 -0700

    Create 048 Rotate Image.cs

---
+++ 048 Rotate Image.cs
@@ -0,0 +1,16 @@
+public void Rotate(int[,] mat) {
+    int N = mat.GetLength(0); // Not: mat.Length
+    int level = (N >> 1);
+    //Console.WriteLine(N+", "+level);     
+    int t = 0;
+    for(int i = 0;i<level;i++){
+        for(int j = i;j<N-i-1;j++){
+            //Console.WriteLine(i+", "+j);
+            t = mat[i,j];
+            mat[i,j] = mat[N-1-j,i];
+            mat[N-1-j,i] = mat[N-1-i,N-1-j];
+            mat[N-1-i,N-1-j] = mat[j,N-1-i];
+            mat[j,N-1-i] = t;                
+        }
+    }
+}
