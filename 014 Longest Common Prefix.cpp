a79c5dc1ee6eea1291b67638512f5474dfacc49a
Jun <junzhengrice@gmail.com>
2017-10-16 19:36:31 -0700

    Update 014 Longest Common Prefix.cpp

--- 014 Longest Common Prefix.cpp
+++ 014 Longest Common Prefix.cpp
@@ -2,20 +2,19 @@
 #include <string>
 #include <vector>
 /*
-14 Longest Common Prefix
-https://oj.leetcode.com/problems/longest-common-prefix/
+Longest Common Prefix
+https://leetcode.com/problems/longest-common-prefix
 */
 std::string longestCommonPrefix(std::vector<std::string> &strs){
     if(strs.size()<1) return "";
     if(strs.size()<2) return strs[0];
-    std::string p = strs[0];
-    for(std::vector<std::string>::iterator i = strs.begin()+1;i!=strs.end();i++){
-        std::string& t = *i;
-		std::size_t j = 0;
+    auto p = strs[0]; // result prefix
+    for(auto i = strs.begin()+1;i!=strs.end();i++){
+        auto &t = *i;
+        std::size_t j = 0;
         for(;j<p.size() && j<t.size();j++)
-            if(p[j] != t[j])
-                break;
-        if(j<p.size()) p = p.substr(0,j);
+            if(p[j] != t[j]) break;
+        p = p.substr(0,j); // j <= p.size()
     }
     return p;
 }
