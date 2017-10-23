d062acbbdfb64cad16aef6f0c278d63283687c33
Jun <junzhengrice@gmail.com>
2017-10-22 18:20:19 -0700

    Update 032 Longest Valid Parentheses.py

--- 032 Longest Valid Parentheses.py
+++ 032 Longest Valid Parentheses.py
@@ -1,53 +1,54 @@
 import random
 '''
 32 Longest Valid Parentheses
-https://oj.leetcode.com/problems/longest-valid-parentheses/
-
-O(n)
+https://leetcode.com/problems/longest-valid-parentheses/description/
+O(n), 2 paths
 '''
 # generate a random Parentheses string with size n
 def randParentheses(n):
     return ''.join(random.choice('('+')') for _ in range(n))
  
-def longestValidParentheses(sent):
-    if len(sent) < 2:
-        return 0
+def longestValidParentheses(src):
+    if len(src) < 2: return 0
+    res = 0
     '''
     Notations for 'buf' :
     '0' : currently paired
     >0  : num of '(' - num of ')'
+    <0  : invalid (abort)
+    '''
+    buf = 0        
+    s = 0 # start index
+    for i in xrange(len(src)):
+        if src[i] == '(':
+            buf += 1
+        else:
+            buf -= 1
+            if buf == 0:
+                res = max(res, i - s + 1)
+            elif buf < 0: # reset
+                buf = 0
+                s = i + 1
+    '''
+    Think the exmaple of (()( or )()) to understand why it needs 2 paths
     '''
     buf = 0
-    max_len = 0
-    start = 0
-    for i in xrange(0, len(sent)):
-        if sent[i] == '(':
+    s = len(src) - 1       
+    for i in xrange(len(src)-1, -1, -1):
+        if src[i] == ')':
             buf += 1
         else:
             buf -= 1
             if buf == 0:
-                max_len = max(max_len, i - start + 1)
+                res = max(res, s - i + 1)
             elif buf < 0:
-                start = i + 1
                 buf = 0
-
-    end = len(sent) - 1
-    buf = 0
-    for i in xrange(len(sent)-1, -1,-1):
-        if sent[i] == ')':
-            buf += 1
-        else:
-            buf -= 1
-            if buf == 0:
-                max_len = max(max_len, end - i + 1)
-            elif buf < 0:
-                end = i - 1
-                buf = 0
-    return max_len
+                s = i - 1                 
+    return res
        
 def main():
     repeat = 5
-    for i in range(0,repeat):
+    for i in xrange(repeat):
         sent = randParentheses(10)
         print sent, longestValidParentheses(sent)
      
