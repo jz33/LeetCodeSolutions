cb4327fc7b889a509b32437fe6d9fb51681ebd5d
Jun <junzhengrice@gmail.com>
2017-10-31 12:56:19 -0700

    Update 028 Implement strStr().cs

--- 028 Implement strStr().cs
+++ 028 Implement strStr().cs
@@ -1,12 +1,10 @@
 int StrStr(string haystack, string needle) {
-    int h = 0;
-    int n = 0;
-    for(; h <= haystack.Length - needle.Length;h++){
+    for(int h = 0; h <= haystack.Length - needle.Length;h++){
+        int n = 0;
         for(; n < needle.Length;n++){
             if (haystack[h+n] != needle[n]) break;
         }
         if (n == needle.Length) return h;
-        n = 0;
     }
     return -1;
 }
