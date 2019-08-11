af266e1ba73233636373f2b8e802ad15825d1175
Jun <junzhengrice@gmail.com>
2019-08-11 16:01:12 -0700

    Rewrite to a simpler method

--- 003 Longest Substring Without Repeating Characters.py
+++ 003 Longest Substring Without Repeating Characters.py
@@ -2,25 +2,21 @@
 003 Longest Substring Without Repeating Characters.py
 https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
 '''
-class Solution(object):
-    def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        if len(s) < 2: return len(s)
-        lt = 0
-        ml = 1 # max length
-        ref = {s[0]:0} # char : index
-        for i in xrange(1,len(s)):
-            c = s[i]
-            prev = ref.get(c)
-            if prev is None:
-                ml = max(ml,i-lt+1)
-            else:
-                for k in xrange(lt,prev+1):
-                    del ref[s[k]]
-                lt = prev + 1
-            ref[c] = i
-        ml = max(ml,len(s)-lt)
-        return ml
+def lengthOfLongestSubstring(s: str) -> int:
+    '''
+    Using sliding window method. No need to delete from cache
+    '''
+    if len(s) < 2:
+        return len(s)
+
+    buf = {} # char : index
+    left = 0 # the left margin
+    res = 0
+    for i, e in enumerate(s):
+        if e in buf:
+            # Simply forward the left margin
+            # Notice buf[e] can be less than left
+            left = max(left, buf[e] + 1)
+        res = max(res, i - left + 1)
+        buf[e] = i
+    return res
