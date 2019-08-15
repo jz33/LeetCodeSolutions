e3705b5bdd9f46d463e308dbd7dc53f2978d94b6
Jun <junzhengrice@gmail.com>
2019-08-14 22:49:28 -0700

    Add DP method

--- 010 Regular Expression Matching.py
+++ 010 Regular Expression Matching.py
@@ -1,5 +1,35 @@
+def isMatch(txt: str, pat: str) -> bool:
+    '''
+    New DP iterative method
+    '''
+
+    # dp[i][j] means whether pat[i:] matches txt[j:]
+    # Notice i and j can be length of the string, which
+    # represents the empty txt or pat       
+    dp = [[False] * (len(txt) + 1) for _ in range(len(pat) + 1)]
+
+    for i in range(len(pat), -1, -1):
+        for j in range(len(txt), -1, -1):
+            matchCurrrent = i < len(pat) and j < len(txt) and (pat[i] == txt[j] or pat[i] == '.')
+
+            if i == len(pat) and j == len(txt):
+                # Of course, empty txt matches empty pat
+                dp[i][j] = True
+
+            elif i + 1 < len(pat) and pat[i+1] == '*':
+                # If next pat is '*', skip it or assume it matches more than 1 txt char
+                dp[i][j] = dp[i+2][j] or matchCurrrent and dp[i][j+1]
+
+            else:
+                # If next pat is not '*' or pat is empty, matches current and move both 1 step
+                dp[i][j] = matchCurrrent and dp[i+1][j+1]
+
+    return dp[0][0]
+
+
 def isMatch(st,si,pt,pi):
     '''
+    Old Recursive methd
     st : string, si: string index
     pt : pattern, pi: pattern index
     Notice Python substring is deep copy
