add6e4c16cb0ec229882db87c5fbd46cce7b1980
Jun <junzhengrice@gmail.com>
2019-08-28 10:53:47 -0700

    Add Comments about the difference of Combination I, II, III, IV

--- 039 Combination Sum.cpp
+++ 039 Combination Sum.cpp
@@ -6,6 +6,29 @@ Combination Sum
 https://leetcode.com/problems/combination-sum/
 
 Classic backtracking
+
+There are 3 type of questions, for example, give [1,2,3], and target = 4
+If only allowing use each number once, result is:
+    (1,3)
+And CL will be:
+    recursive(input,sofar,i+1,tag - input[i]);
+If allowing use each number infinite times, result is:
+    (1, 1, 1, 1)
+    (1, 1, 2)
+    (1, 3)
+    (2, 2)
+And CL will be:
+    recursive(input,sofar,i,tag - input[i]);
+If allowing use each number infinite times, and different result order is count as differen result:
+    (1, 1, 1, 1)
+    (1, 1, 2)
+    (1, 2, 1)
+    (1, 3)
+    (2, 1, 1)
+    (2, 2)
+    (3, 1)
+And CL will be:
+    recursive(input,sofar,0,tag - input[i]);
 */
 std::vector<std::vector<int> > pool;
 
@@ -24,7 +47,7 @@ void recursive(std::vector<int>& input, std::vector<int>& sofar, int start, int 
             if(input[i] <= tag)
             {
                 sofar.push_back(input[i]);
-                recursive(input,sofar,i,tag - input[i]);
+                recursive(input,sofar,i,tag - input[i]); // Critial Line (CL)
                 sofar.pop_back();
             }
         }
