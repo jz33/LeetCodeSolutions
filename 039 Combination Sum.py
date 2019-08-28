e5098dcd5f45a324a830d8331a49d1430a81cfe3
Jun <junzhengrice@gmail.com>
2019-08-27 23:25:23 -0700

    Add comments to DP method

--- 039 Combination Sum.py
+++ 039 Combination Sum.py
@@ -9,10 +9,13 @@ def combinationSum(candidates, target):
     :type target: int
     :rtype: List[List[int]]
     """
-    buf = [[] for _ in xrange(target+1)] # 3-D mat
+    # buf is a 3-D matrix. buf[i] contains all the combination sums to i
+    buf = [[] for _ in xrange(target+1)]
     buf[0].append([])
+    
     for c in candidates:
         for i in xrange(c,target+1):
             for ls in buf[i-c]:
                 buf[i].append(ls + [c])
+
     return buf[target]
