6c94a1e89b7e83e95fe581a23b1ac6e0a4c686c7
Jun <junzhengrice@gmail.com>
2019-09-01 16:13:08 -0700

    Add comments

--- 380 Insert Delete GetRandom O(1).py
+++ 380 Insert Delete GetRandom O(1).py
@@ -4,81 +4,70 @@ https://leetcode.com/problems/insert-delete-getrandom-o1/
 '''
 from random import randint
 
-DEBUG = True
+class RandomizedSet:
 
-class RandomizedSet(object):
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        self.dic = {}
-        self.arr = []
-        self.ti = -1 # tail index, inclusive
-        
-    def insert(self, val):
+        self.dic = {} # {value : index}
+        self.arr = [] # [value]
+        self.ti = -1 # tail index, index to last non-none element, == total - 1
+
+    def insert(self, val: int) -> bool:
         """
         Inserts a value to the set. Returns true if the set did not already contain the specified element.
-        :type val: int
-        :rtype: bool
         """
-        if val not in self.dic:
+        existed = val in self.dic
+        if not existed:
+            # Put into arr
             self.ti += 1
             if self.ti >= len(self.arr):
                 self.arr.append(val)
             else:
                 self.arr[self.ti] = val
+
+            # Put into dic
             self.dic[val] = self.ti
-
-            if DEBUG: print "inserted", self.arr, self.ti
-
-            return True
-        else:
-            return False
         
-    def remove(self, val):
+        return not existed 
+        
+
+    def remove(self, val: int) -> bool:
         """
         Removes a value from the set. Returns true if the set contained the specified element.
-        :type val: int
-        :rtype: bool
         """
-        if val not in self.dic:
-            return False
-        else:
+        existed = val in self.dic
+        if existed:
+            
+            # Get last index of val
             pos = self.dic[val]
-            self.tailess(pos,val)
+            
+            if pos != self.ti:
+                # Swap last value to pos
+                lastVal = self.arr[self.ti]
+                self.arr[pos] = lastVal
+                # Update dic on last value
+                self.dic[lastVal] = pos
+            
+            # pop val from arr
+            self.arr[self.ti] = None
+            self.ti -= 1
+        
+            # pop val from dic
+            del self.dic[val]
 
-            if DEBUG: print "removed", self.arr, self.ti
-            return True
+        return existed  
 
-    def getRandom(self):
+    def getRandom(self) -> int:
         """
         Get a random element from the set.
-        :rtype: int
         """
-        pos = randint(0,self.ti)
-        val = self.arr[pos]
-        # self.tailess(pos,val) # add if remove elements is required
-
-        if DEBUG: print "getRandom", self.arr, self.ti
-        return val
-        
-    def tailess(self,pos,val):
-        if pos != self.ti:
-            last = self.arr[self.ti]
-            self.arr[pos] = last
-            self.dic[last] = pos
-
-        self.arr[self.ti] = None
-        self.ti -= 1
-        del self.dic[val]
+        return self.arr[randint(0,self.ti)]        
 
 
-obj = RandomizedSet()
-
-print obj.insert(1)
-print obj.remove(2)
-print obj.insert(2)
-print obj.getRandom()
-print obj.remove(1)
-print obj.insert(2)
-print obj.getRandom()
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
