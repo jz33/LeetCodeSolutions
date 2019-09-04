843c12ed78f1d3161ecb1b6632b12aa150880dcd
Jun <junzhengrice@gmail.com>
2019-09-04 16:27:47 -0700

    Update & rewrite top k frequent elements

--- 347 Top K Frequent Elements.py
+++ 347 Top K Frequent Elements.py
@@ -2,20 +2,43 @@
 Top K Frequent Elements 
 https://leetcode.com/problems/top-k-frequent-elements/
 '''
-def topKFrequent(nums, count):
-    histo = {}
-    maxFreq = 0
-    for n in nums:
-        freq = histo.get(n,0) + 1
-        maxFreq = max(maxFreq,freq)
-        histo[n] = freq
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        '''
+        A strict O(n) method without sort
+        '''
+        # 1. Build histogram and find maximun frequency
+        histogram = {}
+        maxFreq = 0
+        for n in nums:
+            freq = histogram.get(n,0) + 1
+            maxFreq = max(maxFreq,freq)
+            histogram[n] = freq
         
-    rev = {}
-    for k,v in histo.iteritems():
-        rev[v] = rev.get(v,[]) + [k]
-    
-    res = []
-    while len(res) < count:
-        res.extend(rev.get(maxFreq,[]))
-        maxFreq -= 1
-    return res[:count]
+        # 2. Build reversed histogram
+        revHisto = {}
+        for key, val in histogram.items():
+            revHisto[val] = revHisto.get(val,[]) + [key]
+        
+        # 3. Count from maximun frequency
+        res = []
+        while len(res) < k:
+            res.extend(revHisto.get(maxFreq,[]))
+            maxFreq -= 1
+        return res[:k]
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        '''
+        An O(nlog(n)) method with sort
+        '''
+        # 1. Build histogram
+        histogram = {}
+        for n in nums:
+            histogram[n] = histogram.get(n,0) + 1
+        
+        # 2. Sort
+        ls = sorted(histogram.items(), key = lambda x : -x[1])
+        
+        # 3. Slice
+        return [p[0] for p in ls[:k]]
