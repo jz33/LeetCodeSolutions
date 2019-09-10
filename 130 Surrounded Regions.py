f2429efb61780b894c8b8ba33817d0be4d797181
Jun <junzhengrice@gmail.com>
2019-09-09 23:10:24 -0700

    Rewrite, add detailed comments

--- 130 Surrounded Regions.py
+++ 130 Surrounded Regions.py
@@ -2,79 +2,51 @@
 Surrounded Regions
 https://leetcode.com/problems/surrounded-regions/
 '''
-R = 0
-C = 0
-def mark(mat,pt):
-    dq = []
-    dq.append(pt)
-    while len(dq) > 0:
-        (x,y) = dq.pop()
-        mat[x][y] = 'V'
-        if x + 1 <  R and mat[x+1][y] == 'O':
-            dq.append((x+1,y))
-        if x - 1 > -1 and mat[x-1][y] == 'O':
-            dq.append((x-1,y))
-        if y + 1 < C  and mat[x][y+1] == 'O':
-            dq.append((x,y+1))
-        if y - 1 > -1 and mat[x][y-1] == 'O':
-            dq.append((x,y-1))
+from typing import Tuple
 
-def solve(mat):   
-    global R,C
-    R = len(mat)
-    if R == 0: return
-    C = len(mat[0])
-    
-    # check perimeter only
-    for i in xrange(0,R):
-        if mat[i][0] == 'O':
-            mark(mat,(i,0))
-        if mat[i][C-1] == 'O':
-            mark(mat,(i,C-1))            
-    for i in xrange(1,C-1):
-        if mat[0][i] == 'O':
-            mark(mat,(0,i))
-        if mat[R-1][i] == 'O':
-            mark(mat,(R-1,i))
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        rowCount = len(board)
+        if rowCount == 0:
+            return
+        
+        colCount = len(board[0])
+        
+        def mark(cell: Tuple[int, int]):
+            stack = [cell]
+            while stack:
+                x, y = stack.pop()
+                board[x][y] = 'V'
+                if x + 1 <  rowCount and board[x+1][y] == 'O':
+                    stack.append((x+1,y))
+                if x - 1 > -1 and board[x-1][y] == 'O':
+                    stack.append((x-1,y))
+                if y + 1 < colCount and board[x][y+1] == 'O':
+                    stack.append((x,y+1))
+                if y - 1 > -1 and board[x][y-1] == 'O':
+                    stack.append((x,y-1))
+        
+        # Go through border, if is '0', mark all adjacent cells as 'V'
+        for i in range(rowCount):
+            if board[i][0] == 'O':
+                mark((i,0))
+            if board[i][colCount-1] == 'O':
+                mark((i,colCount-1))
+
+        for i in range(1,colCount-1):
+            if board[0][i] == 'O':
+                mark((0,i))
+            if board[rowCount-1][i] == 'O':
+                mark((rowCount-1,i))
             
-    # after work
-    for i in xrange(0,R):
-        for j in xrange(0,C):
-            if mat[i][j] == 'O':
-                mat[i][j] = 'X'
-            elif mat[i][j] == 'V':
-                mat[i][j] = 'O'
-    
-def preWork(board):
-    mat = []
-    for row in board:
-        mat.append(list(row))
-    solve(mat)
-    return mat
-    
-board = [
-    "XOOOOOOOOOOOOOOOOOOO",
-    "OXOOOOXOOOOOOOOOOOXX",
-    "OOOOOOOOXOOOOOOOOOOX",
-    "OOXOOOOOOOOOOOOOOOXO",
-    "OOOOOXOOOOXOOOOOXOOX",
-    "XOOOXOOOOOXOXOXOXOXO",
-    "OOOOXOOXOOOOOXOOXOOO",
-    "XOOOXXXOXOOOOXXOXOOO",
-    "OOOOOXXXXOOOOXOOXOOO",
-    "XOOOOXOOOOOOXXOOXOOX",
-    "OOOOOOOOOOXOOXOOOXOX",
-    "OOOOXOXOOXXOOOOOXOOO",
-    "XXOOOOOXOOOOOOOOOOOO",
-    "OXOXOOOXOXOOOXOXOXOO",
-    "OOXOOOOOOOXOOOOOXOXO",
-    "XXOOOOOOOOXOXXOOOXOO",
-    "OOXOOOOOOOXOOXOXOXOO",
-    "OOOXOOOOOXXXOOXOOOXO",
-    "OOOOOOOOOOOOOOOOOOOO",
-    "XOOOOXOOOXXOOXOXOXOO"
-    ]
-    
-mat = preWork(board)
-for row in mat:
-    print row
+        # For all cells with 'O', mark it to 'X'
+        # For all cells with 'V', mark it back to 'O'
+        for i in range(rowCount):
+            for j in range(colCount):
+                if board[i][j] == 'O':
+                    board[i][j] = 'X'
+                elif board[i][j] == 'V':
+                    board[i][j] = 'O'
