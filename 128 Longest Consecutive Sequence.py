3ad80ea2ee1a4a5d8d6ba2957eaaa7d97c60e462
Jun <junzhengrice@gmail.com>
2019-09-11 14:54:19 -0700

    Hashmap, Array

--- 128 Longest Consecutive Sequence.py
+++ 128 Longest Consecutive Sequence.py
@@ -2,7 +2,17 @@
 128 Longest Consecutive Sequence.py
 https://oj.leetcode.com/problems/longest-consecutive-sequence/
 
-Use a hash map
+Given an unsorted array of integers, find the length of the longest consecutive elements sequence.
+
+Your algorithm should run in O(n) complexity.
+
+Example:
+
+Input: [100, 4, 200, 1, 3, 2]
+Output: 4
+Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+
+Hashmap, Array
 '''
 def longestConsecutive(ls):
     ref = {}
