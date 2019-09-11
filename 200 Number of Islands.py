92a24d2d97a1a99a5a46a10aee438d20b3c5d811
Jun <junzhengrice@gmail.com>
2019-09-11 14:51:28 -0700

    BFS, Matrix

--- 200 Number of Islands.py
+++ 200 Number of Islands.py
@@ -6,7 +6,24 @@ Given a 2d grid map of '1's (land) and '0's (water), count the number of islands
 An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically.
 You may assume all four edges of the grid are all surrounded by water.
 
-DFS, same method to 130 Surrounded Regions 
+Example 1:
+
+Input:
+11110
+11010
+11000
+00000
+
+Output: 1
+Example 2:
+
+Input:
+11000
+11000
+00100
+00011
+
+Output: 3
 '''
 from typing import Tuple
 
