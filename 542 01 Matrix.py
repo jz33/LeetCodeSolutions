f98ba75befde9905058520edf064a2d3109b6537
Jun <junzhengrice@gmail.com>
2019-09-11 15:05:14 -0700

    BFS, Matrix

--- 542 01 Matrix.py
+++ 542 01 Matrix.py
@@ -1,7 +1,33 @@
 '''
+542 01 Matrix
 https://leetcode.com/problems/01-matrix
-DFS will timeout
-Use 2 pass DP
+Given a matrix consists of 0 and 1, find the distance of the nearest 0 for each cell.
+
+The distance between two adjacent cells is 1.
+
+Example 1:
+
+Input:
+[[0,0,0],
+ [0,1,0],
+ [0,0,0]]
+
+Output:
+[[0,0,0],
+ [0,1,0],
+ [0,0,0]]
+ 
+Example 2:
+
+Input:
+[[0,0,0],
+ [0,1,0],
+ [1,1,1]]
+
+Output:
+[[0,0,0],
+ [0,1,0],
+ [1,2,1]]
 '''
 def updateFromFourDirections(src,dis,R,C,INF,i,j):
     if src[i][j] == 0:
