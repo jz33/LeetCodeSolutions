03b41aeef8660c0d41bdeb096d5e97ed3c05278f
Jun <junzhengrice@gmail.com>
2019-09-11 15:44:54 -0700

    Binary Search

--- 034 Find First and Last Position of Element in Sorted Array.c
+++ 034 Find First and Last Position of Element in Sorted Array.c
@@ -1,10 +1,28 @@
+/*
+34. Find First and Last Position of Element in Sorted Array
+https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+
+Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
+
+Your algorithm's runtime complexity must be in the order of O(log n).
+
+If the target is not found in the array, return [-1, -1].
+
+Example 1:
+
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+
+Example 2:
+
+Input: nums = [5,7,7,8,8,10], target = 6
+Output: [-1,-1]
+
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
-/*
-Search for a Range
-https://oj.leetcode.com/problems/search-for-a-range/
-*/
+
 int randLimit(const int limit)
 {
     int div = RAND_MAX/(limit+1);
