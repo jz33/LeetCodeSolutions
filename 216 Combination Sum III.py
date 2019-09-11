4257c4033bc8c8c5706929830b13feefcee37b45
Jun <junzhengrice@gmail.com>
2019-09-11 16:03:11 -0700

    DP

--- 216 Combination Sum III.py
+++ 216 Combination Sum III.py
@@ -1,3 +1,25 @@
+'''
+216. Combination Sum III
+https://leetcode.com/problems/combination-sum-iii/
+
+Find all possible combinations of k numbers that add up to a number n, given that only numbers from 1 to 9 can be used and each combination should be a unique set of numbers.
+
+Note:
+
+All numbers will be positive integers.
+The solution set must not contain duplicate combinations.
+## Notice, the solution also requires each number only use once
+
+Example 1:
+
+Input: k = 3, n = 7
+Output: [[1,2,4]]
+
+Example 2:
+
+Input: k = 3, n = 9
+Output: [[1,2,6], [1,3,5], [2,3,4]]
+'''
 class Solution:
     def combinationSum3(self, count: int, target: int) -> List[List[int]]:
         candidates = [1,2,3,4,5,6,7,8,9]
