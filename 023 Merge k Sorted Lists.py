5c3cec4930c33e6538b3834a3e2cd729247943b1
Jun <junzhengrice@gmail.com>
2019-09-14 22:26:45 -0700

    Heap

--- 023 Merge k Sorted Lists.py
+++ 023 Merge k Sorted Lists.py
@@ -1,29 +1,53 @@
-import heapq
 '''
-https://leetcode.com/problems/merge-k-sorted-lists/
+23. Merge k Sorted Lists
+
+Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+
+Example:
+
+Input:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+Output: 1->1->2->3->4->4->5->6
+
 '''
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
-class Solution(object):
-    def mergeKLists(self, lists):
-        """
-        :type lists: List[ListNode]
-        :rtype: ListNode
-        """
-        heap = []
-        head = ListNode(None)
-        p = head
-        for node in lists:
-            if node is not None:
-                heapq.heappush(heap,(node.val,node))
+import heapq
+
+class HeapNode:
+    def __init__(self, node: ListNode):
+        self.node = node
+    
+    # This method is for "<", which is necessary for heap comparison
+    def __lt__(self, that) -> bool:
+        return self.node.val < that.node.val
+
+class Solution:
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        root = ListNode(None)
+        
+        heap = []     
+        for n in lists:
+            if n:
+                heapq.heappush(heap, HeapNode(n))
+        
+        p = root
         while len(heap) > 0:
-            _,node = heapq.heappop(heap)
-            p.next = node
-            p = node
-            if node.next is not None:
-                heapq.heappush(heap,(node.next.val,node.next))
-        return head.next
+            hn = heapq.heappop(heap)
+            n = hn.node
+            
+            p.next = n
+            p = n
+            
+            if n.next:
+                heapq.heappush(heap, HeapNode(n.next))
+        
+        return root.next
