6d453d5b6f3668bb7d107767cac75cdbfc61470c
Jun <junzhengrice@gmail.com>
2019-09-15 16:04:37 -0700

    HEAP

--- 253 Meeting Rooms II.py
+++ 253 Meeting Rooms II.py
@@ -1,28 +1,43 @@
-from heapq import heappush, heappop
-# Definition for an interval.
-# class Interval(object):
-#     def __init__(self, s=0, e=0):
-#         self.start = s
-#         self.end = e
+'''
+253. Meeting Rooms II
+https://leetcode.com/problems/meeting-rooms-ii/
 
-class Solution(object):
-    def minMeetingRooms(self, intervals):
-        """
-        :type intervals: List[Interval]
-        :rtype: int
-        """
-        length = len(intervals)
-        if length < 2: return length
-        intervals.sort(key = lambda tup : tup.start)
+Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei),
+find the minimum number of conference rooms required.
+
+Example 1:
+
+Input: [[0, 30],[5, 10],[15, 20]]
+Output: 2
+
+Example 2:
+
+Input: [[7,10],[2,4]]
+Output: 1
+'''
+from heapq import heappush, heappop
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        '''
+        Essentially, this is to ask the most overlapped interval count
+        '''
+        # Sort meetings from start time
+        intervals.sort(key = lambda x : x[0])
         
-        # A min-heap to hold current meeting end times
-        h = []
-        heappush(h,intervals[0].end)
-        maxRoom = 1
-        for i in xrange(1,length):
-            iv = intervals[i]
-            while len(h) > 0 and iv.start >= h[0]:
-                heappop(h)
-            heappush(h,iv.end)
-            maxRoom = max(maxRoom,len(h))
-        return maxRoom
+        roomCount = 0
+        heap = [] # Heap of meeting end time
+        
+        for meeting in intervals:
+            # If current meeting starts after previous meeting,
+            # no need for new room
+            while len(heap) > 0 and meeting[0] >= heap[0]:
+                heappop(heap)
+                
+            heappush(heap, meeting[1])
+            
+            # The meetings in heap now all need there individual room,
+            # because they are overlapped          
+            roomCount = max(roomCount, len(heap))
+            
+        return roomCount
