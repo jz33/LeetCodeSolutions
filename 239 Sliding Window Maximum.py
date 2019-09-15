b94a25c54674e1f4f8b493ebc6c10c25a5495c8a
Jun <junzhengrice@gmail.com>
2019-09-14 17:36:23 -0700

    Sliding Window, Deque

--- 239 Sliding Window Maximum.py
+++ 239 Sliding Window Maximum.py
@@ -1,3 +1,26 @@
+'''
+239. Sliding Window Maximum
+https://leetcode.com/problems/sliding-window-maximum/
+
+Given an array nums, there is a sliding window of size k which is moving from the very left of the array to the very right.
+You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+Return the max sliding window.
+
+Example:
+
+Input: nums = [1,3,-1,-3,5,3,6,7], and k = 3
+Output: [3,3,5,5,6,7] 
+Explanation: 
+
+Window position                Max
+---------------               -----
+[1  3  -1] -3  5  3  6  7       3
+ 1 [3  -1  -3] 5  3  6  7       3
+ 1  3 [-1  -3  5] 3  6  7       5
+ 1  3  -1 [-3  5  3] 6  7       5
+ 1  3  -1  -3 [5  3  6] 7       6
+ 1  3  -1  -3  5 [3  6  7]      7
+'''
 from collections import deque
 
 class Solution:
@@ -20,7 +43,6 @@ class Solution:
             
             if i >= k - 1:
                 res[i - k + 1] = dq[0]
-                
-    
+     
         res[-1] = dq[0]
         return [nums[i] for i in res]
