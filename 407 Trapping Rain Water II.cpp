8ebe1e136124246ea12ccb754700ef5fa551e742
Jun <junzhengrice@gmail.com>
2019-09-15 17:19:42 -0700

    Priority Queue, Heap, Matrix

--- 407 Trapping Rain Water II.cpp
+++ 407 Trapping Rain Water II.cpp
@@ -1,23 +1,34 @@
-#include <iostream>
-#include <vector>
-#include <queue>
+/*
+407. Trapping Rain Water II
+https://leetcode.com/problems/trapping-rain-water-ii/
 
-typedef std::vector<int> VEC;
-typedef std::vector<VEC> MAT;
+Given an m x n matrix of positive integers representing the height of each unit cell in a 2D elevation map,
+compute the volume of water it is able to trap after raining.
 
+Note:
+
+Both m and n are less than 110. The height of each unit cell is greater than 0 and is less than 20,000.
+
+Example:
+
+Given the following 3x6 height map:
+[
+  [1,4,3,1,3,2],
+  [3,2,1,3,2,4],
+  [2,3,3,2,3,1]
+]
+
+Return 4.
+
+
+The above image represents the elevation map [[1,4,3,1,3,2],[3,2,1,3,2,4],[2,3,3,2,3,1]] before the rain.
+
+
+After the rain, water is trapped between the blocks. The total volume of water trapped is 4
+*/
 #define MAX(a,b) (a)>(b)?(a):(b)
 #define MIN(a,b) (a)<(b)?(a):(b)
-#define OUT(a) std::cout<<a<<std::endl
-#define DEBUG 1
-
-void dumpVec(VEC v) {
-    if(DEBUG) {
-        for(auto i = v.begin();i!=v.end();i++) {
-        std::cout<<*i<<" ";
-        }
-        std::cout<<std::endl;
-    }   
-}
+#define OUT(a) std::cout<<a<<std::endl;
 
 class WaterPoint
 {
@@ -34,104 +45,95 @@ public:
    }
 };
 
-int trapRainWater(MAT& heightMap){
-    int RC = heightMap.size(); // row count
-    if (RC == 0) return 0;
-    int CC = heightMap[0].size(); // column count
-    if (CC == 0) return 0;
+class Compare
+{
+public:
+    bool operator() (WaterPoint a, WaterPoint b)
+    {
+        return a.h > b.h; 
+    }
+};
+
+typedef std::priority_queue<WaterPoint,std::vector<WaterPoint>,Compare> MinHeap; 
+const int VISITED = -1;
+
+class Solution {
+public:
+    int pushToHeap(vector<vector<int>>& heightMap,
+        MinHeap& heap,
+        int height,
+        int neightborX,
+        int neightborY)
+    {            
+        // Get height of neighbor
+        auto neighborHeight = heightMap[neightborX][neightborY]; 
+        heightMap[neightborX][neightborY] = VISITED;
+                                
+        // Push in max of height and neighbor height
+        heap.emplace(neightborX,neightborY,MAX(neighborHeight,height));
+        
+        // Return how many water can trap
+        // The neighbor height must be smaller than height to trap water
+        return MAX(height - neighborHeight, 0);
+    }
     
-    /*
-    How to implement min heap in C++?
-    */
-    auto comp = [](WaterPoint& a, WaterPoint& b) { return a.h > b.h; };
-    std::priority_queue<WaterPoint,std::vector<WaterPoint>,decltype(comp)> heap(comp);
-
-    // construct visited matrix
-    MAT visited(RC, VEC(CC,0));
-
-    // push in boundries
-    for(int i=0;i<RC;i++){
-        for(int j=0;j<CC;j++){
-            if (i == 0 || j == 0 || i == RC-1 || j == CC-1){
-                heap.push(WaterPoint(i,j,heightMap[i][j]));
-                visited[i][j] = 1;
-            }         
+    int trapRainWater(vector<vector<int>>& heightMap){
+        const auto rowCount = heightMap.size(); 
+        if (rowCount < 3) return 0;
+        int colCount = heightMap[0].size();
+        if (colCount < 3) return 0;
+      
+        MinHeap heap;
+        
+        // Push in boundries
+        for (int i = 0; i < rowCount; ++i)
+        {
+            heap.emplace(i, 0, heightMap[i][0]);
+            heightMap[i][0] = VISITED;
+            
+            heap.emplace(i, colCount-1, heightMap[i][colCount-1]);
+            heightMap[i][colCount-1] = VISITED;
         }
+        for (int j = 1; j < colCount-1; ++j)
+        {
+            heap.emplace(0, j, heightMap[0][j]);
+            heightMap[0][j] = VISITED;
+            
+            heap.emplace(rowCount-1, j, heightMap[rowCount-1][j]);
+            heightMap[rowCount-1][j] = VISITED;
+        }
+    
+        // Iterate rest of the matrix
+        int res = 0;
+        while (!heap.empty())
+        {
+            auto p = heap.top();
+            heap.pop();
+
+            auto x = p.x;
+            auto y = p.y;
+            auto h = p.h;
+
+            if (x+1 < rowCount-1 && heightMap[x+1][y] != VISITED)
+            {
+                res += pushToHeap(heightMap, heap, h, x+1, y);           
+            }
+            
+            if (x-1 > 0 && heightMap[x-1][y] != VISITED)
+            {
+                res += pushToHeap(heightMap, heap, h, x-1, y);
+            }
+            
+            if (y+1 < colCount-1 && heightMap[x][y+1] != VISITED)
+            {
+                res += pushToHeap(heightMap, heap, h, x, y+1);
+            }
+            
+            if (y-1 > 0 && heightMap[x][y-1] != VISITED)
+            {
+                res += pushToHeap(heightMap, heap, h, x, y-1);
+            }
+        }
+        return res;
     }
-
-    // iterate heap
-    int res = 0;
-    while(!heap.empty()){
-        auto p = heap.top();
-        heap.pop();
-
-        auto x = p.x;
-        auto y = p.y;
-        auto h = p.h;
-
-        if(x+1 < RC-1 && visited[x+1][y] == 0){
-            res += MAX(h-heightMap[x+1][y],0);
-            // push in either water height or building height 
-            heap.push(WaterPoint(x+1,y,MAX(heightMap[x+1][y],h)));
-            visited[x+1][y] =1;
-        }
-        if(x-1 > 0 && visited[x-1][y] == 0){
-            res += MAX(h-heightMap[x-1][y],0);
-            // push in either water height or building height 
-            heap.push(WaterPoint(x-1,y,MAX(heightMap[x-1][y],h)));
-            visited[x-1][y] =1;
-        }
-        if(y+1 < CC-1 && visited[x][y+1] == 0){
-            res += MAX(h-heightMap[x][y+1],0);
-            // push in either water height or building height 
-            heap.push(WaterPoint(x,y+1,MAX(heightMap[x][y+1],h)));
-            visited[x][y+1] =1;
-        }
-        if(y-1 > 0 && visited[x][y-1] == 0){
-            res += MAX(h-heightMap[x][y-1],0);
-            // push in either water height or building height 
-            heap.push(WaterPoint(x,y-1,MAX(heightMap[x][y-1],h)));
-            visited[x][y-1] =1;
-        }
-    }
-
-    return res;
-}
-
-/*
-[
-  [1,4,3,1,3,2],
-  [3,2,1,3,2,4],
-  [2,3,3,2,3,1]
-]
-*/
-void test0(void) {
-    MAT mat;  
-    mat.push_back(VEC{1,4,3,1,3,2});
-    mat.push_back(VEC{3,2,1,3,2,4});
-    mat.push_back(VEC{2,3,3,2,3,1});
-    std::cout<<trapRainWater(mat)<<std::endl;
-}
-/*
-[[12,13,1,12],
-[13,4,13,12],
-[13,8,10,12],
-[12,13,12,12],
-[13,13,13,13]]
-*/
-void test1(void) {
-    MAT mat;
-    mat.push_back(VEC{12,13, 1,12});
-    mat.push_back(VEC{13, 4,13,12});
-    mat.push_back(VEC{13, 8,10,12});
-    mat.push_back(VEC{12,13,12,12});
-    mat.push_back(VEC{13,13,13,13});
-    std::cout<<trapRainWater(mat)<<std::endl;
-}
-
-int main() {
-    test0();
-    test1();
-    return 0;
-}
-
+};
