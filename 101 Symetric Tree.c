4f504220fb37607a6a0fb24b81e928736ca8be0c
Jun <junzhengrice@gmail.com>
2019-09-18 11:37:49 -0700

    Tree

--- 101 Symetric Tree.c
+++ 101 Symetric Tree.c
@@ -1,39 +1,75 @@
 /*
 10 Symmetric Tree
-https://oj.leetcode.com/problems/symmetric-tree/
+https://leetcode.com/problems/symmetric-tree/
+
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+ 
+
+But the following [1,2,2,null,3,null,3] is not:
+
+    1
+   / \
+  2   2
+   \   \
+   3    3
 */
-struct Binary_Search_Tree_Node{
-    int rank;
-    struct Binary_Search_Tree_Node *pl;
-    struct Binary_Search_Tree_Node *pr;
-};
-typedef struct Binary_Search_Tree_Node node;
-/*
-"flag" is init as 1
-*/
-void isSymmetricTreeRec(node* x, node* y, int* flag){
-    if(*flag == 1){
-        if(x == 0 && y == 0) return;
-        if((x != 0 && y ==0) || (x== 0 && y !=0)){
-            *flag = 0;
-            return;
-        }
-        if(x->rank == y->rank){
-            isSymmetricTreeRec(x->pr,y->pl,flag);
-            if(*flag == 1) isSymmetricTreeRec(x->pl,y->pr,flag);
-        } else {
-            *flag = 0;
-        }
-    }
-}
-int isSymmetricTree(node* x){
-    int flag;
-    if(x==0) return 1;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+#include <queue>
+#include <utility>
+
+class Solution {
+public:
     
-    flag = 1;
-    isSymmetricTreeRec(x->pl,x->pr,&flag);
-}
-int main(int argc, char** argv){
-    //TODO: test
-    return 0;
-}
+    // An iterative method
+    bool isSymmetric(TreeNode* root)
+    {
+        if (!root)
+        {
+            return true;
+        }
+        
+        std::queue<std::pair<TreeNode*, TreeNode*>> queue;
+        queue.emplace(root->left, root->right);
+        
+        while (!queue.empty())
+        {
+            auto pair = queue.front();
+            queue.pop();
+            
+            auto left = pair.first;
+            auto right = pair.second;
+            
+            if ((!left && right) || (left && !right))
+            {
+                return false;
+            }
+            if (left && right)
+            {
+                if (left->val != right->val) 
+                {
+                    return false;
+                }
+                
+                queue.emplace(left->left, right->right);
+                queue.emplace(left->right, right->left);
+            }
+        }
+        return true;
+    }
+};
