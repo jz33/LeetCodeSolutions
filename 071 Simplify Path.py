6b8cb780cf591bc2d0fcf33e598c747b4bca9cdd
Jun <junzhengrice@gmail.com>
2019-09-20 15:43:56 -0700

    Stack

--- 071 Simplify Path.py
+++ 071 Simplify Path.py
@@ -1,16 +1,61 @@
 '''
-Simplify Path
+71. Simplify Path
 https://leetcode.com/problems/simplify-path/
+
+Given an absolute path for a file (Unix-style), simplify it. Or in other words, convert it to the canonical path.
+
+In a UNIX-style file system, a period . refers to the current directory.
+Furthermore, a double period .. moves the directory up a level.
+For more information, see: Absolute path vs relative path in Linux/Unix
+
+Note that the returned canonical path must always begin with a slash /,
+and there must be only a single slash / between two directory names.
+The last directory name (if it exists) must not end with a trailing /.
+Also, the canonical path must be the shortest string representing the absolute path.
+
+Example 1:
+
+Input: "/home/"
+Output: "/home"
+Explanation: Note that there is no trailing slash after the last directory name.
+
+Example 2:
+
+Input: "/../"
+Output: "/"
+Explanation: Going one level up from the root directory is a no-op, as the root level is the highest level you can go.
+
+Example 3:
+
+Input: "/home//foo/"
+Output: "/home/foo"
+Explanation: In the canonical path, multiple consecutive slashes are replaced by a single one.
+
+Example 4:
+
+Input: "/a/./b/../../c/"
+Output: "/c"
+
+Example 5:
+
+Input: "/a/../../b/../c//.//"
+Output: "/c"
+
+Example 6:
+
+Input: "/a//b////c/d//././/.."
+Output: "/a/b/c"
 '''
-def simplifyPath(input):
-    stack = []
-    for e in input.split('/'):
-        if e == '.' or e == '': continue
-        if e == '..' and len(stack) > 0:
-            stack.pop()
-        elif e != '..':
-            stack.append(e)
-    return '/' if len(stack) == 0 else '/' + '/'.join(stack)
-        
-input = "/../"
-print simplifyPath(input)
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = []
+        for e in path.split('/'):
+            if e == '.' or e == '':
+                continue # ignore '.'
+                
+            if e != '..':
+                stack.append(e) # normal
+            elif stack:
+                stack.pop() # '..' means go up
+      
+        return '/' + '/'.join(stack)
