a8964ccc182d5813b1c36201d621db8d2eb980ba
Jun <junzhengrice@gmail.com>
2019-09-21 10:59:10 -0700

    BFS, Matrix

--- 130 Surrounded Regions.py
+++ 130 Surrounded Regions.py
@@ -32,43 +32,56 @@ class Solution:
         Do not return anything, modify board in-place instead.
         """
         rowCount = len(board)
-        if rowCount == 0:
+        if not rowCount:
             return
         
         colCount = len(board[0])
+        if not colCount:
+            return
         
+        IsIsland = 'O'
+        IsLand = 'X'
+        # Notice inside the queue and pop out of queue cases are different.
+        # if treat them all as visisted, there will be duplciates in queue!
+        IsInsideQueue = 'Q'
+        IsPoped = 'P'      
         def mark(cell: Tuple[int, int]):
+            # Use a stack to replace queue, as order does not matter
             stack = [cell]
             while stack:
                 x, y = stack.pop()
-                board[x][y] = 'V'
-                if x + 1 <  rowCount and board[x+1][y] == 'O':
+                board[x][y] = IsPoped
+                if x + 1 <  rowCount and board[x+1][y] == IsIsland:
                     stack.append((x+1,y))
-                if x - 1 > -1 and board[x-1][y] == 'O':
+                    board[x+1][y] = IsInsideQueue
+                if x > 0 and board[x-1][y] == IsIsland:
                     stack.append((x-1,y))
-                if y + 1 < colCount and board[x][y+1] == 'O':
+                    board[x-1][y] = IsInsideQueue
+                if y + 1 < colCount and board[x][y+1] == IsIsland:
                     stack.append((x,y+1))
-                if y - 1 > -1 and board[x][y-1] == 'O':
+                    board[x][y+1] = IsInsideQueue
+                if y > 0 and board[x][y-1] == IsIsland:
                     stack.append((x,y-1))
+                    board[x][y-1] = IsInsideQueue
         
         # Go through border, if is '0', mark all adjacent cells as 'V'
         for i in range(rowCount):
-            if board[i][0] == 'O':
+            if board[i][0] == IsIsland:
                 mark((i,0))
-            if board[i][colCount-1] == 'O':
+            if board[i][colCount-1] == IsIsland:
                 mark((i,colCount-1))
 
         for i in range(1,colCount-1):
-            if board[0][i] == 'O':
+            if board[0][i] == IsIsland:
                 mark((0,i))
-            if board[rowCount-1][i] == 'O':
+            if board[rowCount-1][i] == IsIsland:
                 mark((rowCount-1,i))
             
-        # For all cells with 'O', mark it to 'X'
-        # For all cells with 'V', mark it back to 'O'
+        # For island cell, mark it to land, as it is not connected to bound;
+        # For cell who is connected to boundry island, mark it back to island
         for i in range(rowCount):
             for j in range(colCount):
-                if board[i][j] == 'O':
-                    board[i][j] = 'X'
-                elif board[i][j] == 'V':
-                    board[i][j] = 'O'
+                if board[i][j] == IsIsland:
+                    board[i][j] = IsLand
+                elif board[i][j] == IsPoped:
+                    board[i][j] = IsIsland
