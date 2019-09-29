fbfbb8d96826ec1bffedbca231d6e8b19421ec0f
Jun <junzhengrice@gmail.com>
2019-09-29 11:53:08 -0700

    Binary Search, Bit Manipulation

--- 050 Pow(x, n).c
+++ 050 Pow(x, n).c
@@ -1,31 +1,49 @@
-#include <stdio.h>
 /*
 Pow(x,n)
-https://oj.leetcode.com/problems/powx-n/
-http://www.programcreek.com/2012/12/leetcode-powx-n/
+https://leetcode.com/problems/powx-n/
 
-A divide-conquer approach,
-very impractical
+Implement pow(x, n), which calculates x raised to the power n (xn).
+
+Example 1:
+
+Input: 2.00000, 10
+Output: 1024.00000
+Example 2:
+
+Input: 2.10000, 3
+Output: 9.26100
+Example 3:
+
+Input: 2.00000, -2
+Output: 0.25000
+Explanation: 2-2 = 1/22 = 1/4 = 0.25
 */
-double myPow(double x, int n) {
-    double ret = 1.0;
-    unsigned long long p;
-    if (n < 0) {
-        p = -n;
-        x = 1 / x;
-    } else {
-        p = n;
-    }
-    while (p) {
-        if (p & 1)
-            ret *= x;
-        x *= x;
-        p >>= 1;
-    }
-    return ret;
-}
-
-int main()
+double myPow(double x, int n)
 {
-    return 0;
+    double answer = 1;
+    double base = x;
+    long long exponent = n;
+    
+    if (exponent < 0)
+    {
+        exponent = -exponent;
+        base = 1 / base;
+    }
+        
+    while (exponent != 0)
+    {
+        /*
+        Let i be the iteration count, then
+        base is the product component needed on exponent[i]
+        */
+        if (exponent & 1)
+        {
+            answer *= base;
+        }
+        
+        exponent >>= 1;
+        base *= base;
+    }
+    
+    return answer;
 }
