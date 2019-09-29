6af0dc7010d4baf2721b0e6fec03f5628c154e9a
Jun <junzhengrice@gmail.com>
2019-09-28 19:44:08 -0700

    Design

--- 170 Two Sum III - Data structure design.py
+++ 170 Two Sum III - Data structure design.py
@@ -1,35 +1,45 @@
-class TwoSum(object):
+'''
+170. Two Sum III - Data structure design
+https://leetcode.com/problems/two-sum-iii-data-structure-design/
+
+Design and implement a TwoSum class. It should support the following operations: add and find.
+
+add - Add the number to an internal data structure.
+find - Find if there exists any pair of numbers which sum is equal to the value.
+
+Example 1:
+
+add(1); add(3); add(5);
+find(4) -> true
+find(7) -> false
+Example 2:
+
+add(3); add(1); add(2);
+find(3) -> true
+find(6) -> false
+'''
+from collections import Counter
+
+class TwoSum:
 
     def __init__(self):
         """
-        initialize your data structure here
+        Initialize your data structure here.
         """
-        self.ctr = {}
+        self.book = Counter() 
 
-    def add(self, number):
+    def add(self, number: int) -> None:
         """
-        Add the number to an internal data structure.
-        :rtype: nothing
+        Add the number to an internal data structure..
         """
-        if number in self.ctr:
-            self.ctr[number] += 1
-        else:
-            self.ctr[number] = 1
+        self.book[number] += 1      
 
-    def find(self, value):
+    def find(self, value: int) -> bool:
         """
         Find if there exists any pair of numbers which sum is equal to the value.
-        :type value: int
-        :rtype: bool
         """
-        ctr = self.ctr # speeds up
-        for num in ctr:
-            if value - num in ctr and (value - num != num or ctr[num] > 1):
+        book = self.book
+        for k in book.keys():
+            m = value - k
+            if m in book and (m != k or book[m] > 1):
                 return True
-        return False
-        
-
-# Your TwoSum object will be instantiated and called as such:
-# twoSum = TwoSum()
-# twoSum.add(number)
-# twoSum.find(value)
