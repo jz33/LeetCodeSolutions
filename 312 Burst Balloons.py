d90ff6a399b4b251ba371fbfe4aec2ad729d5fab
Jun <junzhengrice@gmail.com>
2019-09-30 22:21:41 -0700

    Dynamic Programming

--- 312 Burst Balloons.py
+++ 312 Burst Balloons.py
@@ -1,28 +1,42 @@
 '''
 Burst Balloons
 https://leetcode.com/problems/burst-balloons/
+
+Given n balloons, indexed from 0 to n-1. Each balloon is painted with a number on it represented by array nums.
+You are asked to burst all the balloons. If the you burst balloon i you will get nums[left] * nums[i] * nums[right] coins.
+Here left and right are adjacent indices of i. After the burst, the left and right then becomes adjacent.
+
+Find the maximum coins you can collect by bursting the balloons wisely.
+
+Note:
+
+You may imagine nums[-1] = nums[n] = 1. They are not real therefore you can not burst them.
+0 ≤ n ≤ 500, 0 ≤ nums[i] ≤ 100
+
+Example:
+
+Input: [3,1,5,8]
+Output: 167 
+Explanation: nums = [3,1,5,8] --> [3,5,8] -->   [3,8]   -->  [8]  --> []
+             coins =  3*1*5      +  3*5*8    +  1*3*8      + 1*8*1   = 167
+             
 '''
-class Solution(object):
-    def maxCoins(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+class Solution:
+    def maxCoins(self, nums: List[int]) -> int:
         nums = [1] + nums + [1]
         n = len(nums)
         
-        # dp[i][j] is max value in [i+1,j-1]
-        dp = [[0 for _ in xrange(n)] for _ in xrange(n)]
+        # dp[i][j] is the max value in nums[i:j]
+        dp = [[0] * n for _ in range(n)]
         
-        for gap in xrange(2,n):
-            for i in xrange(n - gap):
-                j = i + gap
-                for k in xrange(i+1,j):
-                    dp[i][j] = max(dp[i][j], dp[i][k]+dp[k][j]+nums[i]*nums[k]*nums[j])
-        for row in dp:
-            print row
+        # The building dp order:
+        # [0,2], [1,3], ...
+        # [0,3], [1,4], ...
+        # Therefore step is from 2 to n
+        for step in range(2, n):
+            for left in range(n - step):
+                right  = left + step
+                for i in range(left+1,right):
+                    dp[left][right] = max(dp[left][right], dp[left][i]+dp[i][right]+nums[left]*nums[i]*nums[right])
+   
         return dp[0][n-1]
-        
-sol = Solution()
-nums = [1,2,3,4,5]
-print sol.maxCoins(nums)
