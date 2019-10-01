7c03ed961d13c903336de3c847bf87be27765be7
Jun <junzhengrice@gmail.com>
2019-09-30 17:16:01 -0700

    Sliding Window, O(n)

--- 560 Subarray Sum Equals K.py
+++ 560 Subarray Sum Equals K.py
@@ -1,19 +1,27 @@
-"""
-560 Subarray Sum Equals K
-https://leetcode.com/problems/subarray-sum-equals-k
-"""
-def subarraySum(nums, k):
-      """
-      :type nums: List[int]
-      :type k: int
-      :rtype: int
-      Same method like 523 Continous Subarray Sum
-      """
-      ref = {0:1} # total : how many total
-      total = 0
-      counter = 0
-      for i,c in enumerate(nums):
-          total += c
-          counter += ref.get(total-k, 0)
-          ref[total] = ref.get(total,0)+1
-      return counter
+/*
+560. Subarray Sum Equals K
+https://leetcode.com/problems/subarray-sum-equals-k/
+     
+Given an array of integers and an integer k, you need to find the total number of continuous subarrays whose sum equals to k.
+
+Example 1:
+
+Input:nums = [1,1,1], k = 2
+Output: 2
+*/
+from collections import Counter
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        ctr = Counter() # sum : count
+        ctr[0] = 1 # think [1,1,1], 2
+        s = 0
+        res = 0
+        for n in nums:
+            s += n
+            # Similar idea to Continuous Subarray Sum question
+            # So for every j representing the index on ctr[s-k],
+            # if say current index is i, then sum of nums[j+1:i+1]
+            # must be k
+            res += ctr[s-k]
+            ctr[s] += 1
+        return res
