2aa3ea98904955844fa7160064d5e49465801018
Jun <junzhengrice@gmail.com>
2019-10-02 14:28:45 -0700

    BFS

--- 301 Remove Invalid Parentheses.py
+++ 301 Remove Invalid Parentheses.py
@@ -1,63 +1,92 @@
 '''
-Remove Invalid Parentheses
+301. Remove Invalid Parentheses
 https://leetcode.com/problems/remove-invalid-parentheses/
-https://leetcode.com/discuss/67908/java-bfs-solution-16ms-avoid-generating-duplicate-strings
+
+Remove the minimum number of invalid parentheses in order to make the input string valid. Return all possible results.
+
+Note: The input string may contain letters other than the parentheses ( and ).
+
+Example 1:
+
+Input: "()())()"
+Output: ["()()()", "(())()"]
+
+Example 2:
+
+Input: "(a)())()"
+Output: ["(a)()()", "(a())()"]
+
+Example 3:
+
+Input: ")("
+Output: [""]
 '''
-class Solution(object):
-    def isValid(self,s):
+from collections import deque
+
+class Solution:
+    def isValid(self, s: List[str]) -> bool:
+        # If a string has valid parentheses pairs
+        # Notice input is listified str
         ctr = 0
         for ch in s:
             if ch == '(': ctr += 1
             elif ch == ')': ctr -= 1
             if ctr < 0: return False
         return ctr == 0
-    
-    def trim(self,ls):
-        length = len(ls)
-        i = 0
-        while i < length and ls[i] == ')': i += 1
-        ls = ls[i:]
-        length = len(ls)
-        i = length - 1
-        while i > -1 and ls[i] == '(': i -= 1
-        return ls[:i+1]
-    
-    def removeInvalidParentheses(self, s):
-        """
-        :type s: str
-        :rtype: List[str]
-        """
-        ls = self.trim(list(s))
-        if self.isValid(ls): return [''.join(ls)]
 
-        ans = []        
-        # The queue only contains invalid middle steps
-        queue = []
-        # The 3-Tuple is (string, startIndex, lastRemovedChar)
-        queue.append((ls, 0, ')'))
-        while len(queue) > 0:
-            (ls,start,removed) = queue.pop(0)
-            
-            # Observation 2, start from last removal position
-            for i in xrange(start,len(ls)):
-                ch = ls[i]
-                if ch != '(' and ch != ')': continue
-                # Observation 1, do not repeatedly remove from consecutive parentheses
-                if i != start and ls[i-1] == ch: continue            
-                #Observation 3, do not remove a pair of valid parentheses
-                if removed == '(' and ch == ')': continue
-                
-                ls2 = ls[:i] + ls[i+1:]
-                if self.isValid(ls2):
-                    ans.append(''.join(ls2))                   
-                # Avoid adding children
-                elif len(ans) == 0:
-                    queue.append((ls2, i, ch));
-        return ans
+    def trim(self, ls: List[str]) -> List[str]:
+        # Trim off left and right invalid parentheses to speed up the algorithm
+        # Notice input is listified str
+        length = len(ls)
         
-sol = Solution()
-s = "a"
-r = sol.removeInvalidParentheses(s)
-print r
+        left = 0
+        while left < length and ls[left] == ')':
+            left += 1
 
+        right = length - 1
+        while right >= left and ls[right] == '(':
+            right -= 1
+            
+        return ls[left : right+1]
+    
+    def removeInvalidParentheses(self, s: str) -> List[str]:
+        # Trim off left and right invalid parentheses to speed up
+        ls = self.trim(list(s))
+        if self.isValid(ls):
+            return [''.join(ls)]
 
+        res = []        
+        queue = deque() # [(string, startIndex, lastRemovedChar)]
+        queue.append((ls, 0, ')'))
+        
+        while len(queue):
+            node, startIndex, lastRemovedChar = queue.popleft()
+
+            # Start from last removal position
+            for i in range(startIndex, len(node)):
+                ch = node[i]
+                if ch != '(' and ch != ')':
+                    continue
+
+                if i != startIndex and node[i-1] == ch:
+                    # Do not repeatedly remove from consecutive parentheses
+                    # Because essentially the removed strings are all same
+                    continue  
+                    
+                if lastRemovedChar == '(' and ch == ')':
+                    # Do not remove a pair of valid parentheses
+                    continue
+                
+                # Remove ch
+                child = node[:i] + node[i+1:]
+                
+                if self.isValid(child):
+                    res.append(''.join(child))
+                    
+                elif len(res) == 0:
+                    # If no answer is found yet, add to queue and keep BFS.
+                    # Otherwise, no need for next level BFS,
+                    # as we only need to remove least amount of parentheses
+                    queue.append((child, i, ch));
+
+        return res
