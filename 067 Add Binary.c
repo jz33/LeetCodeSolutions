b2ed84b0281072046d90c3f5216dfd5d70d76079
Jun <junzhengrice@gmail.com>
2019-10-03 10:25:53 -0700

    O(n), Math

--- 067 Add Binary.c
+++ 067 Add Binary.c
@@ -1,81 +1,42 @@
-char* addBinary(char* a, char* b)
-{
-    char *at = a,*bt = b,*rt;
-    size_t al = 0, bl = 0, rl;
-    int i,j,c=0,s;
+'''
+Add Binary
+https://leetcode.com/problems/add-binary/
 
-    // move to least significant bits
-    // count length
-    while(*at != '\0')
-    {
-        at++;
-        al++;
-    }
-    at--;
-    while(*bt != '\0')
-    {
-        bt++;
-        bl++;
-    }
-    bt--;
+Given two binary strings, return their sum (also a binary string).
 
-    // return length
-    rl = (al > bl ? al : bl) + 1;
-    rt = (char*)malloc(sizeof(char)*(rl+1));
-    i=0;
+The input strings are both non-empty and contains only characters 1 or 0.
+
+Example 1:
+Input: a = "11", b = "1"
+Output: "100"
     
-    // terminate by '\0'
-    while(i < rl)
-    {
-        rt++;
-        i++;
-    }
-    *rt-- = '\0';
-
-    // sum from right
-    i = al - 1;
-    j = bl - 1;
-    while(i != -1 && j != -1)
-    {
-        s = c + *at---'0' + *bt---'0';
-        c = s >> 1;
-        *rt-- = (s & 1) + 48; // notice the operator precedence!
-        i--,j--;
-    }
-
-    // rest
-    while(i != -1)
-    {
-        s = c + *at---'0';
-        c = s >> 1;
-        *rt-- = (s & 1) + 48;
-        i--;
-    }
-    while(j != -1){
-        s = c + *bt---'0';
-        c = s >> 1;
-        *rt-- = (s & 1)+48;;
-        j--;
-    }
-
-    // return
-    if(c == 1){
-        *rt = '1';
-        return rt;
-    } else {
-        *rt = '\0';
-        return rt+1;
-    }
-}
-
-int main(int argc, char* argv[])
-{
-    char *a, *b, *c;
-    if(argc < 3) return 0;
-    a = argv[1]; puts(a);
-    b = argv[2]; puts(b);
-
-    c = addBinary(a,b);
-    puts(c);
-    return 0; 
-}
+Example 2:
+Input: a = "1010", b = "1011"
+Output: "10101"
+'''
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        res = [] # list of chars
+        i = len(a) - 1
+        j = len(b) - 1
+        acc = 0 # accumulate on 1 bit
+        while i > -1 or j > -1 or acc > 0:
+            left = ord(a[i]) - ord('0') if i > -1 else 0
+            right = ord(b[j]) - ord('0') if j > -1 else 0
+            total = left + right + acc
+            if total == 0:
+                res.append('0')
+                acc = 0
+            elif total == 1:
+                res.append('1')
+                acc = 0
+            elif total == 2:
+                res.append('0')
+                acc = 1
+            elif total == 3:
+                res.append('1')
+                acc = 1
+            i -= 1
+            j -= 1
+                
+        return ''.join(res[::-1])          
