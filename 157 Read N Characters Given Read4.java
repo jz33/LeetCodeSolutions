bd8360a1fd7dfd0ec8f2c12cf9cb78f504240214
Jun <junzhengrice@gmail.com>
2019-10-03 21:55:35 -0700

    Low Level

--- 157 Read N Characters Given Read4.java
+++ 157 Read N Characters Given Read4.java
@@ -1,5 +1,6 @@
 /*
 157. Read N Characters Given Read4
+https://leetcode.com/problems/read-n-characters-given-read4/
 
 Given a file and assume that you can only read the file using a given method read4,
 implement a method to read n characters.
