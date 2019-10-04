696963f5cf701719355ab5520bf265c6d197139d
Jun <junzhengrice@gmail.com>
2019-10-04 14:33:19 -0700

    Backtracking

--- 332 Reconstruct Itinerary.py
+++ 332 Reconstruct Itinerary.py
@@ -1,36 +1,69 @@
 '''
-Reconstruct Itinerary
+332. Reconstruct Itinerary
 https://leetcode.com/problems/reconstruct-itinerary/
-Deep first search
-Record visited status on node
+
+Given a list of airline tickets represented by pairs of departure and arrival airports [from, to],
+reconstruct the itinerary in order. All of the tickets belong to a man who departs from JFK.
+Thus, the itinerary must begin with JFK.
+
+Note:
+
+If there are multiple valid itineraries,
+you should return the itinerary that has the smallest lexical order when read as a single string.
+For example, the itinerary ["JFK", "LGA"] has a smaller lexical order than ["JFK", "LGB"].
+
+All airports are represented by three capital letters (IATA code).
+You may assume all tickets form at least one valid itinerary.
+
+Example 1:
+
+Input: [["MUC", "LHR"], ["JFK", "MUC"], ["SFO", "SJC"], ["LHR", "SFO"]]
+Output: ["JFK", "MUC", "LHR", "SFO", "SJC"]
+
+Example 2:
+
+Input: [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
+Output: ["JFK","ATL","JFK","SFO","ATL","SFO"]
+Explanation: Another possible reconstruction is ["JFK","SFO","ATL","JFK","ATL","SFO"].
+             But it is larger in lexical order.
 '''
-def rec(graph,step_total,step_ctr,itinerary):
-    if step_ctr == step_total:
-        return True
-    f = itinerary[-1]
-    if f in graph:
-        for t in graph[f]:
+from collections import defaultdict
+
+class Solution:
+    def dfs(self, itinerary):
+        '''
+        Do dfs/back tracking, reture True if all route is iterated
+        '''
+        if len(itinerary) == self.total:
+            return True
+        
+        f = itinerary[-1]
+        for t in self.graph[f]:
             if t[1] == False:
                 itinerary.append(t[0])
                 t[1] = True
-                if rec(graph,step_total,step_ctr+1,itinerary):
+                
+                if self.dfs(itinerary):
                     return True
-                else:
-                    itinerary.pop()
-                    t[1] = False
-    return False
+                
+                # Back track, reset
+                itinerary.pop()
+                t[1] = False
     
-def findItinerary(tickets):
-    graph = {} # {from : [[togo,visited]]}
-    for f,t in tickets:
-        if f not in graph:
-            graph[f] = []
-        graph[f].append([t,False])
-    
-    for k in graph.iterkeys():
-        graph[k].sort(key = lambda x : x[0])
-    
-    total = len(tickets)
-    itinerary = ['JFK']
-    rec(graph,total,0,itinerary)
-    return itinerary
+        return False
+
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        # Build graph, sort togo by lexical order
+        graph = defaultdict(list) # {from : [[togo, whether this edge is visited]]}
+        for f, t in tickets:
+            graph[f].append([t, False])
+        
+        for f in graph.keys():
+            graph[f].sort(key = lambda x : x[0])
+            
+        self.total = len(tickets) + 1
+        self.graph = graph
+        
+        itinerary = ['JFK']
+        self.dfs(itinerary)
+        return itinerary
