e988547845f850cadf4f0e226caaf16294629a62
Jun <junzhengrice@gmail.com>
2019-10-05 15:52:54 -0700

    BFS, Matrix

--- 317 Shorted Distance from All Buildings.py
+++ 317 Shorted Distance from All Buildings.py
@@ -1,61 +1,98 @@
 '''
-Shorted Distance from All Buildings
+317. Shortest Distance from All Buildings
 https://leetcode.com/problems/shortest-distance-from-all-buildings/
+
+You want to build a house on an empty land which reaches all buildings in the shortest amount of distance.
+You can only move up, down, left and right. You are given a 2D grid of values 0, 1 or 2, where:
+
+Each 0 marks an empty land which you can pass by freely.
+Each 1 marks a building which you cannot pass through.
+Each 2 marks an obstacle which you cannot pass through.
+
+Example:
+
+Input: [[1,0,2,0,1],[0,0,0,0,0],[0,0,1,0,0]]
+
+1 - 0 - 2 - 0 - 1
+|   |   |   |   |
+0 - 0 - 0 - 0 - 0
+|   |   |   |   |
+0 - 0 - 1 - 0 - 0
+
+Output: 7 
+
+Explanation: Given three buildings at (0,0), (0,4), (2,2), and an obstacle at (0,2),
+             the point (1,2) is an ideal empty land to build a house, as the total 
+             travel distance of 3+3+1=7 is minimal. So return 7.
+Note:
+There will be at least one building. If it is not possible to build such house according to the above rules, return -1.
 '''
-from Queue import Queue
+from typing import Tuple
+from collections import deque
 
-class Solution(object):
-    def shortestDistance(self, grid):
-        """
-        :type grid: List[List[int]]
-        :rtype: int
-        """
-        R = len(grid)
-        if R == 0: return 0
-        C = len(grid[0])
+class Solution:
+    def shortestDistance(self, grid: List[List[int]]) -> int:
+        rowCount = len(grid)
+        if rowCount == 0:
+            return 0
+        
+        colCount = len(grid[0])
+        if colCount == 0:
+            return 0
+        
+        def extend(x: int, y: int, bi: int, queue, d):
+            if x < rowCount and x > -1 and y < colCount and y > -1 and grid[x][y] == 0:
+                old_d = buildingMap[x][y][bi]
+                if old_d is None or old_d > d:
+                    buildingMap[x][y][bi] = d
+                    queue.append((x, y, d))
 
-        #BFS
-        INF = (R+C)*R*C
-        directions = [0,1,0,-1,0]
-        minDist = 0
-        
-        # recording all distances from each BFS 
-        total_grid = [[0 for _ in xrange(C)] for _ in xrange(R)]
-        
-    
-        # 0. Mark visited node; 1. Detect unreachable node
-        walk = 0
-        for i in xrange(R):
-            for j in xrange(C):
+        def broadcast(bp: Tuple[int, int], bi: int):
+            '''
+            Set all distances for a building
+            @bp: building position
+            @bi: building index
+            '''
+            queue = deque() # [(building position x, y, distance)]
+            queue.append((bp[0], bp[1], 0))
+
+            while queue:
+                x, y, d = queue.popleft()
+                extend(x+1, y, bi, queue, d+1)
+                extend(x, y+1, bi, queue, d+1)
+                extend(x-1, y, bi, queue, d+1)
+                extend(x, y-1, bi, queue, d+1)
+
+        buildings = []
+        for i in range(rowCount):
+            for j in range(colCount):
                 if grid[i][j] == 1:
-                    # start BFS
-                    # current distances reachable by current start point
-                    dist_grid = [[0 for _ in xrange(C)] for _ in xrange(R)]
-                    dq = Queue()
-                    dq.put((i,j))
-                    minDist = INF # for each BFS, minDist is reset
-                    while not dq.empty():
-                        a,b = dq.get()
-                        dab = dist_grid[a][b]
-                        for k in xrange(4):
-                            x = a + directions[k]
-                            y = b + directions[k+1]
-                            if x > -1 and x < R and y > -1 and y < C and grid[x][y] == walk:
-                                grid[x][y] -= 1 # mark as visited
-                                d = dab + 1
-                                dist_grid[x][y] = d
-                                total_grid[x][y] += d
-                                minDist = min(minDist,total_grid[x][y])
-                                dq.put((x,y))
-                    if minDist == INF: return -1
-                    walk -= 1
-        return minDist
-        
-        
-sol = Solution()
-grid = [
-    [1,2,1],
-    [0,0,0],
-    [0,1,0]
-    ]
-print sol.shortestDistance(grid)
+                    buildings.append((i,j))
+
+        # A 3-D matrix to record shortest distances to each building.
+        # The buildingMap[i][j][k] means the shortest distance to building k from (i, j).
+        # If (i, j) is obstacle or building itself, the value is always None
+        buildingMap = [[[None] * len(buildings) for _ in range(colCount)] for _ in range(rowCount)]
+        for i, b in enumerate(buildings):
+            broadcast(b, i)
+
+        # Get minimum
+        MaxDist = rowCount * colCount * len(buildings)
+        def sumDist(x: int, y: int) -> int:
+            '''
+            Sum all distances on buildingMap[x][y]
+            If any is None, then this point cannot reach all buildins
+            '''
+            s = 0
+            for d in buildingMap[x][y]:
+                if d is None:
+                    return MaxDist
+                s += d
+            return s
+
+        minDist = MaxDist
+        for i in range(rowCount):
+            for j in range(colCount):
+                minDist = min(minDist, sumDist(i, j))
+
+        return minDist if minDist != MaxDist else -1
