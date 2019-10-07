6f991e3f2133b5304dc07bfc2023584bcf0fb843
Jun <junzhengrice@gmail.com>
2019-10-07 14:37:23 -0700

    Graph, Topological Sort

--- 207 Course Schedule.py
+++ 207 Course Schedule.py
@@ -1,47 +1,54 @@
-from collections import deque
 '''
 207 Course Schedule
 https://leetcode.com/problems/course-schedule/
+
+There are a total of n courses you have to take, labeled from 0 to n-1.
+Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
+Given the total number of courses and a list of prerequisite pairs, is it possible for you to finish all courses?
+
+Example 1:
+
+Input: 2, [[1,0]] 
+Output: true
+Explanation: There are a total of 2 courses to take. 
+             To take course 1 you should have finished course 0. So it is possible.
+Example 2:
+
+Input: 2, [[1,0],[0,1]]
+Output: false
+Explanation: There are a total of 2 courses to take. 
+             To take course 1 you should have finished course 0, and to take course 0 you should
+             also have finished course 1. So it is impossible.
+Note:
+
+The input prerequisites is a graph represented by a list of edges, not adjacency matrices.
+Read more about how a graph is represented.
+You may assume that there are no duplicate edges in the input prerequisites.
 '''
-def canFinish(numCourses, prerequisites):
-    """
-    :type numCourses: int
-    :type prerequisites: List[List[int]]
-    :rtype: bool
-    """
-    parents = [0] * numCourses # parents count
-    graph = {} # parent : set(children)
-    
-    # parse
-    for pair in prerequisites:
-        c = pair[0] # child
-        p = pair[1] # parent
-        cs = graph.get(p,set())
-        cs.add(c)
-        graph[p] = cs
-        parents[c] += 1
-    
-    # entries nodes who have no parents
-    entries = [i for i in xrange(numCourses) if parents[i] == 0]
+from collections import deque
 
-    # DFS
-    dq = deque()
-    dq.extend(entries)   
-    reached = len(dq)
-    while len(dq) > 0:
-        p = dq.popleft()
-        cs = graph.get(p,set())
-        for c in cs:
-            if parents[c] == 1: # ready to take
-                dq.append(c)
-                reached += 1
-            else:
-                parents[c] -= 1
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = [[] for _ in range(numCourses)] # vertice : [togo vertices]
+        ranks = [0] * numCourses # income degree
 
-    return reached == numCourses
+        for edge in prerequisites:
+            a = edge[0]
+            b = edge[1]
+            # Need to take a before b, then edge is b -> a
+            graph[b].append(a)
+            ranks[a] += 1
 
+        # Sort
+        queue = deque([i for i, r in enumerate(ranks) if r == 0])
 
-numCourses = 2
-prerequisites = [[0,1]]
-
-print canFinish(numCourses, prerequisites)
+        reachedCourse = 0
+        while queue:
+            node = queue.popleft()
+            reachedCourse += 1
+            for togo in graph[node]:
+                if ranks[togo] == 1:
+                    queue.append(togo)
+                ranks[togo] -= 1
+            
+        return reachedCourse == numCourses
