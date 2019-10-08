78ee91a886f376fcc6ed43f82952150f23e92175
Jun <junzhengrice@gmail.com>
2019-10-08 10:16:47 -0700

    O(n), Math

--- 007 Reverse Integer.java
+++ 007 Reverse Integer.java
@@ -1,16 +1,33 @@
 /*
-07 Reverse Integer
-https://oj.leetcode.com/problems/reverse-integer/
+7. Reverse Integer
+https://leetcode.com/problems/reverse-integer/
+
+Given a 32-bit signed integer, reverse digits of an integer.
+
+Example 1:
+
+Input: 123
+Output: 321
+Example 2:
+
+Input: -123
+Output: -321
+Example 3:
+
+Input: 120
+Output: 21
+
+Note:
+Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1].
+For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
 */
 public class Solution {
     /*
     No need to consider sign
     */
     public int reverse(int x) {
-        long r = x % 10;
-        x /= 10;
-        while(x != 0){
-            //System.out.println(r);
+        long r = 0;
+        while (x != 0) {
             r  = r * 10 + x % 10;
             x /= 10;
         }
