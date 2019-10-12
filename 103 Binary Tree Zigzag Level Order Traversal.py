12b3d0aaa97b8a5283fdcbcc8a0cf20518cf2066
Jun <junzhengrice@gmail.com>
2019-10-11 19:40:38 -0700

    Level Order Traversal

--- 103 Binary Tree Zigzag Level Order Traversal.py
+++ 103 Binary Tree Zigzag Level Order Traversal.py
@@ -1,33 +1,64 @@
 '''
 103 Binary Tree Zigzag Level Order Traversal
 https://oj.leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
+
+Given a binary tree, return the zigzag level order traversal of its nodes' values.
+(ie, from left to right, then right to left for the next level and alternate between).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its zigzag level order traversal as:
+
+[
+  [3],
+  [20,9],
+  [15,7]
+]
 '''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
 class Solution:
-    # @param {TreeNode} root
-    # @return {integer[][]}
-    def zigzagLevelOrder(self, root):
+    def zigzagLevelOrder(self, root: TreeNode) -> List[List[int]]:
         ans = []
-        if root is None: return ans
+        if not root:
+            return ans
         
-        isEven = True
-        even = [root]
-        oddy = []
+        isOddRow = True
+        stack = [root]    
         
-        while len(even) != 0 or len(oddy) != 0:
-            row = []
-            if isEven:
-                isEven = False
-                oddy = []
-                for e in reversed(even):
-                    row.append(e.val)
-                    if e.left  != None: oddy.append(e.left)
-                    if e.right != None: oddy.append(e.right)
-            else:
-                isEven = True
-                even = []
-                for e in reversed(oddy):
-                    row.append(e.val)
-                    if e.right != None: even.append(e.right)
-                    if e.left  != None: even.append(e.left)                   
-            if len(row) != 0: ans.append(row)
+        while stack:
+            outputRow = []
+            nextStack = []
+            while stack:
+                n = stack.pop()
+                outputRow.append(n.val)
+                    
+                if isOddRow:
+                    # Oddy row nodes will put left child in first  
+                    if n.left:
+                        nextStack.append(n.left)
+                    if n.right:
+                        nextStack.append(n.right)
+                else:
+                    if n.right:
+                        nextStack.append(n.right)
+                    if n.left:
+                        nextStack.append(n.left)
+                        
+            isOddRow = not isOddRow
+            stack = nextStack
+            if outputRow:
+                ans.append(outputRow)
+                
         return ans
