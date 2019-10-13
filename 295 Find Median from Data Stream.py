d09563fba1736ccbd2d24684778a6fac6b008a7f
Jun <junzhengrice@gmail.com>
2019-10-12 22:39:57 -0700

    Heap, Online

--- 295 Find Median from Data Stream.py
+++ 295 Find Median from Data Stream.py
@@ -1,41 +1,61 @@
-from heapq import heappush, heappop, heappushpop
 '''
-Find Median from Data Stream
-https://leetcode.com/problems/find-median-from-data-stream/
+295. Find Median from Data Stream
+
+Median is the middle value in an ordered integer list.
+If the size of the list is even, there is no middle value.
+So the median is the mean of the two middle value.
+
+For example,
+[2,3,4], the median is 3
+
+[2,3], the median is (2 + 3) / 2 = 2.5
+
+Design a data structure that supports the following two operations:
+
+void addNum(int num) - Add a integer number from the data stream to the data structure.
+double findMedian() - Return the median of all elements so far.
+ 
+Example:
+
+addNum(1)
+addNum(2)
+findMedian() -> 1.5
+addNum(3) 
+findMedian() -> 2
+ 
+Follow up:
+
+If all integer numbers from the stream are between 0 and 100, how would you optimize it?
+If 99% of all integer numbers from the stream are between 0 and 100, how would you optimize it?
 '''
+from heapq import heappush, heappop
+
 class MedianFinder:
-    def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.small = []
-        self.large = []        
 
-    def addNum(self, num):
-        """
-        Adds a num into the data structure.
-        :type num: int
-        :rtype: void
-        """
-        if len(self.small) == len(self.large):
-            heappush(self.large, -heappushpop(self.small,-num))
-        else: # len(self.small) + 1 == len(self.large):
-            heappush(self.small, -heappushpop(self.large, num))
+    def __init__(self):     
+        # Either these 2 heaps have same # of elements
+        # Or minHeap has 1 more element than maxHeap
+        self.maxHeap = [] # negatives
+        self.minHeap = [] 
 
-    def findMedian(self):
-        """
-        Returns the median of current data stream
-        :rtype: float
-        """
-        if len(self.small) == len(self.large):
-            return (float(self.large[0]) - float(self.small[0])) / 2.0
-        else: # len(self.small) + 1 == len(self.large):
-            return self.large[0]
+    def addNum(self, n: int) -> None:
+        maxHeap = self.maxHeap
+        minHeap = self.minHeap
+        
+        if len(maxHeap) == len(minHeap):
+            heappush(maxHeap, -n)
+            p = -heappop(maxHeap)
+            heappush(minHeap, p)           
+        else:
+            heappush(minHeap, n)
+            p = heappop(minHeap)
+            heappush(maxHeap, -p)
 
-
-obj = MedianFinder()
-obj.addNum(1)
-obj.addNum(2)
-print obj.findMedian()
-obj.addNum(3) 
-print obj.findMedian()
+    def findMedian(self) -> float:
+        maxHeap = self.maxHeap
+        minHeap = self.minHeap
+        
+        if len(maxHeap) == len(minHeap):
+            return float(-maxHeap[0] + minHeap[0]) / 2.0
+        else:
+            return float(minHeap[0])
