86f33b748154c585002c97c7aaed64b20dacc7a6
Jun <junzhengrice@gmail.com>
2019-10-28 18:28:19 -0700

    Binary Search

---
+++ T-1231 Divide Chocolate.py
@@ -0,0 +1,61 @@
+'''
+1231. Divide Chocolate
+https://leetcode.com/problems/divide-chocolate/
+
+You have one chocolate bar that consists of some chunks. Each chunk has its own sweetness given by the array sweetness.
+
+You want to share the chocolate with your K friends so you start cutting the chocolate bar into K+1 pieces using K cuts,
+each piece consists of some consecutive chunks.
+
+Being generous, you will eat the piece with the minimum total sweetness and give the other pieces to your friends.
+
+Find the maximum total sweetness of the piece you can get by cutting the chocolate bar optimally.
+
+ 
+
+Example 1:
+
+Input: sweetness = [1,2,3,4,5,6,7,8,9], K = 5
+Output: 6
+Explanation: You can divide the chocolate to [1,2,3], [4,5], [6], [7], [8], [9]
+
+Example 2:
+
+Input: sweetness = [5,6,7,8,9,1,2,3,4], K = 8
+Output: 1
+Explanation: There is only one way to cut the bar into 9 pieces.
+
+Example 3:
+
+Input: sweetness = [1,2,2,1,2,2,1,2,2], K = 2
+Output: 5
+Explanation: You can divide the chocolate to [1,2,2], [1,2,2], [1,2,2]
+'''
+class Solution:
+    def maximizeSweetness(self, sweetness: List[int], K: int) -> int:
+        '''
+        Similar method to 410 Split Array Largest Sum
+        '''
+        size = len(sweetness)        
+        left = min(sweetness) 
+        right = sum(sweetness)
+        res = left
+        while left <= right:
+            middle = left + ((right - left) >> 1);
+
+            # Count number of subarrays whose sum is >= middle
+            localSum = 0
+            counter = 0
+            for i in range(size):
+                localSum += sweetness[i]
+                if localSum >= middle:
+                    counter += 1
+                    localSum = 0
+
+            if counter >= K+1:
+                res = max(res, middle)
+                left = middle + 1
+            else:
+                right = middle - 1
+
+        return res
