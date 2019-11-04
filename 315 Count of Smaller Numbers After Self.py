f92f5450603493db7e3ef5693b30aaab5a44b68b
Jun <junzhengrice@gmail.com>
2019-11-04 11:08:09 -0800

    Mergesort

--- 315 Count of Smaller Numbers After Self.py
+++ 315 Count of Smaller Numbers After Self.py
@@ -1,10 +1,23 @@
-from copy import deepcopy
 '''
-Count of Smaller Numbers After Self
+315. Count of Smaller Numbers After Self
 https://leetcode.com/problems/count-of-smaller-numbers-after-self/
 
-Same problem as "Count Inversions"
+You are given an integer array nums and you have to return a new counts array. 
+The counts array has the property where counts[i] is the number of smaller elements to the right of nums[i].
+
+Example:
+
+Input: [5,2,6,1]
+Output: [2,1,1,0] 
+
+Explanation:
+To the right of 5 there are 2 smaller elements (2 and 1).
+To the right of 2 there is only 1 smaller element (1).
+To the right of 6 there is 1 smaller element (1).
+To the right of 1 there is 0 smaller element.
 '''
+from copy import deepcopy
+
 def brutal(arr):
     inv = [0] * len(arr)
     for i in xrange(len(arr)-1):
@@ -55,3 +68,60 @@ def countSmaller(nums):
             merge(arr,buf,inv,lt,mid,rt);
         stride <<= 1
     return inv
+
+'''
+New Python3 recursive code
+'''
+def merge(arr, left, mid, right, buf):
+    '''
+    Regular mergo for mergesort, merge from back.
+    left, mid is from left array.
+    mid+1, right is from right array
+    '''
+    i = mid
+    j = right
+    k = right
+    while i >= left and j > mid:
+        if arr[i][1] <= arr[j][1]:
+            buf[k] = arr[j]
+            k -= 1
+            j -= 1
+        else:
+            buf[k] = arr[i]
+            k -= 1
+            i -= 1
+    
+    # Put rest of right array
+    while j > mid:
+        buf[k] = arr[j]
+        k -= 1
+        j -= 1
+
+    # Copy back
+    arr[left : right+1] = buf[left : right+1]
+
+def mergesort(arr, left, right, buf, res):
+    if left >= right:
+        return
+
+    mid = left + (right - left) // 2
+    mergesort(arr, left, mid, buf, res)
+    mergesort(arr, mid + 1, right, buf, res)
+
+    # Count smaller numbers
+    midBegin = mid + 1
+    j = midBegin
+    for i in range(left, midBegin):
+        while j <= right and arr[i][1] > arr[j][1]:
+            j += 1
+        res[arr[i][0]] += j - midBegin
+
+    merge(arr, left, mid, right, buf);
+
+class Solution:
+    def countSmaller(self, nums: List[int]) -> List[int]:
+        arr = list(enumerate(nums)) # [(index, value)]
+        buf = deepcopy(arr)
+        res = [0] * len(arr)
+        mergesort(arr, 0, len(arr)-1, buf, res)
+        return res
