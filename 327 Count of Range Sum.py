f599cf512951551fc86605eecd83d635bcd8ffd5
Jun <junzhengrice@gmail.com>
2019-11-04 13:16:31 -0800

    Mergesort

--- 327 Count of Range Sum.py
+++ 327 Count of Range Sum.py
@@ -1,55 +1,46 @@
 '''
-Count of Range Sum
+327. Count of Range Sum
 https://leetcode.com/problems/count-of-range-sum/
-Need to handle int overflow to make it pass
+
+Given an integer array nums, return the number of range sums that lie in [lower, upper] inclusive.
+Range sum S(i, j) is defined as the sum of the elements in nums between indices i and j (i ≤ j), inclusive.
+
+Example:
+
+Input: nums = [-2,5,-1], lower = -2, upper = 2,
+Output: 3 
+Explanation: The three ranges are : [0,0], [2,2], [0,2] and their respective sums are: -2, -1, 2
 '''
-def upper_bound(arr,tag):
-    i = 0 
-    j = len(arr) - 1
-    while i < j:
-        m = (i + j >> 1)
-        if arr[m][0] > tag:
-            j = m - 1
-        else:
-            i = m + 1
-    return i if arr[i][0] > tag else i + 1
+def mergesort(arr, lower, upper, left, right):
+    if left >= right:
+        return 0
 
-def lower_bound(arr,tag):
-    i = 0
-    j = len(arr) - 1
-    while i < j:
-        m = (i + j >> 1)
-        if arr[m][0] < tag:
-            i = m + 1
-        else:
-            j = m - 1
-    return i if arr[i][0] >= tag else i + 1
+    mid = left + (right - left) // 2
+    count = mergesort(arr, lower, upper, left, mid) + mergesort(arr, lower, upper, mid + 1, right)
 
-def countRangeSum(nums, lower, upper):
-    """
-    :type nums: List[int]
-    :type lower: int
-    :type upper: int
-    :rtype: int
-    """
-    arr = [(0,-1)]
-    ac = 0
-    for i,e in enumerate(nums):
-        ac += e
-        arr.append((ac,i))
-    arr.sort()
+    # Count of range sum
+    midBegin = mid + 1
+    lo, hi = midBegin, midBegin
+    for e in arr[left : midBegin]:
+        # This is to count how many sum ranges smaller than lower
+        while lo <= right and arr[lo] - e < lower:
+            lo += 1
 
-    ctr = 0
-    for ac,i in arr:
-        lt = lower_bound(arr,ac+lower)
-        rt = upper_bound(arr,ac+upper)
-        for j in xrange(lt,rt):
-            if arr[j][1] < i:
-                ctr +=1
-    return ctr
+        # This is to count how many sum ranges smaller or equal to upper
+        while hi <= right and arr[hi] - e <= upper:
+            hi += 1
 
-nums = [-2,5,-1];print(nums)
-lower = -2
-upper = 2
+        count += hi - lo
 
-print countRangeSum(nums,lower,upper)
+    # Use a lasy simple sort to merge
+    arr[left : right+1] = sorted(arr[left : right+1])
+    return count
+
+class Solution:
+    def countRangeSum(self, nums: List[int], lower: int, upper: int) -> int:
+        # Build accumulation array
+        arr = [0] * (len(nums)+1)
+        for i in range(len(nums)):
+            arr[i+1] = arr[i] + nums[i]
+
+        return mergesort(arr, lower, upper, 0, len(arr)-1)
