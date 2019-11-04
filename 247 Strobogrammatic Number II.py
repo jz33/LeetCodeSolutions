550d50fd17056da3aa5686d6ba75b24db4dcf0c8
Jun <junzhengrice@gmail.com>
2019-11-03 23:33:51 -0800

    Math, Dynamic Programming

---
+++ 247 Strobogrammatic Number II.py
@@ -0,0 +1,23 @@
+'''
+247. Strobogrammatic Number II
+
+A strobogrammatic number is a number that looks the same when rotated 180 degrees (looked at upside down).
+Find all strobogrammatic numbers that are of length = n.
+'''
+class Solution:
+    def findStrobogrammatic(self, n: int) -> List[str]:
+        isEven = (n & 1) == 0
+        base = [''] if isEven else ['0','1','8']    
+        start = 2 if isEven else 3
+        res = base
+        for _ in range(start,n+1,2):
+            newBase = []
+            newRes = []
+            for s in base:
+                for a, b in [('0','0'), ('1','1'), ('6','9'), ('8','8'), ('9','6')]:
+                    newBase.append(a + s + b)
+                for a, b in [('1','1'), ('6','9'), ('8','8'), ('9','6')]:
+                    newRes.append(a + s + b)
+            base = newBase
+            res = newRes
+        return res
