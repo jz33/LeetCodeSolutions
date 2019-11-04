4b5772665947ebbbc45cd02b2b4922809c2d0a4f
Jun <junzhengrice@gmail.com>
2019-11-04 13:28:21 -0800

    Binary Search, Greedy

--- 354 Russian Doll Envelopes.py
+++ 354 Russian Doll Envelopes.py
@@ -17,52 +17,40 @@ Input: [[5,4],[6,4],[6,7],[2,3]]
 Output: 3 
 Explanation: The maximum number of envelopes you can Russian doll is 3 ([2,3] => [5,4] => [6,7]).
 '''
-class Solution:
-    def getInsertionPoint(self, arr: List[int], tag: int, left: int, right: int) -> int:
-        '''
-        Find insertion point of tag in arr, arr is ascendingly sorted.
-        '''
-        while left <= right:
-            mid = left + ((right - left) >> 1)
-            if arr[mid] == tag:
-                return mid
-
-            if mid == left:
-                # left = right - 1 or left == right
-                if tag < arr[left]:
-                    return left;
-                elif tag > arr[left] and tag <= arr[right]:
-                    return right;
-                else: # tag > arr[right]
-                    return right + 1;
-
-            if arr[mid] < tag:
-                left = mid + 1
-            else:
-                right = mid - 1
-
-        return 0 # Reachable when len(arr) = 0
+def searchInsert(arr: List[int], tag: int) -> int:
+    left = 0
+    right = len(arr) - 1       
+    while left <= right:
+        mid = left + ((right - left) >> 1)
+        if arr[mid] == tag:
+            return mid
+        elif arr[mid] < tag:
+            left = mid + 1
+        else:
+            right = mid - 1      
+    return left
     
+class Solution:
     def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
-        # Sort by env[0] ascending and env[1] DESCENDING
-        # And this problem reduces to Longest Increasing Subsequence problem
-        # Why DESCENDING? Think example like [5, 8], [5, 10]
+        # Sort by env[0] ascending and env[1] descending, 
+        # and this problem reduces to Longest Increasing Subsequence problem
+        # Why ascending then descending? Think example like [5, 8], [5, 10]
         # If consider [5, 8] first, 8 is added; when considering [5, 10]
         # 10 will be added after 8, but [5, 10]  cannot cover [5, 8] 
         envelopes.sort(key = lambda x : (x[0], -x[1]))
 
-        # graph is ascendingly sorted env[1]
-        graph = []
+        # arr is ascendingly sorted by env[1]
+        arr = []
         for env in envelopes:
             tag = env[1]
-            ip = self.getInsertionPoint(graph, tag, 0, len(graph)-1);
+            ip = searchInsert(arr, tag)
 
-            if ip > len(graph)-1:
-                graph.append(tag)
+            if ip >= len(arr):
+                arr.append(tag)
             else:
-                graph[ip] = tag
+                arr[ip] = tag
 
-        return len(graph)
+        return len(arr)
         
 # More examples
 ls = [[5,4],[6,4],[6,7],[2,3]] # 3
