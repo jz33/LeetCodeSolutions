ae92ddb2e541656b86b0f4b700421aa1f494356c
Jun <junzhengrice@gmail.com>
2019-11-11 11:44:11 -0800

    Kadane

--- 053 Maximum Subarray.c
+++ 053 Maximum Subarray.c
@@ -9,6 +9,7 @@ Example:
 
 Input: [-2,1,-3,4,-1,2,1,-5,4],
 Output: 6
+
 Explanation: [4,-1,2,1] has the largest sum = 6.
 */
 /*
@@ -32,7 +33,8 @@ int kadaneMax(int* arr, int size)
         if(global < local) global = local;
     }
     return global;
-}  
+}
+
 int kadaneMin(int* arr, int size)
 {
     int local, global, i;
