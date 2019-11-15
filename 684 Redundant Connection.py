37941188c581507a181865b5dd08bef2b9e14289
Jun <junzhengrice@gmail.com>
2019-11-14 23:08:09 -0800

    Union Find on Undirected Graph

--- 684 Redundant Connection.py
+++ 684 Redundant Connection.py
@@ -4,11 +4,16 @@ https://leetcode.com/problems/redundant-connection/
 
 In this problem, a tree is an undirected graph that is connected and has no cycles.
 
-The given input is a graph that started as a tree with N nodes (with distinct values 1, 2, ..., N), with one additional edge added. The added edge has two different vertices chosen from 1 to N, and was not an edge that already existed.
+The given input is a graph that started as a tree with N nodes (with distinct values 1, 2, ..., N),
+with one additional edge added. The added edge has two different vertices chosen from 1 to N,
+and was not an edge that already existed.
 
-The resulting graph is given as a 2D-array of edges. Each element of edges is a pair [u, v] with u < v, that represents an undirected edge connecting nodes u and v.
+The resulting graph is given as a 2D-array of edges.
+Each element of edges is a pair [u, v] with u < v, that represents an undirected edge connecting nodes u and v.
 
-Return an edge that can be removed so that the resulting graph is a tree of N nodes. If there are multiple answers, return the answer that occurs last in the given 2D-array. The answer edge [u, v] should be in the same format, with u < v.
+Return an edge that can be removed so that the resulting graph is a tree of N nodes.
+If there are multiple answers, return the answer that occurs last in the given 2D-array.
+The answer edge [u, v] should be in the same format, with u < v.
 
 Example 1:
 Input: [[1,2], [1,3], [2,3]]
@@ -30,14 +35,12 @@ Every integer represented in the 2D-array will be between 1 and N, where N is th
 '''
 class UnionFind:
     def __init__(self, nodeCount: int):
-        # The Union-Find graph, where
-        # node[i] points to the root of node i+1
-        # nodes[0] is unused
+        # The Union-Find graph, where node[i] points to its parent
+        # (not necessisarily its root). The nodes[0] is unused
         self.nodes = [None] * (nodeCount + 1)
     
     def GetRoot(self, nodeId: int) -> int:
         nodes = self.nodes        
-        # Trace to the root
         while nodes[nodeId]:
             nodeId = nodes[nodeId]
         return nodeId
@@ -46,7 +49,6 @@ class UnionFind:
         '''
         The left and right are 2 node ids that together they represent an edge
         Union is to connect the root of left and right
-        Root node id should be equal or smaller child id
         Return true if unioned, i.e., left and right are NOT already connected
         '''
         left_root = self.GetRoot(left)
@@ -55,21 +57,16 @@ class UnionFind:
         if left_root == right_root:
             # If left and right share same root, they are already connected
             return False
-        
-        nodes = self.nodes
-        
+                
         # Union, connect the 2 roots
         if left_root < right_root:
-            nodes[right_root] = left_root
+            self.nodes[right_root] = left_root
         else:
-            nodes[left_root] = right_root
+            self.nodes[left_root] = right_root
         return True
                 
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        '''
-        Union Find
-        '''
         redundant = None
         graph = UnionFind(len(edges))
         for edge in edges:
