1cffe3692023daf7fcd91f18cf444cc0b55f520c
Jun <junzhengrice@gmail.com>
2019-11-21 00:15:21 -0800

    Constant State Dynamic Programming

--- 032 Longest Valid Parentheses.py
+++ 032 Longest Valid Parentheses.py
@@ -1,56 +1,46 @@
-import random
 '''
 32 Longest Valid Parentheses
 https://leetcode.com/problems/longest-valid-parentheses/description/
-O(n), 2 paths
+
+Given a string containing just the characters '(' and ')',
+find the length of the longest valid (well-formed) parentheses substring.
+
+Example 1:
+
+Input: "(()"
+Output: 2
+Explanation: The longest valid parentheses substring is "()"
+
+Example 2:
+
+Input: ")()())"
+Output: 4
+Explanation: The longest valid parentheses substring is "()()"
 '''
-# generate a random Parentheses string with size n
-def randParentheses(n):
-    return ''.join(random.choice('('+')') for _ in range(n))
- 
-def longestValidParentheses(src):
-    if len(src) < 2: return 0
-    res = 0
-    '''
-    Notations for 'buf' :
-    '0' : currently paired
-    >0  : num of '(' - num of ')'
-    <0  : invalid (abort)
-    '''
-    buf = 0        
-    s = 0 # start index
-    for i in xrange(len(src)):
-        if src[i] == '(':
-            buf += 1
-        else:
-            buf -= 1
-            if buf == 0:
-                res = max(res, i - s + 1)
-            elif buf < 0: # reset
-                buf = 0
-                s = i + 1
-    '''
-    Think the exmaple of (()( or )()) to understand why it needs 2 paths
-    '''
-    buf = 0
-    s = len(src) - 1       
-    for i in xrange(len(src)-1, -1, -1):
-        if src[i] == ')':
-            buf += 1
-        else:
-            buf -= 1
-            if buf == 0:
-                res = max(res, s - i + 1)
-            elif buf < 0:
-                buf = 0
-                s = i - 1                 
-    return res
-       
-def main():
-    repeat = 5
-    for i in xrange(repeat):
-        sent = randParentheses(10)
-        print sent, longestValidParentheses(sent)
-     
-if __name__ == "__main__":
-    main()
+class Solution:
+    def OneDirection(self, src: str, leftBracket: str) -> int:
+        '''
+        In left to right order, @leftBracket is '('
+        In reversed order, @leftBracket is ')'
+        '''
+        longest = 0
+        start = 0
+        brackets = 0
+        for i, c in enumerate(src):
+            if c == leftBracket:
+                brackets += 1
+            else:
+                brackets -= 1
+        
+            if brackets == 0:
+                # Notice do not reset "start" here
+                longest = max(longest, i - start + 1)             
+            elif brackets < 0:
+                # Reset
+                brackets = 0
+                start = i + 1
+
+        return longest
+        
+    def longestValidParentheses(self, src: str) -> int:        
+        return max(self.OneDirection(src, '('), self.OneDirection(src[::-1], ')'))
