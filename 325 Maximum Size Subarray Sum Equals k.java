1813f6dbbfff826b423f03a7b8d221b19a6ed2cb
Jun <junzhengrice@gmail.com>
2019-11-23 18:11:49 -0800

    Hashmap on Array

--- 325 Maximum Size Subarray Sum Equals k.java
+++ 325 Maximum Size Subarray Sum Equals k.java
@@ -1,17 +1,35 @@
-import java.util.*;
 /*
-325 Maximum Size Subarray Sum Equals k
-https://leetcode.com/problems/maximum-size-subarray-sum-equals-k/
+325. Maximum Size Subarray Sum Equals k
+https://leetcode.com/problems/maximum-size-subarray-sum-equals-k/submissions/
+
+Given an array nums and a target value k, find the maximum length of a subarray that sums to k.
+If there isn't one, return 0 instead.
+
+Note:
+The sum of the entire nums array is guaranteed to fit within the 32-bit signed integer range.
+
+Example 1:
+
+Input: nums = [1, -1, 5, -2, 3], k = 3
+Output: 4 
+Explanation: The subarray [1, -1, 5, -2] sums to 3 and is the longest.
+Example 2:
+
+Input: nums = [-2, -1, 2, 1], k = 1
+Output: 2 
+Explanation: The subarray [-1, 2] sums to 1 and is the longest.
+Follow Up:
+Can you do it in O(n) time?
 */
-public class Solution {
+class Solution {
     public int maxSubArrayLen(int[] nums, int k) {
-        // sum : leftmost index
+        // The map stores {sum : leftmost index}
         Map<Integer,Integer> map = new HashMap<Integer,Integer>();
         map.put(0,-1);
-        int length = nums.length;
+
         int accu = 0;
         int maxlen = 0;
-        for(int i = 0;i<length;i++){
+        for (int i = 0;i<nums.length;i++) {
             accu += nums[i];
             maxlen = Math.max(maxlen, i - map.getOrDefault(accu - k,i));
             map.putIfAbsent(accu,i);
