7ec491a2aaeda4f61e9bd1a90a15be0109793120
Jun <junzhengrice@gmail.com>
2019-11-28 11:39:58 -0800

    KMP + Brutal Force

--- 214 Shortest Palindrome.py
+++ 214 Shortest Palindrome.py
@@ -1,27 +1,67 @@
 '''
-Shortest Palindrome
+214. Shortest Palindrome
 https://leetcode.com/problems/shortest-palindrome/
+
+Given a string s, you are allowed to convert it to a palindrome by adding characters in front of it.
+Find and return the shortest palindrome you can find by performing this transformation.
+
+Example 1:
+
+Input: "aacecaaa"
+Output: "aaacecaaa"
+
+Example 2:
+
+Input: "abcd"
+Output: "dcbabcd"
 '''
-def shortestPalindrome(ori):
-    rev = ori[::-1]
-    ndl = ori + '#' + rev;
+def kmpTable(needle: str) -> List[str]:
+    # T[i] is the length of the longest proper prefix of ndl[:i+1]
+    # which is also the suffix of ndl[:i+1]
+    # T[0] is always 0
+    n_len = len(needle)
+    T = [0] * n_len
     
-    #build KMP table
-    T = [0] * len(ndl)
-    T[0] = -1
-    i = 0
-    j = 2
-    while j < len(ndl):
-        if ndl[j-1] == ndl[i]:
+    ll = 0; # The length of the previous longest prefix suffix
+    i = 1; # Iterator of T
+    while i < n_len:
+        if needle[i] == needle[ll]:
+            # If current char matches ll-th char from beginning of needle,
+            # and because needle[:ll] == needle[i-ll:i],
+            # therefore needle[:ll+1] == needle[i-ll:i+1]
+            ll += 1
+            T[i] = ll;
             i += 1
-            T[j] = i
-            j += 1
-        elif i > 0:
-            i = T[i]
+        elif ll > 0:
+            # No match, try to reduce ll to see if matching
+            ll = T[ll-1];
         else:
-            T[j] = 0
-            j += 1
+            # No match no matter what, move on
+            # T[i] = 0, by default
+            i += 1
+
+    return T
+
+class Solution:
+    def shortestPalindrome(self, s: str) -> str:
+        if not s:
+            return s
+        
+        t = s + '#' + s[::-1]
+        kmp = kmpTable(t)
+
+        # By definition of KMP algorithm, kmp[-1] will be
+        # the length of longest prefix of twith equals the suffix of t
+        lp = kmp[-1]
+        return s[lp:][::-1] + s
     
-    print T
-    # return
-    return rev[:len(ori) - (T[-1]+1)] + ori; 
+class Solution:
+    def shortestPalindrome(self, s: str) -> str:
+        if not s:
+            return s
+        
+        rev = s[::-1]
+        for i in range(len(s)):
+            if s.startswith(rev[i:]):
+                break
+        return rev[:i] + s 
