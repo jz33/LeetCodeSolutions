6b20a8346de9a4f52a88adb07058ebd4a671ff48
Jun <junzhengrice@gmail.com>
2019-12-01 14:08:17 -0800

    Sliding Window, Deque

--- 239 Sliding Window Maximum.py
+++ 239 Sliding Window Maximum.py
@@ -21,28 +21,24 @@ Window position                Max
  1  3  -1  -3 [5  3  6] 7       6
  1  3  -1  -3  5 [3  6  7]      7
 '''
-from collections import deque
-
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        if len(nums) == 0: return []
-        res = [None] * (len(nums) - k + 1)
-        
-        # dq is descending, means dq[0] is the max
-        dq = deque()
-        for i,e in enumerate(nums):
-            # Build the desceding deque
-            while len(dq) > 0 and nums[i] > nums[dq[-1]]:
+        dq = collections.deque()
+        res = []
+         
+        for i,c in enumerate(nums):
+            # dq is descending, dq[0] is the max of current substring
+            while dq and c > nums[dq[-1]]:
                 dq.pop()
-
-            # Pop left the out of bound indexes
-            while len(dq) > 0 and dq[0] <= i - k:
+            
+            # pop the out of bound indexes
+            # i-k+1 is the smallest index of current substring
+            while dq and i-k+1 > dq[0]:
                 dq.popleft()
-
+                
             dq.append(i)
             
-            if i >= k - 1:
-                res[i - k + 1] = dq[0]
-     
-        res[-1] = dq[0]
-        return [nums[i] for i in res]
+            if i-k+1 >= 0:
+                res.append(nums[dq[0]])
+                
+        return res
