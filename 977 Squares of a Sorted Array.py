177dbe798334992426b038f2cd66ef5216b24349
Jun <junzhengrice@gmail.com>
2019-12-02 14:49:53 -0800

    Mergesort

---
+++ 977 Squares of a Sorted Array.py
@@ -0,0 +1,56 @@
+'''
+977. Squares of a Sorted Array
+https://leetcode.com/problems/squares-of-a-sorted-array/
+
+Given an array of integers A sorted in non-decreasing order, return an array of the squares of each number,
+also in sorted non-decreasing order.
+
+Example 1:
+
+Input: [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+Example 2:
+
+Input: [-7,-3,2,3,11]
+Output: [4,9,9,49,121]
+ 
+Note:
+
+1 <= A.length <= 10000
+-10000 <= A[i] <= 10000
+A is sorted in non-decreasing order.
+'''
+class Solution:
+    def sortedSquares(self, A: List[int]) -> List[int]:
+        if not A:
+            return []
+        
+        # Find the minimum absolute value element
+        mm = min(range(len(A)), key = lambda i : abs(A[i]))
+ 
+        res = [A[mm]**2]
+        i = mm - 1
+        j = mm + 1
+        while i > -1 and j < len(A):
+            si = A[i]**2
+            sj = A[j]**2
+            if si == sj:
+                res += [si, sj]
+                i -= 1
+                j += 1
+            elif si < sj:
+                res.append(si)
+                i -= 1
+            else:
+                res.append(sj)
+                j += 1
+            
+        while i > -1:
+            res.append(A[i]**2)
+            i -= 1
+            
+        while j < len(A):
+            res.append(A[j]**2)
+            j += 1
+            
+        return res
