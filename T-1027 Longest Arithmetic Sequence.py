aff8a1b628706b0a0d73d0e3487dba2582b4e450
Jun <junzhengrice@gmail.com>
2019-12-02 15:20:48 -0800

    Dynamic Programming

---
+++ T-1027 Longest Arithmetic Sequence.py
@@ -0,0 +1,41 @@
+'''
+1027. Longest Arithmetic Sequence
+
+Given an array A of integers, return the length of the longest arithmetic subsequence in A.
+
+Recall that a subsequence of A is a list A[i_1], A[i_2], ..., A[i_k] with 0 <= i_1 < i_2 < ... < i_k <= A.length - 1,
+and that a sequence B is arithmetic if B[i+1] - B[i] are all the same value (for 0 <= i < B.length - 1).
+
+Example 1:
+
+Input: [3,6,9,12]
+Output: 4
+Explanation: 
+The whole array is an arithmetic sequence with steps of length = 3.
+Example 2:
+
+Input: [9,4,7,2,10]
+Output: 3
+Explanation: 
+The longest arithmetic subsequence is [4,7,10].
+Example 3:
+
+Input: [20,1,15,3,10,5,8]
+Output: 4
+Explanation: 
+The longest arithmetic subsequence is [20,15,10,5].
+'''
+from collections import Counter
+
+class Solution:
+    def longestArithSeqLength(self, A: List[int]) -> int:
+        dp = [Counter()] # list of counters {difference : count}
+        maxLength = 1
+        for i in range(1, len(A)):
+            cc = Counter()
+            for j in range(i):
+                diff = A[i] - A[j]
+                cc[diff] = dp[j].get(diff, 0) + 1
+            maxLength = max(maxLength, cc.most_common(1)[0][1] + 1)
+            dp.append(cc)        
+        return maxLength
