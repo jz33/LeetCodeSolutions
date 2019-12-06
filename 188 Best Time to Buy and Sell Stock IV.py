867754572c36563a61eff299c80b5ee480e18fc8
Jun <junzhengrice@gmail.com>
2019-12-06 14:48:50 -0800

    Dynamic Programming

--- 188 Best Time to Buy and Sell Stock IV.py
+++ 188 Best Time to Buy and Sell Stock IV.py
@@ -1,26 +1,51 @@
-from sys import maxint
 '''
-Best Time to Buy and Sell Stock IV
+188. Best Time to Buy and Sell Stock IV
 https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iv/
-Can complete at most k transactions
+
+Say you have an array for which the i-th element is the price of a given stock on day i.
+Design an algorithm to find the maximum profit. You may complete at most k transactions.
+
+Note:
+You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+
+Example 1:
+
+Input: [2,4,1], k = 2
+Output: 2
+Explanation: Buy on day 1 (price = 2) and sell on day 2 (price = 4), profit = 4-2 = 2.
+Example 2:
+
+Input: [3,2,6,5,0,3], k = 2
+Output: 7
+Explanation: Buy on day 2 (price = 2) and sell on day 3 (price = 6), profit = 6-2 = 4.
+             Then buy on day 5 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
 '''
-def largeCase(prices):
-    '''
-    When k >= len(prices) / 2
-    '''
-    profit = 0
-    for i in xrange(1,len(prices)):
-        if prices[i] > prices[i - 1]:
-            profit += prices[i] - prices[i - 1]
-    return profit
-    
-def maxProfit(k,prices):
-    if len(prices) < 2 or k <= 0: return 0
-    if k >= len(prices) / 2 : return largeCase(prices)
-    sold = [0]*(k+1)
-    bought = [-maxint-1]*(k+1)
-    for p in prices:
-        for j in xrange(k,0,-1):
-            sold[j] = max(sold[j], bought[j] + p)
-            bought[j] = max(bought[j], sold[j-1] - p)
-    return sold[k]
+class Solution:
+    def largeCase(self, prices: List[int]) -> int:
+        '''
+        For case when k is more than half of days
+        '''
+        profit = 0
+        for i in range(1,len(prices)):
+            if prices[i] > prices[i - 1]:
+                profit += prices[i] - prices[i - 1]
+        return profit
+
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        if len(prices) < 2 or k <= 0:
+            return 0
+
+        if k >= len(prices) // 2 :
+            return self.largeCase(prices)
+
+        '''
+        @sells[i] is the largest gain when doing i times sell
+        @buys[i] is the largest gain when doing i times buy
+        '''
+        sells = [0] * (k + 1)
+        buys = [float('-inf')] * (k + 1)
+        for p in prices:
+            for j in range(k, 0, -1):
+                sells[j] = max(sells[j], buys[j] + p)
+                buys[j] = max(buys[j], sells[j-1] - p)
+        return sells[k]
