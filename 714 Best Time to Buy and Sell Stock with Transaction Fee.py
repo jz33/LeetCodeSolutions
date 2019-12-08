1a6e954b61ae743bba7be608948663449b990d67
Jun <junzhengrice@gmail.com>
2019-12-07 17:33:08 -0800

    Constant State Dynamic Programming

---
+++ 714 Best Time to Buy and Sell Stock with Transaction Fee.py
@@ -0,0 +1,34 @@
+'''
+714. Best Time to Buy and Sell Stock with Transaction Fee
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/
+
+Your are given an array of integers prices, for which the i-th element is the price of a given stock on day i;
+and a non-negative integer fee representing a transaction fee.
+
+You may complete as many transactions as you like, but you need to pay the transaction fee for each transaction.
+You may not buy more than 1 share of a stock at a time (ie. you must sell the stock share before you buy again.)
+
+Return the maximum profit you can make.
+
+Example 1:
+
+Input: prices = [1, 3, 2, 8, 4, 9], fee = 2
+Output: 8
+Explanation: The maximum profit can be achieved by:
+Buying at prices[0] = 1
+Selling at prices[3] = 8
+Buying at prices[4] = 4
+Selling at prices[5] = 9
+The total profit is ((8 - 1) - 2) + ((9 - 4) - 2) = 8.'
+'''
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:   
+        # sells[i] = max(sells[i-1], buys[i-1] + p - fee)
+        # buys[i] = max(buys[i-1], sells[i-1] - p)
+        sells = [0] * 2
+        buys = [float('-inf')] * 2
+        for i, p in enumerate(prices):
+            j = i % 2
+            sells[j] = max(sells[j-1], buys[j-1] + p - fee)
+            buys[j] = max(buys[j-1], sells[j-1] - p)
+        return sells[(len(prices)-1) % 2]
