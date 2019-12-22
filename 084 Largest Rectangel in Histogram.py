e0489d7695a4eede016a4aa9b59b8368b9c2d085
Jun <junzhengrice@gmail.com>
2019-12-21 22:55:46 -0800

    Stack, O(n)

--- 084 Largest Rectangel in Histogram.py
+++ 084 Largest Rectangel in Histogram.py
@@ -1,49 +1,21 @@
 '''
 84 Largest Rectangle in Histogram
-https://oj.leetcode.com/problems/largest-rectangle-in-histogram/
+https://leetcode.com/company/google/
 http://www.geeksforgeeks.org/largest-rectangle-under-histogram/
 '''
-# O(n^2), a normal approach
-def largestRectangleArea(ls):
-    curr = ls[0]
-    maxArea = curr
-    prev = curr
-    for i in range(1,len(ls)):
-        curr = ls[i]
-        if curr == prev :
-            continue
-        lt = i - 1
-        while lt > -1 and ls[lt] >= curr:
-            lt -= 1
-        rt = i + 1
-        while rt < len(ls) and ls[rt] >= curr:
-            rt += 1
-        prev = curr
-        maxArea = max((rt-lt-1)*curr,maxArea)
-    return maxArea
-
-# O(n), use a stack
-def largestRectangleAreaStack(ls):
-    ls = [0] + ls + [0]
-    stack = [0]
-    maxArea = 0
-    for i, v in enumerate(ls):
-        while v < ls[stack[-1]]:
-            h = ls[stack.pop()]
-            maxArea = max(maxArea, (i-1-stack[-1])*h)
-        stack.append(i)
-    return maxArea
-
-def main():
-    ls = [2,1,5,6,2,3]
-    ls = [2,1,5,6,2,3,0]
-    ls = [3,3,2,2]
-    ls = [1,2,3,4]
-    #ls = randInList(12,0,10)
-    
-    print ls
-    print largestRectangleArea(ls)
-    print largestRectangleAreaStack(ls)
-        
-if __name__ == "__main__":
-    main()
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:        
+        heights = [0] + heights + [0] # just to make code simpler
+        stack = [0] # record indexes, where heights are increasing
+        maxRec = 0
+        for i, h in enumerate(heights):
+            while h < heights[stack[-1]]:
+                j = stack.pop()
+                recHeight = heights[j]
+                # recHeight is the target rectangle's height, 
+                # its right is before i, its left is after stack[-1], 
+                # all heights between stack[-1] and j (even not in stack)
+                # should be equal or larger than recHeight
+                maxRec = max(maxRec, (i-1-stack[-1]) * recHeight)
+            stack.append(i)
+        return maxRec
