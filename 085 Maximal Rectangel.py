71ffdb4d463627e8e527f686b60bb6e781eba052
Jun <junzhengrice@gmail.com>
2019-12-21 23:00:27 -0800

    Stack, O(N^2)

--- 085 Maximal Rectangel.py
+++ 085 Maximal Rectangel.py
@@ -1,49 +1,54 @@
 '''
 85 Maximal Rectangle
-https://oj.leetcode.com/problems/maximal-rectangle/
-Inspired by "Largest Rectangle in Histogram"
+https://leetcode.com/problems/maximal-rectangle
+
+Given a 2D binary matrix filled with 0's and 1's, find the largest rectangle containing only 1's and return its area.
+
+Example:
+
+Input:
+[
+  ["1","0","1","0","0"],
+  ["1","0","1","1","1"],
+  ["1","1","1","1","1"],
+  ["1","0","0","1","0"]
+]
+Output: 6
 '''
-# O(n), use a stack
-def largestRectangleInHistogram(ls):
-    ls = [0] + ls + [0]
-    stack = [0]
-    maxArea = 0
-    for i, v in enumerate(ls):
-        while v < ls[stack[-1]]:
-            h = ls[stack.pop()]
-            maxArea = max(maxArea, (i-1-stack[-1])*h)
-        stack.append(i)
-    return maxArea
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:        
+        heights = [0] + heights + [0] # just to make code simpler
+        stack = [0] # record indexes, where heights are increasing
+        maxRec = 0
+        for i, h in enumerate(heights):
+            while h < heights[stack[-1]]:
+                j = stack.pop()
+                recHeight = heights[j]
+                # recHeight is the target rectangle's height, 
+                # its right is before i, its left is after stack[-1], 
+                # all heights between stack[-1] and j (even not in stack)
+                # should be equal or larger than recHeight
+                maxRec = max(maxRec, (i-1-stack[-1]) * recHeight)
+            stack.append(i)
+        return maxRec
 
-def largestRectangleInMatrix(mat,rowSize, colSize):
-    ls = [0] * colSize
-    maxArea = 0
-    for i in range(0,rowSize):
-        for j in range(0,colSize):
-            ls[j] = 0 if mat[i][j] == '0' else ls[j] + 1
-        maxArea = max(maxArea, largestRectangleInHistogram(ls))
-    return maxArea
-
-#API
-def maximalRectangle(mat):
-    rowSize = len(mat)
-    if rowSize == 0: return 0
-    colSize = len(mat[0])
-    if colSize == 0: return 0
-    return largestRectangleInMatrix(mat,rowSize,colSize)
+    def maximalRectangle(self, matrix: List[List[str]]) -> int:
+        '''
+        Cannot use DP method of 221. Maximal Square
+        Use 84 Largest Rectangle in Histogram.
+        '''
+        rowCount = len(matrix)
+        if not rowCount:
+            return 0
+        colCount = len(matrix[0])
+        if not colCount:
+            return 0
         
-def main():
-    
-    mat = [\
-          '000100',\
-          '011011',\
-          '011111',\
-          '111011',\
-          '110111',\
-          ]
-    
-    #mat = ['0']
-    print maximalRectangle(mat)
-    
-if __name__ == "__main__":
-    main()
+        heights = [0] * colCount  
+        maxArea = 0
+        for i in range(rowCount):
+            for j in range(colCount):
+                heights[j] = 0 if matrix[i][j] == '0' else heights[j] + 1
+            maxArea = max(maxArea, self.largestRectangleArea(heights))
+            
+        return maxArea
