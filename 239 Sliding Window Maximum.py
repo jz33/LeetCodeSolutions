12d064ef44b17c58655bc69a1e0875141914116e
Jun <junzhengrice@gmail.com>
2019-12-21 23:03:02 -0800

    Sliding Window, Monotonic Deque

--- 239 Sliding Window Maximum.py
+++ 239 Sliding Window Maximum.py
@@ -2,8 +2,10 @@
 239. Sliding Window Maximum
 https://leetcode.com/problems/sliding-window-maximum/
 
-Given an array nums, there is a sliding window of size k which is moving from the very left of the array to the very right.
-You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+Given an array nums, there is a sliding window of size k which is moving from
+the very left of the array to the very right.
+You can only see the k numbers in the window.
+Each time the sliding window moves right by one position.
 Return the max sliding window.
 
 Example:
