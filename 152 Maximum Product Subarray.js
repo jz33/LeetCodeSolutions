0a117b458b7d7d0123657f02bf43b32b53467d43
Jun <junzhengrice@gmail.com>
2019-12-24 22:03:23 -0800

    Kadane

--- 152 Maximum Product Subarray.js
+++ 152 Maximum Product Subarray.js
@@ -1,23 +1,45 @@
 /*
-152 Maximum Product Subarray
-https://oj.leetcode.com/problems/maximum-product-subarray/
+152. Maximum Product Subarray
+https://leetcode.com/problems/maximum-product-subarray
+
+Given an integer array nums, find the contiguous subarray within an array
+(containing at least one number) which has the largest product.
+
+Example 1:
+
+Input: [2,3,-2,4]
+Output: 6
+Explanation: [2,3] has the largest product 6.
+
+Example 2:
+
+Input: [-2,0,-1]
+Output: 0
+Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
 */
 /**
  * @param {number[]} nums
  * @return {number}
  */
-var maxProduct = function(arr) {
-    if(arr.length === 0) return 0;
+var maxProduct = function(nums) {
+    if (nums.length === 0) {
+        return 0;
+    }
     
-    var vMax = arr[0], vMin = arr[0], prev_vMax = 0,r = arr[0];
+    var maxVal = nums[0];
+    var minVal = nums[0];
+    var res = nums[0];
     
-    arr.forEach(function(v,i,a){
-        if(i > 0){
-            prev_vMax = vMax;
-            vMax = Math.max(v, v * vMax, v * vMin);
-            vMin = Math.min(v, v * prev_vMax, v * vMin);
-            r = Math.max(r,vMax);
+    var nextMaxVal, nextMinVal;
+    nums.forEach(function(v, i) {
+        if(i > 0) {
+            nextMaxVal = Math.max(v, v * maxVal, v * minVal);
+            nextMinVal = Math.min(v, v * maxVal, v * minVal);
+            maxVal = nextMaxVal;
+            minVal = nextMinVal;
+            res = Math.max(res, maxVal);
         }
     });
-    return r;
+    
+    return res;
 };
