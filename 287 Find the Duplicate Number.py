d3ce295152790c33a27f50aecf084abf524eb8c2
Jun <junzhengrice@gmail.com>
2019-12-29 00:20:15 -0800

    Linked List Cycle

--- 287 Find the Duplicate Number.py
+++ 287 Find the Duplicate Number.py
@@ -1,29 +1,49 @@
 '''
-Find the Duplicate Number
+287. Find the Duplicate Number
 https://leetcode.com/problems/find-the-duplicate-number/
-http://keithschwarz.com/interesting/code/?dir=find-duplicate
+
+Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive),
+prove that at least one duplicate number must exist.
+Assume that there is only one duplicate number, find the duplicate one.
+
+Example 1:
+
+Input: [1,3,4,2,2]
+Output: 2
+
+Example 2:
+
+Input: [3,1,3,4,2]
+Output: 3
+Note:
+
+You must not modify the array (assume the array is read only).
+You must use only constant, O(1) extra space.
+Your runtime complexity should be less than O(n2).
+There is only one duplicate number in the array, but it could be repeated more than once.
 '''
-def findIntersection(arr):
-    if len(arr) == 0 : return -1
-    slow = 0
-    fast = 0
-    maxLoop = len(arr) * 2
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        '''
+        Use the idea of 142. Linked List Cycle II
+        Because there is 1 duplcate, there must be a cycle
+        '''
+        slow = 0
+        fast = 0
+        maxLoopCount = len(nums) * 2
 
-    for i in xrange(0,maxLoop):
-        slow = arr[slow]
-        fast = arr[fast]
-        fast = arr[fast]
-        if slow == fast:
-            break
+        for _ in range(maxLoopCount):
+            slow = nums[slow]
+            fast = nums[fast]
+            fast = nums[fast]
+            if slow == fast:
+                break
 
-    find = 0
-    for i in xrange(0,maxLoop):
-        slow = arr[slow]
-        find = arr[find]
-        if slow == find:
-            return find
-    return -1
-
-arr = [1,2,4,4,3]
-f = findIntersection(arr)
-print f
+        dupl = 0
+        for _ in range(maxLoopCount):
+            slow = nums[slow]
+            dupl = nums[dupl]
+            if slow == dupl:
+                return dupl
+            
+        return -1
