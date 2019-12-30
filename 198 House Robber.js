125e4781fb83970b74245b358c13dd9afaf4c257
Jun <junzhengrice@gmail.com>
2019-12-30 11:52:24 -0800

    Constant State Dynamic Programming

--- 198 House Robber.js
+++ 198 House Robber.js
@@ -1,36 +1,38 @@
-/*
-House Robber
+'''
+198. House Robber
 https://leetcode.com/problems/house-robber/
-*/
-/**
- * @param {number[]} nums
- * @return {number}
- */
-var rob = function(nums) {
-    var arrayLength = nums.length;
-    
-    if(arrayLength === 0) return 0;
-    if(arrayLength === 1) return nums[0];
-    if(arrayLength === 2) return Math.max(nums[0],nums[1]);
-    
-    var three_steps = nums[0]; // max at i - 3
-    var two_steps = Math.max(nums[0],nums[1]); // max at i - 2
-    var one_step = Math.max(nums[0]+nums[2],nums[1]); // max at i - 1
-    
-    // max at i = 2
-    var maxIncome = one_step;
-    
-    for (var i = 3; i < arrayLength; i++) {
-        maxIncome = Math.max(maxIncome, Math.max(three_steps,two_steps) + nums[i]);
-        three_steps = two_steps;
-        two_steps = one_step;
-        one_step = maxIncome;
-    }
-    
-    return maxIncome;
-};
 
-var r = 0
-var arr = new Array(2,7,9,3,1)
-r = rob(arr)
-console.log(r);
+You are a professional robber planning to rob houses along a street.
+Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them
+is that adjacent houses have security system connected and it will automatically contact the police if
+    two adjacent houses were broken into on the same night.
+
+Given a list of non-negative integers representing the amount of money of each house,
+determine the maximum amount of money you can rob tonight without alerting the police.
+
+Example 1:
+
+Input: [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+             Total amount you can rob = 1 + 3 = 4.
+Example 2:
+
+Input: [2,7,9,3,1]
+Output: 12
+Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+             Total amount you can rob = 2 + 9 + 1 = 12.
+'''
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+  
+        dp = [nums[0], max(nums[0], nums[1]), None]
+        for i in range(2, len(nums)):
+            di = i % 3
+            dp[di] = max(dp[di-1], dp[di-2] + nums[i])
+            
+        return dp[(len(nums) - 1) % 3]
