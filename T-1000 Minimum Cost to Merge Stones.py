644dfc82c7ae91c7cb89c0a5433f2b8534b19707
Jun <junzhengrice@gmail.com>
2019-12-30 17:32:06 -0800

    Min/Max Dynamic Programming

--- T-1000 Minimum Cost to Merge Stones.py
+++ T-1000 Minimum Cost to Merge Stones.py
@@ -33,6 +33,9 @@ We merge [3, 8, 6] for a cost of 17, and we are left with [17].
 The total cost was 25, and this is the minimum possible.
 '''
 class Solution:
+    '''
+    A bottom-up solution
+    '''
     def mergeStones(self, stones: List[int], K: int) -> int:
         n = len(stones)
         if (n - 1) % (K - 1) != 0:
@@ -72,3 +75,61 @@ class Solution:
                     dp[left][right] += sumArray[right+1] - sumArray[left]
 
         return dp[0][n-1]
+
+class Solution:
+    '''
+    A Top-down solution
+    '''
+    def getSum(self, left: int, right: int):
+        '''
+        Return sum of stones in [left, right + 1]
+        '''
+        return self.accu[right+1] - self.accu[left]
+        
+    def topDown(self, left: int, right: int) -> int:
+        '''
+        Top down dynamic programming computing in range [left, right + 1]
+        So both indexes are inclusive
+        '''
+        if self.caches[left][right] is not None:
+            return self.caches[left][right]
+        
+        result = 0
+        width = right - left + 1
+        if width < self.K:
+            result = 0
+        elif width == self.K:
+            result = self.getSum(left, right)
+        else:
+            result = float('inf')
+
+            # Current result is the combination of 2 children results.
+            # i is the break point. Notice its increment is K - 1
+            for i in range(left, right, self.K - 1):
+                result = min(result, self.topDown(left, i) + self.topDown(i+1, right))
+
+            # If current subarray a form into a stone, add its cost
+            if (width - 1) % (self.K - 1) == 0:
+                result += self.getSum(left, right)       
+
+        self.caches[left][right] = result
+        return result
+    
+    def mergeStones(self, stones: List[int], K: int) -> int:
+        n = len(stones)
+        if (n - 1) % (K - 1) != 0:
+            return -1
+        
+        self.K = K
+        
+        # Prepare prefix sum query
+        accu = [0] * (n+1)
+        for i in range(n):
+            accu[i+1] = accu[i] + stones[i]
+        self.accu = accu
+        
+        # Prepare top-down dynamic programming caches
+        # caches[i][j] is the minimum cost from stone i to stone j inclusively
+        self.caches = [[None] * n for _ in range(n)]
+        
+        return self.topDown(0, n-1)
