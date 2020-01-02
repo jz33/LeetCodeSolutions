2d1851536bd86b93675d26e5b56a3760257c4f76
Jun <junzhengrice@gmail.com>
2020-01-02 12:23:56 -0800

    Math

--- 296 Best Meeting Point.py
+++ 296 Best Meeting Point.py
@@ -1,41 +1,47 @@
 '''
-Best Meeting Point
+296. Best Meeting Point
 https://leetcode.com/problems/best-meeting-point/
+
+A group of two or more people wants to meet and minimize the total travel distance.
+You are given a 2D grid of values 0 or 1, where each 1 marks the home of someone in the group.
+The distance is calculated using Manhattan Distance, where distance(p1, p2) = |p2.x - p1.x| + |p2.y - p1.y|.
+
+Example:
+
+Input: 
+
+1 - 0 - 0 - 0 - 1
+|   |   |   |   |
+0 - 0 - 0 - 0 - 0
+|   |   |   |   |
+0 - 0 - 1 - 0 - 0
+
+Output: 6 
+
+Explanation: Given three people living at (0,0), (0,4), and (2,2):
+             The point (0,2) is an ideal meeting point, as the total travel distance 
+             of 2+2+2=6 is minimal. So return 6.
 '''
-class Solution(object):
-    def minTotalDistance(self, grid):
-        """
-        :type grid: List[List[int]]
-        :rtype: int
-        """
-        R = len(grid)
-        if R == 0: return 0
-        C = len(grid[0])
+class Solution:
+    def minTotalDistance(self, grid: List[List[int]]) -> int:
+        if not grid or not grid[0]:
+            return 0
+        rowCount = len(grid)
+        colCount = len(grid[0])
         
-        # find all nodes
-        nodes = []
-        for i in xrange(R):
-            for j in xrange(C):
-                if grid[i][j] == 1:
-                    nodes.append((i,j))
+        # 1. Find all people
+        # Notice the iteration order, i is before j
+        people = [(i,j) for i in range(rowCount) for j in range(colCount) if grid[i][j] == 1]
+        if not people:
+            return 0
+                
+        # 2. Get middle row index
+        # Notice people coordinates are already sorted by row
+        mr = people[len(people) // 2][0]
         
-        # middle of rows
-        # nodes.sort(key = lambda x : x[0])
-        cr = nodes[len(nodes) / 2][0]
+        # 3. Get middle colunm index
+        people.sort(key = lambda x : x[1])
+        mc = people[len(people) // 2][1]
         
-        # middle of columns
-        nodes.sort(key = lambda x : x[1])
-        cc = nodes[len(nodes) / 2][1]
-        
-        dist = 0
-        for (x,y) in nodes:
-            dist += abs(x - cr) + abs(y - cc)
-        return dist
-        
-sol = Solution()
-grid = [
-    [1,0,1],
-    [0,0,0],
-    [0,1,0]
-    ]
-print sol.minTotalDistance(grid)
+        # 4. Sum
+        return sum(abs(i-mr) + abs(j-mc) for i,j in people)
