47f7fd860ee192db836509ad01293ac8775c0ac4
Jun <junzhengrice@gmail.com>
2020-01-04 11:34:04 -0800

    2-D Dynamic Programming, Matrix

--- 221 Maximal Square.py
+++ 221 Maximal Square.py
@@ -16,24 +16,21 @@ Output: 4
 '''
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
+        if not matrix or not matrix[0]:
+            return 0
+        
         rowCount = len(matrix)
-        if rowCount == 0:
-            return 0
         colCount = len(matrix[0])
-        if colCount == 0:
-            return 0
         
-        # dp[i][j] records the largest square ends in i, j
+        # dp[i][j] keeps the size of maximal square on (i,j)
         dp = [[0] * colCount for _ in range(rowCount)]
-        
-        maxLen = 0
         for i in range(rowCount):
             for j in range(colCount):
                 if matrix[i][j] == '1':
                     if i > 0 and j > 0:
-                        dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])+1
+                        dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
                     else:
                         dp[i][j] = 1
-                    maxLen = max(maxLen, dp[i][j])
         
-        return maxLen * maxLen
+        maxSize = max(dp[i][j] for i in range(rowCount) for j in range(colCount))
+        return maxSize * maxSize
