7a89df0b68c11f79fb3f654be6176fc9ae319526
Jun <junzhengrice@gmail.com>
2020-01-04 16:50:49 -0800

    Intervals

--- 253 Meeting Rooms II.py
+++ 253 Meeting Rooms II.py
@@ -15,29 +15,18 @@ Example 2:
 Input: [[7,10],[2,4]]
 Output: 1
 '''
-from heapq import heappush, heappop
-
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        '''
-        Essentially, this is to ask the most overlapped interval count in any given time
-        '''
-        # Sort meetings from start time
-        intervals.sort(key = lambda x : x[0])
+        points = []
+        for s, e in intervals:
+            points.append((s, 1))
+            points.append((e, -1))          
+        points.sort()
         
-        roomCount = 0
-        heap = [] # Heap of meeting end time
+        maxOverlapped = 0
+        overlapped = 0
+        for p in points:
+            overlapped += p[1]
+            maxOverlapped = max(maxOverlapped, overlapped)
         
-        for meeting in intervals:
-            # If current meeting starts after previous meeting,
-            # no need for new room
-            while len(heap) > 0 and meeting[0] >= heap[0]:
-                heappop(heap)
-                
-            heappush(heap, meeting[1])
-            
-            # The meetings in heap now all need there individual room,
-            # because they are overlapped          
-            roomCount = max(roomCount, len(heap))
-            
-        return roomCount
+        return maxOverlapped
