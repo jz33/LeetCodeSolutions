0964406cfd91ab92d70972c390d5e7e2246488f4
Jun <junzhengrice@gmail.com>
2020-01-04 16:44:09 -0800

    Trivial

--- 252 Meeting Rooms.py
+++ 252 Meeting Rooms.py
@@ -1,17 +1,24 @@
-# Definition for an interval.
-# class Interval(object):
-#     def __init__(self, s=0, e=0):
-#         self.start = s
-#         self.end = e
+'''
+252. Meeting Rooms
+https://leetcode.com/problems/meeting-rooms/
 
-class Solution(object):
-    def canAttendMeetings(self, intervals):
-        """
-        :type intervals: List[Interval]
-        :rtype: bool
-        """
-        intervals.sort(key = lambda tup : tup.start)
-        for i,v in enumerate(intervals):
-            if i > 0 and v.start < intervals[i-1].end:
+Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei),
+determine if a person could attend all meetings.
+
+Example 1:
+
+Input: [[0,30],[5,10],[15,20]]
+Output: false
+
+Example 2:
+
+Input: [[7,10],[2,4]]
+Output: true
+'''
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        intervals.sort()
+        for i, m in enumerate(intervals):
+            if i > 0 and m[0] < intervals[i-1][1]:
                 return False
         return True
