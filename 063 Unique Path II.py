67e87b79f2efca9cdb0f24caee7ff1c491319a4a
Jun <junzhengrice@gmail.com>
2020-01-06 23:18:43 -0800

    2-D Dynamic Programming

--- 063 Unique Path II.py
+++ 063 Unique Path II.py
@@ -1,18 +1,53 @@
 '''
-063 Unique Path II.py
-Shorter DP approach
+63. Unique Paths II
+https://leetcode.com/problems/unique-paths-ii/
+
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+The robot can only move either down or right at any point in time.
+The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+Now consider if some obstacles are added to the grids. How many unique paths would there be?
+
+An obstacle and empty space is marked as 1 and 0 respectively in the grid.
+
+Note: m and n will be at most 100.
+
+Example 1:
+
+Input:
+[
+  [0,0,0],
+  [0,1,0],
+  [0,0,0]
+]
+Output: 2
+
+Explanation:
+There is one obstacle in the middle of the 3x3 grid above.
+There are two ways to reach the bottom-right corner:
+1. Right -> Right -> Down -> Down
+2. Down -> Down -> Right -> Right
 '''
- def uniquePathsWithObstacles(obstacleGrid):
-      """
-      Try to understand why only 1-D buf is good enough
-      """
-      width = len(obstacleGrid[0])
-      buf = [0]*width
-      buf[0] = 1
-      for row in obstacleGrid:
-          for j in xrange(width):
-              if row[j] == 1:
-                  buf[j] = 0
-              elif j > 0:
-                  buf[j] += buf[j-1] # from left + up
-      return buf[width-1]
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        if not obstacleGrid or not obstacleGrid[0]:
+            return 0
+        
+        m = len(obstacleGrid)
+        n = len(obstacleGrid[0])
+        dp = [[0] * n for _ in range(m)]
+        
+        for i in range(m):
+            for j in range(n):
+                if obstacleGrid[i][j] == 0:
+                    if i == 0 and j == 0:
+                        dp[i][j] = 1
+                    elif i == 0:
+                        dp[i][j] = dp[i][j-1]
+                    elif j == 0:
+                        dp[i][j] = dp[i-1][j]
+                    else:
+                        dp[i][j] = dp[i][j-1] + dp[i-1][j]
+                        
+        return dp[-1][-1]
