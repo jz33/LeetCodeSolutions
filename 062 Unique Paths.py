7fba691e6690a7bdca3c488cbfdae59d8776c70a
Jun <junzhengrice@gmail.com>
2020-01-06 23:10:21 -0800

    Combinations

--- 062 Unique Paths.py
+++ 062 Unique Paths.py
@@ -1,10 +1,37 @@
-from math import factorial
 '''
-Unique Paths
+62. Unique Paths
 https://leetcode.com/problems/unique-paths/
+
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+The robot can only move either down or right at any point in time.
+The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+How many possible unique paths are there?
+
+Above is a 7 x 3 grid. How many possible unique paths are there?
+
+Note: m and n will be at most 100.
+
+Example 1:
+
+Input: m = 3, n = 2
+Output: 3
+
+Explanation:
+From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+1. Right -> Right -> Down
+2. Right -> Down -> Right
+3. Down -> Right -> Right
+
+Example 2:
+
+Input: m = 7, n = 3
+Output: 28
 '''
-def comb(up,dn):
-    return factorial(dn) / factorial(up) / factorial(dn - up)
-    
-def uniquePaths(self, m, n):
-    return comb(m-1,(m+n)-2);
+from math import factorial
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        # The result is a combination (m-1)|(m+n-2)
+        return factorial(m+n-2) // factorial(m-1) // factorial(n-1)
