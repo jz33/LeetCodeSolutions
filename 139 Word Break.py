544f2a327ada4c54e98d5e513bbfc12c960d2209
Jun <junzhengrice@gmail.com>
2020-01-12 15:43:29 -0800

    DFS, 2-D Dynamic Programming

--- 139 Word Break.py
+++ 139 Word Break.py
@@ -29,20 +29,41 @@ Input: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
 Output: false
 '''
 class Solution:
+    '''
+    A dfs solution
+    '''
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        if s in wordDict:
-            return True
+        if not wordDict:
+            return False
         
-        size = len(s)
+        book = set(wordDict)
+        maxSize = len(max(wordDict, key = len)) # max size of the word in dict
+        solutions = {} # solution cache of s[i:]
+        solutions[len(s)] = True
         
-        # dp [i] means whether s[:i] is composable
-        dp = [False] * (size + 1)
-        dp[0] = True
+        def dfs(i: int) -> bool:
+            if i not in solutions:
+                # to make sure s[i:j] in book, then j-i <= maxSize
+                solutions[i] = any(s[i:j] in book and dfs(j) for j in range(i+1, min(maxSize+i+1, len(s)+1)))              
+            return solutions[i]
         
-        for i in range(1, size+1):
-            for j in range(i):
-                dp[i] = dp[j] and s[j:i] in wordDict
-                if dp[i]:
-                    break
+        return dfs(0)
+
+
+class Solution:
+    '''
+    An iterative solution
+    '''
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        if not wordDict:
+            return False
         
+        book = set(wordDict)
+        maxSize = len(max(wordDict, key = len)) # max size of the word in dict
+        
+        # dp[i] means whether s[:i] is composable
+        dp = [True] + [False] * len(s)
+        for i in range(1, len(s)+1):
+            dp[i] = any(dp[j] and s[j:i] in book for j in range(max(0, i-maxSize), i))
+            
         return dp[-1]
