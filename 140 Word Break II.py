bbe14e1b1efceb8295e0cc16aaf6f72bbab06b7f
Jun <junzhengrice@gmail.com>
2020-01-12 15:24:31 -0800

    DFS, 2-D Dynamic Programming

--- 140 Word Break II.py
+++ 140 Word Break II.py
@@ -1,49 +1,104 @@
 '''
-Word Break II
+40. Word Break II
 https://leetcode.com/problems/word-break-ii/
+
+Given a non-empty string s and a dictionary wordDict containing a list of non-empty words,
+add spaces in s to construct a sentence where each word is a valid dictionary word.
+Return all such possible sentences.
+
+Note:
+
+The same word in the dictionary may be reused multiple times in the segmentation.
+You may assume the dictionary does not contain duplicate words.
+
+Example 1:
+
+Input:
+s = "catsanddog"
+wordDict = ["cat", "cats", "and", "sand", "dog"]
+Output:
+[
+  "cats and dog",
+  "cat sand dog"
+]
+
+Example 2:
+
+Input:
+s = "pineapplepenapple"
+wordDict = ["apple", "pen", "applepen", "pine", "pineapple"]
+Output:
+[
+  "pine apple pen apple",
+  "pineapple pen apple",
+  "pine applepen apple"
+]
+Explanation: Note that you are allowed to reuse a dictionary word.
+
+Example 3:
+
+Input:
+s = "catsandog"
+wordDict = ["cats", "dog", "sand", "and", "cat"]
+Output:
+[]
 '''
-def wordBreakRec(s, src):
+class Solution:
     '''
-    Accepted
+    DFS will scope down the problem very quickly
     '''
-    ret = [s] if s in src else []
-    # At least 1 suffix is in dict
-    for i in xrange(len(s)-1,-1,-1):
-        if s[i:] in src: break
-    else:     
-        return ret # Not found
-    
-    # Loop each prefix
-    for i in xrange(1,len(s)):
-        prefix = s[:i]
-        if prefix in src:
-            tails = wordBreakRec(s[i:],src)
-            for t in tails:
-                ret.append(prefix + ' ' + t)
-    return ret
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        if not wordDict:
+            return []
+        
+        book = set(wordDict)
+        maxSize = len(max(wordDict, key = len)) # max size of the word in dict
+        solutions = {} # solution cache of s[i:]
+        solutions[len(s)] = ['']
+        
+        def dfs(i: int) -> List[str]:
+            sol = []
+            if i not in solutions:
+                # to make sure s[i:j] in book, then j-i <= maxSize
+                for j in range(i+1, min(maxSize+i+1, len(s)+1)):
+                    suffix = s[i:j]
+                    if suffix in book:
+                        for sub in dfs(j):
+                            sol.append(suffix + (sub and ' ' + sub))
+                solutions[i] = sol              
+            else:
+                sol = solutions[i]
+            return sol
+        
+        return dfs(0)        
 
 class Solution:
     '''
-    Time limit Exceeded
+    This solutin will result "Time Limit Exceeded" for case:
+    "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+["a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"]
+    Because the solution built-up is exponential
     '''
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        if not wordDict:
+            return []
+        
         size = len(s)
-        book = dict(zip(wordDict, range(len(wordDict)))) # word : index
+        maxSize = len(max(wordDict, key = len)) # max size of the word in dict
+        book = dict(zip(wordDict, range(len(wordDict)))) # {word : index}
 
         # dp[i] contains all possible paths for s[:i]
         dp = [[] for _ in range(size+1)]
         dp[0].append([])
         
         for i in range(1, size+1):
-            for j in range(i):
-                # Build dp[i] based on dp[j] and s[j:i]
-                for path in dp[j]:
-                    index = book.get(s[j:i])
+            # Build dp[i] based on dp[j] + s[j:i]
+            # Of course len(s[j:i]) <= maxSize
+            for j in range(i-1,max((i-maxSize), 0) - 1,-1):
+                if len(dp[j]) > 0:
+                    suffix = s[j:i]
+                    index = book.get(suffix)
                     if index is not None:
-                        # Only append index to save time
-                        dp[i].append(path + [index])
+                        dp[i] += [path + [index] for path in dp[j]]
 
-        res = []
-        for path in dp[-1]:
-            res.append(' '.join([wordDict[index] for index in path]))
-        return res
+        return [' '.join(wordDict[i] for i in path) for path in dp[-1]]
