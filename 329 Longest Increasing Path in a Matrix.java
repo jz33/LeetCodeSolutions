6996ab05e51af75c469a8cdbf4c5045c7203b6e7
Jun <junzhengrice@gmail.com>
2020-01-12 14:38:33 -0800

    BFS

--- 329 Longest Increasing Path in a Matrix.java
+++ 329 Longest Increasing Path in a Matrix.java
@@ -1,41 +1,71 @@
-import java.util.LinkedList;
-import java.util.Queue;
 /*
-Longest Increasing Path in a Matrix
+329. Longest Increasing Path in a Matrix
 https://leetcode.com/problems/longest-increasing-path-in-a-matrix/
+
+Given an integer matrix, find the length of the longest increasing path.
+
+From each cell, you can either move to four directions: left, right, up or down.
+You may NOT move diagonally or move outside of the boundary (i.e. wrap-around is not allowed).
+
+Example 1:
+
+Input: nums = 
+[
+  [9,9,4],
+  [6,6,8],
+  [2,1,1]
+] 
+Output: 4 
+Explanation: The longest increasing path is [1, 2, 6, 9].
+Example 2:
+
+Input: nums = 
+[
+  [3,4,5],
+  [3,2,6],
+  [2,2,1]
+] 
+Output: 4 
+Explanation: The longest increasing path is [3, 4, 5, 6]. Moving diagonally is not allowed.
 */
-public int longestIncreasingPath(int[][] mat) {
-    int R = mat.length;
-    if(R == 0) return 0;
-    int C = mat[0].length;
-    if(C == 0) return 0;
-    int[][] ref = new int[R][C];
-    
-    int[][] directions = {{0,1},{0,-1},{1,0},{-1,0}};
-    int maxDepth = 1;
-    
-    for(int i = 0;i<R;i++){
-        for(int j = 0;j<C;j++){
-            if(ref[i][j] == 0){
-                Queue<Integer[]> queue = new LinkedList<Integer[]>();
-                queue.add(new Integer[]{i,j});
-                int depth = 1;
-                while(queue.size() > 0){
-                    depth++;
-                    int size = queue.size();
-                    for(int k = 0;k<size;k++){
-                        Integer[] from = queue.poll();
-                        int i1 = from[0];
-                        int j1 = from[1];
-                        for(int d=0;d<4;d++){
-                            int i2 = i1 + directions[d][0];
-                            if(i2 > -1 && i2 < R){
-                                int j2 = j1 + directions[d][1];
-                                if(j2 > -1 && j2 < C){
-                                    if(mat[i2][j2] > mat[i1][j1] && depth > ref[i2][j2]){
-                                        ref[i2][j2] = depth;
-                                        maxDepth = Math.max(maxDepth,depth);
-                                        queue.add(new Integer[]{i2,j2});
+class Solution {
+    public int longestIncreasingPath(int[][] mat) {
+        int rowCount = mat.length;
+        if (rowCount == 0) {
+            return 0;
+        }
+        int colCount = mat[0].length;
+        if (colCount == 0) {
+            return 0;
+        }
+        
+        // A matrix records longest path visited on (i,j)
+        // visited[i][j] == 0 means not visitet yet
+        int[][] visited = new int[rowCount][colCount]; 
+        int[][] directions = {{0,1},{0,-1},{1,0},{-1,0}};
+
+        for (int i = 0;i < rowCount;i++) {
+            for (int j = 0;j < colCount;j++) {
+                if (visited[i][j] == 0) {
+                    Queue<Integer[]> queue = new LinkedList<Integer[]>();
+                    queue.add(new Integer[]{i,j});
+                    int depth = 1;
+                    while (queue.size() > 0) {
+                        depth++;
+                        int size = queue.size();
+                        for (int k = 0;k < size;k++) {
+                            Integer[] from = queue.poll();
+                            int i1 = from[0];
+                            int j1 = from[1];
+                            for (int[] inc : directions) {
+                                int i2 = i1 + inc[0];
+                                if (i2 > -1 && i2 < rowCount) {
+                                    int j2 = j1 + inc[1];
+                                    if (j2 > -1 && j2 < colCount) {
+                                        if (mat[i2][j2] > mat[i1][j1] && depth > visited[i2][j2]) {
+                                            visited[i2][j2] = depth;
+                                            queue.add(new Integer[]{i2,j2});
+                                        }
                                     }
                                 }
                             }
@@ -44,6 +74,13 @@ public int longestIncreasingPath(int[][] mat) {
                 }
             }
         }
+        
+        int maxDepth = 1;
+        for (int i = 0;i < rowCount;i++) {
+            for (int j = 0;j < colCount;j++) {
+                maxDepth = Math.max(maxDepth, visited[i][j]);
+            }
+        }
+        return maxDepth;
     }
-    return maxDepth;
 }
