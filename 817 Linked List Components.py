bd7106645501dcd6eb063ba6e2287030fdff70b1
Jun <junzhengrice@gmail.com>
2020-01-14 23:06:37 -0800

    Linked List, O(n)

---
+++ 817 Linked List Components.py
@@ -0,0 +1,51 @@
+'''
+817. Linked List Components
+https://leetcode.com/problems/linked-list-components/
+
+We are given head, the head node of a linked list containing unique integer values.
+
+We are also given the list G, a subset of the values in the linked list.
+
+Return the number of connected components in G, where two values are connected if
+they appear consecutively in the linked list.
+
+Example 1:
+
+Input: 
+head: 0->1->2->3
+G = [0, 1, 3]
+Output: 2
+Explanation: 
+0 and 1 are connected, so [0, 1] and [3] are the two connected components.
+
+Example 2:
+
+Input: 
+head: 0->1->2->3->4
+G = [0, 3, 1, 4]
+Output: 2
+Explanation: 
+0 and 1 are connected, 3 and 4 are connected, so [0, 1] and [3, 4] are the two connected components.
+'''
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def numComponents(self, head: ListNode, G: List[int]) -> int:
+        ss = set(G)
+        ctr = 0
+        broken = True # whether the chain is broken or connected
+        while head is not None:
+            if head.val in ss:
+                if broken:
+                    broken = False
+                    ctr +=1
+            else:
+                if not broken:
+                    broken = True                 
+            head = head.next
+            
+        return ctr
