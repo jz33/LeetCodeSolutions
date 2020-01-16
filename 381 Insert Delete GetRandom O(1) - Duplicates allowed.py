e3c6447f68852483ef1bcc3f06ddb9db911c65ea
Jun <junzhengrice@gmail.com>
2020-01-15 23:25:01 -0800

    Hashmap

--- 381 Insert Delete GetRandom O(1) - Duplicates allowed.py
+++ 381 Insert Delete GetRandom O(1) - Duplicates allowed.py
@@ -33,71 +33,57 @@ collection.remove(1);
 // getRandom should return 1 and 2 both equally likely.
 collection.getRandom();
 '''
-from random import randint
-
 class RandomizedCollection:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.dic = {} # {value : set(indexes)}
-        self.arr = [] # [value]
-        self.ti = -1 # tail index, index to last non-none element, == total - 1
+        self.dic = collections.defaultdict(list) # {value : [indexes]}
+        self.arr = [] # [values]
         
+
     def insert(self, val: int) -> bool:
         """
         Inserts a value to the collection. Returns true if the collection did not already contain the specified element.
         """
         existed = val in self.dic
-        
+            
         # Put into arr
-        self.ti += 1
-        if self.ti >= len(self.arr):
-            self.arr.append(val)
-        else:
-            self.arr[self.ti] = val
-
+        self.arr.append(val)
+            
         # Put into dic
-        if existed:
-            self.dic[val].add(self.ti)
-        else:
-            self.dic[val] = set([self.ti])
+        self.dic[val].append(len(self.arr) - 1)
         
-        return not existed        
+        return not existed
 
     def remove(self, val: int) -> bool:
         """
         Removes a value from the collection. Returns true if the collection contained the specified element.
         """
-        existed = val in self.dic
+        existed = val in self.dic        
         if existed:
             
-            # Get an arbitrary index of val
+            # Pop from dic
             pos = self.dic[val].pop()
+            if len(self.dic[val]) == 0:
+                del self.dic[val]
             
-            if pos != self.ti:
-                
-                # Update arr at pos to last value
-                lastVal = self.arr[self.ti]
+            # Swap last value to pos
+            lastIndex = len(self.arr) - 1
+            if pos != lastIndex:
+                lastVal = self.arr[-1]
                 self.arr[pos] = lastVal
                 
-                # Update dic on last value from index self.ti to pos
-                self.dic[lastVal].remove(self.ti)
-                self.dic[lastVal].add(pos)
+                # Update dic
+                self.dic[lastVal].remove(lastIndex) # for simplicity
+                self.dic[lastVal].append(pos)
             
-            # pop last value from arr
-            self.arr[self.ti] = None
-            self.ti -= 1
+            # Pos from arr
+            self.arr.pop()
         
-            # pop val from dic
-            if len(self.dic[val])== 0:
-                del self.dic[val]
-
         return existed
+        
 
     def getRandom(self) -> int:
         """
         Get a random element from the collection.
         """
-        return self.arr[randint(0,self.ti)]
+        return random.choice(self.arr)
