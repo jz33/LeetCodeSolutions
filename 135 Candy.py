8cee3a04388536184dbcde4505bd71c1babb5223
Jun <junzhengrice@gmail.com>
2020-01-18 14:17:46 -0800

    Greedy on Array

--- 135 Candy.py
+++ 135 Candy.py
@@ -1,30 +1,38 @@
 '''
-135 Candy
-https://oj.leetcode.com/problems/candy/
-'''  
-def candy(ratings):
-    n = len(ratings)
-    if n == 0 : return
+135. Candy
+https://leetcode.com/problems/candy/
 
-    candies = [1 for x in xrange(0,n)]
-    for i in range(1,n):
-        if ratings[i] > ratings[i-1]:
-            candies[i] = candies[i-1]+1
-        else:
-            candies[i] = 1
-            
-    for i in range(n-2,-1,-1):
-        if ratings[i] > ratings[i+1] and candies[i] <= candies[i+1]:
-            candies[i] = candies[i+1] + 1;
+There are N children standing in a line. Each child is assigned a rating value.
 
-    print candies # debug
-    sum = 0
-    for v in candies: sum += v
-    return sum
+You are giving candies to these children subjected to the following requirements:
 
-def main():
-    ratings = [1,2,2]
-    print candy(ratings)
-    
-if __name__ == "__main__":
-    main()
+Each child must have at least one candy.
+Children with a higher rating get more candies than their neighbors.
+What is the minimum candies you must give?
+
+Example 1:
+
+Input: [1,0,2]
+Output: 5
+Explanation: You can allocate to the first, second and third child with 2, 1, 2 candies respectively.
+
+Example 2:
+
+Input: [1,2,2]
+Output: 4
+Explanation: You can allocate to the first, second and third child with 1, 2, 1 candies respectively.
+             The third child gets 1 candy because it satisfies the above two conditions.
+'''
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        candies = [1] * len(ratings)
+        
+        for i in range(1,len(ratings)):
+            if ratings[i] > ratings[i-1]: # and candies[i] <= candies[i-1]
+                candies[i] = candies[i-1] + 1
+        
+        for i in range(len(ratings)-2,-1,-1):
+            if ratings[i] > ratings[i+1] and candies[i] <= candies[i+1]:
+                candies[i] = candies[i+1] + 1;
+
+        return sum(candies)
