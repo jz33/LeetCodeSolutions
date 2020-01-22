87abf6d9325b73a962bce3217f2bdf3bb035df94
Jun <junzhengrice@gmail.com>
2020-01-22 09:16:35 -0800

    Prefix Sum

--- 304 Range Sum Query 2D - Immutable.py
+++ 304 Range Sum Query 2D - Immutable.py
@@ -1,52 +1,46 @@
-import pprint
 '''
-Range Sum Query 2D - Immutable
+304. Range Sum Query 2D - Immutable
 https://leetcode.com/problems/range-sum-query-2d-immutable/
-'''
-pp = pprint.PrettyPrinter(indent=4)
 
+Given a 2D matrix matrix, find the sum of the elements inside the rectangle defined by
+its upper left corner (row1, col1) and lower right corner (row2, col2).
+
+Range Sum Query 2D
+The above rectangle (with the red border) is defined by (row1, col1) = (2, 1) and (row2, col2) = (4, 3),
+which contains sum = 8.
+
+Example:
+
+Given matrix = [
+  [3, 0, 1, 4, 2],
+  [5, 6, 3, 2, 1],
+  [1, 2, 0, 1, 5],
+  [4, 1, 0, 1, 7],
+  [1, 0, 3, 0, 5]
+]
+
+sumRegion(2, 1, 4, 3) -> 8
+sumRegion(1, 1, 2, 2) -> 11
+sumRegion(1, 2, 2, 4) -> 12
+'''
 class NumMatrix(object):
     def __init__(self, matrix):
         """
         initialize your data structure here.
         :type matrix: List[List[int]]
         """
-        self.mat = []
-        ROW = len(matrix)
-        if ROW == 0: return
-        COL = len(matrix[0])
-        if COL == 0: return
-        mat = [[0 for i in xrange(COL+1)] for j in xrange(ROW+1)]
-        for i in xrange(1,ROW+1):
-            for j in xrange(1,COL+1):
-                mat[i][j] = matrix[i-1][j-1] + mat[i-1][j] + mat[i][j-1] - mat[i-1][j-1]
-        self.mat = mat
+        if not len(matrix) or not len(matrix[0]):
+            return
         
-        pp.pprint(mat)
-
+        rowCount = len(matrix)
+        colCount = len(matrix[0])
+        
+        prefix = [[0] * (colCount+1) for _ in range(rowCount+1)]
+        for i in range(rowCount):
+            for j in range(colCount):
+                prefix[i+1][j+1] = matrix[i][j] + prefix[i+1][j] + prefix[i][j+1] - prefix[i][j]
+        self.prefix = prefix
+ 
     def sumRegion(self, row1, col1, row2, col2):
-        """
-        sum of elements matrix[(row1,col1)..(row2,col2)], inclusive.
-        :type row1: int
-        :type col1: int
-        :type row2: int
-        :type col2: int
-        :rtype: int
-        """
-        mat = self.mat
-        col2 += 1
-        row2 += 1
-        return mat[row2][col2] + mat[row1][col1] - mat[row1][col2] - mat[row2][col1]
-
-matrix = [
-    [3,0,1,4,2],
-    [5,6,3,2,1],
-    [1,2,0,1,5],
-    [4,1,0,1,7],
-    [1,0,3,0,5]
-    ]        
-obj = NumMatrix(matrix)
-print obj.sumRegion(2,1,4,3)
-print obj.sumRegion(1,1,2,2)
-print obj.sumRegion(1,2,2,4)     
-        
+        prefix = self.prefix
+        return prefix[row2+1][col2+1] - prefix[row1][col2+1] - prefix[row2+1][col1] + prefix[row1][col1]
