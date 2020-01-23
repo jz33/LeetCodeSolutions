52c5e7c9c5decae7c4fe6e1f874fcd310e1ad429
Jun <junzhengrice@gmail.com>
2020-01-23 00:02:55 -0800

    Trivial

--- 266 Palindrome Permutation.py
+++ 266 Palindrome Permutation.py
@@ -1,16 +1,31 @@
-class Solution(object):
-    def canPermutePalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
+'''
+266. Palindrome Permutation
+https://leetcode.com/problems/palindrome-permutation/
+
+Given a string, determine if a permutation of the string could form a palindrome.
+
+Example 1:
+
+Input: "code"
+Output: false
+
+Example 2:
+
+Input: "aab"
+Output: true
+
+Example 3:
+
+Input: "carerac"
+Output: true
+'''
+class Solution:
+    def canPermutePalindrome(self, s: str) -> bool:
+        histo = collections.Counter(s)
         oddy = 0
-        map = {}
-        for c in s:
-            ctr = map.get(c,0)
-            if ctr % 2 == 0:
+        for ctr in histo.values():
+            if ctr % 2 == 1:
                 oddy += 1
-            else:
-                oddy -= 1
-            map[c] = ctr + 1
-        return oddy < 2
+            if oddy > 1:
+                return False
+        return True
