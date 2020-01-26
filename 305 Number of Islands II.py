662c14d679b57364feb88aa1b218a6aa020fa147
Jun <junzhengrice@gmail.com>
2020-01-26 13:57:03 -0800

    Union Find

--- 305 Number of Islands II.py
+++ 305 Number of Islands II.py
@@ -1,67 +1,89 @@
 '''
-Number of Islands II
+305. Number of Islands II
 https://leetcode.com/problems/number-of-islands-ii/
-Union Find, Matrix
+
+A 2d grid map of m rows and n columns is initially filled with water.
+We may perform an addLand operation which turns the water at position (row, col) into a land.
+Given a list of positions to operate, count the number of islands after each addLand operation.
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically.
+You may assume all four edges of the grid are all surrounded by water.
+
+Example:
+
+Input: m = 3, n = 3, positions = [[0,0], [0,1], [1,2], [2,1]]
+Output: [1,1,2,3]
+Explanation:
+
+Initially, the 2d grid grid is filled with water. (Assume 0 represents water and 1 represents land).
+
+0 0 0
+0 0 0
+0 0 0
+Operation #1: addLand(0, 0) turns the water at grid[0][0] into a land.
+
+1 0 0
+0 0 0   Number of islands = 1
+0 0 0
+Operation #2: addLand(0, 1) turns the water at grid[0][1] into a land.
+
+1 1 0
+0 0 0   Number of islands = 1
+0 0 0
+Operation #3: addLand(1, 2) turns the water at grid[1][2] into a land.
+
+1 1 0
+0 0 1   Number of islands = 2
+0 0 0
+Operation #4: addLand(2, 1) turns the water at grid[2][1] into a land.
+
+1 1 0
+0 0 1   Number of islands = 3
+0 1 0
+Follow up:
+
+Can you do it in time complexity O(k log mn), where k is the length of the positions?
 '''
-def getParent(tree,pt):
-    '''
-    :type tree: {(int,int):(int,int)} 
-    :type pt: (int,int)
-    :rtype: return root of pt. If pt itself is root, return itself
-    pt must be in tree
-    '''
-    steps = 0
-    t = tree.get(pt,None)
-    while t is not None:
-        pt = t
-        t = tree.get(pt,None)
-        steps += 1
-    return pt,steps
+from typing import Tuple
 
-def update(tree,i,j,res):
-    neighbors = [] # root node of neighbors
-    maxDepth, maxDepthRoot = 0, None
+class Solution:
+    def getParent(self, node: Tuple[int,int]) -> Tuple[int,int]:
+        tree = self.tree
+        while node in tree and tree[node] != node:
+            node = tree[node]
+        return node
     
-    # iterate neighbors, find the neighbor that has max depth root
-    for node in (i+1, j), (i-1, j), (i, j+1), (i, j-1):
-        if node in tree:
-            root,depth = getParent(tree,node)
-            if maxDepthRoot is None or depth > maxDepth:
-                maxDepth, maxDepthRoot = depth, root
-            neighbors.append(root)
+    def union(self, start) -> int:
+        tree = self.tree
+        
+        # 1. Find root of all 4 neighbors
+        roots = set()
+        i,j = start
+        for neighbor in (i+1, j), (i-1, j), (i, j+1), (i, j-1):
+            if neighbor in tree:
+                roots.add(self.getParent(neighbor))
 
-    tree[(i,j)] = maxDepthRoot # maxDepthRoot can be None
-
-    # union all neighbors, and get unique root count
-    s = set()
-    for root in neighbors:
-        if root != maxDepthRoot: # critical!
-            tree[root] = maxDepthRoot # maxDepthRoot cannot be None
-        s.add(root)
-
-    res.append(res[-1] + 1 - len(s))
-
-def numIslands2(m, n, points):
-    """
-    :type m: int
-    :type n: int
-    :type points: List[List[int]]
-    :rtype: List[int]
-    """
-    '''
-    Tree is defined as {child:parent}.
-    If a child is root node, its parent is None
-    '''
-    tree = {}
-    res = [0]
-    for i,j in points:
-        if (i,j) not in tree: 
-            update(tree,i,j,res)   
-    return res[1:]
+        rootsCount = len(roots)
+        if not roots:
+            tree[start] = start
+        else:
+            root = roots.pop()
+            for nr in roots:
+                tree[nr] = root
+            tree[start] = root
+            
+        return rootsCount
     
-
-m = 3
-n = 3
-points = [[0,0],[0,1],[1,2],[2,1]]
-points = [[0,1],[1,2],[2,1],[1,0],[0,2],[0,0],[1,1]]
-print numIslands2(m, n, points)
+    def numIslands2(self, m: int, n: int, positions: List[List[int]]) -> List[int]:
+        self.tree = {} # Union Find tree, {point : parent point}
+        counts = []
+        for positions in positions:
+            point = (positions[0], positions[1])
+            previousCount = 0 if not counts else counts[-1]
+            
+            if point not in self.tree: 
+                rootsCount = self.union(point)
+                counts.append(previousCount + 1 - rootsCount)
+            else:
+                counts.append(previousCount)
+                
+        return counts
