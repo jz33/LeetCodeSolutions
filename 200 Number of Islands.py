f30633fc49fc0de2505a64bb4a96cfb85d0d8dce
Jun <junzhengrice@gmail.com>
2020-01-26 12:48:21 -0800

    BFS, Matrix

--- 200 Number of Islands.py
+++ 200 Number of Islands.py
@@ -1,5 +1,5 @@
 '''
-Number of Islands
+200. Number of Islands
 https://leetcode.com/problems/number-of-islands/
 
 Given a 2d grid map of '1's (land) and '0's (water), count the number of islands.
@@ -27,42 +27,27 @@ Input:
 Output: 3
 '''
 class Solution:
+    def bfs(self, grid, visited, start):
+        queue = collections.deque([start])
+        visited[start[0]][start[1]] = True
+        while queue:
+            i,j = queue.popleft()
+            for x,y in (i,j+1),(i+1,j),(i,j-1),(i-1,j):
+                if 0 <= x < len(grid) and 0 <= y < len(grid[0]) and grid[x][y] == '1' and visited[x][y] is False:
+                    queue.append((x,y))
+                    visited[x][y] = True
+                
     def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid or not grid[0]:
+            return 0
+        
         rowCount = len(grid)
-        if rowCount == 0:
-            return 0
-        
         colCount = len(grid[0])
-        if colCount == 0:
-            return 0
-        
-        LAND = '1'
-        INQUEUE = '2'
-        POPED = '3'
-        
-        def mark(i: int, j: int):
-            stack = [(i,j)]
-            while len(stack) > 0:
-                x, y = stack.pop()
-                grid[x][y] = POPED
-                if x+1 < rowCount and grid[x+1][y] == LAND:
-                    stack.append((x+1,y))
-                    grid[x+1][y] = INQUEUE # Mark here to avoid duplicates in queue!
-                if x-1 > -1 and grid[x-1][y] == LAND:
-                    stack.append((x-1,y))
-                    grid[x-1][y] = INQUEUE
-                if y+1 < colCount and grid[x][y+1] == LAND:
-                    stack.append((x,y+1))
-                    grid[x][y+1] = INQUEUE
-                if y-1 > -1 and grid[x][y-1] == LAND:
-                    stack.append((x,y-1))
-                    grid[x][y-1] = INQUEUE
-                 
-        res = 0
+        visited = [[False] * colCount for _ in range(rowCount)]
+        island = 0
         for i in range(rowCount):
             for j in range(colCount):
-                if grid[i][j] == LAND:
-                    res += 1
-                    mark(i,j)
-        
-        return res
+                if grid[i][j] == '1' and visited[i][j] is False:
+                    self.bfs(grid, visited, (i,j))
+                    island += 1
+        return island
