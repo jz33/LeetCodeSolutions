d97be1ff40e50528b6ed67bd0b7032bccc85eb6c
Jun <junzhengrice@gmail.com>
2020-01-26 14:15:15 -0800

    Hashing

--- 249 Group Shifted Strings.py
+++ 249 Group Shifted Strings.py
@@ -1,29 +1,46 @@
-import inspect
 '''
-Group Shifted Strings
+249. Group Shifted Strings
 https://leetcode.com/problems/group-shifted-strings/
+
+Given a string, we can "shift" each of its letter to its successive letter,
+for example: "abc" -> "bcd". We can keep "shifting" which forms the sequence:
+
+"abc" -> "bcd" -> ... -> "xyz"
+Given a list of strings which contains only lowercase alphabets,
+group all strings that belong to the same shifting sequence.
+
+Example:
+
+Input: ["abc", "bcd", "acef", "xyz", "az", "ba", "a", "z"],
+Output: 
+[
+  ["abc","bcd","xyz"],
+  ["az","ba"],
+  ["acef"],
+  ["a","z"]
+]
 '''
-def groupStrings(strings):
-    """
-    :type strings: List[str]
-    :rtype: List[List[str]]
-    """
-    map = {}
-    for s in strings:
-        hash = []
-        for i in xrange(1,len(s)):
-            diff = ord(s[i]) - ord(s[i-1])
-            if diff < 0 : diff += 26
-            hash.append(diff)
-        tup = tuple(hash)
-        v = map.get(tup,[])
-        v.append(s)
-        map[tup] = v
-    
-    res = []
-    for v in map.itervalues():
-        res.append(sorted(v))
-    return res
-     
-ls = []
-print inspect.getfile(__builtin__)
+class Solution:
+    def getHash(self, s: str) -> str:
+        if not s:
+            return ''
+        
+        A = ord('a')
+        
+        diff = ord(s[0]) - A
+        res = []
+        for e in s:
+            d = ord(e) - A - diff
+            if d < 0:
+                d += 26
+            res.append(chr(d + A))
+            
+        return ''.join(res)
+        
+    def groupStrings(self, strings: List[str]) -> List[List[str]]:
+        book = collections.defaultdict(list)
+        for word in strings:
+            h = self.getHash(word)
+            book[h].append(word)
+        
+        return list(book.values())
