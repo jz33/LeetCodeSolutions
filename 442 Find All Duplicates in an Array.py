6370e3760a2e1cf436301e0b4145c96bfd08e1ee
Jun <junzhengrice@gmail.com>
2020-02-02 00:35:36 -0800

    Trivial

--- 442 Find All Duplicates in an Array.py
+++ 442 Find All Duplicates in an Array.py
@@ -1,11 +1,25 @@
 '''
 442 Find All Duplicates in an Array
 https://leetcode.com/problems/find-all-duplicates-in-an-array/
-Similar solution to 448
+
+Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+
+Find all the elements that appear twice in this array.
+
+Could you do it without extra space and in O(n) runtime?
+
+Example:
+
+Input:
+[4,3,2,7,8,2,3,1]
+
+Output:
+[2,3]
 '''
-def findDuplicates(nums: List[int]) -> List[int]:
-    n = len(nums) + 1
-    for e in nums:
-        real_e = e % n
-        nums[real_e - 1] += n # record e
-    return [i for i in range(1,n) if nums[i-1] // n == 2]
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        n = len(nums) + 1
+        for e in nums:
+            real_e = e % n
+            nums[real_e - 1] += n # record e
+        return [i for i in range(1,n) if nums[i-1] // n == 2]
