7f39362e52876eaf263c65482e6f3ebe97a72eef
Jun <junzhengrice@gmail.com>
2020-02-06 15:56:20 -0800

    Heap, Matrix

--- 378 Kth Smallest Element in a Sorted Matrix.py
+++ 378 Kth Smallest Element in a Sorted Matrix.py
@@ -1,37 +1,49 @@
+'''
+378. Kth Smallest Element in a Sorted Matrix
+https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/
+
+Given a n x n matrix where each of the rows and columns are sorted in ascending order,
+find the kth smallest element in the matrix.
+
+Note that it is the kth smallest element in the sorted order, not the kth distinct element.
+
+Example:
+
+matrix = [
+   [ 1,  5,  9],
+   [10, 11, 13],
+   [12, 13, 15]
+],
+k = 8,
+
+return 13.
+
+Note:
+
+You may assume k is always valid, 1 ≤ k ≤ n2.
+'''
 from heapq import heappush, heappop
-'''
-378 Kth Smallest Element in a Sorted Matrix
-https://leetcode.com/problems/
-'''
-def kthSmallest(mat, k):
-    '''
-    The distance from this point to top-left is less than k
-    There will be better filter before put into heap
-    '''
-    n = len(mat)
-    h = []
-    for i in xrange(min(k+1,n)):
-        for j in xrange(min(k-i,n)):
-            heappush(h,mat[i][j])
-    
-    print h
-    for i in xrange(k-1):
-        heappop(h)
-    return heappop(h)
 
-mat = [\
-[1, 5, 9],\
-[10,11,13],\
-[12,13,15],\
-]
-
-# mat = [\
-# [2,3,8,11,15],\
-# [4,8,12,15,18],\
-# [5,8,17,20,22],\
-# [6,12,18,20,25],\
-# [9,14,21,24,25],\
-# ]
-
-for i in xrange(1,len(mat)**2+1):
-    print kthSmallest(mat,i)
+class Solution:
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        n = len(matrix)
+        heap = []
+        
+        def push(i,j):
+            nonlocal heap
+            if i < n and j < n:
+                heappush(heap, (matrix[i][j], i, j))
+                
+        push(0,0)
+        ctr = 0
+        while heap and ctr < k:
+            r, i, j = heappop(heap)
+            ctr += 1
+            if ctr == k:
+                return r
+            
+            push(i, j + 1)
+            if j == 0:
+                push(i + 1, j)
+                
+        return -1        
