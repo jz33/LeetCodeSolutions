44b8fc509df6f84c232481c6027262eced159c19
Jun <junzhengrice@gmail.com>
2020-02-06 23:34:42 -0800

    Greedy

--- 392 Is Subsequence.py
+++ 392 Is Subsequence.py
@@ -1,4 +1,4 @@
-/*
+'''
 392. Is Subsequence
 https://leetcode.com/problems/is-subsequence/
 
@@ -20,32 +20,18 @@ Example 2:
 s = "axc", t = "ahbgdc"
 
 Return false.
-*/
-class Solution {
-public:
-    bool isSubsequence(string needle, string haystack)
-    {
-        if (needle.size() == 0)
-        {
-            return true;
-        }
-
-        size_t ni = 0;
-        for (auto hc : haystack)
-        {
-            auto nc =  needle[ni];
-
-            // Use Greedy. As long as a hc matches nc,
-            // increase ni, because later matching does not matter
-            if (nc == hc)
-            {
-                ni++;
-            }
-            if (ni == needle.size())
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-};
+'''
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if not s:
+            return True
+        
+        si = 0
+        sl = len(s)
+                
+        for te in t:
+            if te == s[si]: # as long as on si match, move si
+                si += 1
+                if si == sl:
+                    return True
+        return False
