4f56f9d6e728e2200dd4c7c684a339ba4a934f07
Jun <junzhengrice@gmail.com>
2020-02-08 15:32:38 -0800

    Postorder Traversal

--- 095 Unique Binary Search Trees II.py
+++ 095 Unique Binary Search Trees II.py
@@ -1,25 +1,51 @@
-import itertools
 '''
-Unique Binary Search Trees II
+95. Unique Binary Search Trees II
 https://leetcode.com/problems/unique-binary-search-trees-ii/
-'''
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
 
-def dfs(nodes):
-    trees = []
-    for i in range(len(nodes)):
-        branches = itertools.product(dfs(nodes[:i]), dfs(nodes[i+1:]))
-        for b in branches:
-            root = TreeNode(nodes[i])
-            root.left, root.right = b[0], b[1]
-            trees.append(root)
-    return trees or [None]
+Given an integer n, generate all structurally unique BST's (binary search trees) that store values 1 ... n.
+
+Example:
+
+Input: 3
+Output:
+[
+  [1,null,3,2],
+  [3,2,null,1],
+  [3,1,null,null,2],
+  [2,1,3],
+  [1,null,2,null,3]
+]
+Explanation:
+The above output corresponds to the 5 unique BST's shown below:
+
+   1         3     3      2      1
+    \       /     /      / \      \
+     3     2     1      1   3      2
+    /     /       \                 \
+   2     1         2                 3
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def postorder(self, values: List[int]) -> List[TreeNode]:
+        trees = []
+        for i in range(len(values)):
+            lefts = self.postorder(values[:i])
+            rights = self.postorder(values[i+1:])
+            for left in lefts:
+                for right in rights:
+                    root = TreeNode(values[i])
+                    root.left = left
+                    root.right = right
+                    trees.append(root)
+        return trees if trees else [None]
     
-def generateTrees(n):
-    return dfs([i+1 for i in range(n)])
-    
-# UNTESTED
+    def generateTrees(self, n: int) -> List[TreeNode]:
+        if n < 1:
+            return []
+        return self.postorder(list(range(1 ,n+1)))
