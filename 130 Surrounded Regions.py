839afc6122d8b7d4a04db72cfdc7f258846de2c2
Jun <junzhengrice@gmail.com>
2020-02-08 14:25:56 -0800

    BFS

--- 130 Surrounded Regions.py
+++ 130 Surrounded Regions.py
@@ -24,64 +24,50 @@ Surrounded regions shouldn’t be on the border, which means that any 'O' on the
 Any 'O' that is not on the border and it is not connected to an 'O' on the border will be flipped to 'X'.
 Two cells are connected if they are adjacent cells connected horizontally or vertically.
 '''
-from typing import Tuple
+Isle = 'O'
+Land = 'X'
+Visited = 'V'
 
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
         """
         Do not return anything, modify board in-place instead.
         """
-        rowCount = len(board)
-        if not rowCount:
+        if not board or not board[0]:
             return
         
+        rowCount = len(board)        
         colCount = len(board[0])
-        if not colCount:
-            return
         
-        IsIsland = 'O'
-        IsLand = 'X'
-        # Notice inside the queue and pop out of queue cases are different.
-        # if treat them all as visisted, there will be duplciates in queue!
-        IsInsideQueue = 'Q'
-        IsPoped = 'P'      
-        def mark(cell: Tuple[int, int]):
-            # Use a stack to replace queue, as order does not matter
-            stack = [cell]
+        def mark(i,j):
+            nonlocal board
+            stack = [(i,j)]
+            board[i][j] = Visited
             while stack:
                 x, y = stack.pop()
-                board[x][y] = IsPoped
-                if x + 1 <  rowCount and board[x+1][y] == IsIsland:
-                    stack.append((x+1,y))
-                    board[x+1][y] = IsInsideQueue
-                if x > 0 and board[x-1][y] == IsIsland:
-                    stack.append((x-1,y))
-                    board[x-1][y] = IsInsideQueue
-                if y + 1 < colCount and board[x][y+1] == IsIsland:
-                    stack.append((x,y+1))
-                    board[x][y+1] = IsInsideQueue
-                if y > 0 and board[x][y-1] == IsIsland:
-                    stack.append((x,y-1))
-                    board[x][y-1] = IsInsideQueue
+                for i,j in (x, y+1), (x, y-1), (x+1, y), (x-1, y):
+                    if 0 <= i < rowCount and 0 <= j < colCount and board[i][j] == Isle:
+                        stack.append((i,j))
+                        board[i][j] = Visited
         
         # Go through border, if is '0', mark all adjacent cells as 'V'
         for i in range(rowCount):
-            if board[i][0] == IsIsland:
-                mark((i,0))
-            if board[i][colCount-1] == IsIsland:
-                mark((i,colCount-1))
+            if board[i][0] == Isle:
+                mark(i,0)
+            if board[i][colCount-1] == Isle:
+                mark(i,colCount-1)
 
-        for i in range(1,colCount-1):
-            if board[0][i] == IsIsland:
-                mark((0,i))
-            if board[rowCount-1][i] == IsIsland:
-                mark((rowCount-1,i))
+        for i in range(colCount):
+            if board[0][i] == Isle:
+                mark(0,i)
+            if board[rowCount-1][i] == Isle:
+                mark(rowCount-1,i)
             
         # For island cell, mark it to land, as it is not connected to bound;
         # For cell who is connected to boundry island, mark it back to island
         for i in range(rowCount):
             for j in range(colCount):
-                if board[i][j] == IsIsland:
-                    board[i][j] = IsLand
-                elif board[i][j] == IsPoped:
-                    board[i][j] = IsIsland
+                if board[i][j] == Isle:
+                    board[i][j] = Land
+                elif board[i][j] == Visited:
+                    board[i][j] = Isle
