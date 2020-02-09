9f002e815c58e69e2fbd54340a080c927bec7ba3
Jun <junzhengrice@gmail.com>
2020-02-09 10:06:08 -0800

    One liner

---
+++ 412 Fizz Buzz.py
@@ -0,0 +1,35 @@
+'''
+412. Fizz Buzz
+https://leetcode.com/problems/fizz-buzz/
+
+Write a program that outputs the string representation of numbers from 1 to n.
+
+But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”.
+For numbers which are multiples of both three and five output “FizzBuzz”.
+
+Example:
+
+n = 15,
+
+Return:
+[
+    "1",
+    "2",
+    "Fizz",
+    "4",
+    "Buzz",
+    "Fizz",
+    "7",
+    "8",
+    "Fizz",
+    "Buzz",
+    "11",
+    "Fizz",
+    "13",
+    "14",
+    "FizzBuzz"
+]
+'''
+class Solution:
+    def fizzBuzz(self, n):
+        return ['Fizz' * (not i % 3) + 'Buzz' * (not i % 5) or str(i) for i in range(1, n+1)]
