80ed71af34de0fece81b74f28789e8a678723472
Jun <junzhengrice@gmail.com>
2020-02-10 15:13:20 -0800

    Binary Search

--- 034 Find First and Last Position of Element in Sorted Array.py
+++ 034 Find First and Last Position of Element in Sorted Array.py
@@ -1,4 +1,4 @@
-/*
+'''
 34. Find First and Last Position of Element in Sorted Array
 https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
 
@@ -17,50 +17,25 @@ Example 2:
 
 Input: nums = [5,7,7,8,8,10], target = 6
 Output: [-1,-1]
-*/
+'''
 class Solution:
-    '''
-    O(log(N))
-    '''
-    def findLeftBound(self, nums: List[int], target: int) -> int:
-        '''
-        Find left bound (the smallest index) of nums who equals to target
-        '''
+    def findBound(self, nums: List[int], target: int, foundLeftBound: bool) -> int:
+        bound = -1
         left = 0
         right = len(nums) - 1
-    
-        bound = -1
         while left <= right:
-            mid = left + ((right - left) >> 1)
-            e = nums[mid]
-            if e == target:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
                 bound = mid
-                right = mid - 1
-            elif e > target:
-                right = mid - 1
+                if foundLeftBound:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            elif nums[mid] < target:
+                left = mid + 1
             else:
-                left = mid + 1
+                right = mid -1
         return bound
-                
-    def findRightBound(self, nums: List[int], target: int) -> int:
-        '''
-        Find right bound (the largest index) of nums who equals to target
-        '''
-        left = 0
-        right = len(nums) - 1
-    
-        bound = -1
-        while left <= right:
-            mid = left + ((right - left) >> 1)
-            e = nums[mid]
-            if e == target:
-                bound = mid
-                left = mid + 1
-            elif e > target:
-                right = mid - 1
-            else:
-                left = mid + 1
-        return bound
-                
+        
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        return [self.findLeftBound(nums, target), self.findRightBound(nums, target)]
+        return [self.findBound(nums, target, True), self.findBound(nums, target, False)]
