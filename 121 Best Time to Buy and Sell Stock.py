7d78830ad730cd8a03817955fad0bd8a6b9a68c3
Jun <junzhengrice@gmail.com>
2020-02-14 16:02:14 -0800

    Greedy

--- 121 Best Time to Buy and Sell Stock.py
+++ 121 Best Time to Buy and Sell Stock.py
@@ -1,14 +1,34 @@
 '''
-Best Time to Buy and Sell Stock
+121. Best Time to Buy and Sell Stock
 https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
-Only 1 transaction allowed
+
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+If you were only permitted to complete at most one transaction
+(i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+Note that you cannot sell a stock before you buy one.
+
+Example 1:
+
+Input: [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+             Not 7-1 = 6, as selling price needs to be larger than buying price.
+             
+Example 2:
+
+Input: [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transaction is done, i.e. max profit = 0.
 '''
-def maxProfit(prices):
-    if len(prices) < 2: return 0
-    prices.append(-1)
-    mp,lo = 0,0
-    for i in xrange(1,len(prices)):
-        if prices[i] < prices[i-1]:
-            mp = max(mp,prices[i-1] - prices[lo])
-            if prices[i] < prices[lo]: lo = i
-    return mp
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        lowestPrice = float('inf')
+        for price in prices:
+            if price < lowestPrice:
+                lowestPrice = price
+            else:
+                profit = max(profit, price - lowestPrice)
+        return profit
