30a78d29018af97a1588969fe446bf8a0cb9d5c7
Jun <junzhengrice@gmail.com>
2020-02-22 10:51:10 -0800

    Boyer Moore

--- 169 Majority Element.py
+++ 169 Majority Element.py
@@ -1,32 +1,36 @@
-import random
 '''
-169 Majority Element
-https://oj.leetcode.com/problems/majority-element/
-http://www.cs.utexas.edu/~moore/best-ideas/mjrty/example.html#step13
+169. Majority Element
+https://leetcode.com/problems/majority-element/
+
+Given an array of size n, find the majority element.
+The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+
+You may assume that the array is non-empty and the majority element always exist in the array.
+
+Example 1:
+
+Input: [3,2,3]
+Output: 3
+
+Example 2:
+
+Input: [2,2,1,1,1,2,2]
+Output: 2
 '''
-def creatRandomList(size):
-    ls = []
-    for i in range(0,size):
-        ls.append(random.randint(0,size))
-    return ls
-
-def majorityElement(ls):
-    if len(ls) == 1: return ls[0]
-
-    major = ls[0]
-    counter = 1
-    for i in xrange(1,len(ls)):
-        e = ls[i]
-        if major == e:
-            counter += 1
-        else:
-            counter -= 1
-        if counter == 0:
-            major = e
-            counter = 1
-    return major        
-
-    
-ls = [3,2,3]
-print ls
-print majorityElement(ls)
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        '''
+        Boyer Moore
+        '''
+        major = nums[0]
+        count = 1
+        for i in range(1, len(nums)):
+            e = nums[i]
+            if e == major:
+                count += 1
+            elif count == 0:
+                major = e
+                count = 1
+            else:
+                count -= 1
+        return major
