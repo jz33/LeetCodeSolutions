d7c81ee00ef6ba999d4127e5f8489d15b749a948
Jun <junzhengrice@gmail.com>
2020-02-23 16:41:34 -0800

    Trivial

--- 349 Intersection of Two Arrays.py
+++ 349 Intersection of Two Arrays.py
@@ -1,12 +1,25 @@
-def intersection(nums1, nums2):
-    """
-    :type nums1: List[int]
-    :type nums2: List[int]
-    :rtype: List[int]
-    """
-    ref = set(nums1)
-    res = set()
-    for e in nums2:
-        if e in ref:
-            res.add(e)
-    return list(res)
+'''
+349. Intersection of Two Arrays
+https://leetcode.com/problems/intersection-of-two-arrays/
+
+Given two arrays, write a function to compute their intersection.
+
+Example 1:
+
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2]
+
+Example 2:
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [9,4]
+Note:
+
+Each element in the result must be unique.
+The result can be in any order.
+'''
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        set1 = set(nums1)
+        res = set(e for e in nums2 if e in set1)
+        return list(res)
