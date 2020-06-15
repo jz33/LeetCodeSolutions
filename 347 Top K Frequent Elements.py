7a900dacc9006b36c54d310de18a7c9adfafead1
Jun <junzhengrice@gmail.com>
2020-06-15 09:12:40 -0700

    Heap, Sort

--- 347 Top K Frequent Elements.py
+++ 347 Top K Frequent Elements.py
@@ -1,6 +1,29 @@
 '''
-Top K Frequent Elements 
-https://leetcode.com/problems/top-k-frequent-elements/
+692. Top K Frequent Words
+https://leetcode.com/problems/top-k-frequent-words/
+
+Given a non-empty list of words, return the k most frequent elements.
+
+Your answer should be sorted by frequency from highest to lowest. If two words have the same frequency, 
+then the word with the lower alphabetical order comes first.
+
+Example 1:
+Input: ["i", "love", "leetcode", "i", "love", "coding"], k = 2
+Output: ["i", "love"]
+Explanation: "i" and "love" are the two most frequent words.
+    Note that "i" comes before "love" due to a lower alphabetical order.
+
+Example 2:
+Input: ["the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"], k = 4
+Output: ["the", "is", "sunny", "day"]
+Explanation: "the", "is", "sunny" and "day" are the four most frequent words,
+    with the number of occurrence being 4, 3, 2 and 1 respectively.
+
+Note:
+You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+Input words contain only lowercase letters.
+Follow up:
+Try to solve it in O(n log k) time and O(n) extra space.
 '''
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
@@ -42,3 +65,24 @@ class Solution:
         
         # 3. Slice
         return [p[0] for p in ls[:k]]
+
+from heapq import heappush, heappop
+from collections import Counter
+
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        '''
+        Use heap
+        '''
+        counter = Counter(words)
+
+        heap = [] # min heap
+        for key, ctr in counter.items():
+            heappush(heap, (ctr, key))
+            if len(heap) > k:
+                heappop(heap)
+        
+        res = []
+        while heap:
+            res.append(heappop(heap)[1])
+        return res[::-1]
