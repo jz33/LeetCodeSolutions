2f2f067f51199c930fdbe5ab6f626f53b1ac38fe
Jun <junzhengrice@gmail.com>
2020-06-16 13:29:21 -0700

    Rolling Hashing

--- 187 Repeated DNA Sequences.cpp
+++ 187 Repeated DNA Sequences.cpp
@@ -1,61 +1,47 @@
-#include<iostream>
-#include<sstream>
-#include<vector>
-using namespace std;
 /*
 Repeated DNA Sequences 
 https://leetcode.com/problems/repeated-dna-sequences/
-*/
-#define SIZE 0x100000
-#define SIZE_1 0xFFFFF
 
-/*
-How to avoid switch:
-'A' - 64 : 1 : 00001 : 001 : 00
-'C' - 64 : 3 : 00011 : 011 : 01
-'G' - 64 : 7 : 00111 : 111 : 11
-'T' - 64 :20 : 10100 : 100 : 10
-*/
-std::vector<std::string> findRepeatedDnaSequences(std::string src)
-{
-    size_t i,length;
-    char c;
-    int hash;
-    std::vector<std::string> ret;
-    if(src.size() <= 10) return ret;
-    
-    char table[SIZE] = {0};
-    hash = 0;
-    length = src.size();
-    for(i = 0;i<10;i++)
-    {
-        c = src[i];
-        hash = ((hash << 2) | ((c - 64 & 6) >> 1));
-    }
-    table[hash] = 1;
-    
-    for(;i<length;i++)
-    {
-        c = src[i];
-        hash = (((hash << 2) | ((c - 64 & 6) >> 1)) & SIZE_1);
-        if(table[hash] == 1)
-        {
-            ret.push_back(src.substr(i-9,10));
-            table[hash] = 2;
-        }
-        else if(table[hash] == 0)
-        {
-            table[hash] = 1;
-        }
-    }
-    return ret;
-}
+All DNA is composed of a series of nucleotides abbreviated as A, C, G, and T,
+for example: "ACGAATTCCG". When studying DNA, it is sometimes useful to identify repeated sequences within the DNA.
 
-int main()
-{
-    std::string src = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT";
-    std::vector<std::string>  ret = findRepeatedDnaSequences(src);
-    for(auto i = ret.begin();i!=ret.end();i++)
-        std::cout<<*i<<endl;
-    return 0;
-}
+Write a function to find all the 10-letter-long sequences (substrings) that occur more than once in a DNA molecule.
+
+Example:
+
+Input: s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"
+Output: ["AAAAACCCCC", "CCCCCAAAAA"]
+*/
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {  
+        vector<string> res;
+        std::unordered_map<int, int> counter; // {hash : count}
+        int hash = 0;
+        for (size_t i = 0; i < s.size(); ++i) {
+            char c = s[i];
+            
+            /*
+            Minus 64       Binary   &6   >>1
+            'A' - 64 : 1 : 00001 : 001 :  00
+            'C' - 64 : 3 : 00011 : 011 :  01
+            'G' - 64 : 7 : 00111 : 111 :  11
+            'T' - 64 :20 : 10100 : 100 :  10
+            */
+            int newHash = ((c - 64) & 6) >> 1;
+            
+            // Accumulate hash by 2 bits, and clip into 20 bits
+            hash = ((hash << 2) | newHash) & 0xFFFFF;
+            
+            if (i >= 9) {
+                if (counter[hash] == 1) {
+                    res.emplace_back(s.substr(i - 9, 10));
+                    counter[hash] = 2;
+                } else if(counter[hash] == 0) {
+                    counter[hash] = 1;
+                }
+            }       
+        }
+        return res;
+    }
+};
