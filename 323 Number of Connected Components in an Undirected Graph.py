a1f33e07740e362ca287e6d9c9545f302b0bae3e
Jun <junzhengrice@gmail.com>
2020-06-16 20:34:08 -0700

    Union Find

--- 323 Number of Connected Components in an Undirected Graph.py
+++ 323 Number of Connected Components in an Undirected Graph.py
@@ -1,37 +1,29 @@
 '''
-Number of Connected Components in an Undirected Graph
+323. Number of Connected Components in an Undirected Graph
 https://leetcode.com/problems/number-of-connected-components-in-an-undirected-graph/
 '''
-def getParent(tree,i):
-    '''
-    Return parent,depth
-    '''
-    for d in xrange(len(tree)):
-        if tree[i] == -1: break;
-        i = tree[i]
-    return i,d
+class UnionFind:
+    def __init__(self, nodeCount: int):
+        self.tree = list(range(nodeCount))
+
+    def find(self, node: int) -> int:
+        tree = self.tree
+        if tree[node] != node:
+            tree[node] = self.find(tree[node])
+        return tree[node]
     
-def countComponents(n, edges):
-    """
-    A classic Union Find implementation
-    :type n: int
-    :type edges: List[List[int]]
-    :rtype: int
-    """
-    tree = [-1] * n
-    for ed in edges:
-        n0, n1 = ed[0],ed[1]
-        (p0,d0) = getParent(tree,n0)
-        (p1,d1) = getParent(tree,n1)
-        
-        # link shallow tree's root to deep tree's root
-        if p0 != p1:
-            if d0 < d1:
-                tree[p0] = p1
-            else:
-                tree[p1] = p0
-    return sum(1 for n in tree if n == -1)
+    def union(self, x: int, y: int):
+        rx = self.find(x)
+        ry = self.find(y)
+        if rx != ry:
+            self.tree[rx] = ry
+            
+    def rootCount(self) -> int:
+        return sum(self.tree[node] == node for node in range(len(self.tree)))
     
-n = 10
-edges = [[5,8],[3,5],[1,9],[4,5],[0,2],[1,9],[7,8],[4,9]]
-print countComponents(n,edges)
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        graph = UnionFind(n)
+        for x, y in edges:
+            graph.union(x, y)
+        return graph.rootCount()
