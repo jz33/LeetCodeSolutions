265eb9d40eadae8f6ce1aee42db1086c34c54fed
Jun <junzhengrice@gmail.com>
2020-07-02 15:42:56 -0700

    BFS

--- 695 Max Area of Island.py
+++ 695 Max Area of Island.py
@@ -1,36 +1,58 @@
-from collections import deque
 '''
-https://leetcode.com/problems/max-area-of-island
-'''
-def dfs(grid,R,C,i,j):    
-    area = 0
-    grid[i][j] = 2 # mark before push to deque
-    dq = deque()
-    dq.append((i,j))
-    while len(dq) > 0:
-        x,y = dq.popleft()
-        area += 1     
-        if x+1 < R and grid[x+1][y] == 1:
-            grid[x+1][y] = 2
-            dq.append((x+1,y))
-        if x-1 >-1 and grid[x-1][y] == 1: 
-            grid[x-1][y] = 2
-            dq.append((x-1,y))
-        if y+1 < C and grid[x][y+1] == 1: 
-            grid[x][y+1] = 2
-            dq.append((x,y+1))
-        if y-1 >-1 and grid[x][y-1] == 1:
-            grid[x][y-1] = 2
-            dq.append((x,y-1))
-    return area
+695. Max Area of Island
+https://leetcode.com/problems/max-area-of-island/
 
-def maxAreaOfIsland(grid):
-    maxArea = 0
-    R = len(grid)
-    C = len(grid[0])
-    for i in xrange(R):
-        for j in xrange(C):
-            if grid[i][j] == 1:
-                area = dfs(grid,R,C,i,j)
-                maxArea = max(maxArea, area)
-    return maxArea
+Given a non-empty 2D array grid of 0's and 1's, an island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.)
+You may assume all four edges of the grid are surrounded by water.
+
+Find the maximum area of an island in the given 2D array.
+(If there is no island, the maximum area is 0.)
+
+Example 1:
+
+[[0,0,1,0,0,0,0,1,0,0,0,0,0],
+ [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ [0,1,1,0,1,0,0,0,0,0,0,0,0],
+ [0,1,0,0,1,1,0,0,1,0,1,0,0],
+ [0,1,0,0,1,1,0,0,1,1,1,0,0],
+ [0,0,0,0,0,0,0,0,0,0,1,0,0],
+ [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ [0,0,0,0,0,0,0,1,1,0,0,0,0]]
+ 
+Given the above grid, return 6. Note the answer is not 11,
+because the island must be connected 4-directionally.
+
+Example 2:
+
+[[0,0,0,0,0,0,0,0]]
+Given the above grid, return 0.
+Note: The length of each dimension in the given grid does not exceed 50.
+'''
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        if not grid or not grid[0]:
+            return 0
+        
+        rowCount = len(grid)
+        colCount = len(grid[0])
+        
+        def bfs(i: int, j: int) -> int:
+            area = 1
+            grid[i][j] = 2 # Mark 2 as visited
+            stack = [(i,j)]
+            while stack:
+                i,j = stack.pop()
+                for x, y in (i,j+1), (i,j-1), (i+1,j),(i-1,j):
+                    if 0 <= x < rowCount and 0 <= y < colCount and grid[x][y] == 1:
+                        # Mark visited before push to avoid pushing duplicates into stack
+                        grid[x][y] = 2
+                        area += 1
+                        stack.append((x,y))
+            return area
+
+        maxArea = 0
+        for i in range(rowCount):
+            for j in range(colCount):
+                if grid[i][j] == 1:
+                    maxArea = max(maxArea, bfs(i,j))
+        return maxArea
