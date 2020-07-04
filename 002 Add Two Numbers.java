11d77df3f1559f44e4662ce40e7c421543557701
Jun <junzhengrice@gmail.com>
2020-07-04 16:21:39 -0700

    Single Linked List

---
+++ 002 Add Two Numbers.java
@@ -0,0 +1,60 @@
+/*
+2. Add Two Numbers
+https://leetcode.com/problems/add-two-numbers/
+
+You are given two non-empty linked lists representing two non-negative integers.
+The digits are stored in reverse order and each of their nodes contain a single digit.
+Add the two numbers and return it as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Example:
+
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+Explanation: 342 + 465 = 807.
+*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+    
+        ListNode head = new ListNode(0);
+        ListNode p = head;
+        int sum = 0;
+        while (l1 != null || l2 != null) {
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            
+            p.next = new ListNode(sum % 10);
+            p = p.next;
+            sum = sum >= 10 ? 1 : 0;
+        }
+        
+        if (sum > 0) {
+            p.next = new ListNode(1);
+        }
+        return head.next;
+    }
+}
