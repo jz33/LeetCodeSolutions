37684685fce84cff0ddf8cf3d668fe272896c230
Jun <junzhengrice@gmail.com>
2020-07-06 09:34:40 -0700

    Hashing

--- 049 Group Anagrams.py
+++ 049 Group Anagrams.py
@@ -21,6 +21,9 @@ The order of your output does not matter
 PRIMES =[2, 3, 5, 7, 11 ,13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 107]
 
 class Solution:
+    '''
+    Use Prime product, can overflow
+    '''
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         book = collections.defaultdict(list) # {hash : [strings]}
         for s in strs:
@@ -30,3 +33,17 @@ class Solution:
                 h *= PRIMES[ord(c) - ord('a')]
             book[h].append(s)
         return book.values()
+      
+class Solution:
+    '''
+    Use char histogram
+    '''
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        A = ord('a')
+        res = collections.defaultdict(list) # {char histogram : [strings]}
+        for s in strs:
+            histo = [0] * 26
+            for c in s:
+                histo[ord(c) - A] += 1
+            res[tuple(histo)].append(s)
+        return res.values()
