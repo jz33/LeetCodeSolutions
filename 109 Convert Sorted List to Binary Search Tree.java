047768c518f521e6faca5949d8813e3c8999fde4
Jun <junzhengrice@gmail.com>
2020-07-06 20:43:39 -0700

    BST, Inorder Traversal

---
+++ 109 Convert Sorted List to Binary Search Tree.java
@@ -0,0 +1,83 @@
+/*
+109. Convert Sorted List to Binary Search Tree
+https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree/
+
+Given a singly linked list where elements are sorted in ascending order,
+convert it to a height balanced BST.
+
+For this problem, a height-balanced binary tree is defined as a binary tree in which
+the depth of the two subtrees of every node never differ by more than 1.
+
+Example:
+
+Given the sorted linked list: [-10,-3,0,5,9],
+
+One possible answer is: [0,-3,9,-10,null,5], which represents the following height balanced BST:
+
+      0
+     / \
+   -3   9
+   /   /
+ -10  5
+*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private ListNode m_listHead;
+    
+    private TreeNode inorder(int leftBound, int rightBound) {
+        if (leftBound > rightBound) {
+            return null;
+        }
+        
+        int middle = leftBound + (int)((rightBound - leftBound) / 2);     
+        TreeNode leftNode = inorder(leftBound, middle - 1);
+        
+        TreeNode res = new TreeNode(m_listHead.val);
+        m_listHead = m_listHead.next;
+        
+        TreeNode rightNode = inorder(middle + 1, rightBound);
+        
+        res.left = leftNode;
+        res.right = rightNode;
+        return res;
+    }
+    
+    private int getSize(ListNode head) {
+        int count = 0;
+        while (head != null) {
+            head = head.next;
+            count += 1;
+        }
+        return count;
+    }
+
+    public TreeNode sortedListToBST(ListNode head) {
+        m_listHead = head;
+        int size = getSize(head);
+        return inorder(0, size - 1);
+    }
+}
