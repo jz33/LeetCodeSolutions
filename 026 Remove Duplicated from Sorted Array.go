4ebde36b644ef8897b8c601cd68a402524168f46
Jun <junzhengrice@gmail.com>
2020-07-06 17:01:42 -0700

    1-D Iteration

--- 026 Remove Duplicated from Sorted Array.go
+++ 026 Remove Duplicated from Sorted Array.go
@@ -1,19 +1,54 @@
-'''
-Remove Duplicated from Sorted Array
-https://oj.leetcode.com/problems/remove-duplicates-from-sorted-array/
-'''
+/*
+26. Remove Duplicates from Sorted Array
+https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
+Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+Example 1:
+
+Given nums = [1,1,2],
+
+Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+
+It doesn't matter what you leave beyond the returned length.
+Example 2:
+
+Given nums = [0,0,1,1,1,2,2,3,3,4],
+
+Your function should return length = 5, with the first five elements of nums being modified to 0, 1, 2, 3, and 4 respectively.
+
+It doesn't matter what values are set beyond the returned length.
+Clarification:
+
+Confused why the returned value is an integer but your answer is an array?
+
+Note that the input array is passed in by reference, which means modification to the input array will be known to the caller as well.
+
+Internally you can think of this:
+
+// nums is passed in by reference. (i.e., without making a copy)
+int len = removeDuplicates(nums);
+
+// any modification to nums in your function would be known by the caller.
+// using the length returned by your function, it prints the first len elements.
+for (int i = 0; i < len; i++) {
+    print(nums[i]);
+}
+*/
 func removeDuplicates(nums []int) int {
-    // The widht is the maximun allowd count for a distint value
-    // Therefore if width is 2, this solution can be used for Remove Duplicates from Sorted Array II
-    const WIDTH = 1
+    // Width is the maximum count for a distinct value
+    // If width is 2, same solution can be used for 80. Remove Duplicates from Sorted Array II
+    const c_width = 1
     
     // The n is the length of the return array
     n := 0
     for _, e := range nums {
-        if n-WIDTH < 0 || nums[n-WIDTH] < e {
+        if n - c_width < 0 || nums[n - c_width] < e {
             nums[n] = e
             n++
         }
-    }   
+    }
     return n
 }
