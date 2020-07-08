931efb8bf15ff4565521652e96c9d571d7a45898
Jun <junzhengrice@gmail.com>
2020-07-08 11:40:45 -0700

    BFS

--- 317 Shorted Distance from All Buildings.py
+++ 317 Shorted Distance from All Buildings.py
@@ -27,76 +27,48 @@ Explanation: Given three buildings at (0,0), (0,4), (2,2), and an obstacle at (0
 Note:
 There will be at least one building. If it is not possible to build such house according to the above rules, return -1.
 '''
-from collections import deque
-from collections import deque
-
-INT_MAX = float('inf')
-
-class Position:
-    '''
-    Represents a empty position on the grid.
-    It records distances to all buildings,
-    as well as last reached building index
-    '''
-    def __init__(self, buildingCount):
-        self.distances = [INT_MAX] * buildingCount
-        self.lastReachedBuildIndex = -1
-
-    def isReachedBefore(self, buildingIndex: int) -> bool:
-        '''
-        If this position is reached by some builidings before.
-        This position can either be reached by previous builidng, or current building
-        '''
-        return self.lastReachedBuildIndex == buildingIndex - 1 or self.lastReachedBuildIndex == buildingIndex
-
-    def __repr__(self):
-        return '{' + str(self.distances) + "," + str(self.lastReachedBuildIndex) + '}'
-    
 class Solution:
-    def bfs(self, buildings, builidngMap):
-        for bi, bp in enumerate(buildings):
-            queue = deque()
-            queue.append(bp)
-            distance = 1
-            while queue:
-                for _ in range(len(queue)):
-                    x,y = queue.popleft()
-                    for i,j in [(x+1,y), (x,y+1), (x-1,y), (x,y-1)]:
-                        if 0 <= i < self.rowCount and 0 <= j < self.colCount and self.grid[i][j] == 0:
-                            position = builidngMap[i][j]
-                            if position.isReachedBefore(bi) and distance < position.distances[bi]:
-                                position.distances[bi] = distance
-                                position.lastReachedBuildIndex = bi
-                                queue.append((i,j))
-                distance += 1
-        
     def shortestDistance(self, grid: List[List[int]]) -> int:
+        if not grid or not grid[0]:
+            return 0
+        
         rowCount = len(grid)
-        if rowCount == 0:
-            return 0
-        
         colCount = len(grid[0])
-        if colCount == 0:
-            return 0
         
-        self.rowCount = rowCount
-        self.colCount = colCount
-        self.grid = grid
-        
-        # Find all buildings coordinates
-        buildings = [(i,j) for i in range(rowCount) for j in range(colCount) if grid[i][j] == 1]
-        buildingCount = len(buildings)
-        
-        # An equivalent matrix to record position information from each building
-        buildingMap = [[Position(buildingCount) for _ in range(colCount)] for _ in range(rowCount)]
-
-        # BFS for all buildings
-        self.bfs(buildings, buildingMap)
-            
-        minDist = INT_MAX
+        # Find all buildings
+        buildings = [] # [(x,y)]
+        lands = {} # {(x, y) : [distances]}
         for i in range(rowCount):
             for j in range(colCount):
-                if buildingMap[i][j].lastReachedBuildIndex == buildingCount - 1:
-                    minDist = min(minDist, sum(buildingMap[i][j].distances))
+                if grid[i][j] == 1:
+                    buildings.append((i,j))
+                elif grid[i][j] == 0:
+                    lands[i,j] = []
+                    
+        if not lands or not buildings:
+            return -1
 
-        return minDist if minDist != INT_MAX else -1
+        # Apply BFS from each buildings
+        for bi, building in enumerate(buildings):  
+            dist = 1
+            stack = [building]
+            while stack:
+                newStack = []
+                for x, y in stack:
+                    for i, j in (x+1,y), (x,y+1), (x-1,y), (x,y-1):
+                        # Be careful about how many buildings have already reached this land
+                        if 0 <= i < rowCount and 0 <= j < colCount and grid[i][j] == 0 and len(lands[i,j]) == bi:
+                            lands[i,j].append(dist)
+                            newStack.append((i, j))
+
+                stack = newStack
+                dist += 1
+            
+        # Get minimum distance
+        minDist = float('inf')
+        for value in lands.values():
+            # Make sure this land is reached by all buildings
+            if len(value) == len(buildings):
+                minDist = min(minDist, sum(value))
+                
+        return minDist if minDist != float('inf') else -1
