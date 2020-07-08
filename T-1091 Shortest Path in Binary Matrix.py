7b2b46bb062fa78828e473bf7c5db600c8868f2b
Jun <junzhengrice@gmail.com>
2020-07-08 15:17:21 -0700

    BFS

--- T-1091 Shortest Path in Binary Matrix.py
+++ T-1091 Shortest Path in Binary Matrix.py
@@ -13,36 +13,34 @@ If C_i is located at (r, c), then grid[r][c] is empty (ie. grid[r][c] == 0).
 Return the length of the shortest such clear path from top-left to bottom-right.
 If such a path does not exist, return -1.
 '''
-from collections import deque
-
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        if not grid or not grid[0] or grid[0][0] != 0 or grid[-1][-1] != 0:
+            return -1
+        
         rowCount = len(grid)
-        if rowCount == 0:
-            return -1
-
         colCount = len(grid[0])
-        if colCount == 0:
-            return -1
+        
+        # Notice the trap of this question.
+        # Since there are diagonal movement, the BFS iteration
+        # with steps count won't work. (Think why)
+        # Instead record smallest steps on each node
+        MAX_VALUE = (rowCount + 1) * (colCount + 1)
+        seen = [[MAX_VALUE] * colCount for _ in range(rowCount)]
+        seen[0][0] = 1
+        
+        points = {(0,0)} # Use set to avoid duplicates
+        while points:
+            newPoints = set()
+            for x, y in points:
+                steps = seen[x][y]
+                
+                # Notice the direction order here. Go diagonals first
+                for d0, d1 in [(1,1),(0,1),(1,0),(1,-1),(0,-1),(-1,1),(-1,0),(-1,-1)]:
+                    i, j = x + d0, y + d1
+                    if 0 <= i < rowCount and 0 <= j < colCount and grid[i][j] == 0 and steps + 1 < seen[i][j]:
+                        seen[i][j] = steps + 1
+                        newPoints.add((i,j))
+            points = newPoints
 
-        if grid[0][0] != 0 or grid[-1][-1] != 0:
-            return -1
-
-        # Record shortest paths
-        MAX_VALUE = (rowCount+1)*(colCount+1)
-        paths = [[MAX_VALUE for _ in range(colCount)] for _ in range(rowCount)]
-
-        queue = deque()
-        queue.append((0,0))
-        paths[0][0] = 1
-
-        while queue:
-            x,y = queue.popleft()
-            p = paths[x][y]
-            for d0, d1 in [(0,1),(1,1),(1,0),(1,-1),(0,-1),(-1,-1),(-1,0),(-1,1)]:
-                i, j = x + d0, y + d1
-                if -1 < i < rowCount and -1 < j < colCount and grid[i][j] != 1 and p+1 < paths[i][j]:
-                    queue.append((i,j))
-                    paths[i][j] = p+1
-
-        return paths[-1][-1] if paths[-1][-1] != MAX_VALUE else -1
+        return seen[-1][-1] if seen[-1][-1] != MAX_VALUE else -1
