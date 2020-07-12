ab1be6cb21f89c6dc4eee323e0c0f71ae3af54f9
Jun <junzhengrice@gmail.com>
2020-07-12 00:07:09 -0700

    BFS

--- 994 Rotting Oranges.py
+++ 994 Rotting Oranges.py
@@ -35,8 +35,6 @@ Note:
 1 <= grid[0].length <= 10
 grid[i][j] is only 0, 1, or 2.
 '''
-INF = float('inf')
-
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         if not grid or not grid[0]:
@@ -46,7 +44,7 @@ class Solution:
         
         # Gather all rotten oranges, fresh orange count
         freshCount = 0
-        rottens = [] # Use simpler stakc
+        rottens = []
         for i in range(rowCount):
             for j in range(colCount):
                 v = grid[i][j]
@@ -54,17 +52,19 @@ class Solution:
                     freshCount += 1
                 elif v == 2:
                     rottens.append((i,j))
-                
-        # Apply BFS for all rotten oranges. This is faster than apply 1 by 1.
+  
+        # Apply BFS for all rotten oranges.
+        # Notice not all oranges are going to be rotten
         steps = 0
         while rottens and freshCount:
             newRottens = []
             for x, y in rottens:
                 for a, b in (x+1,y),(x-1,y),(x,y+1),(x,y-1):
                     if 0 <= a < rowCount and 0 <= b < colCount and grid[a][b] == 1:
-                        freshCount -= 1
                         grid[a][b] = 2
                         newRottens.append((a,b))
+
+            freshCount -= len(newRottens)
             rottens = newRottens
             steps += 1
  
