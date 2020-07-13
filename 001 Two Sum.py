c0e52ec40e5794615c41a3564b88c8c5efb9dc8e
Jun <junzhengrice@gmail.com>
2020-07-13 15:48:52 -0700

    Hashmap

--- 001 Two Sum.py
+++ 001 Two Sum.py
@@ -1,67 +1,27 @@
-import random
 '''
-01 Two Sum
-https://oj.leetcode.com/problems/two-sum/
-'''
-def createRandList(n,a,b):
-    A = []
-    for i in range(n):
-        A.append(random.randint(a,b))
-    return A
-'''
-Normal 2 sum problem
-'''
-def twoSumNormal(numbers,target):
-    numbers.sort()
-    i = 0
-    j = len(numbers) - 1
-    while i < j:
-        lt = numbers[i]
-        rt = numbers[j]
-        if lt + rt == target:
-            print "(",i,":",j,")"
-            i += 1
-            while i < j and numbers[i] == lt: i += 1
-            j -= 1
-            while i < j and numbers[j] == rt: j -= 1
-        elif lt + rt < target:
-            i += 1
-            while i < j and numbers[i] == lt: i += 1
-        else:
-            j -= 1
-            while i < j and numbers[j] == rt: j -= 1
-     
-def twoSum(nums, target):
-    """
-    Sorting way
-    :type nums: List[int]
-    :type target: int
-    :rtype: List[int]
-    """
-    arr = [(i,nums[i]) for i in range(len(nums))]
-    arr.sort(key = lambda x:x[1])
-    i = 0
-    j = len(arr) - 1
-    while i < j:
-        s = arr[i][1] + arr[j][1] 
-        if s == target:
-            return (arr[i][0],arr[j][0])
-        elif s < target:
-            i += 1
-        else:
-            j -= 1
-    return None
+1. Two Sum
+https://leetcode.com/problems/two-sum/
 
-def twoSum(self, nums, target):
-    """
-    Hashtable way
-    :type nums: List[int]
-    :type target: int
-    :rtype: List[int]
-    """
-    hashtable = {}
-    for ind,val in enumerate(nums):
-        x = target-val
-        if x in hashtable:
-            return [hashtable[x],ind]
-        hashtable[val] = ind
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+'''
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        '''
+        @nums is not sorted, indicates hash map
+        '''
+        seen = {} # {seen number : index}
+        for index, val in enumerate(nums):
+            found = seen.get(target - val)
+            if found is not None:
+                return [found,index]
+            seen[val] = index     
+        return None # not reachable for this question
