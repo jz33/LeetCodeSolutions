a1186fa5c50be0c2be9264bded3cfe132412af08
Jun <junzhengrice@gmail.com>
2020-07-16 13:17:03 -0700

    Sliding Window

--- 076 Minimum Window Substring.py
+++ 076 Minimum Window Substring.py
@@ -1,5 +1,5 @@
 '''
-Minimum Window Substring
+76. Minimum Window Substring
 https://leetcode.com/problems/minimum-window-substring/
 
 Given a string S and a string T, find the minimum window in S which will contain all the characters in T in complexity O(n).
@@ -13,50 +13,50 @@ Note:
 If there is no such window in S that covers all characters in T, return the empty string "".
 If there is such window, you are guaranteed that there will always be only one unique minimum window in S.
 '''
-    from collections import Counter
-
+class SlidingWindow:
+    def __init__(self, pattern: str):
+        self.patternCounter = collections.Counter(pattern)
+        self.patternSize = len(pattern)
+        self.validCount = 0
+        self.counter = collections.Counter()
+    
+    def isMatched(self) -> bool:
+        return self.validCount == self.patternSize
+    
+    def add(self, char: str):
+        self.counter[char] += 1
+        if self.counter[char] <= self.patternCounter[char]:
+            self.validCount += 1
+    
+    def remove(self, char: str):
+        self.counter[char] -= 1
+        if self.counter[char] < self.patternCounter[char]:
+            self.validCount -= 1
+    
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        # Pre-process t
-        t_book = Counter(t)
-        t_size = len(t)
-        
-        # Result
-        s_size = len(s)
-        minWinSize = s_size + 1
-        minWinLeft = -1
-        
-        # Iteration recorder
-        left = 0 # left index of current found
-        i = 0 # like "right" index
-        book = Counter()
-        # How many "valid" chars found
-        # "valid" means the cound of chars is smaller than that in t_book
-        validCount = 0 
-        
-        # If s is still under iteration or current found still matches
-        # even though s is already iterated over
-        while i < s_size or validCount == t_size:           
-            if validCount < t_size:
+        sw = SlidingWindow(t)
+        resultStart = None
+        resultSize = len(s) + 1 # Not len(s)
+        i = 0
+        start = 0
+        while i < len(s) or sw.isMatched():        
+            if not sw.isMatched():
                 # Not match yet
                 c = s[i]
                 i += 1
-                book[c] += 1
-                if book[c] <= t_book[c]:
-                    validCount += 1
+                sw.add(c)
             else:
                 # Matched.
                 # Try update result
-                count = i - left
-                if count < minWinSize:
-                    minWinSize = count
-                    minWinLeft = left
-                    
-                # Try shrink from left
-                c = s[left]
-                left += 1
-                book[c] -= 1
-                if book[c] < t_book[c]:
-                    validCount -= 1
-                    
-        return s[minWinLeft:minWinLeft+minWinSize] if minWinLeft != -1 else ''
+                size = i - start
+                if size < resultSize:
+                    resultSize = size
+                    resultStart = start
+
+                # Shrink from left
+                c = s[start]
+                start += 1
+                sw.remove(c)
+    
+        return s[resultStart : resultStart + resultSize] if resultStart is not None else ''
