3a64eef0f27cf5c1938585859a8708e35bd9dcf0
Jun <junzhengrice@gmail.com>
2020-08-04 14:27:45 -0700

    Top Down Dynamic Programming, Bitmap

--- 464 Can I Win.py
+++ 464 Can I Win.py
@@ -31,47 +31,40 @@ If the first player choose 1, the second player can only choose integers from 2 
 The second player will win by choosing 10 and get a total = 11, which is >= desiredTotal.
 Same with other integers chosen by the first player, the second player will always win.
 '''
+from functools import lru_cache
+
 class Solution:
-    def dfs(self, combination, total) -> bool:
-        '''
-        @combination: current combination of numbers represeted by a bitmap
-        @total: the sum of current combination numbers
-        @return: if current player (can be player 1 or 2) can win
-        '''
-        if self.pool[combination] is not None:
-            return self.pool[combination]
-
-        if total >= self.desiredTotal:
-            # Total is picked by opponent
-            # To fully understand this, for example, think this is turn 3,
-            # and there are 2 numbers picked, if reached here, player 1 is lost
-            # If this is turn 4, there are 3 numbers, and so player 2 is lost
-            return False
-
-        for i in range(self.maxChoosableInteger):
-            # If i+1 is not yet picked
-            if (combination & (1 << i)) == 0:
-                newCombination = (combination | (1 << i))
-                # If my opponenet cannot win, I win
-                if not self.dfs(newCombination, total+i+1):
-                    self.pool[combination] = True
-                    return True
-
-        self.pool[combination] = False
-        return False
-
     def canIWin(self, maxChoosableInteger: int, desiredTotal: int) -> bool:
         if maxChoosableInteger >= desiredTotal:
             return True
-
+        
         summation = (maxChoosableInteger * (maxChoosableInteger+1)) >> 1
         if summation < desiredTotal:
             return False
 
-        self.maxChoosableInteger = maxChoosableInteger
-        self.desiredTotal = desiredTotal
+        @lru_cache(None)
+        def dfs(combination: int, total: int) -> bool:
+            '''
+            @combination: current combination of numbers represeted by a bitmap
+            @total: the sum of current combination numbers
+            @return: if current player (can be player 1 or 2) can win
+            '''
+            if total >= desiredTotal:
+                # Total is picked by opponent.
+                # To fully understand this, for example, think this is turn 3,
+                # and there are 2 numbers picked, if reached here, player 1 is lost
+                # If this is turn 4, there are 3 numbers, and so player 2 is lost
+                return False
 
-        # The pool is a large array contains whether a combination can win or not
-        # Each pool element is a integer of bitmap
-        self.pool = [None] * (1 << maxChoosableInteger)
-        return self.dfs(0, 0)
+            for i in range(maxChoosableInteger):              
+                # Check if i+1 is not yet picked
+                if (combination & (1 << i)) == 0:
+                    newCombination = (combination | (1 << i))
+                    newTotal = total + i + 1                 
+                    # If my opponenet cannot win, I win
+                    if not dfs(newCombination, newTotal):
+                        return True
+
+            return False
+
+        return dfs(0, 0)
