e4d01d6b374b9bac39fde547718b4a5f87bbef77
Jun <junzhengrice@gmail.com>
2020-08-11 15:23:53 -0700

    Stack

--- 020 Valid Parentheses.py
+++ 020 Valid Parentheses.py
@@ -1,26 +1,60 @@
 '''
 20 Valid Parentheses
-https://oj.leetcode.com/problems/valid-parentheses/
+https://leetcode.com/problems/valid-parentheses/
+
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']',
+determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Note that an empty string is also considered valid.
+
+Example 1:
+
+Input: "()"
+Output: true
+
+Example 2:
+
+Input: "()[]{}"
+Output: true
+
+Example 3:
+
+Input: "(]"
+Output: false
+
+Example 4:
+
+Input: "([)]"
+Output: false
+
+Example 5:
+
+Input: "{[]}"
+Output: true
 '''
 class Solution:
     def isValid(self, s: str) -> bool:
-        lefts = ['(', '[', '{'] # left parts of parentheses
+        leftParentheses = ['(', '[', '{']
         
-        def rightToLeft(s: str) -> str:
-            if s == ')':
-                return '('
-            elif s == ']':
-                return '['
-            elif s == '}':
-                return '{'
+        def arePaired(left: str, right: str) -> bool:
+            if left == '(':
+                return right == ')'
+            elif left == '[':
+                return right == ']'
+            elif left == '{':
+                return right == '}'
         
-        buf = []
+        stack = []
         for c in s:
-            if c in lefts:
-                buf.append(c)
+            if c in leftParentheses:
+                stack.append(c)
             else:
-                if len(buf) == 0 or buf[-1] != rightToLeft(c):
+                if not stack or not arePaired(stack[-1], c):
                     return False
-                buf.pop()
+                stack.pop()
                 
-        return len(buf) == 0
+        return not stack
