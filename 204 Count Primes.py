753cdd28054e032dcc5ab7a972d9811e0fdd6fda
Jun <junzhengrice@gmail.com>
2020-08-17 13:06:36 -0700

    Math

--- 204 Count Primes.py
+++ 204 Count Primes.py
@@ -1,26 +1,33 @@
-import sys
 '''
-Count Primes
+204. Count Primes
 https://leetcode.com/problems/count-primes/
-Regular Eratosthenes method
-'''
-def eratosthenes(x):
-    if x < 2: return 0
-    
-    bc = [0 for i in xrange(0,x)];
-    counter = 0;
-    
-    bc[0] = 1; # 1
-    bc[1] = 1;
-    for i in xrange(1,x):
-        if bc[i] == 0:
-            counter += 1;
-            
-            j = i * i
-            while j < x:
-                bc[j] = 1
-                j += i
- 
-    return counter;
 
-print eratosthenes(int(sys.argv[1]))
+Count the number of prime numbers less than a non-negative number, n.
+
+Example:
+
+Input: 10
+Output: 4
+Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+'''
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        '''
+        Eratosthenes
+        '''
+        result = 0
+        
+        # primes[i] is prime if True
+        primes = [True] * n
+        
+        for i in range(2, n):
+            if primes[i]:
+                result += 1
+                
+                # Mark all multiple as non-prime
+                m = i
+                while m * i < n:
+                    primes[m * i] = False
+                    m += 1
+
+        return result
