0c47652addff1c9548df501927fcf1c4b6e7b603
Jun <junzhengrice@gmail.com>
2020-09-01 14:25:20 -0700

    Constant State Dynamic Programming

--- 198 House Robber.py
+++ 198 House Robber.py
@@ -3,9 +3,10 @@
 https://leetcode.com/problems/house-robber/
 
 You are a professional robber planning to rob houses along a street.
-Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them
+Each house has a certain amount of money stashed,
+the only constraint stopping you from robbing each of them
 is that adjacent houses have security system connected and it will automatically contact the police if
-    two adjacent houses were broken into on the same night.
+two adjacent houses were broken into on the same night.
 
 Given a list of non-negative integers representing the amount of money of each house,
 determine the maximum amount of money you can rob tonight without alerting the police.
@@ -16,6 +17,7 @@ Input: [1,2,3,1]
 Output: 4
 Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
              Total amount you can rob = 1 + 3 = 4.
+
 Example 2:
 
 Input: [2,7,9,3,1]
