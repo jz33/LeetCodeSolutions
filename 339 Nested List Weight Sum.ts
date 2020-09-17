91c4fb2339817bae28934356190a5f59d4f38502
Jun <junzhengrice@gmail.com>
2020-09-17 14:21:39 -0700

    Level Order Traversal

---
+++ 339 Nested List Weight Sum.ts
@@ -0,0 +1,81 @@
+/*
+339. Nested List Weight Sum
+https://leetcode.com/problems/nested-list-weight-sum/
+
+Given a nested list of integers, return the sum of all integers in the list weighted by their depth.
+
+Each element is either an integer, or a list -- whose elements may also be integers or other lists.
+
+Example 1:
+
+Input: [[1,1],2,[1,1]]
+Output: 10 
+Explanation: Four 1's at depth 2, one 2 at depth 1.
+Example 2:
+
+Input: [1,[4,[6]]]
+Output: 27 
+Explanation: One 1 at depth 1, one 4 at depth 2, and one 6 at depth 3; 1 + 4*2 + 6*3 = 27.
+*/
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *     If value is provided, then it holds a single integer
+ *     Otherwise it holds an empty nested list
+ *     constructor(value?: number) {
+ *         ...
+ *     };
+ *
+ *     Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     isInteger(): boolean {
+ *         ...
+ *     };
+ *
+ *     Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     Return null if this NestedInteger holds a nested list
+ *     getInteger(): number | null {
+ *         ...
+ *     };
+ *
+ *     Set this NestedInteger to hold a single integer equal to value.
+ *     setInteger(value: number) {
+ *         ...
+ *     };
+ *
+ *     Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+ *     add(elem: NestedInteger) {
+ *         ...
+ *     };
+ *
+ *     Return the nested list that this NestedInteger holds,
+ *     or an empty list if this NestedInteger holds a single integer
+ *     getList(): NestedInteger[] {
+ *         ...
+ *     };
+ * };
+ */
+
+function depthSum(nestedList: NestedInteger[]): number {
+    let res: number = 0
+    let depth: number = 1
+    let stack: NestedInteger[] = nestedList
+
+    while (stack.length > 0) {
+        let newStack: NestedInteger[] = []
+        for (const node of stack) {
+            const val: number | null = node.getInteger()
+            if (val !== null) {
+                res += val * depth
+            }
+            else {
+                for (const newNode of node.getList()) {
+                    newStack.push(newNode)
+                }
+            }
+        }
+        stack = newStack
+        depth++
+    }
+    return res
+};
