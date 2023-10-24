982376bd5fa51fe0eaf5c8a3d92332f998634e74
Jun <junzhengrice@gmail.com>
2023-10-24 12:05:49 -0700

    Tree O(log(N))

---
+++ T-1650 Lowest Common Ancestor of a Binary Tree III.ts
@@ -0,0 +1,78 @@
+/*
+1650. Lowest Common Ancestor of a Binary Tree III
+https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree-iii/description/
+
+Given two nodes of a binary tree p and q, return their lowest common ancestor (LCA).
+
+Each node will have a reference to its parent node. The definition for Node is below:
+
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node parent;
+}
+
+According to the definition of LCA on Wikipedia: "The lowest common ancestor of two nodes p and q in a tree T is the lowest node that has both p and q as descendants (where we allow a node to be a descendant of itself)."
+
+Example 1:
+
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+Output: 3
+Explanation: The LCA of nodes 5 and 1 is 3.
+
+Example 2:
+
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+Output: 5
+Explanation: The LCA of nodes 5 and 4 is 5 since a node can be a descendant of itself according to the LCA definition.
+
+Example 3:
+
+Input: root = [1,2], p = 1, q = 2
+Output: 1
+
+Constraints:
+    The number of nodes in the tree is in the range [2, 105].
+    -109 <= Node.val <= 109
+    All Node.val are unique.
+    p != q
+    p and q exist in the tree.
+*/
+/**
+ * Definition for a binary tree node.
+ * class Node {
+ *     val: number
+ *     left: Node | null
+ *     right: Node | null
+ *     parent: Node | null
+ *     constructor(val?: number, left?: Node | null, right?: Node | null, parent?: Node | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *         this.parent = (parent===undefined ? null : parent)
+ *     }
+ * }
+ */
+
+function lowestCommonAncestor(p: Node | null, q: Node | null): Node | null {
+  // For a node p, its LCA to any other node can only be p or one of p's parents
+
+  // Build p & p's parents' set
+  const parents = new Set();
+  let curr: Node | null = p;
+  while (curr) {
+    parents.add(curr.val);
+    curr = curr.parent;
+  }
+
+  // Iterate from q until root to find LCA
+  curr = q;
+  while (curr) {
+    if (parents.has(curr.val)) {
+      return curr;
+    }
+    curr = curr.parent;
+  }
+  return null;
+}
