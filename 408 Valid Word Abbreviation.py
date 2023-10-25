3ea5b9f6aa01b3c4de4698df199395b7bed59aac
Jun <junzhengrice@gmail.com>
2023-10-25 12:10:57 -0700

    Two Pointers

--- 408 Valid Word Abbreviation.py
+++ 408 Valid Word Abbreviation.py
@@ -2,47 +2,68 @@
 408. Valid Word Abbreviation
 https://leetcode.com/problems/valid-word-abbreviation/
 
-Given a non-empty string s and an abbreviation abbr, return whether the string matches with the given abbreviation.
+A string can be abbreviated by replacing any number of non-adjacent, non-empty substrings with their lengths. The lengths should not have leading zeros.
 
-A string such as "word" contains only the following valid abbreviations:
+For example, a string such as "substitution" could be abbreviated as (but not limited to):
 
-["word", "1ord", "w1rd", "wo1d", "wor1", "2rd", "w2d", "wo2", "1o1d", "1or1", "w1r1", "1o2", "2r1", "3d", "w3", "4"]
+    "s10n" ("s ubstitutio n")
+    "sub4u4" ("sub stit u tion")
+    "12" ("substitution")
+    "su3i1u2on" ("su bst i t u ti on")
+    "substitution" (no substrings replaced)
 
-Notice that only the above abbreviations are valid abbreviations of the string "word".
-Any other string is not a valid abbreviation of "word".
+The following are not valid abbreviations:
 
-Note:
-Assume s contains only lowercase letters and abbr contains only lowercase letters and digits.
+    "s55n" ("s ubsti tutio n", the replaced substrings are adjacent)
+    "s010n" (has leading zeros)
+    "s0ubstitution" (replaces an empty substring)
+
+Given a string word and an abbreviation abbr, return whether the string matches the given abbreviation.
+
+A substring is a contiguous non-empty sequence of characters within a string.
 
 Example 1:
 
-Given s = "internationalization", abbr = "i12iz4n":
-
-Return true.
+Input: word = "internationalization", abbr = "i12iz4n"
+Output: true
+Explanation: The word "internationalization" can be abbreviated as "i12iz4n" ("i nternational iz atio n").
 
 Example 2:
 
-Given s = "apple", abbr = "a2e":
+Input: word = "apple", abbr = "a2e"
+Output: false
+Explanation: The word "apple" cannot be abbreviated as "a2e".
 
-Return false.
+Constraints:
+
+    1 <= word.length <= 20
+    word consists of only lowercase English letters.
+    1 <= abbr.length <= 10
+    abbr consists of lowercase English letters and digits.
+    All the integers in abbr will fit in a 32-bit integer.
+
+
 '''
 class Solution:
     def validWordAbbreviation(self, word: str, abbr: str) -> bool:
-        i = 0 # index of word
-        num = 0
+        i = 0 # index of `word`
+        num = 0 # number in abbr
         for a in abbr:
             if a.isalpha():
                 if num > 0:
                     i += num
                     num = 0
-                
+
                 if i >= len(word) or word[i] != a:
-                    return False              
+                    # Not the same character
+                    return False
+                    
                 i += 1
-                
             else:
                 if num == 0 and a == '0':
+                    # Leading zeros
                     return False
+                
                 num = num * 10 + int(a)
                 
         if num > 0:
