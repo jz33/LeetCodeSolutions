2c228a974ae7f7cab9af50d4b75646568edd73e1
Jun <junzhengrice@gmail.com>
2023-10-27 10:56:13 -0700

    Level Order Traversal

--- 339 Nested List Weight Sum.ts
+++ 339 Nested List Weight Sum.ts
@@ -2,20 +2,34 @@
 339. Nested List Weight Sum
 https://leetcode.com/problems/nested-list-weight-sum/
 
-Given a nested list of integers, return the sum of all integers in the list weighted by their depth.
+You are given a nested list of integers nestedList. Each element is either an integer or a list whose elements may also be integers or other lists.
 
-Each element is either an integer, or a list -- whose elements may also be integers or other lists.
+The depth of an integer is the number of lists that it is inside of. For example, the nested list [1,[2,2],[[3],2],1] has each integer's value set to its depth.
+
+Return the sum of each integer in nestedList multiplied by its depth.
 
 Example 1:
 
-Input: [[1,1],2,[1,1]]
-Output: 10 
-Explanation: Four 1's at depth 2, one 2 at depth 1.
+Input: nestedList = [[1,1],2,[1,1]]
+Output: 10
+Explanation: Four 1's at depth 2, one 2 at depth 1. 1*2 + 1*2 + 2*1 + 1*2 + 1*2 = 10.
+
 Example 2:
 
-Input: [1,[4,[6]]]
-Output: 27 
-Explanation: One 1 at depth 1, one 4 at depth 2, and one 6 at depth 3; 1 + 4*2 + 6*3 = 27.
+Input: nestedList = [1,[4,[6]]]
+Output: 27
+Explanation: One 1 at depth 1, one 4 at depth 2, and one 6 at depth 3. 1*1 + 4*2 + 6*3 = 27.
+
+Example 3:
+
+Input: nestedList = [0]
+Output: 0
+
+Constraints:
+
+    1 <= nestedList.length <= 50
+    The values of the integers in the nested list is in the range [-100, 100].
+    The maximum depth of any integer is less than or equal to 50.
 */
 
 /**
@@ -58,23 +72,22 @@ Explanation: One 1 at depth 1, one 4 at depth 2, and one 6 at depth 3; 1 + 4*2 +
  */
 
 function depthSum(nestedList: NestedInteger[]): number {
-    let res: number = 0
-    let depth: number = 1
-    let stack: NestedInteger[] = nestedList
+    let result: number = 0;
+    let depth: number = 1;
+    let stack: NestedInteger[] = nestedList;
 
-    while (stack.length > 0) {
-        let newStack: NestedInteger[] = []
+    while (stack.length) {
+        let newStack: NestedInteger[] = [];
         for (const node of stack) {
-            const val: number | null = node.getInteger()
-            if (val !== null) {
-                res += val * depth
-            }
-            else {
-                newStack = newStack.concat(node.getList())
+            const element: number | null = node.getInteger();
+            if (element) {
+                result += element * depth;
+            } else {
+                newStack = newStack.concat(node.getList());
             }
         }
-        stack = newStack
-        depth++
+        stack = newStack;
+        depth++;
     }
-    return res
+    return result;
 }
