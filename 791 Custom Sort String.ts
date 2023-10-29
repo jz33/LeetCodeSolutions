bd68f83d73b2f28df73afbe5aa3efb1ccf3da332
Jun <junzhengrice@gmail.com>
2023-10-29 14:42:09 -0700

    Sort

--- 791 Custom Sort String.ts
+++ 791 Custom Sort String.ts
@@ -2,49 +2,75 @@
 791. Custom Sort String
 https://leetcode.com/problems/custom-sort-string/
 
-S and T are strings composed of lowercase letters. In S, no letter occurs more than once.
+You are given two strings order and s. All the characters of order are unique and were sorted in some custom order previously.
 
-S was sorted in some custom order previously.
-We want to permute the characters of T so that they match the order that S was sorted.
-More specifically, if x occurs before y in S, then x should occur before y in the returned string.
+Permute the characters of s so that they match the order that order was sorted. More specifically, if a character x occurs before a character y in order, then x should occur before y in the permuted string.
 
-Return any permutation of T (as a string) that satisfies this property.
+Return any permutation of s that satisfies this property.
 
-Example :
-Input: 
-S = "cba"
-T = "abcd"
+Example 1:
+
+Input: order = "cba", s = "abcd"
+Output: "cbad"
+Explanation: 
+"a", "b", "c" appear in order, so the order of "a", "b", "c" should be "c", "b", and "a". 
+Since "d" does not appear in order, it can be at any position in the returned string. "dcba", "cdba", "cbda" are also valid outputs.
+
+Example 2:
+
+Input: order = "cbafg", s = "abcd"
 Output: "cbad"
 
-Explanation: 
-"a", "b", "c" appear in S, so the order of "a", "b", "c" should be "c", "b", and "a". 
-Since "d" does not appear in S, it can be at any position in T. "dcba", "cdba", "cbda" are also valid outputs.
- 
-Note:
-
-S has length at most 26, and no character is repeated in S.
-T has length at most 200.
-S and T consist of lowercase letters only.
+Constraints:
+    1 <= order.length <= 26
+    1 <= s.length <= 200
+    order and s consist of lowercase English letters.
+    All the characters of order are unique.
 */
-const BaseCharCode: number = 'a'.charCodeAt(0)
+// Use custom sort
+function customSortString(order: string, target: string): string {
+    // Use codePointAt, as charCodeAt only returns [0, 65536]
+    const toBaseCode = (char: string): number => {
+        return char.codePointAt(0)! - 'a'.codePointAt(0)!;
+    };
 
-function toBaseCode(char: string): number {
-    return char.charCodeAt(0) - BaseCharCode
-}
-
-function customSortString(S: string, T: string): string {
-    let ranks: number[] = []
-    for (let i = 0; i < 26; ++i) {
-        ranks[i] = 0
+    // Build a histogram of order
+    const ranks: number[] = Array(26).fill(0);
+    for (let i = 0; i < order.length; ++i) {
+        ranks[toBaseCode(order[i])] = i + 1;
     }
 
-    for (let i = 0; i < S.length; ++i) {
-        ranks[toBaseCode(S[i])] = i + 1
+    return target
+        .split('')
+        .sort((a, b) => ranks[toBaseCode(a)] - ranks[toBaseCode(b)])
+        .join('');
+}
+
+// Another method using Hashmap, without custom sort
+function customSortString(order: string, target: string): string {
+    const targetMap = new Map<string, number>();
+    for (const char of target) {
+        targetMap.set(char, (targetMap.get(char) ?? 0) + 1);
     }
 
-    let ls: string[] = T.split('').sort(function(a: string, b: string): number {
-        return ranks[toBaseCode(a)] - ranks[toBaseCode(b)]
-    })
+    const result: string[] = [];
+    for (const char of order) {
+        const count = targetMap.get(char);
+        if (count) {
+            // count !== undefined && count > 0
+            result.push(new Array(count).fill(char).join(''));
+            targetMap.set(char, 0);
+        }
+    }
 
-    return ls.join('')
+    Array.from(targetMap.entries()).map((entry) => {
+        const [char, count] = entry;
+        if (count) {
+            result.push(new Array(count).fill(char).join(''));
+        }
+    });
+
+    return result.join('');
 }
+
+
