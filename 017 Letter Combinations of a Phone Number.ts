b6f066ec97e53c64617b62e063a97297e7c5f857
Jun <junzhengrice@gmail.com>
2023-10-30 23:54:11 -0700

    Permutations

---
+++ 017 Letter Combinations of a Phone Number.ts
@@ -0,0 +1,58 @@
+'''
+17. Letter Combinations of a Phone Number
+https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+
+Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+
+A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+Example 1:
+
+Input: digits = "23"
+Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+
+Example 2:
+
+Input: digits = ""
+Output: []
+
+Example 3:
+
+Input: digits = "2"
+Output: ["a","b","c"]
+
+Constraints:
+    0 <= digits.length <= 4
+    digits[i] is a digit in the range ['2', '9'].
+'''
+const PhoneDigitCharsMap: Record<string, string[]> = {
+    '2': ['a', 'b', 'c'],
+    '3': ['d', 'e', 'f'],
+    '4': ['g', 'h', 'i'],
+    '5': ['j', 'k', 'l'],
+    '6': ['m', 'n', 'o'],
+    '7': ['p', 'q', 'r', 's'],
+    '8': ['t', 'u', 'v'],
+    '9': ['w', 'x', 'y', 'z'],
+};
+
+function letterCombinations(digits: string): string[] {
+    if (!digits) {
+        return [];
+    }
+
+    // Incrementally increase pool size by appending new chars
+    let pool: string[][] = [];
+    pool[0] = [];
+
+    for (const digit of digits) {
+        const newPool: string[][] = [];
+        for (const row of pool) {
+            for (const char of PhoneDigitCharsMap[digit]) {
+                newPool.push(row.concat([char]));
+            }
+        }
+        pool = newPool;
+    }
+    return pool.map((row) => row.join(''));
+}
