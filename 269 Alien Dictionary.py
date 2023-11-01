7681d1201d0cf3236b3b12baf95a62a714b96298
Jun <junzhengrice@gmail.com>
2023-10-31 17:44:00 -0700

    Topological Sort

--- 269 Alien Dictionary.py
+++ 269 Alien Dictionary.py
@@ -2,91 +2,81 @@
 269. Alien Dictionary
 https://leetcode.com/problems/alien-dictionary/
 
-There is a new alien language which uses the latin alphabet.
-However, the order among letters are unknown to you.
-You receive a list of non-empty words from the dictionary,
-where words are sorted lexicographically by the rules of this new language.
-Derive the order of letters in this language.
+There is a new alien language that uses the English alphabet. However, the order of the letters is unknown to you.
+
+You are given a list of strings words from the alien language's dictionary. Now it is claimed that the strings in words are
+sorted lexicographically
+by the rules of this new language.
+
+If this claim is incorrect, and the given arrangement of string in words cannot correspond to any order of letters, return "".
+
+Otherwise, return a string of the unique letters in the new alien language sorted in lexicographically increasing order by the new language's rules.
+If there are multiple solutions, return any of them.
 
 Example 1:
 
-Input:
-[ "wrt", "wrf", "er", "ett", "rftt"]
+Input: words = ["wrt","wrf","er","ett","rftt"]
 Output: "wertf"
 
 Example 2:
 
-Input:
-["z", "x"]
+Input: words = ["z","x"]
 Output: "zx"
 
 Example 3:
 
-Input:["z", "x", "z"] 
-Output: "" 
-
+Input: words = ["z","x","z"]
+Output: ""
 Explanation: The order is invalid, so return "".
 
-Example 4:
-
-Input:["z", "z"] 
-Output: "z" 
-
-Note:
-
-You may assume all letters are in lowercase.
-You may assume that if a is a prefix of b, then a must appear before b in the given dictionary.
-If the order is invalid, return an empty string.
-There may be multiple valid order of letters, return any one of them is fine.
+Constraints:
+    1 <= words.length <= 100
+    1 <= words[i].length <= 100
+    words[i] consists of only lowercase English letters.
 '''
-from typing import List, Tuple
-from collections import defaultdict, Counter, deque
+from typing import List, Tuple, Union
 
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
 class Graph(object):
     def __init__(self):
-        self.ranks = Counter() # How many time the node is reached?
-        self.edges = defaultdict(set) # {from char: set(togo chars)}
+        self.ranks = {} # {char : reach count}
+        self.edges = {} # {from char : set(togo chars)}
         
-    def compare(self, a: str, b: str) -> Tuple[str, str]:
-        '''
-        Compare 2 words, return why a is in front of b
-        '''
+    def compare(self, a: str, b: str) -> Union[Tuple[str, str], None]:
+        # Compare 2 words, return why a is in front of b
         size = min(len(a), len(b))
         for i in range(size):
-            ca = a[i]
-            cb = b[i]
-            if ca != cb:
-                return ca, cb
-
+            if a[i] != b[i]:
+                return a[i], b[i]
         return None
 
-    def add(self, words: List[str]):
+    def build(self, words: List[str])-> bool:
         ranks = self.ranks
         edges = self.edges
 
-        # 1. Put all chars (nodes)
+        # Put all chars (nodes)
         for word in words:
-            for c in word:
-                if c not in ranks:
-                    ranks[c] = 0
-                    edges[c] = set()
-
-        # 2. Add edges
-        for i in range(len(words)):
-            wi = words[i]
-            for j in range(i+1, len(words)):
-                wj = words[j]
-                pair = self.compare(wi, wj)
-                if pair:
-                    f, t = pair[0], pair[1]
-                    if t not in edges[f]:
-                        edges[f].add(t)
-                        ranks[t] += 1
- 
-     
+            for char in word:
+                if char not in ranks:
+                    ranks[char] = 0
+                    edges[char] = set()
+                    
+        # Compare each word with all its following words
+        for i in range(len(words) - 1):
+            pair = self.compare(words[i], words[i+1])
+            if pair:
+               fromChar, togoChar = pair[0], pair[1]
+               if togoChar not in edges[fromChar]:
+                  edges[fromChar].add(togoChar)
+                  ranks[togoChar] += 1
+            elif len(words[i]) > len(words[i+1]):
+                # For cases like ['abc', 'ab'], 'abc' should never go in front of 'ab'
+                return False
+            
+        return True
+         
     def debug(self):
         pp.pprint(self.ranks)
         pp.pprint(self.edges)
@@ -94,25 +84,26 @@ class Graph(object):
     def topologicalSort(self) -> List[str]:
         ranks = self.ranks
         edges = self.edges
-        res = []
         
-        queue = deque([i for i,d in ranks.items() if d == 0])    
-        while queue:
-            # Notice size of queue can be bigger than 1,
-            # which means more than 1 possible order
-            node = queue.popleft()
-            res.append(node)
+        # Get the starting nodes
+        queue = [i for i,d in ranks.items() if d == 0]
+        queueIndex = 0
+        while queueIndex < len(queue):
+            node = queue[queueIndex]
 
             for togo in edges[node]:
-                d = ranks[togo]
-                if d == 1:
+                reachCount = ranks[togo]
+                if reachCount == 1:
                     queue.append(togo)
-                ranks[togo] -= 1
+                ranks[togo] = reachCount - 1
+            
+            queueIndex += 1
     
-        return res if len(res) == len(ranks) else ''
+        return queue if len(queue) == len(ranks) else []
     
 class Solution:
     def alienOrder(self, words: List[str]) -> str:
         graph = Graph()
-        graph.add(words)
-        return ''.join(graph.topologicalSort())
+        if graph.build(words):
+            return ''.join(graph.topologicalSort())
+        return ''
