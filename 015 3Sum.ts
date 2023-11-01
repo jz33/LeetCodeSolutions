0b3e099e8fd1d6d32a3c56e73a1db8488490c12c
Jun <junzhengrice@gmail.com>
2023-11-01 12:02:58 -0700

    Two Pointers

--- 015 3Sum.ts
+++ 015 3Sum.ts
@@ -2,73 +2,88 @@
 15. 3Sum
 https://leetcode.com/problems/3sum/
 
-Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0?
-Find all unique triplets in the array which gives the sum of zero.
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
 
-Note:
+Notice that the solution set must not contain duplicate triplets.
 
-The solution set must not contain duplicate triplets.
+Example 1:
 
-Example:
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Explanation: 
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].
+Notice that the order of the output and the order of the triplets does not matter.
 
-Given array nums = [-1, 0, 1, 2, -1, -4],
+Example 2:
 
-A solution set is:
-[
-  [-1, 0, 1],
-  [-1, -1, 2]
-]
+Input: nums = [0,1,1]
+Output: []
+Explanation: The only possible triplet does not sum up to 0.
+
+Example 3:
+
+Input: nums = [0,0,0]
+Output: [[0,0,0]]
+Explanation: The only possible triplet sums up to 0.
+
+Constraints:
+
+    3 <= nums.length <= 3000
+    -105 <= nums[i] <= 105
 */
-type MovePredict = (it: number) => boolean
+function twoSum(nums: number[], target: number): [number, number][] {
+    const result: [number, number][] = [];
+    let left = 0;
+    let right = nums.length - 1;
+    while (left < right) {
+        const leftVal = nums[left];
+        const rightVal = nums[right];
+        const total = leftVal + rightVal;
+        if (total === target) {
+            result.push([leftVal, rightVal]);
+        }
+        if (total <= target) {
+            left++;
+            while (left < right && nums[left] == leftVal) {
+                left++;
+            }
+        }
+        if (total >= target) {
+            right--;
+            while (left < right && nums[right] == rightVal) {
+                right--;
+            }
+        }
+    }
+    return result;
+}
 
-function threeSum(nums: number[]): number[][] {
-    let result: number[][] = []
-    
-    // NOT: nums.sort(),
+function threeSum(nums: number[]): [number, number, number][] {
+    let result: [number, number, number][] = [];
+
+    // NOT nums.sort(),
     // because for negative numbers it will be wrong
     nums.sort((x, y) => x - y);
-    const size: number = nums.length
-    
-    function moveLeft(it: number, pred: MovePredict): number{
-        it--;
-        while (pred(it)) {
-            it--;
+
+    let start = 0;
+    while (start < nums.length - 2) {
+        const startVal = nums[start];
+        const twoSumResult = twoSum(nums.slice(start + 1), 0 - startVal);
+        result = result.concat(
+            twoSumResult.map((twoSum) => [startVal].concat(twoSum)) as [
+                number,
+                number,
+                number,
+            ][]
+        );
+        start++;
+        while (start < nums.length - 2 && nums[start] == startVal) {
+            start++;
         }
-        return it;
     }
-    
-    function moveRight(it: number, pred: MovePredict): number{
-        it++;
-        while (pred(it)) {
-            it++;
-        }
-        return it;
-    }
-    
-    let i: number = 0;
-    while (i < size - 2) {
-        let a = nums[i]
-        let j = i + 1
-        let k = size - 1
-        while (j < k) {
-            let b = nums[j]
-            let c = nums[k]
-            let s = a + b + c
-            if (s === 0) {
-                result.push([a, b, c])
-                
-                j = moveRight(j, (v) => {return v < k && nums[v] === b})
-                k = moveLeft(k, (v) => {return v > j && nums[v] === c})
-            }
-            else if (s < 0) {
-                j = moveRight(j, (v) => {return v < k && nums[v] === b})
-            }
-            else {
-                k = moveLeft(k, (v) => {return v > j && nums[v] === c})
-            }
-        }
-        i = moveRight(i, (v) => {return v < size && nums[v] === a})
-    }
-    
-    return result
-};
+
+    return result;
+}
