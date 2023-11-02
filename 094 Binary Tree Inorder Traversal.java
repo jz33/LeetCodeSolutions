8e9e4ec7230fa063b67b06df0f3c5c7a4088af1a
Jun <junzhengrice@gmail.com>
2023-11-02 13:16:43 -0700

    Inorder Traversal

--- 094 Binary Tree Inorder Traversal.java
+++ 094 Binary Tree Inorder Traversal.java
@@ -1,26 +1,58 @@
-import java.util.*;
+/*
+94. Binary Tree Inorder Traversal
+https://leetcode.com/problems/binary-tree-inorder-traversal/
+
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+Example 1:
+
+Input: root = [1,null,2,3]
+Output: [1,3,2]
+
+Example 2:
+
+Input: root = []
+Output: []
+
+Example 3:
+
+Input: root = [1]
+Output: [1]
+
+Constraints:
+    The number of nodes in the tree is in the range [0, 100].
+    -100 <= Node.val <= 100
+*/
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
-public List<Integer> inorderTraversal(TreeNode root) {
-    List<Integer> res = new ArrayList<Integer>();
-    Stack<TreeNode> stack = new Stack<TreeNode>();
-    TreeNode p = root;
-    while(p != null || !stack.empty()){
-        if(p != null){
-            stack.push(p);
-            p = p.left;
-        } else {
-            p = stack.pop();
-            res.add(p.val);
-            p = p.right;
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        var result = new ArrayList<Integer>();
+        var stack = new Stack<TreeNode>();
+        var curr = root;
+        while(curr != null || !stack.empty()){
+            if (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            } else {
+                curr = stack.pop();
+                result.add(curr.val);
+                curr = curr.right;
+            }
         }
+        return result;
     }
-    return res;
 }
