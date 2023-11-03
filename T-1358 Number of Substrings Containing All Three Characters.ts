47a8ed6e19110f5496d2a5faf19295ce81e3c851
Jun <junzhengrice@gmail.com>
2023-11-03 12:30:45 -0700

    Sliding Window

--- T-1358 Number of Substrings Containing All Three Characters.ts
+++ T-1358 Number of Substrings Containing All Three Characters.ts
@@ -6,7 +6,6 @@ Given a string s consisting only of characters a, b and c.
 
 Return the number of substrings containing at least one occurrence of all these characters a, b and c.
 
-
 Example 1:
 
 Input: s = "abcabc"
