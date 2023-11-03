d3dc75226760a119c745d20ae22293250a3da404
Jun <junzhengrice@gmail.com>
2023-11-03 14:01:20 -0700

    1-D Iteration, Greedy

---
+++ 053 Maximum Subarray.ts
@@ -0,0 +1,42 @@
+/*
+53. Maximum Subarray
+https://leetcode.com/problems/maximum-subarray/
+
+Example 1:
+
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+
+Example 2:
+
+Input: nums = [1]
+Output: 1
+Explanation: The subarray [1] has the largest sum 1.
+
+Example 3:
+
+Input: nums = [5,4,-1,7,8]
+Output: 23
+Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+
+Constraints:
+    1 <= nums.length <= 105
+    -104 <= nums[i] <= 104
+*/
+function maxSubArray(nums: number[]): number {
+    if (!nums.length) {
+        return 0;
+    }
+    let result = nums[0];
+    let total = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+        if (total < 0) {
+            total = nums[i];
+        } else {
+            total = total + nums[i];
+        }
+        result = Math.max(result, total);
+    }
+    return result;
+}
