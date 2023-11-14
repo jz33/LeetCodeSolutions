368c5c939de3783ee8656e4bd83ef627c2e11146
Jun <junzhengrice@gmail.com>
2023-11-13 22:33:03 -0800

    Backtracking

---
+++ 022 Generate Parentheses.py
@@ -0,0 +1,33 @@
+'''
+22. Generate Parentheses
+https://leetcode.com/problems/generate-parentheses/
+
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+Example 1:
+
+Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+
+Example 2:
+
+Input: n = 1
+Output: ["()"]
+
+Constraints:
+    1 <= n <= 8
+'''
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        pool = []
+
+        def backtrack(sofar: List[str], left: int, right: int):
+            if left == 0:
+                pool.append(''.join(sofar + [')'] * right))
+            else:
+                backtrack(sofar + ['('], left - 1, right)
+                if right > left:
+                    backtrack(sofar + [')'], left, right - 1)
+        
+        backtrack([], n, n)
+        return pool
