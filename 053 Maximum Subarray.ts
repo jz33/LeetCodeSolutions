24b5ea7056090b15dda4d15404e0919db9f5fc75
Jun <junzhengrice@gmail.com>
2023-11-19 10:15:37 -0800

    Constant State Dynamic Programming

--- 053 Maximum Subarray.ts
+++ 053 Maximum Subarray.ts
@@ -2,6 +2,8 @@
 53. Maximum Subarray
 https://leetcode.com/problems/maximum-subarray/
 
+Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
 Example 1:
 
 Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
