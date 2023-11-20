e8f2721c8edddaaa16c497391d52487fe4593bfa
Jun <junzhengrice@gmail.com>
2023-11-19 20:49:21 -0800

    Graph, BFS

--- 886 Possible Bipartition.ts
+++ 886 Possible Bipartition.ts
@@ -2,99 +2,68 @@
 886. Possible Bipartition
 https://leetcode.com/problems/possible-bipartition/
 
-Given a set of N people (numbered 1, 2, ..., N), we would like to split everyone into two groups of any size.
+We want to split a group of n people (labeled from 1 to n) into two groups of any size.
+Each person may dislike some other people, and they should not go into the same group.
 
-Each person may dislike some other people, and they should not go into the same group. 
-
-Formally, if dislikes[i] = [a, b], it means it is not allowed to put the people numbered a and b into the same group.
-
-Return true if and only if it is possible to split everyone into two groups in this way.
+Given the integer n and the array dislikes where dislikes[i] = [ai, bi] indicates that the person labeled ai does not like the person labeled bi,
+return true if it is possible to split everyone into two groups in this way.
 
 Example 1:
 
-Input: N = 4, dislikes = [[1,2],[1,3],[2,4]]
+Input: n = 4, dislikes = [[1,2],[1,3],[2,4]]
 Output: true
-Explanation: group1 [1,4], group2 [2,3]
+Explanation: The first group has [1,4], and the second group has [2,3].
 
 Example 2:
 
-Input: N = 3, dislikes = [[1,2],[1,3],[2,3]]
+Input: n = 3, dislikes = [[1,2],[1,3],[2,3]]
 Output: false
-
-Example 3:
-
-Input: N = 5, dislikes = [[1,2],[2,3],[3,4],[4,5],[1,5]]
-Output: false
+Explanation: We need at least 3 groups to divide them. We cannot put them in two groups.
 
 Constraints:
+    1 <= n <= 2000
+    0 <= dislikes.length <= 104
+    dislikes[i].length == 2
+    1 <= ai < bi <= n
+    All the pairs of dislikes are unique.
 
-1 <= N <= 2000
-0 <= dislikes.length <= 10000
-dislikes[i].length == 2
-1 <= dislikes[i][j] <= N
-dislikes[i][0] < dislikes[i][1]
-There does not exist i != j for which dislikes[i] == dislikes[j].
 */
-function possibleBipartition(N: number, dislikes: number[][]): boolean {
-    // Build graph matrix. Notice graph[i] can be undefined
-    let graph: number[][] = []
+function possibleBipartition(n: number, dislikes: number[][]): boolean {
+    let dislikeGraph: number[][] = [];
     for (const edge of dislikes) {
-        let start = edge[0] - 1
-        let togo = edge[1] - 1
-        if (graph[start] === undefined) {
-            graph[start] = [togo]
-        }
-        else {
-            graph[start].push(togo)
-        }
-        if (graph[togo] === undefined) {
-            graph[togo] = [start]
-        }
-        else {
-            graph[togo].push(start)
-        }
+        let start = edge[0] - 1;
+        let togo = edge[1] - 1;
+        dislikeGraph[start] = (dislikeGraph.at(start) ?? []).concat(togo);
+        dislikeGraph[togo] = (dislikeGraph.at(togo) ?? []).concat(start);
     }
-    
-    // Colors of each node, can be undefined / 1 / 0
-    let colors: number[] = []
-    
-    function bfs(node: number): boolean {
-        // Assign color to starting node
-        colors[node] = 0
-        
-        let stack: number[] = [node]
-        while (stack.length > 0) {
-            let newStack: number[] = []
-            
-            for (const start of stack) {
-                let startColor = colors[start]
-                let togos = graph[start]
-                
-                // Notice the start node can have no connections at all
-                if (togos != undefined) {
+
+    // Mark colors on each node:
+    // 0: unvisited
+    // 1: red
+    // -1: blue
+    const colors = new Array(n).fill(0);
+
+    for (let start = 0; start < n; start++) {
+        if (colors[start] === 0) {
+            colors[start] = 1;
+            let stack: number[] = [start];
+            while (stack.length) {
+                const newStack: number[] = [];
+                for (const node of stack) {
+                    const togos = dislikeGraph[node] ?? [];
                     for (const togo of togos) {
-                        if (colors[togo] === undefined) {
-                            colors[togo] = 1 - startColor
-                            newStack.push(togo)
-                        }
-                        else if (colors[togo] + startColor !== 1) {
-                            return false
+                        if (colors[togo] === 0) {
+                            colors[togo] = colors[node] * -1;
+                            newStack.push(togo);
+                        } else if (colors[togo] + colors[node] !== 0) {
+                            return false;
                         }
                     }
                 }
+                stack = newStack;
             }
-            stack = newStack
         }
-        return true
     }
+    return true;
+}
 
-    // Notice the graph is not necessarily connected
-    for (let i = 0; i < N; ++i) {
-        if (colors[i] === undefined) {
-            if (!bfs(i)) {
-                return false
-            }
-        }
-    }
-    return true
-};
