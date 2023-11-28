35a74412cb4d29f8c90f1444188c1b4a57f628e9
Jun <junzhengrice@gmail.com>
2023-11-28 10:21:28 -0800

    Stack, Calculator

--- 394 Decode String.py
+++ 394 Decode String.py
@@ -1,41 +1,61 @@
 '''
 394. Decode String
-https://leetcode.com/problems/decode-string/
+https://leetcode.com/problems/sentence-screen-fitting/
 
 Given an encoded string, return its decoded string.
 
-The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly k times.
+The encoding rule is: k[encoded_string],
+where the encoded_string inside the square brackets is being repeated exactly k times.
 Note that k is guaranteed to be a positive integer.
 
-You may assume that the input string is always valid; No extra white spaces, square brackets are well-formed, etc.
+You may assume that the input string is always valid; there are no extra white spaces,
+square brackets are well-formed, etc.
+Furthermore, you may assume that the original data does not contain any digits and
+that digits are only for those repeat numbers, k.
+For example, there will not be input like 3a or 2[4].
 
-Furthermore, you may assume that the original data does not contain any digits and that digits are only for those repeat numbers, k.
-For example, there won't be input like 3a or 2[4].
+The test cases are generated so that the length of the output will never exceed 105.
 
-Examples:
+Example 1:
 
-s = "3[a]2[bc]", return "aaabcbc".
-s = "3[a2[c]]", return "accaccacc".
-s = "2[abc]3[cd]ef", return "abcabccdcdcdef".
+Input: s = "3[a]2[bc]"
+Output: "aaabcbc"
+
+Example 2:
+
+Input: s = "3[a2[c]]"
+Output: "accaccacc"
+
+Example 3:
+
+Input: s = "2[abc]3[cd]ef"
+Output: "abcabccdcdcdef"
+
+Constraints:
+    1 <= s.length <= 30
+    s consists of lowercase English letters, digits, and square brackets '[]'.
+    s is guaranteed to be a valid input.
+    All the integers in s are in the range [1, 300].
 '''
 class Solution:
     def decodeString(self, s: str) -> str:
-        '''
-        Similar pattern to Calculator question
-        '''
-        stack = [] # [counts or chars]
+        stack = [] # counts or chars
         count = 0
         for c in s:
             if c == '[':
                 stack.append(count)
                 count = 0
             elif c == ']':
-                strs = []
-                while isinstance(stack[-1], str):
-                    strs.append(stack.pop())
-                stack.append(''.join(strs[::-1]) * stack.pop())
-            elif c.isalpha():
-                stack.append(c)
-            else: # c.isdigit(), most expensive call
+                # Find last saved mutiplier
+                i = -1
+                while isinstance(stack[i], str):
+                    i -= 1
+                chars = ''.join(stack[i+1:])
+                mutiplier = stack[i]
+                stack = stack[:i]
+                stack.append(chars * mutiplier)
+            elif c.isdigit():
                 count = count * 10 + int(c)
+            else:
+                stack.append(c)
         return ''.join(stack)
