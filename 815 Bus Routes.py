dccd5136f224f6fc277969173a6e55f6644d4495
Jun <junzhengrice@gmail.com>
2023-11-29 10:23:41 -0800

    BFS, graph

--- 815 Bus Routes.py
+++ 815 Bus Routes.py
@@ -2,50 +2,57 @@
 815. Bus Routes
 https://leetcode.com/problems/bus-routes/
 
-We have a list of bus routes.
-Each routes[i] is a bus route that the i-th bus repeats forever. For example if routes[0] = [1, 5, 7],
-this means that the first bus (0-th indexed) travels in the sequence 1->5->7->1->5->7->1->... forever.
+You are given an array routes representing bus routes where
+routes[i] is a bus route that the ith bus repeats forever.
 
-We start at bus stop S (initially not on a bus), and we want to go to bus stop T.
-Travelling by buses only, what is the least number of buses we must take to reach our destination? Return -1
-if it is not possible.
+For example, if routes[0] = [1, 5, 7], this means that the 0th bus travels
+in the sequence 1 -> 5 -> 7 -> 1 -> 5 -> 7 -> 1 -> ... forever.
 
-Example:
+You will start at the bus stop source (You are not on any bus initially),
+and you want to go to the bus stop target.
+You can travel between bus stops by buses only.
 
-Input: 
-routes = [[1, 2, 7], [3, 6, 7]]
-S = 1
-T = 6
+Return the least number of buses you must take to travel from source to target.
+Return -1 if it is not possible.
 
+Example 1:
+
+Input: routes = [[1,2,7],[3,6,7]], source = 1, target = 6
 Output: 2
+Explanation: The best strategy is take the first bus to the bus stop 7, then take the second bus to the bus stop 6.
 
-Explanation:
-The best strategy is take the first bus to the bus stop 7, then take the second bus to the bus stop 6.
+Example 2:
+
+Input: routes = [[7,12],[4,5,15],[6],[15,19],[9,12,13]], source = 15, target = 12
+Output: -1
+
+Constraints:
+    1 <= routes.length <= 500.
+    1 <= routes[i].length <= 105
+    All the values of routes[i] are unique.
+    sum(routes[i].length) <= 105
+    0 <= routes[i][j] < 106
+    0 <= source, target < 106
 '''
 '''
 Real interview question of Convoy, inc, 20200814
 '''
-
-from collections import defaultdict
-
 class Solution:
     def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
-        # Build a {stop : [bus ids]} map
-        stopToBuses = defaultdict(list)
+        stopToBuses = collections.defaultdict(list) # {stop : [bus ids]}
         for busId, stops in enumerate(routes):
             for stop in stops:
                 stopToBuses[stop].append(busId)
-
-        # Save the bus id to improve performance
-        visitedBuses = set() # set(busIds)
-        visitedStops = { source } # set(stopIds)
+        
+        visitedStops = { source }
+        visitedBuses = set()
         stops = [source]
-        steps = 0
+        busCount = 0
         while stops:
             newStops = []
             for stop in stops:
                 if stop == target:
-                    return steps
+                    return busCount
                 
                 for busId in stopToBuses[stop]:
                     if busId not in visitedBuses:
@@ -54,6 +61,8 @@ class Solution:
                             if stop not in visitedStops:
                                 visitedStops.add(stop)
                                 newStops.append(stop)
+                            
             stops = newStops
-            steps += 1
+            busCount += 1
+    
         return -1
