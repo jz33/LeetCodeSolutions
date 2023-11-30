21f3a854b599b82469a3af4f68f77caac56e4ea9
Jun <junzhengrice@gmail.com>
2023-11-30 01:43:07 -0800

    Treemap, SortedList

--- 480 Sliding Window Median.py
+++ 480 Sliding Window Median.py
@@ -2,38 +2,44 @@
 480. Sliding Window Median
 https://leetcode.com/problems/sliding-window-median/
 
-Median is the middle value in an ordered integer list.
+The median is the middle value in an ordered integer list.
 If the size of the list is even, there is no middle value.
-So the median is the mean of the two middle value.
+So the median is the mean of the two middle values.
 
-Examples:
-[2,3,4] , the median is 3
+    For examples, if arr = [2,3,4], the median is 3.
+    For examples, if arr = [1,2,3,4], the median is (2 + 3) / 2 = 2.5.
 
-[2,3], the median is (2 + 3) / 2 = 2.5
+You are given an integer array nums and an integer k.
+There is a sliding window of size k which is moving from the very left of the array to the very right.
+You can only see the k numbers in the window. Each time the sliding window moves right by one position.
 
-Given an array nums, there is a sliding window of size k which is moving
-from the very left of the array to the very right. You can only see the k numbers in the window.
-Each time the sliding window moves right by one position.
-Your job is to output the median array for each window in the original array.
+Return the median array for each window in the original array.
+Answers within 10-5 of the actual value will be accepted.
 
-For example,
-Given nums = [1,3,-1,-3,5,3,6,7], and k = 3.
+Example 1:
 
+Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+Output: [1.00000,-1.00000,-1.00000,3.00000,5.00000,6.00000]
+Explanation: 
 Window position                Median
----------------               -----
-[1  3  -1] -3  5  3  6  7       1
+---------------                -----
+[1  3  -1] -3  5  3  6  7        1
  1 [3  -1  -3] 5  3  6  7       -1
  1  3 [-1  -3  5] 3  6  7       -1
- 1  3  -1 [-3  5  3] 6  7       3
- 1  3  -1  -3 [5  3  6] 7       5
- 1  3  -1  -3  5 [3  6  7]      6
-Therefore, return the median sliding window as [1,-1,-1,3,5,6].
+ 1  3  -1 [-3  5  3] 6  7        3
+ 1  3  -1  -3 [5  3  6] 7        5
+ 1  3  -1  -3  5 [3  6  7]       6
 
-Note:
-You may assume k is always valid, ie: k is always smaller than input array's size for non-empty array.
+Example 2:
+
+Input: nums = [1,2,3,4,2,3,1,4,2], k = 3
+Output: [2.00000,3.00000,3.00000,3.00000,2.00000,3.00000,2.00000]
+
+Constraints:
+    1 <= k <= nums.length <= 105
+    -231 <= nums[i] <= 231 - 1
 '''
 from sortedcontainers import SortedList
-# http://www.grantjenks.com/docs/sortedcontainers/#api-documentation
 
 class Solution:
     def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
@@ -41,7 +47,7 @@ class Solution:
         window = SortedList()
 
         for i, e in enumerate(nums):
-            window.add(nums[i])
+            window.add(e)
             if i >= k:
                 window.remove(nums[i - k])
                 
