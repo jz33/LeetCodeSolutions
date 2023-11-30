0c41e4c4d275dfed9282eb789558c4d386906ab0
Jun <junzhengrice@gmail.com>
2023-11-29 21:47:31 -0800

    Hashmap, O(n)

--- 217 Contains Duplicate.py
+++ 217 Contains Duplicate.py
@@ -2,24 +2,32 @@
 217. Contains Duplicate
 https://leetcode.com/problems/contains-duplicate/
 
-Given an array of integers, find if the array contains any duplicates.
-
-Your function should return true if any value appears at least twice in the array,
-and it should return false if every element is distinct.
+Given an integer array nums,
+return true if any value appears at least twice in the array,
+and return false if every element is distinct.
 
 Example 1:
 
-Input: [1,2,3,1]
+Input: nums = [1,2,3,1]
 Output: true
 Example 2:
 
-Input: [1,2,3,4]
+Input: nums = [1,2,3,4]
 Output: false
 Example 3:
 
-Input: [1,1,1,3,3,4,3,2,4,2]
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
 Output: true
+
+Constraints:
+1 <= nums.length <= 105
+-109 <= nums[i] <= 109
 '''
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        return len(set(nums)) < len(nums)
+        seen = set()
+        for e in nums:
+            if e in seen:
+                return True
+            seen.add(e)
+        return False
