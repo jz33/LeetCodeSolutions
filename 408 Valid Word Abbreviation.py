39b83df63134a6ddfa64598b15a2fd154b9dce85
Jun <junzhengrice@gmail.com>
2023-11-30 10:19:29 -0800

    1-D Iteration

--- 408 Valid Word Abbreviation.py
+++ 408 Valid Word Abbreviation.py
@@ -2,7 +2,9 @@
 408. Valid Word Abbreviation
 https://leetcode.com/problems/valid-word-abbreviation/
 
-A string can be abbreviated by replacing any number of non-adjacent, non-empty substrings with their lengths. The lengths should not have leading zeros.
+A string can be abbreviated by replacing any number of non-adjacent,
+non-empty substrings with their lengths.
+The lengths should not have leading zeros.
 
 For example, a string such as "substitution" could be abbreviated as (but not limited to):
 
@@ -18,7 +20,8 @@ The following are not valid abbreviations:
     "s010n" (has leading zeros)
     "s0ubstitution" (replaces an empty substring)
 
-Given a string word and an abbreviation abbr, return whether the string matches the given abbreviation.
+Given a string word and an abbreviation abbr,
+return whether the string matches the given abbreviation.
 
 A substring is a contiguous non-empty sequence of characters within a string.
 
@@ -35,30 +38,28 @@ Output: false
 Explanation: The word "apple" cannot be abbreviated as "a2e".
 
 Constraints:
-
     1 <= word.length <= 20
     word consists of only lowercase English letters.
     1 <= abbr.length <= 10
     abbr consists of lowercase English letters and digits.
     All the integers in abbr will fit in a 32-bit integer.
-
-
 '''
 class Solution:
     def validWordAbbreviation(self, word: str, abbr: str) -> bool:
-        i = 0 # index of `word`
+        wi = 0 # index of `word`
         num = 0 # number in abbr
         for a in abbr:
             if a.isalpha():
                 if num > 0:
-                    i += num
+                    # End of num. Move wi.
+                    wi += num
                     num = 0
 
-                if i >= len(word) or word[i] != a:
+                if wi >= len(word) or word[wi] != a:
                     # Not the same character
                     return False
                     
-                i += 1
+                wi += 1
             else:
                 if num == 0 and a == '0':
                     # Leading zeros
@@ -66,7 +67,5 @@ class Solution:
                 
                 num = num * 10 + int(a)
                 
-        if num > 0:
-            i += num
-        
-        return i == len(word)
+        wi += num
+        return wi == len(word)
