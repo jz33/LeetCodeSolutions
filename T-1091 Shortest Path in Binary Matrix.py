34de3717f27bfac90726914df9048d5a2bad6447
Jun <junzhengrice@gmail.com>
2023-12-04 10:17:14 -0800

    BFS

--- T-1091 Shortest Path in Binary Matrix.py
+++ T-1091 Shortest Path in Binary Matrix.py
@@ -2,40 +2,58 @@
 1091. Shortest Path in Binary Matrix
 https://leetcode.com/problems/shortest-path-in-binary-matrix/
 
-In an N by N square grid, each cell is either empty (0) or blocked (1).
+Given an n x n binary matrix grid, return the length of the shortest clear path in the matrix.
+If there is no clear path, return -1.
 
-A clear path from top-left to bottom-right has length k if and only if it is composed of cells C_1, C_2, ..., C_k such that:
+A clear path in a binary matrix is a path from the top-left cell (i.e., (0, 0))
+to the bottom-right cell (i.e., (n - 1, n - 1)) such that:
 
-Adjacent cells C_i and C_{i+1} are connected 8-directionally (ie., they are different and share an edge or corner)
-C_1 is at location (0, 0) (ie. has value grid[0][0])
-C_k is at location (N-1, N-1) (ie. has value grid[N-1][N-1])
-If C_i is located at (r, c), then grid[r][c] is empty (ie. grid[r][c] == 0).
-Return the length of the shortest such clear path from top-left to bottom-right.
-If such a path does not exist, return -1.
+All the visited cells of the path are 0.
+All the adjacent cells of the path are 8-directionally connected
+(i.e., they are different and they share an edge or a corner).
+
+The length of a clear path is the number of visited cells of this path.
+
+Example 1:
+
+Input: grid = [[0,1],[1,0]]
+Output: 2
+
+Example 2:
+
+Input: grid = [[0,0,0],[1,1,0],[1,1,0]]
+Output: 4
+
+Example 3:
+
+Input: grid = [[1,0,0],[1,1,0],[1,1,0]]
+Output: -1
+
+Constraints:
+    n == grid.length
+    n == grid[i].length
+    1 <= n <= 100
+    grid[i][j] is 0 or 1
 '''
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         if not grid or not grid[0] or grid[0][0] != 0 or grid[-1][-1] != 0:
             return -1
         
-        rowCount = len(grid)
-        colCount = len(grid[0])
-        if rowCount == 1 and colCount == 1:
-            return 1
-
-        seen = [[False] * colCount for _ in range(rowCount)]
+        width = len(grid)
+        seen = [[False] * width for _ in range(width)]
         seen[0][0] = True
         
         points = [(0,0)]
-        steps = 1
+        steps = 0
         while points:
             newPoints = []
-            for x, y in points:   
+            for x, y in points:
+                if x == width - 1 and y == width - 1:
+                    return steps + 1 
                 for d0, d1 in [(1,1),(0,1),(1,0),(1,-1),(0,-1),(-1,1),(-1,0),(-1,-1)]:
                     i, j = x + d0, y + d1
-                    if i == rowCount - 1 and j == colCount - 1:
-                        return steps + 1
-                    if 0 <= i < rowCount and 0 <= j < colCount and grid[i][j] == 0 and not seen[i][j]:
+                    if 0 <= i < width and 0 <= j < width and grid[i][j] == 0 and not seen[i][j]:
                         seen[i][j] = True
                         newPoints.append((i,j))
             steps += 1
