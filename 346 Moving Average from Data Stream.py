da6062d77b4598539a0144ebde90323a6e266a75
Jun <junzhengrice@gmail.com>
2023-12-05 15:26:42 -0800

    Sliding Window

--- 346 Moving Average from Data Stream.py
+++ 346 Moving Average from Data Stream.py
@@ -1,24 +1,38 @@
 '''
 346. Moving Average from Data Stream
-https://leetcode.com/problems/moving-average-from-data-stream/
+https://leetcode.com/problems/moving-average-from-data-stream
 
 Given a stream of integers and a window size,
 calculate the moving average of all integers in the sliding window.
 
-Example:
+Implement the MovingAverage class:
 
-MovingAverage m = new MovingAverage(3);
-m.next(1) = 1
-m.next(10) = (1 + 10) / 2
-m.next(3) = (1 + 10 + 3) / 3
-m.next(5) = (10 + 3 + 5) / 3
+    MovingAverage(int size) Initializes the object with the size of the window size.
+    
+    double next(int val) Returns the moving average of the last size values of the stream.
+   
+Example 1:
+
+Input
+["MovingAverage", "next", "next", "next", "next"]
+[[3], [1], [10], [3], [5]]
+Output
+[null, 1.0, 5.5, 4.66667, 6.0]
+
+Explanation
+MovingAverage movingAverage = new MovingAverage(3);
+movingAverage.next(1); // return 1.0 = 1 / 1
+movingAverage.next(10); // return 5.5 = (1 + 10) / 2
+movingAverage.next(3); // return 4.66667 = (1 + 10 + 3) / 3
+movingAverage.next(5); // return 6.0 = (10 + 3 + 5) / 3
+
+Constraints:
+    1 <= size <= 1000
+    -105 <= val <= 105
+    At most 104 calls will be made to next.
 '''
 class MovingAverage:
-
     def __init__(self, size: int):
-        """
-        Initialize your data structure here.
-        """
         self.size = size
         self.queue = collections.deque()
         self.total = 0
@@ -29,5 +43,4 @@ class MovingAverage:
         if len(self.queue) > self.size:
             poped = self.queue.popleft()
             self.total -= poped
-            
         return self.total / len(self.queue)
