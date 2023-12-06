756e89f620a9c2a52ecbca694a4e23c6a21bd09d
Jun <junzhengrice@gmail.com>
2023-12-06 10:30:01 -0800

    Two Pointers Merge

--- 088 Merge Sorted Array.ts
+++ 088 Merge Sorted Array.ts
@@ -2,16 +2,18 @@
 88 Merge Sorted Array
 https://leetcode.com/problems/merge-sorted-array/
 
-You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n,
+You are given two integer arrays nums1 and nums2,
+sorted in non-decreasing order, and two integers m and n,
 representing the number of elements in nums1 and nums2 respectively.
 
 Merge nums1 and nums2 into a single array sorted in non-decreasing order.
 
-The final sorted array should not be returned by the function, but instead be stored inside the array nums1.
-To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged,
-and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
-
- 
+The final sorted array should not be returned by the function,
+but instead be stored inside the array nums1.
+To accommodate this, nums1 has a length of m + n,
+where the first m elements denote the elements that should be merged,
+and the last n elements are set to 0 and should be ignored.
+nums2 has a length of n.
 
 Example 1:
 
@@ -35,20 +37,14 @@ Explanation: The arrays we are merging are [] and [1].
 The result of the merge is [1].
 Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
 
- 
-
 Constraints:
-
     nums1.length == m + n
     nums2.length == n
     0 <= m, n <= 200
     1 <= m + n <= 200
     -109 <= nums1[i], nums2[j] <= 109
 
- 
-
 Follow up: Can you come up with an algorithm that runs in O(m + n) time?
-
 */
 function merge(nums1: number[], m: number, nums2: number[], n: number): void {
     let i: number = m - 1; // Reader of nums1
