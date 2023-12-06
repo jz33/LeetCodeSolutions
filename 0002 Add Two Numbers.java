5248e31d2dbfd8da28b5b45733b5ec327d66e0ea
Jun <junzhengrice@gmail.com>
2023-12-05 22:48:05 -0800

    Two Pointers Merge

--- 0002 Add Two Numbers.java
+++ 0002 Add Two Numbers.java
@@ -25,6 +25,7 @@ Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
 Output: [8,9,9,9,0,0,0,1]
 
 Constraints:
+
     The number of nodes in each linked list is in the range [1, 100].
     0 <= Node.val <= 9
     It is guaranteed that the list represents a number that does not have leading zeros.
