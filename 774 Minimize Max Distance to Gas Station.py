13267913917d58b418a8b35e2300289fd8cf13f4
Jun <junzhengrice@gmail.com>
2023-12-05 22:28:35 -0800

    Binary Guess

--- 774 Minimize Max Distance to Gas Station.py
+++ 774 Minimize Max Distance to Gas Station.py
@@ -1,59 +1,70 @@
-'''
+"""
 774. Minimize Max Distance to Gas Station
 https://leetcode.com/problems/minimize-max-distance-to-gas-station/
 
-On a horizontal number line, we have gas stations at positions stations[0], stations[1], ..., stations[N-1],
-where N = stations.length.
+You are given an integer array stations that represents the positions
+of the gas stations on the x-axis. You are also given an integer k.
 
-Now, we add K more gas stations so that D, the maximum distance between adjacent gas stations, is minimized.
+You should add k new gas stations.
+You can add the stations anywhere on the x-axis, and not necessarily on an integer position.
 
-Return the smallest possible value of D.
+Let penalty() be the maximum distance between adjacent gas stations after adding the k new stations.
 
-Example:
+Return the smallest possible value of penalty().
+Answers within 10-6 of the actual answer will be accepted.
 
-Input: stations = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], K = 9
-Output: 0.500000
-Note:
+Example 1:
 
-stations.length will be an integer in range [10, 2000].
-stations[i] will be an integer in range [0, 10^8].
-K will be an integer in range [1, 10^6].
-Answers within 10^-6 of the true value will be accepted as correct.
-'''
-episilon = 10 ** (-6)
+Input: stations = [1,2,3,4,5,6,7,8,9,10], k = 9
+Output: 0.50000
+
+Example 2:
+
+Input: stations = [23,24,36,39,46,56,57,65,84,98], k = 1
+Output: 14.00000
+
+Constraints:
+    10 <= stations.length <= 2000
+    0 <= stations[i] <= 108
+    stations is sorted in a strictly increasing order.
+    1 <= k <= 106
+"""
+
+epsilon = 10 ** (-6)
+
 
 class Solution:
     def minmaxGasDist(self, stations: List[int], K: int) -> float:
-        '''
+        """
         Use idea from 875 Koko Eating Bananas
-        '''
+        """
         histo = collections.Counter()
         for i in range(1, len(stations)):
-            dist = stations[i] - stations[i-1]
+            dist = stations[i] - stations[i - 1]
             histo[dist] += 1
 
         # left is minimum possible distance
         # right is maximum possible distance
-        right = float(max(histo.items(), key = lambda x : x[0])[0])
+        right = float(max(histo.items(), key=lambda x: x[0])[0])
         left = right / (K + 1)
 
         res = right
         while left <= right:
             mid = left + (right - left) / 2.0
-            
+
             # Count total cut needed if every distance is less or equal to mid
             cuts = 0
-            for dist,ctr in histo.items():
+            for dist, ctr in histo.items():
                 t = dist / mid
                 if math.isclose(t, int(t)):
                     cuts += ctr * (int(t) - 1)
                 else:
                     cuts += ctr * (int(t))
 
-            if cuts <= K:          
+            if cuts <= K:
                 res = mid
-                right = mid - episilon
+                right = mid - epsilon
             else:
-                left = mid + episilon
+                left = mid + epsilon
 
         return res
