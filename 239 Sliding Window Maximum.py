364e4f0e064fe9e67287b3fbc452ea06c0bd1665
Jun <junzhengrice@gmail.com>
2023-12-07 11:38:27 -0800

    Sliding Window

--- 239 Sliding Window Maximum.py
+++ 239 Sliding Window Maximum.py
@@ -2,18 +2,19 @@
 239. Sliding Window Maximum
 https://leetcode.com/problems/sliding-window-maximum/
 
-Given an array nums, there is a sliding window of size k which is moving from
-the very left of the array to the very right.
+You are given an array of integers nums,
+there is a sliding window of size k which is moving
+from the very left of the array to the very right.
 You can only see the k numbers in the window.
 Each time the sliding window moves right by one position.
+
 Return the max sliding window.
 
-Example:
+Example 1:
 
-Input: nums = [1,3,-1,-3,5,3,6,7], and k = 3
-Output: [3,3,5,5,6,7] 
+Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+Output: [3,3,5,5,6,7]
 Explanation: 
-
 Window position                Max
 ---------------               -----
 [1  3  -1] -3  5  3  6  7       3
@@ -22,25 +23,30 @@ Window position                Max
  1  3  -1 [-3  5  3] 6  7       5
  1  3  -1  -3 [5  3  6] 7       6
  1  3  -1  -3  5 [3  6  7]      7
+
+Example 2:
+
+Input: nums = [1], k = 1
+Output: [1]
+
+Constraints:
+    1 <= nums.length <= 105
+    -104 <= nums[i] <= 104
+    1 <= k <= nums.length
 '''
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        dq = collections.deque()
-        res = []
-         
-        for i,c in enumerate(nums):
-            # dq is descending, dq[0] is the max of current substring
-            while dq and c > nums[dq[-1]]:
-                dq.pop()
-            
-            # pop the out of bound indexes
-            # i-k+1 is the smallest index of current substring
-            while dq and i-k+1 > dq[0]:
-                dq.popleft()
-                
-            dq.append(i)
-            
-            if i-k+1 >= 0:
-                res.append(nums[dq[0]])
-                
-        return res
+        window = collections.deque() # [index of large numbers, the left is largest]
+        result = []
+        for i, v in enumerate(nums):
+            # Extend
+            while window and v > nums[window[-1]]:
+                window.pop()
+            # Shrink
+            if window and window[0] <= i - k:
+                window.popleft()
+            # Result
+            window.append(i)
+            if i >= k - 1:
+                result.append(nums[window[0]])
+        return result
