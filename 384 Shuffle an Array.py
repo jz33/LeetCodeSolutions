3a6a85153f6022c5be11d1836646381ff1d6285f
Jun <junzhengrice@gmail.com>
2023-12-07 13:04:16 -0800

    1-D Iteration

--- 384 Shuffle an Array.py
+++ 384 Shuffle an Array.py
@@ -2,44 +2,48 @@
 384. Shuffle an Array
 https://leetcode.com/problems/shuffle-an-array/
 
-Shuffle a set of numbers without duplicates.
+Given an integer array nums, design an algorithm to randomly shuffle the array.
+All permutations of the array should be equally likely as a result of the shuffling.
 
-Example:
+Implement the Solution class:
 
-// Init an array with set 1, 2, and 3.
-int[] nums = {1,2,3};
-Solution solution = new Solution(nums);
+    Solution(int[] nums) Initializes the object with the integer array nums.
+    int[] reset() Resets the array to its original configuration and returns it.
+    int[] shuffle() Returns a random shuffling of the array.
 
-// Shuffle the array [1,2,3] and return its result. Any permutation of [1,2,3] must equally likely to be returned.
-solution.shuffle();
+Example 1:
 
-// Resets the array back to its original configuration [1,2,3].
-solution.reset();
+Input
+["Solution", "shuffle", "reset", "shuffle"]
+[[[1, 2, 3]], [], [], []]
+Output
+[null, [3, 1, 2], [1, 2, 3], [1, 3, 2]]
 
-// Returns the random shuffling of array [1,2,3].
-solution.shuffle();
+Explanation
+Solution solution = new Solution([1, 2, 3]);
+solution.shuffle();    // Shuffle the array [1,2,3] and return its result.
+                       // Any permutation of [1,2,3] must be equally likely to be returned.
+                       // Example: return [3, 1, 2]
+solution.reset();      // Resets the array back to its original configuration [1,2,3]. Return [1, 2, 3]
+solution.shuffle();    // Returns the random shuffling of array [1,2,3]. Example: return [1, 3, 2]
+
+Constraints:
+    1 <= nums.length <= 50
+    -106 <= nums[i] <= 106
+    All the elements of nums are unique.
+    At most 104 calls in total will be made to reset and shuffle.
 '''
-from random import randint
-from copy import deepcopy
-
 class Solution:
 
     def __init__(self, nums: List[int]):
-        self.arr = nums
+        self.nums = nums
 
     def reset(self) -> List[int]:
-        """
-        Resets the array to its original configuration and return it.
-        """
-        return self.arr
+        return self.nums
 
     def shuffle(self) -> List[int]:
-        """
-        Returns a random shuffling of the array.
-        """
-        res = deepcopy(self.arr)
-        size = len(res)
-        for i in range(size - 1):
-            togo = randint(i, size - 1)
-            res[i], res[togo] = res[togo], res[i]
-        return res
+        copy = self.nums[:]
+        for i in range(len(copy) - 1):
+            togo = random.randint(i, len(copy) - 1)
+            copy[i], copy[togo] = copy[togo], copy[i]
+        return copy
