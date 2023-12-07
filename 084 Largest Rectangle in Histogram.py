1cfc09821a856a2e94cbfbb4bf9e251c32a20ff5
Jun <junzhengrice@gmail.com>
2023-12-07 13:56:52 -0800

    Monotonic Stack

--- 084 Largest Rectangle in Histogram.py
+++ 084 Largest Rectangle in Histogram.py
@@ -1,30 +1,39 @@
 '''
 84 Largest Rectangle in Histogram
-https://leetcode.com/company/google/
+https://leetcode.com/problems/largest-rectangle-in-histogram/
 
-Given n non-negative integers representing the histogram's bar height where the width of each bar is 1,
-find the area of largest rectangle in the histogram.
+Given an array of integers heights representing the histogram's bar height
+where the width of each bar is 1,
+return the area of the largest rectangle in the histogram.
 
-Above is a histogram where width of each bar is 1, given height = [2,1,5,6,2,3].
+Example 1:
 
-The largest rectangle is shown in the shaded area, which has area = 10 unit.
-
-Example:
-
-Input: [2,1,5,6,2,3]
+Input: heights = [2,1,5,6,2,3]
 Output: 10
+Explanation: The above is a histogram where width of each bar is 1.
+The largest rectangle is shown in the red area, which has an area = 10 units.
+
+Example 2:
+
+Input: heights = [2,4]
+Output: 4
+
+Constraints:
+    1 <= heights.length <= 105
+    0 <= heights[i] <= 104
 '''
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        stack = []
+        stack = [] # [decreasing indexes of heights]
         maxArea = 0
         
         def compute(i, h):
             nonlocal stack, maxArea
+            # Compute previous rectangle size if height decreased on i,
+            # as i cannot build a rectange with previous indexes
             while stack and h < heights[stack[-1]]:
-                j = stack.pop()
-                recHeight = heights[j]
-                
+                last = stack.pop()
+                recHeight = heights[last]
                 # The rectangle width is from stack[-1] (exclusive) to i (exclusive)
                 recWidth = i - 1 - stack[-1] if stack else i
                 maxArea = max(maxArea, recHeight * recWidth)
