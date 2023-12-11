1bedeed698100c83db3eacf923e1fff20b795aee
junzhengdev <jun.zheng@convoy.com>
2023-12-10 23:16:30 -0800

    Level Order Traversal

--- 314 Binary Tree Vertical Order Traversal.py
+++ 314 Binary Tree Vertical Order Traversal.py
@@ -1,113 +1,58 @@
 '''
 314. Binary Tree Vertical Order Traversal
-https://leetcode.com/problems/binary-tree-vertical-order-traversal/
+https://leetcode.com/problems/binary-tree-vertical-order-traversal
 
-Given a binary tree, return the vertical order traversal of its nodes' values.
-(ie, from top to bottom, column by column).
+Given the root of a binary tree,
+return the vertical order traversal of its nodes' values.
+(i.e., from top to bottom, column by column).
 
 If two nodes are in the same row and column, the order should be from left to right.
 
-Examples 1:
+Example 1:
 
-Input: [3,9,20,null,null,15,7]
+Input: root = [3,9,20,null,null,15,7]
+Output: [[9],[3,15],[20],[7]]
 
-   3
-  /\
- /  \
- 9  20
-    /\
-   /  \
-  15   7 
+Example 2:
 
-Output:
+Input: root = [3,9,8,4,0,1,7]
+Output: [[4],[9],[3,0,1],[8],[7]]
 
-[
-  [9],
-  [3,15],
-  [20],
-  [7]
-]
-Examples 2:
+Example 3:
 
-Input: [3,9,8,4,0,1,7]
+Input: root = [3,9,8,4,0,1,7,null,null,null,2,5]
+Output: [[4],[9,5],[3,0,1],[8,2],[7]]
 
-     3
-    /\
-   /  \
-   9   8
-  /\  /\
- /  \/  \
- 4  01   7 
-
-Output:
-
-[
-  [4],
-  [9],
-  [3,0,1],
-  [8],
-  [7]
-]
-Examples 3:
-
-Input: [3,9,8,4,0,1,7,null,null,null,2,5] (0's right child is 2 and 1's left child is 5)
-
-     3
-    /\
-   /  \
-   9   8
-  /\  /\
- /  \/  \
- 4  01   7
-    /\
-   /  \
-   5   2
-
-Output:
-
-[
-  [4],
-  [9,5],
-  [3,0,1],
-  [8,2],
-  [7]
-]
+Constraints:
+    The number of nodes in the tree is in the range [0, 100].
+    -100 <= Node.val <= 100
 '''
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
+    '''
+    Very similar to 987. Vertical Order Traversal of a Binary Tree
+    '''
     def verticalOrder(self, root: TreeNode) -> List[List[int]]:
         if not root:
             return []
         
-        pairs = [(root, 0)] # [(node, x offset)]
+        nodes = [(root, 0)] # [(node, x offset)]
         book = collections.defaultdict(list) # {x offset : [values]}
         minOffset = 0
         maxOffset = 0
-        while pairs:
-            newPairs = []
-            for node, offset in pairs:
+        while nodes:
+            newNodes = []
+            for node, offset in nodes:
                 book[offset].append(node.val)
                 minOffset = min(minOffset, offset)
                 maxOffset = max(maxOffset, offset)
                 if node.left:
-                    newPairs.append((node.left, offset - 1))
+                    newNodes.append((node.left, offset - 1))
                 if node.right:
-                    newPairs.append((node.right, offset + 1))
-            pairs = newPairs
+                    newNodes.append((node.right, offset + 1))
+            nodes = newNodes
             
         # User min/max offset to avoid sorting
-        res = []
+        result = []
         for offset in range(minOffset, maxOffset + 1):
-            res.append(book[offset])
-        return res
+            result.append(book[offset])
+        return result
