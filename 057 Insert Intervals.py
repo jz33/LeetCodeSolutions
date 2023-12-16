d9b8739e5981df1708936294386bff698dff3875
Jun <junzhengrice@gmail.com>
2023-12-16 00:26:29 -0800

    1-D Iteration, Intervals

--- 057 Insert Intervals.py
+++ 057 Insert Intervals.py
@@ -2,45 +2,53 @@
 57. Insert Interval
 https://leetcode.com/problems/insert-interval/
 
-Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
+You are given an array of non-overlapping intervals intervals where
+intervals[i] = [starti, endi] represent the start and the end of the ith interval and
+intervals is sorted in ascending order by starti.
+You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
 
-You may assume that the intervals were initially sorted according to their start times.
+Insert newInterval into intervals such that intervals is still sorted in ascending order by
+starti and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
+
+Return intervals after the insertion.
 
 Example 1:
 
 Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
 Output: [[1,5],[6,9]]
+
 Example 2:
 
 Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
 Output: [[1,2],[3,10],[12,16]]
-
 Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+
+Constraints:
+    0 <= intervals.length <= 104
+    intervals[i].length == 2
+    0 <= starti <= endi <= 105
+    intervals is sorted by starti in ascending order.
+    newInterval.length == 2
+    0 <= start <= end <= 105
 '''
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        res = []
-        
-        # left and right boundary for overlapped interval
-        left, right = newInterval[0], newInterval[1] 
-        
-        for it in intervals:
-            if it[1] < newInterval[0]:
-                # 1. Non-overlapping, it is in left branch
-                res.append(it)
-            elif it[0] > newInterval[1]:
-                # 2. Non-overlapping, it is in right branch
-                # Need to append the overlapped interval
-                if left is not None:
-                    res.append([left, right])
-                    left = None # indicates overlapped interval is inserted
-                res.append(it)
+        result = []
+        newLeft, newRight = newInterval
+        inserted = False
+        for interval in intervals:
+            left, right = interval
+            if right < newLeft:
+                result.append(interval)
+            elif left > newRight:
+                if not inserted:
+                    inserted = True
+                    result.append([newLeft, newRight])
+                result.append(interval)
             else:
-                # 3. Overlapping now. Update boundaries
-                left = min(left, it[0])
-                right = max(right, it[1])
-        
-        if left is not None:
-            res.append([left, right])
-            
-        return res
+                newLeft = min(newLeft, left)
+                newRight = max(newRight, right)
+                
+        if not inserted:
+            result.append([newLeft, newRight])
+        return result
