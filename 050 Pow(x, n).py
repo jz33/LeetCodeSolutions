bf5ee01b3f08d2d8034832b2635ad84ef76e812e
junzhengdev <jun.zheng@convoy.com>
2023-12-18 11:12:36 -0800

    Math Binary

---
+++ 050 Pow(x, n).py
@@ -0,0 +1,44 @@
+'''
+50. Pow(x, n)
+https://leetcode.com/problems/powx-n/
+
+Example 1:
+
+Input: x = 2.00000, n = 10
+Output: 1024.00000
+
+Example 2:
+
+Input: x = 2.10000, n = 3
+Output: 9.26100
+
+Example 3:
+
+Input: x = 2.00000, n = -2
+Output: 0.25000
+Explanation: 2-2 = 1/22 = 1/4 = 0.25
+
+Constraints:
+    -100.0 < x < 100.0
+    -231 <= n <= 231-1
+    n is an integer.
+    Either x is not zero or n > 0.
+    -104 <= xn <= 104
+'''
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        result = 1
+        base = x
+        exp = n
+        if exp < 0:
+            exp = -exp
+            base = 1 / base
+
+        # Use binary to achieve constant time
+        while exp > 0:
+            if exp & 1:
+                result *= base
+            # The exp shifts right equals base double
+            exp >>= 1
+            base *= base
+        return result
