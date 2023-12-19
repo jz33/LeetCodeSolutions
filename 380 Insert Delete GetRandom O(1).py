357f5b2bbfdd0fa791908c511cd079f068201dcf
junzhengdev <jun.zheng@convoy.com>
2023-12-19 14:02:37 -0800

    hashmap

--- 380 Insert Delete GetRandom O(1).py
+++ 380 Insert Delete GetRandom O(1).py
@@ -2,87 +2,86 @@
 380. Insert Delete GetRandom O(1)
 https://leetcode.com/problems/insert-delete-getrandom-o1/
 
-Design a data structure that supports all following operations in average O(1) time.
+Implement the RandomizedSet class:
 
-insert(val): Inserts an item val to the set if not already present.
-remove(val): Removes an item val from the set if present.
-getRandom: Returns a random element from current set of elements.
+RandomizedSet() Initializes the RandomizedSet object.
+
+bool insert(int val) Inserts an item val into the set if not present.
+Returns true if the item was not present, false otherwise.
+
+bool remove(int val) Removes an item val from the set if present.
+Returns true if the item was present, false otherwise.
+
+int getRandom() Returns a random element from the current set of elements
+(it's guaranteed that at least one element exists when this method is called).
 Each element must have the same probability of being returned.
 
-Example:
+You must implement the functions of the class such that each function works in average O(1) time complexity.
 
-// Init an empty set.
-RandomizedSet randomSet = new RandomizedSet();
+Example 1:
 
-// Inserts 1 to the set. Returns true as 1 was inserted successfully.
-randomSet.insert(1);
+Input
+["RandomizedSet", "insert", "remove", "insert", "getRandom", "remove", "insert", "getRandom"]
+[[], [1], [2], [2], [], [1], [2], []]
+Output
+[null, true, false, true, 2, true, false, 2]
 
-// Returns false as 2 does not exist in the set.
-randomSet.remove(2);
+Explanation
+RandomizedSet randomizedSet = new RandomizedSet();
+randomizedSet.insert(1); // Inserts 1 to the set. Returns true as 1 was inserted successfully.
+randomizedSet.remove(2); // Returns false as 2 does not exist in the set.
+randomizedSet.insert(2); // Inserts 2 to the set, returns true. Set now contains [1,2].
+randomizedSet.getRandom(); // getRandom() should return either 1 or 2 randomly.
+randomizedSet.remove(1); // Removes 1 from the set, returns true. Set now contains [2].
+randomizedSet.insert(2); // 2 was already in the set, so return false.
+randomizedSet.getRandom(); // Since 2 is the only number in the set, getRandom() will always return 2.
 
-// Inserts 2 to the set, returns true. Set now contains [1,2].
-randomSet.insert(2);
-
-// getRandom should return either 1 or 2 randomly.
-randomSet.getRandom();
-
-// Removes 1 from the set, returns true. Set now contains [2].
-randomSet.remove(1);
-
-// 2 was already in the set, so return false.
-randomSet.insert(2);
-
-// Since 2 is the only number in the set, getRandom always return 2.
-randomSet.getRandom();
+Constraints:
+    -231 <= val <= 231 - 1
+    At most 2 * 105 calls will be made to insert, remove, and getRandom.
+    There will be at least one element in the data structure when getRandom is called.
 '''
 class RandomizedSet:
-
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.dic = {} # {value : index}
-        self.arr = [] # [values]
+        self.valueToIndex = {} # { value : index}
+        self.values = []
         
-
     def insert(self, val: int) -> bool:
         """
         Inserts a value to the set. Returns true if the set did not already contain the specified element.
         """
-        existed = val in self.dic
-        if not existed:
-            self.arr.append(val)
-            self.dic[val] = len(self.arr) - 1
-        return not existed
+        if val in self.valueToIndex:
+            return False
+        else:
+            self.values.append(val)
+            self.valueToIndex[val] = len(self.values) - 1
+            return True
         
-
     def remove(self, val: int) -> bool:
         """
         Removes a value from the set. Returns true if the set contained the specified element.
         """
-        dic, arr = self.dic, self.arr
-        existed = val in dic
-        if existed:
-            # Remove from dic
-            pos = dic[val]
-            del dic[val]
+        index = self.valueToIndex.get(val, None)
+        if index is not None:
+            # Remove from amp
+            del self.valueToIndex[val]
             
             # Swap last value to pos
-            if pos != len(arr) - 1:               
-                lastVal = arr[-1]
-                arr[pos] = lastVal
+            if index != len(self.values) - 1:               
+                lastVal = self.values[-1]
+                self.values[index] = lastVal
                 
-                # Update dic on last value
-                dic[lastVal] = pos
+                # Update map for last value
+                self.valueToIndex[lastVal] = index
             
             # pop val from arr
-            arr.pop()
-
-        return existed  
-        
+            self.values.pop()
+            return True
+        else:
+            return False
 
     def getRandom(self) -> int:
         """
         Get a random element from the set.
         """
-        return random.choice(self.arr)
+        return random.choice(self.values)
