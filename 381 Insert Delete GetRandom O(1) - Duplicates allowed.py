73b2cdd2bcd656004276a8910b149469547963eb
junzheng2023 <jzheng3@flexport.com>
2024-01-03 14:36:33 -0800

    Hashmap

--- 381 Insert Delete GetRandom O(1) - Duplicates allowed.py
+++ 381 Insert Delete GetRandom O(1) - Duplicates allowed.py
@@ -2,84 +2,97 @@
 381. Insert Delete GetRandom O(1) - Duplicates allowed
 https://leetcode.com/problems/insert-delete-getrandom-o1-duplicates-allowed/
 
-Design a data structure that supports all following operations in average O(1) time.
+RandomizedCollection is a data structure that contains a collection of numbers, possibly duplicates (i.e., a multiset). It should support inserting and removing specific elements and also reporting a random element.
 
-Note: Duplicate elements are allowed.
-insert(val): Inserts an item val to the collection.
-remove(val): Removes an item val from the collection if present.
-getRandom: Returns a random element from current collection of elements.
-The probability of each element being returned is linearly related to the number of same value the collection contains.
+Implement the RandomizedCollection class:
 
-Example:
+    RandomizedCollection() Initializes the empty RandomizedCollection object.
+    bool insert(int val) Inserts an item val into the multiset, even if the item is already present. Returns true if the item is not present, false otherwise.
+    bool remove(int val) Removes an item val from the multiset if present. Returns true if the item is present, false otherwise. Note that if val has multiple occurrences in the multiset, we only remove one of them.
+    int getRandom() Returns a random element from the current multiset of elements. The probability of each element being returned is linearly related to the number of the same values the multiset contains.
 
-// Init an empty collection.
-RandomizedCollection collection = new RandomizedCollection();
+You must implement the functions of the class such that each function works on average O(1) time complexity.
 
-// Inserts 1 to the collection. Returns true as the collection did not contain 1.
-collection.insert(1);
+Note: The test cases are generated such that getRandom will only be called if there is at least one item in the RandomizedCollection.
 
-// Inserts another 1 to the collection. Returns false as the collection contained 1. Collection now contains [1,1].
-collection.insert(1);
+Example 1:
 
-// Inserts 2 to the collection, returns true. Collection now contains [1,1,2].
-collection.insert(2);
+Input
+["RandomizedCollection", "insert", "insert", "insert", "getRandom", "remove", "getRandom"]
+[[], [1], [1], [2], [], [1], []]
+Output
+[null, true, false, true, 2, true, 1]
 
-// getRandom should return 1 with the probability 2/3, and returns 2 with the probability 1/3.
-collection.getRandom();
+Explanation
+RandomizedCollection randomizedCollection = new RandomizedCollection();
+randomizedCollection.insert(1);   // return true since the collection does not contain 1.
+                                  // Inserts 1 into the collection.
+randomizedCollection.insert(1);   // return false since the collection contains 1.
+                                  // Inserts another 1 into the collection. Collection now contains [1,1].
+randomizedCollection.insert(2);   // return true since the collection does not contain 2.
+                                  // Inserts 2 into the collection. Collection now contains [1,1,2].
+randomizedCollection.getRandom(); // getRandom should:
+                                  // - return 1 with probability 2/3, or
+                                  // - return 2 with probability 1/3.
+randomizedCollection.remove(1);   // return true since the collection contains 1.
+                                  // Removes 1 from the collection. Collection now contains [1,2].
+randomizedCollection.getRandom(); // getRandom should return 1 or 2, both equally likely.
 
-// Removes 1 from the collection, returns true. Collection now contains [1,2].
-collection.remove(1);
-
-// getRandom should return 1 and 2 both equally likely.
-collection.getRandom();
+Constraints:
+    -231 <= val <= 231 - 1
+    At most 2 * 105 calls in total will be made to insert, remove, and getRandom.
+    There will be at least one element in the data structure when getRandom is called.
 '''
+import collections, random
+
 class RandomizedCollection:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.dic = collections.defaultdict(set) # {value : set[indexes]}
-        self.arr = [] # [values]
+        self.valueToIndexes = collections.defaultdict(set)
+        self.values = []
 
     def insert(self, val: int) -> bool:
-        """
-        Inserts a value to the collection. Returns true if the collection did not already contain the specified element.
-        """
-        existed = val in self.dic
-        self.arr.append(val)
-        self.dic[val].add(len(self.arr) - 1)
-        return not existed
+        # valueToIndexes is a defaultdict, so don't use valueToIndexes.get(val),
+        # which will return [] if not present
+        self.values.append(val)
+        indexes = self.valueToIndexes[val]
+        indexes.add(len(self.values)-1)
+        return len(indexes) == 1
         
     def remove(self, val: int) -> bool:
-        """
-        Removes a value from the collection. Returns true if the collection contained the specified element.
-        """
-        dic, arr = self.dic, self.arr
-        existed = val in dic        
-        if existed:
+        indexes = self.valueToIndexes[val]
+        if indexes:
+            index = indexes.pop()
+            if len(indexes) == 0:
+                self.valueToIndexes.pop(val)
             
-            # Remove a random index from dic
-            pos = dic[val].pop()
-            if not dic[val]:
-                del dic[val]
-            
-            # Swap last value to pos
-            lastIndex = len(arr) - 1
-            if pos != lastIndex:
-                lastVal = arr[-1]
-                arr[pos] = lastVal
+            lastIndex = len(self.values) - 1
+            if index != lastIndex:
+                # If index is not the last val, update last val's references
+                lastVal = self.values[lastIndex]
+                self.values[index] = lastVal
 
-                dic[lastVal].remove(lastIndex)
-                dic[lastVal].add(pos)
+                lastValIndexes = self.valueToIndexes[lastVal]
+                lastValIndexes.remove(lastIndex)
+                lastValIndexes.add(index)
             
-            # Remove from arr
-            arr.pop()
+            self.values.pop()
+            return True
+        else:
+            return False
         
-        return existed
-
     def getRandom(self) -> int:
-        """
-        Get a random element from the collection.
-        """
-        return random.choice(self.arr)
+        return random.choice(self.values)
+    
+
+    def debug(self):
+        print(self.values)
+        print(self.valueToIndexes)
+
+
+sol = RandomizedCollection()
+sol.insert(1)
+sol.insert(1)
+sol.debug()
+sol.remove(1)
+sol.debug()
