58dd9f1393ad3c3bcc1219e551783a195b6669d7
jz33 <jun.zheng@convoy.com>
2024-01-05 00:44:18 -0800

    Preorder Traversal

---
+++ 129 Sum Root to Leaf Numbers.py
@@ -0,0 +1,64 @@
+'''
+129 Sum Root to Leaf Numbers
+https://leetcode.com/problems/sum-root-to-leaf-numbers/
+
+You are given the root of a binary tree containing digits from 0 to 9 only.
+
+Each root-to-leaf path in the tree represents a number.
+
+    For example, the root-to-leaf path 1 -> 2 -> 3 represents the number 123.
+
+Return the total sum of all root-to-leaf numbers.
+
+Test cases are generated so that the answer will fit in a 32-bit integer.
+
+A leaf node is a node with no children.
+
+Example 1:
+
+Input: root = [1,2,3]
+Output: 25
+Explanation:
+The root-to-leaf path 1->2 represents the number 12.
+The root-to-leaf path 1->3 represents the number 13.
+Therefore, sum = 12 + 13 = 25.
+
+Example 2:
+
+Input: root = [4,9,0,5,1]
+Output: 1026
+Explanation:
+The root-to-leaf path 4->9->5 represents the number 495.
+The root-to-leaf path 4->9->1 represents the number 491.
+The root-to-leaf path 4->0 represents the number 40.
+Therefore, sum = 495 + 491 + 40 = 1026.
+
+Constraints:
+    The number of nodes in the tree is in the range [1, 1000].
+    0 <= Node.val <= 9
+    The depth of the tree will not exceed 10.
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        
+        total = 0
+        def preorder(node: TreeNode, sofar: int):
+            nonlocal total
+            sofar = sofar * 10 + node.val
+            if not node.left and not node.right:
+                total += sofar
+            if node.left:
+                preorder(node.left, sofar)
+            if node.right:
+                preorder(node.right, sofar)
+        
+        preorder(root, 0)
+        return total
