2df32e5f9ea1939c73be1ad202b6bff8c3a287da
Jun <junzhengrice@gmail.com>
2024-01-08 11:02:02 -0800

    Binary Guess

--- 875 Koko Eating Bananas.py
+++ 875 Koko Eating Bananas.py
@@ -2,41 +2,53 @@
 875. Koko Eating Bananas
 https://leetcode.com/problems/koko-eating-bananas/
 
-Koko loves to eat bananas.There are N piles of bananas, the i-th pile has piles[i] bananas.
-The guards have gone and will come back in H hours.
+Koko loves to eat bananas. There are n piles of bananas,
+the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.
 
-Koko can decide her bananas-per-hour eating speed of K.
-Each hour, she chooses some pile of bananas, and eats K bananas from that pile.
-If the pile has less than K bananas, she eats all of them instead, and won't eat any more bananas during this hour.
+Koko can decide her bananas-per-hour eating speed of k.
+Each hour, she chooses some pile of bananas and eats k bananas from that pile.
+If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour.
 
-Koko likes to eat slowly, but still wants to finish eating all the bananas before the guards come back.
+Koko likes to eat slowly but still wants to finish eating all the bananas before the guards return.
 
-Return the minimum integer K such that she can eat all the bananas within H hours.
+Return the minimum integer k such that she can eat all the bananas within h hours.
 
 Example 1:
 
-Input: piles = [3,6,7,11], H = 8
+Input: piles = [3,6,7,11], h = 8
 Output: 4
 
 Example 2:
 
-Input: piles = [30,11,23,4,20], H = 5
+Input: piles = [30,11,23,4,20], h = 5
 Output: 30
+
+Example 3:
+
+Input: piles = [30,11,23,4,20], h = 6
+Output: 23
+
+Constraints:
+    1 <= piles.length <= 104
+    piles.length <= h <= 109
+    1 <= piles[i] <= 109
 '''
 class Solution:
-    def getHours(self, piles: List[int], k: int) -> int:
-        return sum(p // k if p % k == 0 else p // k + 1 for p in piles)
-        
-    def minEatingSpeed(self, piles: List[int], H: int) -> int:
+    '''
+    Binary Guess
+    '''
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        # The initial guess range is [1 : max(piles)]
         left = 1
         right = max(piles)
         k = right
         while left <= right:
             mid = left + (right - left) // 2
-            hours = self.getHours(piles, mid)
-            if hours <= H:
-                k = min(k, mid)
+            eatingHours = sum(p // mid if p % mid == 0 else p // mid + 1 for p in piles)
+            if eatingHours <= h:
+                k = mid
                 right = mid - 1
             else:
                 left = mid + 1
         return k
+
