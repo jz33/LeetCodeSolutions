426b2192988a91aceb4e959152a01766ab69abb5
junzheng2023 <jzheng3@flexport.com>
2024-01-12 14:42:57 -0800

    Sliding Window

--- 076 Minimum Window Substring.py
+++ 076 Minimum Window Substring.py
@@ -3,8 +3,7 @@
 https://leetcode.com/problems/minimum-window-substring/
 
 Given two strings s and t of lengths m and n respectively, return the minimum window
-substring
-of s such that every character in t (including duplicates) is included in the window.
+substring of s such that every character in t (including duplicates) is included in the window.
 If there is no such substring, return the empty string "".
 
 The testcases will be generated such that the answer is unique.
@@ -28,19 +27,19 @@ Output: ""
 Explanation: Both 'a's from t must be included in the window.
 Since the largest window of s only has one 'a', return empty string.
 
-
 Constraints:
     m == s.length
     n == t.length
     1 <= m, n <= 105
     s and t consist of uppercase and lowercase English letters.
 '''
+from collections import Counter
 class SlidingWindow:
     def __init__(self, pattern: str):
-        self.patternCounter = collections.Counter(pattern)
+        self.patternCounter = Counter(pattern)
         self.patternSize = len(pattern)
         self.validCount = 0
-        self.counter = collections.Counter()
+        self.counter = Counter()
     
     def isMatched(self) -> bool:
         return self.validCount == self.patternSize
