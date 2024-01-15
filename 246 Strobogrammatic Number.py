5bf7ad15d3f941e6076737f8eee9dcad0bb37e70
jz33 <jun.zheng@convoy.com>
2024-01-14 22:10:19 -0800

    Numbers

--- 246 Strobogrammatic Number.py
+++ 246 Strobogrammatic Number.py
@@ -1,25 +1,30 @@
 '''
-Strobogrammatic Number
+246. Strobogrammatic Number
 https://leetcode.com/problems/strobogrammatic-number/
+
+Given a string num which represents an integer, return true if num is a strobogrammatic number.
 
 A strobogrammatic number is a number that looks the same when rotated 180 degrees (looked at upside down).
 
-Write a function to determine if a number is strobogrammatic. The number is represented as a string.
-
 Example 1:
 
-Input:  "69"
+Input: num = "69"
 Output: true
 
 Example 2:
 
-Input:  "88"
+Input: num = "88"
 Output: true
 
 Example 3:
 
-Input:  "962"
+Input: num = "962"
 Output: false
+
+Constraints:
+    1 <= num.length <= 50
+    num consists of only digits.
+    num does not contain any leading zeros except for zero itself.
 '''
 class Solution:
     def isStrobogrammatic(self, num: str) -> bool:
