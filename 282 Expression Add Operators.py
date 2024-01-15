01d6606d2fec872cf92f9683f44be09372c26924
junzheng2023 <jzheng3@flexport.com>
2024-01-15 00:37:16 -0800

    Top down Dynamic Programming

--- 282 Expression Add Operators.py
+++ 282 Expression Add Operators.py
@@ -2,75 +2,71 @@
 282. Expression Add Operators
 https://leetcode.com/problems/expression-add-operators/
 
-Given a string that contains only digits 0-9 and a target value, return all possibilities
-to add binary operators (not unary) +, -, or * between the digits so they evaluate to the target value.
+Given a string num that contains only digits and an integer target,
+return all possibilities to insert the binary operators '+', '-', and/or '*'
+between the digits of num so that the resultant expression evaluates to the target value.
+
+Note that operands in the returned expressions should not contain leading zeros.
 
 Example 1:
 
 Input: num = "123", target = 6
-Output: ["1+2+3", "1*2*3"] 
+Output: ["1*2*3","1+2+3"]
+Explanation: Both "1*2*3" and "1+2+3" evaluate to 6.
 
 Example 2:
 
 Input: num = "232", target = 8
-Output: ["2*3+2", "2+3*2"]
+Output: ["2*3+2","2+3*2"]
+Explanation: Both "2*3+2" and "2+3*2" evaluate to 8.
 
 Example 3:
 
-Input: num = "105", target = 5
-Output: ["1*0+5","10-5"]
-
-Example 4:
-
-Input: num = "00", target = 0
-Output: ["0+0", "0-0", "0*0"]
-
-Example 5:
-
 Input: num = "3456237490", target = 9191
 Output: []
+Explanation: There are no expressions that can be created from "3456237490" to evaluate to 9191.
+
+Constraints:
+    1 <= num.length <= 10
+    num consists of only digits.
+    -231 <= target <= 231 - 1
 '''
-
 class Solution:
-    def dfs(self, i: int, expr: List[str], val: int, productVal: int):
-        '''
-        @i: current index in self.num
-        @expr: current expression
-        @val: value of current expression
-        @productVal: the value of last product. Notice if last operator is not '*',
-            treat it like 1/-1 * last value
-        '''
-        num = self.num
-        if i == len(num):
-            if val == self.target:
-                self.pool.append(''.join(expr))
-            return
-        
-        for j in range(i+1, len(num)+1):
-            # Get each integer from num[i:]
-            n = int(num[i:j])
-            s = str(n)
-            
-            if len(s) < j - i:
-                # '01', integer will be 1, and then interger string
-                # is '1', which should break, because all later j
-                # will have same issue
-                break
-            
-            if i == 0:
-                # Cannot put operator in beginning!
-                self.dfs(j, expr + [s], n, n)
-            else:
-                self.dfs(j, expr + ['+', s], val + n, n)
-                self.dfs(j, expr + ['-', s], val - n, -n)
-
-                # productVal * n is the new product value, and old one (productVal) should be removed
-                self.dfs(j, expr + ['*', s], val - productVal + productVal * n, productVal * n)
-    
+    '''
+    '''
     def addOperators(self, num: str, target: int) -> List[str]:
-        self.num = num
-        self.target = target
-        self.pool = []
+        pool = []
+
+        def dfs(ni: int, expr: List[str], exprVal: int, lastVal: int):
+            '''
+            @ni: current index in num
+            @expr: current expression
+            @exprVal: value of current expression
+            @lastVal: last computed value
+            '''
+            if ni == len(num):
+                if exprVal == target:
+                    pool.append(''.join(expr))
+                return
+            
+            # Iterate on each integer num[ni : j] 
+            for j in range(ni + 1, len(num) + 1):
+                newNum = int(num[ni:j])
+                newNumStr = str(newNum)
+                
+                if len(newNumStr) < j - ni:
+                    # Example '01', integer will be 1, and then integer string
+                    # is '1', which should break, because all later j
+                    # will have same issue
+                    break
+                
+                if ni == 0:
+                    # Cannot put operator in beginning!
+                    dfs(j, [newNumStr], newNum, newNum)
+                else:
+                    dfs(j, expr + ['+', newNumStr], exprVal + newNum, newNum)
+                    dfs(j, expr + ['-', newNumStr], exprVal - newNum, -newNum)
+                    dfs(j, expr + ['*', newNumStr], exprVal - lastVal + lastVal * newNum, lastVal * newNum)
         
-        self.dfs(0, [], 0, 0)
-        return self.pool
+        dfs(0, [], 0, 0)
+        return pool
