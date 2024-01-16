1d07b756d40c51bad4d987fc2212ea7fa808d120
junzheng2023 <jzheng3@flexport.com>
2024-01-16 10:18:02 -0800

    Trivial

--- T-1779 Find Nearest Point That Has the Same X or Y Coordinate.py
+++ T-1779 Find Nearest Point That Has the Same X or Y Coordinate.py
@@ -2,9 +2,12 @@
 1779. Find Nearest Point That Has the Same X or Y Coordinate
 https://leetcode.com/problems/find-nearest-point-that-has-the-same-x-or-y-coordinate/
 
-You are given two integers, x and y, which represent your current location on a Cartesian grid: (x, y). You are also given an array points where each points[i] = [ai, bi] represents that a point exists at (ai, bi). A point is valid if it shares the same x-coordinate or the same y-coordinate as your location.
+You are given two integers, x and y, which represent your current location on a Cartesian grid: (x, y).
+You are also given an array points where each points[i] = [ai, bi] represents that a point exists at (ai, bi).
+A point is valid if it shares the same x-coordinate or the same y-coordinate as your location.
 
-Return the index (0-indexed) of the valid point with the smallest Manhattan distance from your current location. If there are multiple, return the valid point with the smallest index. If there are no valid points, return -1.
+Return the index (0-indexed) of the valid point with the smallest Manhattan distance from your current location.
+If there are multiple, return the valid point with the smallest index. If there are no valid points, return -1.
 
 The Manhattan distance between two points (x1, y1) and (x2, y2) is abs(x1 - x2) + abs(y1 - y2).
 
