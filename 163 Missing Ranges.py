98052997879fb6ff4effd3dcdd894ce26eabd0a1
jz33 <jun.zheng@convoy.com>
2024-01-16 13:07:32 -0800

    Trivial

--- 163 Missing Ranges.py
+++ 163 Missing Ranges.py
@@ -2,29 +2,46 @@
 163. Missing Ranges
 https://leetcode.com/problems/missing-ranges/
 
-Given a sorted integer array nums, where the range of elements are in the inclusive range [lower, upper],
-return its missing ranges.
+You are given an inclusive range [lower, upper] and a sorted unique integer array nums,
+where all elements are within the inclusive range.
 
-Example:
+A number x is considered missing if x is in the range [lower, upper] and x is not in nums.
 
-Input: nums = [0, 1, 3, 50, 75], lower = 0 and upper = 99,
-Output: ["2", "4->49", "51->74", "76->99"]
+Return the shortest sorted list of ranges that exactly covers all the missing numbers.
+That is, no element of nums is included in any of the ranges, and each missing number is covered by one of the ranges.
+
+Example 1:
+
+Input: nums = [0,1,3,50,75], lower = 0, upper = 99
+Output: [[2,2],[4,49],[51,74],[76,99]]
+Explanation: The ranges are:
+[2,2]
+[4,49]
+[51,74]
+[76,99]
+
+Example 2:
+
+Input: nums = [-1], lower = -1, upper = -1
+Output: []
+Explanation: There are no missing ranges since there are no missing numbers.
+
+Constraints:
+    -109 <= lower <= upper <= 109
+    0 <= nums.length <= 100
+    lower <= nums[i] <= upper
+    All the values of nums are unique
 '''
 class Solution:
-    def findMissingRanges(self, nums: List[int], lower: int, upper: int) -> List[str]:
-        res = []
-        i = lower
-        for e in nums:
-            if i < e:
-                if i == e - 1:
-                    res.append(str(i))
-                else:
-                    res.append(str(i)+'->'+str(e - 1))
-            i = e + 1
-            
-        if i == upper:
-            res.append(str(i))
-        elif i < upper:
-            res.append(str(i)+'->'+str(upper))
-            
-        return res
+    def findMissingRanges(self, nums: List[int], lower: int, upper: int) -> List[List[int]]:
+        if not nums:
+            return [[lower, upper]]
+        result = []
+        if nums[0] - lower > 0:
+            result.append([lower, nums[0]-1])
+        for i in range(1, len(nums)):
+            if nums[i] - nums[i-1] > 1:
+                result.append([nums[i-1]+1, nums[i]-1])
+        if upper - nums[-1] > 0:
+            result.append([nums[-1]+1, upper])
+        return result
