7866717911fec43976d33d97a216bae662a273fb
junzheng2023 <jzheng3@flexport.com>
2024-01-19 15:54:22 -0800

    Tree constructions

--- 105 Construct Binary Tree from Preorder and Inorder Traversal.py
+++ 105 Construct Binary Tree from Preorder and Inorder Traversal.py
@@ -2,68 +2,59 @@
 105. Construct Binary Tree from Preorder and Inorder Traversal
 https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
 
-Given preorder and inorder traversal of a tree, construct the binary tree.
+Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and
+inorder is the inorder traversal of the same tree, construct and return the binary tree.
 
-Note:
-You may assume that duplicates do not exist in the tree.
+Example 1:
 
-For example, given
+Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+Output: [3,9,20,null,null,15,7]
 
-preorder = [3,9,20,15,7]
-inorder = [9,3,15,20,7]
-Return the following binary tree:
+Example 2:
 
-    3
-   / \
-  9  20
-    /  \
-   15   7
-Accepted
+Input: preorder = [-1], inorder = [-1]
+Output: [-1]
+
+Constraints:
+    1 <= preorder.length <= 3000
+    inorder.length == preorder.length
+    -3000 <= preorder[i], inorder[i] <= 3000
+    preorder and inorder consist of unique values.
+    Each value of inorder also appears in preorder.
+    preorder is guaranteed to be the preorder traversal of the tree.
+    inorder is guaranteed to be the inorder traversal of the tree.
 '''
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from typing import List, Optional
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
-    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
         size = len(inorder)
         
-        # Create a {value : index} map from inorder array
-        valueToIndex = dict(zip(inorder, range(size)))
+        # Create a {value : index} map from inorder array for quick lookup
+        # The 2 lists must contain unique values, otherwise there can be more than 1 way to build
+        valueToIndex = dict([(v, i) for i, v in enumerate(inorder)])
         
-        def topDown(left: int, right: int, start: int) -> TreeNode:
-            '''
-            @left: left index of preorder subarray inclusive
-            @right: right index of preorder subarray, inclusive
-            @start: left index of inorder subarray, inclusive
-            '''
+        def topDown(left: int, right: int, inorderStart: int) -> Optional[TreeNode]:
+            if left == right:
+                return TreeNode(preorder[left])
+            
             if left > right:
                 return None
             
-            # Find the value in inorder array, 
-            # compute cut width
+            # Find the value in inorder array, compute cut width
             value = preorder[left]
-            mid = valueToIndex[value] 
-            width = mid - start
+            inorderIndex = valueToIndex[value] 
+            cut = inorderIndex - inorderStart
             
             node = TreeNode(value)
-            node.left = topDown(left + 1, left + width, start)
-            node.right = topDown(left + width + 1, right, mid + 1)
+            node.left = topDown(left + 1, left + cut, inorderStart)
+            node.right = topDown(left + cut + 1, right, inorderIndex + 1)
             return node
         
         return topDown(0, size - 1, 0)
--- 106 Construct Binary Tree from Inorder and Postorder Traversal.py
+++ 106 Construct Binary Tree from Inorder and Postorder Traversal.py
@@ -2,60 +2,59 @@
 106. Construct Binary Tree from Inorder and Postorder Traversal
 https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/
 
-Given inorder and postorder traversal of a tree, construct the binary tree.
+Given two integer arrays inorder and postorder where inorder is the inorder traversal of a binary tree
+and postorder is the postorder traversal of the same tree, construct and return the binary tree.
 
-Note:
-You may assume that duplicates do not exist in the tree.
+Example 1:
 
-For example, given
+Input: inorder = [9,3,15,20,7], postorder = [9,15,7,20,3]
+Output: [3,9,20,null,null,15,7]
 
-inorder = [9,3,15,20,7]
-postorder = [9,15,7,20,3]
-Return the following binary tree:
+Example 2:
 
-    3
-   / \
-  9  20
-    /  \
-   15   7
+Input: inorder = [-1], postorder = [-1]
+Output: [-1]
+
+Constraints:
+    1 <= inorder.length <= 3000
+    postorder.length == inorder.length
+    -3000 <= inorder[i], postorder[i] <= 3000
+    inorder and postorder consist of unique values.
+    Each value of postorder also appears in inorder.
+    inorder is guaranteed to be the inorder traversal of the tree.
+    postorder is guaranteed to be the postorder traversal of the tree.
 '''
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from typing import List, Optional
+
 class Solution:
-    def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
         size = len(inorder)
         
-        # Create a {value : index} map from inorder array
-        valueToIndex = dict(zip(inorder, range(size)))
+        # Create a {value : index} map from inorder array for quick lookup
+        # The 2 lists must contain unique values, otherwise there can be more than 1 way to build
+        valueToIndex = dict([(v, i) for i, v in enumerate(inorder)])
         
-        def topDown(left: int, right: int, start: int) -> TreeNode:
-            '''
-            @left: left index of postorder subarray inclusive
-            @right: right index of postorder subarray, inclusive
-            @start: left index of inorder subarray, inclusive
-            '''
+        def topDown(left: int, right: int, inorderStart: int) -> Optional[TreeNode]:
+            if left == right:
+                return TreeNode(postorder[right])
+            
             if left > right:
                 return None
             
-            # Find the value in inorder array, 
-            # compute cut width
+            # Find the value in inorder array, compute cut width
             value = postorder[right]
-            mid = valueToIndex[value] 
-            width = mid - start
+            inorderIndex = valueToIndex[value] 
+            cut = inorderIndex - inorderStart
             
             node = TreeNode(value)
-            node.left = topDown(left, left + width - 1, start)
-            node.right = topDown(left + width, right - 1, mid + 1)
+            node.left = topDown(left, left + cut - 1, inorderStart)
+            node.right = topDown(left + cut, right - 1, inorderIndex + 1)
             return node
         
         return topDown(0, size - 1, 0)
