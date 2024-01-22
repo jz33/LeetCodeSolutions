70f133cfa392c30a69da207fa080154dd6d755e8
jz33 <jun.zheng@convoy.com>
2024-01-21 22:44:12 -0800

    Binary Search

--- 162 Find Peak Element.py
+++ 162 Find Peak Element.py
@@ -2,29 +2,33 @@
 162. Find Peak Element
 https://leetcode.com/problems/find-peak-element/
 
-A peak element is an element that is greater than its neighbors.
+A peak element is an element that is strictly greater than its neighbors.
 
-Given an input array nums, where nums[i] ≠ nums[i+1], find a peak element and return its index.
+Given a 0-indexed integer array nums, find a peak element, and return its index.
+If the array contains multiple peaks, return the index to any of the peaks.
 
-The array may contain multiple peaks, in that case return the index to any one of the peaks is fine.
+You may imagine that nums[-1] = nums[n] = -∞. In other words,
+an element is always considered to be strictly greater than a neighbor that is outside the array.
 
-You may imagine that nums[-1] = nums[n] = -∞.
+You must write an algorithm that runs in O(log n) time.
 
 Example 1:
 
 Input: nums = [1,2,3,1]
 Output: 2
 Explanation: 3 is a peak element and your function should return the index number 2.
-    
+
 Example 2:
 
 Input: nums = [1,2,1,3,5,6,4]
-Output: 1 or 5 
-Explanation: Your function can return either index number 1 where the peak element is 2, 
-             or index number 5 where the peak element is 6.
-Note:
+Output: 5
+Explanation: Your function can return either index number 1 where the peak element is 2,
+or index number 5 where the peak element is 6.
 
-Your solution should be in logarithmic complexity.
+Constraints:
+    1 <= nums.length <= 1000
+    -231 <= nums[i] <= 231 - 1
+    nums[i] != nums[i + 1] for all valid i
 '''
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
