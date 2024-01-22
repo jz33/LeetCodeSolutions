4af6c336145791bb0c513e3f3245e6c39e0f9238
junzheng2023 <jzheng3@flexport.com>
2024-01-22 13:50:38 -0800

    Numbers

--- 012 Integer to Roman.py
+++ 012 Integer to Roman.py
@@ -1,4 +1,56 @@
-IntergerToRoman = {
+'''
+12. Integer to Roman
+https://leetcode.com/problems/integer-to-roman/
+
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+
+For example, 2 is written as II in Roman numeral, just two one's added together.
+12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right.
+However, the numeral for four is not IIII.
+Instead, the number four is written as IV.
+Because the one is before the five we subtract it making four.
+The same principle applies to the number nine, which is written as IX.
+There are six instances where subtraction is used:
+
+    I can be placed before V (5) and X (10) to make 4 and 9. 
+    X can be placed before L (50) and C (100) to make 40 and 90. 
+    C can be placed before D (500) and M (1000) to make 400 and 900.
+
+Given an integer, convert it to a roman numeral.
+
+Example 1:
+
+Input: num = 3
+Output: "III"
+Explanation: 3 is represented as 3 ones.
+
+Example 2:
+
+Input: num = 58
+Output: "LVIII"
+Explanation: L = 50, V = 5, III = 3.
+
+Example 3:
+
+Input: num = 1994
+Output: "MCMXCIV"
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+Constraints:
+    1 <= num <= 3999
+'''
+NumberToRoman = {
     1   : 'I',
     5   : 'V',
     10  : 'X',
@@ -14,22 +66,25 @@ class Solution:
         Use a more generalized method other than many if else conditions.
         Real onsite interview question with Amazon 2019-10-11
         '''
-        res = []
-        for p in range(3,-1,-1): # 3, 2, 1, 0
-            base = 10 ** p
+        result = []
+        for power in range(3,-1,-1): # 3, 2, 1, 0
+            base = 10 ** power
             if num >= 9 * base:
-                res += [IntergerToRoman[base], IntergerToRoman[base*10]]
+                # Notice the biggest possible Roman number is 3999,
+                # so this part is unreachable when base = 1000
+                result += [NumberToRoman[base], NumberToRoman[10 * base]]
                 num -= 9 * base             
             elif num >= 5 * base:
-                res += [IntergerToRoman[base*5]]
+                result += [NumberToRoman[5 * base]]
                 num -= 5 * base
             elif num >= 4 * base:
-                res += [IntergerToRoman[base], IntergerToRoman[base*5]]
+                result += [NumberToRoman[base], NumberToRoman[5 * base]]
                 num -= 4 * base
-            
-            t = num // base
-            if t > 0:             
-                num -= t * base
-                res += [IntergerToRoman[base]] * t
+
+            # When num >= 5 * base or num < 4 * base
+            count = num // base
+            if count > 0:             
+                num -= count * base
+                result += [NumberToRoman[base]] * count
         
-        return ''.join(res)
+        return ''.join(result)
--- 013 Roman To Integer.py
+++ 013 Roman To Integer.py
@@ -1,4 +1,59 @@
-SymbolToInteger = {
+'''
+13. Roman to Integer
+https://leetcode.com/problems/roman-to-integer/
+
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+
+For example, 2 is written as II in Roman numeral, just two ones added together.
+12 is written as XII, which is simply X + II.
+The number 27 is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right.
+However, the numeral for four is not IIII. Instead, the number four is written as IV.
+Because the one is before the five we subtract it making four.
+The same principle applies to the number nine, which is written as IX.
+
+There are six instances where subtraction is used:
+
+    I can be placed before V (5) and X (10) to make 4 and 9. 
+    X can be placed before L (50) and C (100) to make 40 and 90. 
+    C can be placed before D (500) and M (1000) to make 400 and 900.
+
+Given a roman numeral, convert it to an integer.
+
+Example 1:
+
+Input: s = "III"
+Output: 3
+Explanation: III = 3.
+
+Example 2:
+
+Input: s = "LVIII"
+Output: 58
+Explanation: L = 50, V= 5, III = 3.
+
+Example 3:
+
+Input: s = "MCMXCIV"
+Output: 1994
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+Constraints:
+    1 <= s.length <= 15
+    s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+    It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+'''
+SymbolToNumber = {
     'I' : 1,
     'V' : 5,
     'X' : 10,
@@ -14,25 +69,14 @@ class Solution:
         Use a more generalized method other than many if else conditions.
         Real onsite interview question with Amazon 2019-10-11
         '''
-        res = 0
-        i = 0
-        while i < len(roman):
-            c = roman[i]
-            c_int = SymbolToInteger[c]
-            inc = 0 # increase for i
+        result = 0
+        for i, s in enumerate(roman):
+            number = SymbolToNumber[s]
+
+            # Check subtraction cases: IV, IX, XL, XC, CD, DM
+            if i > 0 and SymbolToNumber[roman[i-1]] < number:
+                # Remove previous number then subtract previous number
+                result -= SymbolToNumber[roman[i-1]] * 2
             
-            if i + 1 < len(roman):
-                d = roman[i+1]
-                d_int = SymbolToInteger[d]
-                if d_int > c_int:
-                    # IV, IX, XL, XC, CD, DM
-                    res += d_int - c_int
-                    inc = 2
-            
-            if inc == 0:
-                res += c_int
-                inc = 1
-            
-            i += inc
-            
-        return res
+            result += number
+        return result
