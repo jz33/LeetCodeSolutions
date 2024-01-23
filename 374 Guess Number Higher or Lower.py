e71b6e8e9c1cf40dbc290262f81797b66be7a1fd
junzheng2023 <jzheng3@flexport.com>
2024-01-23 10:58:05 -0800

    Binary Search

--- 374 Guess Number Higher or Lower.py
+++ 374 Guess Number Higher or Lower.py
@@ -5,25 +5,44 @@ We are playing the Guess Game. The game is as follows:
 
 I pick a number from 1 to n. You have to guess which number I picked.
 
-Every time you guess wrong, I'll tell you whether the number is higher or lower.
+Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
 
-You call a pre-defined API guess(int num) which returns 3 possible results (-1, 1, or 0):
+You call a pre-defined API int guess(int num), which returns three possible results:
 
--1 : My number is lower
- 1 : My number is higher
- 0 : Congrats! You got it!
+    -1: Your guess is higher than the number I picked (i.e. num > pick).
+    1: Your guess is lower than the number I picked (i.e. num < pick).
+    0: your guess is equal to the number I picked (i.e. num == pick).
 
-Example :
+Return the number that I picked.
+
+Example 1:
 
 Input: n = 10, pick = 6
 Output: 6
+
+Example 2:
+
+Input: n = 1, pick = 1
+Output: 1
+
+Example 3:
+
+Input: n = 2, pick = 1
+Output: 1
+
+Constraints:
+    1 <= n <= 231 - 1
+    1 <= pick <= n
 '''
-class Solution(object):
-    def guessNumber(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num: int) -> int:
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
         left = 1
         right = n
         while left <= right:
