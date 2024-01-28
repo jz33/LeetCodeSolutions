0e5f5fc685b976b3a9abcb5e116d615e013aa1ff
junzheng2023 <jzheng3@flexport.com>
2024-01-27 16:47:05 -0800

    Intervals

--- 435 Non-overlapping Intervals.py
+++ 435 Non-overlapping Intervals.py
@@ -2,40 +2,40 @@
 435. Non-overlapping Intervals
 https://leetcode.com/problems/non-overlapping-intervals/
 
-Given a collection of intervals, find the minimum number of intervals you need to remove to
-make the rest of the intervals non-overlapping.
+Given an array of intervals intervals where intervals[i] = [starti, endi],
+return the minimum number of intervals you need to remove to make the rest of the intervals non-overlapping.
 
 Example 1:
 
-Input: [[1,2],[2,3],[3,4],[1,3]]
+Input: intervals = [[1,2],[2,3],[3,4],[1,3]]
 Output: 1
-Explanation: [1,3] can be removed and the rest of intervals are non-overlapping.
+Explanation: [1,3] can be removed and the rest of the intervals are non-overlapping.
 
 Example 2:
 
-Input: [[1,2],[1,2],[1,2]]
+Input: intervals = [[1,2],[1,2],[1,2]]
 Output: 2
-Explanation: You need to remove two [1,2] to make the rest of intervals non-overlapping.
+Explanation: You need to remove two [1,2] to make the rest of the intervals non-overlapping.
 
 Example 3:
 
-Input: [[1,2],[2,3]]
+Input: intervals = [[1,2],[2,3]]
 Output: 0
 Explanation: You don't need to remove any of the intervals since they're already non-overlapping.
- 
-Note:
 
-You may assume the interval's end point is always bigger than its start point.
-Intervals like [1,2] and [2,3] have borders "touching" but they don't overlap each other.
+Constraints:
+    1 <= intervals.length <= 105
+    intervals[i].length == 2
+    -5 * 104 <= starti < endi <= 5 * 104
 '''
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
-        # Sort by endpoint
-        intervals.sort(key = lambda tup : (tup[1], tup[0]))
+        # Sort by end then start
+        intervals.sort(key = lambda interval : (interval[1], interval[0]))
         toRemove = 0
-        previousEnd = float('-inf')
+        previousEnd = None
         for start, end in intervals:
-            if start < previousEnd:
+            if previousEnd is not None and start < previousEnd:
                 toRemove += 1
             else:
                 previousEnd = end
