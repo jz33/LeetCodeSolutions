3196a080a94bdcdabb70e1859f16aa6baa33c830
junzheng2023 <jzheng3@flexport.com>
2024-01-29 13:08:59 -0800

    2-D Matrix Iterations

--- 074 Search a 2D Matrix.py
+++ 074 Search a 2D Matrix.py
@@ -2,52 +2,49 @@
 74 Search a 2D Matrix
 https://leetcode.com/problems/search-a-2d-matrix/
 
-Write an efficient algorithm that searches for a value in an m x n matrix.
-This matrix has the following properties:
+You are given an m x n integer matrix matrix with the following two properties:
 
-Integers in each row are sorted from left to right.
-The first integer of each row is greater than the last integer of the previous row.
+    Each row is sorted in non-decreasing order.
+    The first integer of each row is greater than the last integer of the previous row.
+
+Given an integer target, return true if target is in matrix or false otherwise.
+
+You must write a solution in O(log(m * n)) time complexity.
 
 Example 1:
 
-Input:
-matrix = [
-  [1,   3,  5,  7],
-  [10, 11, 16, 20],
-  [23, 30, 34, 50]
-]
-target = 3
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
 Output: true
 
 Example 2:
 
-Input:
-matrix = [
-  [1,   3,  5,  7],
-  [10, 11, 16, 20],
-  [23, 30, 34, 50]
-]
-target = 13
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
 Output: false
+
+Constraints:
+    m == matrix.length
+    n == matrix[i].length
+    1 <= m, n <= 100
+    -104 <= matrix[i][j], target <= 104
 '''
 class Solution:
+    '''
+    Same method to 240. Search a 2D Matrix II
+    '''
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        if not matrix or not matrix[0]:
-            return False
-        
         rowCount = len(matrix)
         colCount = len(matrix[0])
         
         # Start from top right
-        x, y = 0, colCount - 1
-        while x < rowCount and y > -1:
-            val = matrix[x][y]
+        r, c = 0, colCount - 1
+        while r < rowCount and c > -1:
+            val = matrix[r][c]
             if val == target:
                 return True
             
             if val < target:
-                x += 1
+                r += 1
             else:
-                y -= 1
+                c -= 1
         
         return False
--- 240 Search a 2D Matrix II.py
+++ 240 Search a 2D Matrix II.py
@@ -2,48 +2,48 @@
 240. Search a 2D Matrix II
 https://leetcode.com/problems/search-a-2d-matrix-ii/
 
-Write an efficient algorithm that searches for a value in an m x n matrix.
-This matrix has the following properties:
+Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
 
-Integers in each row are sorted in ascending from left to right.
-Integers in each column are sorted in ascending from top to bottom.
-    
-Example:
+    Integers in each row are sorted in ascending from left to right.
+    Integers in each column are sorted in ascending from top to bottom.
 
-Consider the following matrix:
+Example 1:
 
-[
-  [1,   4,  7, 11, 15],
-  [2,   5,  8, 12, 19],
-  [3,   6,  9, 16, 22],
-  [10, 13, 14, 17, 24],
-  [18, 21, 23, 26, 30]
-]
-Given target = 5, return true.
+Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5
+Output: true
 
-Given target = 20, return false.
+Example 2:
+
+Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 20
+Output: false
+
+Constraints:
+    m == matrix.length
+    n == matrix[i].length
+    1 <= n, m <= 300
+    -109 <= matrix[i][j] <= 109
+    All the integers in each row are sorted in ascending order.
+    All the integers in each column are sorted in ascending order.
+    -109 <= target <= 109
 '''
 class Solution:
+    '''
+    Same method to 74. Search a 2D Matrix
+    '''
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        '''
-        Same method to 74. Search a 2D Matrix
-        '''
-        if not matrix or not matrix[0]:
-            return False
-        
         rowCount = len(matrix)
         colCount = len(matrix[0])
         
         # Start from top right
-        x, y = 0, colCount - 1
-        while x < rowCount and y > -1:
-            val = matrix[x][y]
+        r, c = 0, colCount - 1
+        while r < rowCount and c > -1:
+            val = matrix[r][c]
             if val == target:
                 return True
             
             if val < target:
-                x += 1
+                r += 1
             else:
-                y -= 1
+                c -= 1
         
         return False
