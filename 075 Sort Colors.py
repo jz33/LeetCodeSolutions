c2c4248b6f8b9de8e13f662ad766b637ec842414
jz33 <jun.zheng@convoy.com>
2024-01-31 19:31:15 -0800

    1-D Iteration

--- 075 Sort Colors.py
+++ 075 Sort Colors.py
@@ -1,36 +1,44 @@
 '''
 75. Sort Colors
 https://leetcode.com/problems/sort-colors/solution/
-Given an array with n objects colored red, white or blue,
-sort them in-place so that objects of the same color are adjacent,
-with the colors in the order red, white and blue.
 
-Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
+Given an array nums with n objects colored red, white, or blue, sort them in-place so that
+objects of the same color are adjacent, with the colors in the order red, white, and blue.
 
-Note: You are not suppose to use the library's sort function for this problem.
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
 
-Example:
+You must solve this problem without using the library's sort function.
 
-Input: [2,0,2,1,1,0]
+Example 1:
+
+Input: nums = [2,0,2,1,1,0]
 Output: [0,0,1,1,2,2]
+
+Example 2:
+
+Input: nums = [2,0,1]
+Output: [0,1,2]
+
+Constraints:
+    n == nums.length
+    1 <= n <= 300
+    nums[i] is either 0, 1, or 2
 '''
 class Solution:
     def sortColors(self, nums: List[int]) -> None:  
-        zi = 0 # destination index for '0'
-        ti = len(nums) - 1 # destination index for '2'
+        left = 0 # destination index for '0'
+        right = len(nums) - 1 # destination index for '2'
         i = 0
-        while i <= ti:
+        while i <= right:
             if nums[i] == 0:
                 # Put '0' to left
-                nums[i], nums[zi] =  nums[zi], nums[i]
+                nums[i], nums[left] =  nums[left], nums[i]
                 i += 1
-                zi += 1
-                # Increase i here, if i == zi, nums[i] is 0,
-                # and if i > zi, nums[i] is 1              
+                left += 1
             elif nums[i] == 1:
                 i += 1
             else: # nums[i] == 2
                 # Put '2' to right
-                nums[i], nums[ti] =  nums[ti], nums[i]
-                ti -= 1
-                # Cannot increse i here, because nums[i] could be 2
+                nums[i], nums[right] =  nums[right], nums[i]
+                right -= 1
+                # Cannot increase i here, because nums[i] could be 2
