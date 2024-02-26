9c9a29575e384c237577a191c85027e3d2a02095
jz33 <jun.zheng@convoy.com>
2024-02-25 22:32:01 -0800

    Two Pointers Both Sides

--- 011 Container With Most Water.py
+++ 011 Container With Most Water.py
@@ -2,26 +2,42 @@
 11. Container With Most Water
 https://leetcode.com/problems/container-with-most-water/
 
-Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai).
-n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0).
-Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+You are given an integer array height of length n. There are n vertical lines
+drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
 
-Note: You may not slant the container and n is at least 2.
+Find two lines that together with the x-axis form a container,
+such that the container contains the most water.
 
-Example:
+Return the maximum amount of water a container can store.
 
-Input: [1,8,6,2,5,4,8,3,7]
+Notice that you may not slant the container.
+
+Example 1:
+
+Input: height = [1,8,6,2,5,4,8,3,7]
 Output: 49
+Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7].
+In this case, the max area of water (blue section) the container can contain is 49.
+
+Example 2:
+
+Input: height = [1,1]
+Output: 1
+
+Constraints:
+    n == height.length
+    2 <= n <= 105
+    0 <= height[i] <= 104
 '''
 class Solution:
-    def maxArea(self, height: List[int]) -> int:
+    def maxArea(self, heights: List[int]) -> int:
         left = 0
-        right = len(height) - 1
-        res = 0
+        right = len(heights) - 1
+        result = 0
         while left < right:
-            res = max(res, abs(left - right) * min(height[left], height[right]))
-            if height[left] < height[right]:
+            result = max(result, abs(left - right) * min(heights[left], heights[right]))
+            if heights[left] < heights[right]:
                 left += 1
             else:
                 right -= 1
-        return res
+        return result
