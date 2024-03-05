fff50972ae4ea9de52e5833ab59c3206e3eba642
junzheng2023 <jzheng3@flexport.com>
2024-03-04 22:35:48 -0800

    Trivial

---
+++ T-2235 Add Two Integers.py
@@ -0,0 +1,10 @@
+'''
+2235. Add Two Integers
+https://leetcode.com/problems/add-two-integers/
+'''
+class Solution:
+    '''
+    Similar to 415. Add Strings
+    '''
+    def sum(self, num1: int, num2: int) -> int:
+        return num1 + num2
