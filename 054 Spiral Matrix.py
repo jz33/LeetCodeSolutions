cdfa91cee35e585b569b57c32f88103ded372ac4
junzheng2023 <jzheng3@flexport.com>
2024-03-06 09:20:09 -0800

    2-D Matrix Iterations

--- 054 Spiral Matrix.py
+++ 054 Spiral Matrix.py
@@ -2,62 +2,53 @@
 54. Spiral Matrix
 https://leetcode.com/problems/spiral-matrix/
 
-Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in spiral order.
+Given an m x n matrix, return all elements of the matrix in spiral order.
 
 Example 1:
 
-Input:
-[
- [ 1, 2, 3 ],
- [ 4, 5, 6 ],
- [ 7, 8, 9 ]
-]
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
 Output: [1,2,3,6,9,8,7,4,5]
+
 Example 2:
 
-Input:
-[
-  [1, 2, 3, 4],
-  [5, 6, 7, 8],
-  [9,10,11,12]
-]
+Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
 Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+
+Constraints:
+    m == matrix.length
+    n == matrix[i].length
+    1 <= m, n <= 10
+    -100 <= matrix[i][j] <= 100
 '''
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         rowCount = len(matrix)
-        if not rowCount:
-            return []
-        
         colCount = len(matrix[0])
-        if not colCount:
-            return []
-        
         total = rowCount * colCount        
-        res = [None] * total
+        result = [None] * total
         
         x, y = 0, 0
         level = 0 # circular layer levels
         directions = [(0,1),(1,0),(0,-1),(-1,0)] # right, down, left, up
-        di = 0
+        dir = 0
         for i in range(total):
-            res[i] = matrix[x][y]
-            
-            if di == 0:
+            result[i] = matrix[x][y]
+
+            if dir == 0:
                 if y == colCount - level - 1:
-                    di = 1
-            elif di == 1:
+                    dir = 1
+            elif dir == 1:
                 if x == rowCount - level - 1:
-                    di = 2
-            elif di == 2:
+                    dir = 2
+            elif dir == 2:
                 if y == level:
-                    di = 3
-            elif di == 3:
+                    dir = 3
+            elif dir == 3:
                 if x == level + 1:
-                    di = 0
+                    dir = 0
                     level += 1
 
-            x += directions[di][0]
-            y += directions[di][1]
+            x += directions[dir][0]
+            y += directions[dir][1]
 
-        return res         
+        return result         
