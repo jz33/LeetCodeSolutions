fa4495177b245263d4bfa103b15161a04531fd4b
junzheng2023 <jzheng3@flexport.com>
2024-03-08 09:45:26 -0800

    Preorder Traversal

--- T-1028 Recover a Tree From Preorder Traversal.py
+++ T-1028 Recover a Tree From Preorder Traversal.py
@@ -2,50 +2,60 @@
 1028. Recover a Tree From Preorder Traversal
 https://leetcode.com/problems/recover-a-tree-from-preorder-traversal/
 
-We run a preorder depth first search on the root of a binary tree.
+We run a preorder depth-first search (DFS) on the root of a binary tree.
 
 At each node in this traversal, we output D dashes (where D is the depth of this node),
-then we output the value of this node.
-(If the depth of a node is D, the depth of its immediate child is D+1. The depth of the root node is 0.)
+then we output the value of this node.  If the depth of a node is D,
+the depth of its immediate child is D + 1.  The depth of the root node is 0.
 
 If a node has only one child, that child is guaranteed to be the left child.
 
-Given the output S of this traversal, recover the tree and return its root.
+Given the output traversal of this traversal, recover the tree and return its root.
 
 Example 1:
 
-Input: "1-2--3--4-5--6--7"
+Input: traversal = "1-2--3--4-5--6--7"
 Output: [1,2,5,3,4,6,7]
 
 Example 2:
 
-Input: "1-2--3---4-5--6---7"
+Input: traversal = "1-2--3---4-5--6---7"
 Output: [1,2,5,3,null,6,null,4,null,7]
- 
+
 Example 3:
 
-Input: "1-401--349---90--88"
+Input: traversal = "1-401--349---90--88"
 Output: [1,401,null,349,88,90]
 
-Note:
-
-The number of nodes in the original tree is between 1 and 1000.
-Each node will have a value between 1 and 10^9.
+Constraints:
+    The number of nodes in the original tree is in the range [1, 1000].
+    1 <= Node.val <= 109
 '''
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 class Solution:
-    def recoverFromPreorder(self, S: str) -> TreeNode:
-        if not S:
+    def recoverFromPreorder(self, traversal: str) -> Optional[TreeNode]:
+        if not traversal:
             return None
         
-        def addNode(node):
-            nonlocal stack, root, depth
+        stack = [] # [(node, depth)]
+        root = None
+        value = None
+        depth = 0
+
+        def addNode():
+            nonlocal stack, root, value, depth
+
+            if value is None:
+                return
+            
+            node = TreeNode(value)
+            value = None
             
             if depth == 0:
                 root = node
@@ -63,23 +73,17 @@ class Solution:
                     parent.right = node
 
             stack.append((node, depth))
+            depth = 0
         
-        stack = [] # [(node, depth)]
-        root = None
-        value = 0
-        depth = 0
-        for c in S:
+        for c in traversal:
             if c.isdigit():
-                value = value * 10 + int(c)
+                if value is None:
+                    value = int(c)
+                else:
+                    value = value * 10 + int(c)
             elif c == '-':
-                if value > 0:
-                    addNode(TreeNode(value))
-                    value = 0
-                    depth = 0
-                    
+                addNode() 
                 depth += 1
         
-        if value > 0:
-            addNode(TreeNode(value))
-        
+        addNode()
         return root
