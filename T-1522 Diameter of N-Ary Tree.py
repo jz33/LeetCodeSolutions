ca971f05da642823aaf1047d1bfdec45ec00adf3
jz33 <junzhengrice@gmail.com>
2024-03-13 15:07:38 -0700

    Postorder Traversal

--- T-1522 Diameter of N-Ary Tree.py
+++ T-1522 Diameter of N-Ary Tree.py
@@ -1,6 +1,6 @@
 '''
 1522. Diameter of N-Ary Tree
-https://leetcode.com/problems/diameter-of-binary-tree/
+https://leetcode.com/problems/diameter-of-n-ary-tree/description/
 
 Given a root of an N-ary tree, you need to compute the length of the diameter of the tree.
 
@@ -30,13 +30,22 @@ Constraints:
     The depth of the n-ary tree is less than or equal to 1000.
     The total number of nodes is between [1, 104].
 '''
-"""
 # Definition for a Node.
 class Node:
     def __init__(self, val=None, children=None):
         self.val = val
         self.children = children if children is not None else []
-"""
+
+def getTop2(values: List[int]):
+    top1 = max(values[0], values[1])
+    top2 = min(values[0], values[1])
+    for i in range(2, len(values)):
+        val = values[i]
+        if val >= top1:
+            top1, top2 = val, top1
+        elif top1 > val > top2:
+            top2 = val
+    return top1, top2
 
 class Solution:
     '''
@@ -48,19 +57,17 @@ class Solution:
         
         maxNodeCount = 1
         def postorder(node: 'Node') -> int:
-            '''
-            @return: max depth from this node to any other nodes
-            '''
             nonlocal maxNodeCount
-            sortedDepths = sorted([postorder(child) for child in node.children])
-            if len(sortedDepths) >= 2:
-                maxNodeCount = max(maxNodeCount, sortedDepths[-1] + sortedDepths[-2] + 1)
-                return sortedDepths[-1] + 1
-            elif len(sortedDepths) == 1:
-                maxNodeCount = max(maxNodeCount, sortedDepths[-1] + 1)
-                return sortedDepths[-1] + 1
-            else:
+            childrenDepths = [postorder(child) for child in node.children]
+            if len(childrenDepths) == 0:
                 return 1
+            elif len(childrenDepths) == 1:
+                maxNodeCount = max(maxNodeCount, childrenDepths[0] + 1)
+                return childrenDepths[0] + 1
+            else:
+                top1, top2 = getTop2(childrenDepths)
+                maxNodeCount = max(maxNodeCount, top1 + top2 + 1)
+                return top1 + 1
         
         postorder(root)
         return maxNodeCount - 1
