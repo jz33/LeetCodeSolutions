6f8c3c49096588ebf9f7ab1721f40bf94c3ab82c
jz33 <junzhengrice@gmail.com>
2024-03-13 15:53:21 -0700

    Single Linked List

---
+++ T-1206 Design Skiplist.py
@@ -0,0 +1,109 @@
+'''
+1206. Design Skiplist
+https://leetcode.com/problems/design-skiplist/
+
+Design a Skiplist without using any built-in libraries.
+
+A skiplist is a data structure that takes O(log(n)) time to add, erase and search.
+Comparing with treap and red-black tree which has the same function and performance,
+the code length of Skiplist can be comparatively short and
+the idea behind Skiplists is just simple linked lists.
+
+For example, we have a Skiplist containing [30,40,50,60,70,90] and we want to add 80 and 45 into it. The Skiplist works this way:
+
+
+Artyom Kalinin [CC BY-SA 3.0], via Wikimedia Commons
+
+You can see there are many layers in the Skiplist. Each layer is a sorted linked list. With the help of the top layers, add, erase and search can be faster than O(n). It can be proven that the average time complexity for each operation is O(log(n)) and space complexity is O(n).
+
+See more about Skiplist: https://en.wikipedia.org/wiki/Skip_list
+
+Implement the Skiplist class:
+
+    Skiplist() Initializes the object of the skiplist.
+    bool search(int target) Returns true if the integer target exists in the Skiplist or false otherwise.
+    void add(int num) Inserts the value num into the SkipList.
+    bool erase(int num) Removes the value num from the Skiplist and returns true. If num does not exist in the Skiplist, do nothing and return false. If there exist multiple num values, removing any one of them is fine.
+
+Note that duplicates may exist in the Skiplist, your code needs to handle this situation.
+
+Example 1:
+
+Input
+["Skiplist", "add", "add", "add", "search", "add", "search", "erase", "erase", "search"]
+[[], [1], [2], [3], [0], [4], [1], [0], [1], [1]]
+Output
+[null, null, null, null, false, null, true, false, true, false]
+
+Explanation
+Skiplist skiplist = new Skiplist();
+skiplist.add(1);
+skiplist.add(2);
+skiplist.add(3);
+skiplist.search(0); // return False
+skiplist.add(4);
+skiplist.search(1); // return True
+skiplist.erase(0);  // return False, 0 is not in skiplist.
+skiplist.erase(1);  // return True
+skiplist.search(1); // return False, 1 has already been erased.
+'''
+from random import random
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+        self.down = None
+    
+    def __repr__(self):
+        return '({}, {})'.format(self.val, self.down.val if self.down else 'none')
+
+# Use 16 because maximum number is 2 * 10^4, while 2 ^ 16 = 64K
+CAP = 16
+
+class Skiplist:
+    def __init__(self):
+        levels = [Node(-math.inf) for _ in range(CAP)]
+        for i in range(CAP-1):
+            levels[i].down = levels[i+1]
+        self.levels = levels
+
+    def __iter(self, val: int):
+        result = []
+        node = self.levels[0]
+        while node:
+            while node.next and node.next.val < val:
+                node = node.next
+            result.append(node)
+            node = node.down
+        return result
+
+    def search(self, val: int) -> bool:
+        nodes = self.__iter(val) # nodes is always size of CAP
+        last = nodes[-1]
+        return last.next and last.next.val == val
+
+    def add(self, val: int) -> None:
+        nodes = self.__iter(val)
+        prev = None
+        for i in range(CAP-1,-1,-1):
+            oldNode = nodes[i]
+            newNode = Node(val)
+            # add newNode to next to oldNode
+            newNode.next = oldNode.next
+            oldNode.next = newNode
+            # point newNode.down to down level node
+            newNode.down = prev
+            prev = newNode
+            if random() > 0.5:
+                break
+
+    def erase(self, val: int) -> bool:
+        found = False
+        nodes = self.__iter(val)
+        for node in nodes:
+            if node.next is not None and node.next.val == val:
+                # Delete node.next. It should delete all nodes with val
+                node.next = node.next.next
+                found = True
+        return found
