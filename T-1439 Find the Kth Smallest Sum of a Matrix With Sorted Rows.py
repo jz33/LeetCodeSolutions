9bb80a00762e2d57199da4f5c733fe68cea665c8
jz33 <junzhengrice@gmail.com>
2024-03-14 23:02:56 -0700

    Heap

--- T-1439 Find the Kth Smallest Sum of a Matrix With Sorted Rows.py
+++ T-1439 Find the Kth Smallest Sum of a Matrix With Sorted Rows.py
@@ -2,17 +2,16 @@
 1439. Find the Kth Smallest Sum of a Matrix With Sorted Rows
 https://leetcode.com/problems/find-the-kth-smallest-sum-of-a-matrix-with-sorted-rows/
 
-You are given an m * n matrix, mat, and an integer k, which has its rows sorted in non-decreasing order.
-
-You are allowed to choose exactly 1 element from each row to form an array.
-Return the Kth smallest array sum among all possible arrays.
+You are given an m x n matrix mat that has its rows sorted in non-decreasing order and an integer k.
+You are allowed to choose exactly one element from each row to form an array.
+Return the kth smallest array sum among all possible arrays.
 
 Example 1:
 
 Input: mat = [[1,3,11],[2,4,6]], k = 5
 Output: 7
 Explanation: Choosing one element from each row, the first k smallest sum are:
-[1,2], [1,4], [3,2], [3,4], [1,6]. Where the 5th sum is 7.  
+[1,2], [1,4], [3,2], [3,4], [1,6]. Where the 5th sum is 7.
 
 Example 2:
 
@@ -24,50 +23,46 @@ Example 3:
 Input: mat = [[1,10,10],[1,4,5],[2,3,6]], k = 7
 Output: 9
 Explanation: Choosing one element from each row, the first k smallest sum are:
-[1,1,2], [1,1,3], [1,4,2], [1,4,3], [1,1,6], [1,5,2], [1,5,3]. Where the 7th sum is 9.
-
-Example 4:
-
-Input: mat = [[1,1,10],[2,2,9]], k = 7
-Output: 12
- 
+[1,1,2], [1,1,3], [1,4,2], [1,4,3], [1,1,6], [1,5,2], [1,5,3]. Where the 7th sum is 9.  
 
 Constraints:
-
-m == mat.length
-n == mat.length[i]
-1 <= m, n <= 40
-1 <= k <= min(200, n ^ m)
-1 <= mat[i][j] <= 5000
-mat[i] is a non decreasing array.
-'''
-from heapq import heappush, heappop
+    m == mat.length
+    n == mat.length[i]
+    1 <= m, n <= 40
+    1 <= mat[i][j] <= 5000
+    1 <= k <= min(200, nm)
+    mat[i] is a non-decreasing array.
+''' 
 
 class Solution:
-    def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[int]:
-        '''
-        Re-use 373. Find K Pairs with Smallest Sums
-        '''
-        heap = [] # [(sum, index of nums1, index of nums2)]
-        
-        def push(i, j):
-            nonlocal heap
-            if i < len(nums1) and j < len(nums2):
-                heappush(heap, (nums1[i] + nums2[j], i, j))
-        
-        res = []
-        push(0, 0)
-        while heap and len(res) < k:
-            s, i, j = heappop(heap)
-            res.append(s)
-            push(i, j + 1)
-            if j == 0:
-                push(i + 1, j)
-                
-        return res
-
+    '''
+    Based on 373. Find K Pairs with Smallest Sums
+    Time Complexity: rowCount * colCount * log(k)
+    '''
     def kthSmallest(self, mat: List[List[int]], k: int) -> int:
-        res = mat[0]
+        def kSmallestPairs(nums1: List[int], nums2: List[int]) -> List[int]:
+            '''
+            Based on 373. Find K Pairs with Smallest Sums
+            For every 2 rows, get the top k sums
+            '''
+            heap = [] # [(sum, nums1 index, nums2 index)]
+            
+            def push(i1: int, i2: int):
+                nonlocal heap
+                if i1 < len(nums1) and i2 < len(nums2):
+                    heappush(heap, (nums1[i1] + nums2[i2], i1, i2))
+            
+            push(0, 0)
+            row = []
+            while heap and len(row) < k:
+                t, i1, i2 = heappop(heap)
+                row.append(t)
+                push(i1, i2 + 1)
+                if i2 == 0:
+                    push(i1 + 1, i2)
+            return row 
+        
+        lastRow = mat[0]
         for i in range(1, len(mat)):
-            res = self.kSmallestPairs(res, mat[i], k)
-        return res[k-1]
+            lastRow = kSmallestPairs(lastRow, mat[i])
+        return lastRow[k-1]
