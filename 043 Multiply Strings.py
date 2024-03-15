4014323fa841b8bc3ee9f88d8f31c3ff747a0942
jz33 <junzhengrice@gmail.com>
2024-03-15 10:27:47 -0700

    Math

--- 043 Multiply Strings.py
+++ 043 Multiply Strings.py
@@ -5,6 +5,8 @@ https://leetcode.com/problems/multiply-strings/
 Given two non-negative integers num1 and num2 represented as strings,
 return the product of num1 and num2, also represented as a string.
 
+Note: You must not use any built-in BigInteger library or convert the inputs to integer directly.
+
 Example 1:
 
 Input: num1 = "2", num2 = "3"
@@ -14,11 +16,17 @@ Example 2:
 
 Input: num1 = "123", num2 = "456"
 Output: "56088"
+
+Constraints:
+    1 <= num1.length, num2.length <= 200
+    num1 and num2 consist of digits only.
+    Both num1 and num2 do not contain any leading zero, except the number 0 itself.
 '''
+def productOfChars(c1: str, c2: str) -> int:
+    return (ord(c1) - ord('0')) * (ord(c2) - ord('0'))
+    
 class Solution:
-    def productOfChars(self, c1: str, c2: str) -> int:
-        return (ord(c1) - ord('0')) * (ord(c2) - ord('0'))
-        
+
     def multiply(self, num1: str, num2: str) -> str:
         if num1 == '0' or num2 == '0':
             return '0'
@@ -28,10 +36,9 @@ class Solution:
         res = [0] * (len(num1) + len(num2))
 
         for i in range(len(num1)-1,-1,-1):
-            
             carry = 0
             for j in range(len(num2)-1,-1,-1):
-                total = res[i+j+1] + self.productOfChars(num1[i], num2[j]) + carry
+                total = res[i+j+1] + productOfChars(num1[i], num2[j]) + carry
                 res[i+j+1] = total % 10
                 carry = total // 10       
             res[i] += carry
