2ed3727ded8b7b31c43e11cc10569dd6813adb81
junzheng2023 <jzheng3@flexport.com>
2024-03-17 17:47:57 -0700

    Intervals

--- 252 Meeting Rooms.py
+++ 252 Meeting Rooms.py
@@ -2,23 +2,25 @@
 252. Meeting Rooms
 https://leetcode.com/problems/meeting-rooms/
 
-Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei),
+Given an array of meeting time intervals where intervals[i] = [starti, endi],
 determine if a person could attend all meetings.
 
 Example 1:
 
-Input: [[0,30],[5,10],[15,20]]
+Input: intervals = [[0,30],[5,10],[15,20]]
 Output: false
 
 Example 2:
 
-Input: [[7,10],[2,4]]
+Input: intervals = [[7,10],[2,4]]
 Output: true
+
+Constraints:
+    0 <= intervals.length <= 104
+    intervals[i].length == 2
+    0 <= starti < endi <= 106
 '''
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
         intervals.sort()
-        for i, m in enumerate(intervals):
-            if i > 0 and m[0] < intervals[i-1][1]:
-                return False
-        return True
+        return not any(i > 0 and m[0] < intervals[i-1][1] for i, m in enumerate(intervals))
--- 253 Meeting Rooms II.py
+++ 253 Meeting Rooms II.py
@@ -2,25 +2,31 @@
 253. Meeting Rooms II
 https://leetcode.com/problems/meeting-rooms-ii/
 
-Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei),
-find the minimum number of conference rooms required.
+Given an array of meeting time intervals intervals where intervals[i] = [starti, endi],
+return the minimum number of conference rooms required.
 
 Example 1:
 
-Input: [[0, 30],[5, 10],[15, 20]]
+Input: intervals = [[0,30],[5,10],[15,20]]
 Output: 2
 
 Example 2:
 
-Input: [[7,10],[2,4]]
+Input: intervals = [[7,10],[2,4]]
 Output: 1
+
+Constraints:
+    1 <= intervals.length <= 104
+    0 <= starti < endi <= 106
 '''
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         points = []
-        for s, e in intervals:
-            points.append((s, 1))
-            points.append((e, -1))          
+        for start, end in intervals:
+            points.append((start, 1))
+            points.append((end, -1))
+
+        # The end will appear before start        
         points.sort()
         
         maxOverlapped = 0
@@ -28,5 +34,4 @@ class Solution:
         for p in points:
             overlapped += p[1]
             maxOverlapped = max(maxOverlapped, overlapped)
-        
         return maxOverlapped
